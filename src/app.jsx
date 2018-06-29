751a6e87c067247f606d3e438e5e6f3a4c10ba49
Morten <morten.bujordet@bekk.no>
2018-06-29 15:17:01 +0200

    Add timetable and update structure

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const App = () => (
-    <div>
-        <p>Hei Verden!</p>
-    </div>
-)
+import App from './containers/App'
 
 ReactDOM.render(<App />, document.getElementById('app'))
