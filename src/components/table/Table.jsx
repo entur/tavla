0063167de46a9e3880e40979279081dd05b392e4
Guro Golberg Brøto <guro.broto@bekk.no>
2018-07-05 09:17:41 +0200

    Fix lint

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -34,8 +34,7 @@ class Table extends React.Component {
                 stationData: [east, west],
             })
         })
-        service.getStopPlaceDepartures({stopPlaceId: 'NSR:StopPlace:4227', onForBoarding: true, departures: 10}).then(departures => {
-            console.log(departures)
+        service.getStopPlaceDepartures({ stopPlaceId: 'NSR:StopPlace:4227', onForBoarding: true, departures: 10 }).then(departures => {
             const lineData = departures.filter(departure => departure && departure.forBoarding).map(departure => {
                 const { expectedDepartureTime, destinationDisplay, serviceJourney } = departure
                 const { line } = serviceJourney.journeyPattern
