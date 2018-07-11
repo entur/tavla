416e0155d9b66c6820c36823f809fd09d4de0ef6
Guro Golberg Brøto <guro.broto@bekk.no>
2018-07-11 13:20:21 +0200

    Add input field so that user can speficy latlong when getting user location is not applicable

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,6 +8,7 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.addLocation = this.addLocation.bind(this)
+        this.handleLatlongSubmit = this.handleLatlongSubmit.bind(this)
     }
 
     componentDidMount() {
@@ -23,9 +24,19 @@ class App extends React.Component {
     addLocation() {
         navigator.geolocation.getCurrentPosition(position => {
             const latlong = { lat: position.coords.latitude, long: position.coords.longitude }
-            myStorage.setItem('initialData', JSON.stringify(latlong))
-            this.props.history.push('/dashboard')
+            this.goToDepartureBoard(latlong)
         })
+    }
+
+    handleLatlongSubmit(event) {
+        event.preventDefault()
+        const latlong = { lat: event.target.lat.value, long: event.target.long.value }
+        this.goToDepartureBoard(latlong)
+    }
+
+    goToDepartureBoard(latlong) {
+        myStorage.setItem('initialData', JSON.stringify(latlong))
+        this.props.history.push('/dashboard')
     }
 
     renderAnimation() {
@@ -50,8 +61,16 @@ class App extends React.Component {
         return (
             <div>
                 <h1>Velkommen til tavla.en-tur.no</h1>
-                <p>For at vi skal lage en tavle til deg trenger vi at du godtar at vi kan bruke din posisjon</p>
+                <p>For at vi skal lage en tavle til deg trenger vi at du godtar at vi kan bruke din posisjon...</p>
                 <button onClick={this.addLocation}>Godta bruk av posisjon</button>
+                <p>...eller fyller inn relevante koordinater</p>
+                <form onSubmit={this.handleLatlongSubmit}>
+                    <label>Breddegrad </label>
+                    <input type="text" name="lat" />
+                    <label>Lengdegrad </label>
+                    <input type="text" name="long" />
+                    <input type="submit" value="Lag tavle" />
+                </form>
             </div>
         )
     }
