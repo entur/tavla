4338350266bfe8644fe5dd5348bde91e5e814ee1
Guro Golberg Brøto <guro.broto@bekk.no>
2018-07-11 11:47:26 +0200

    Add button for the user to accept the use of location on landing page

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,6 +5,21 @@ import * as animationData from '../components/animations/cycle_animation.json'
 const myStorage = window.localStorage
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.addLocation = this.addLocation.bind(this)
+    }
+
+    componentDidMount() {
+        const initData = myStorage.getItem('initialData')
+        if (initData) {
+            const { lat, long } = JSON.parse(initData)
+            if (lat && long) {
+                this.props.history.push('/dashboard')
+            }
+        }
+    }
+
     addLocation() {
         navigator.geolocation.getCurrentPosition(position => {
             const latlong = { lat: position.coords.latitude, long: position.coords.longitude }
@@ -13,15 +28,7 @@ class App extends React.Component {
         })
     }
 
-    componentDidMount() {
-        const initData = myStorage.getItem('initialData')
-        if (!initData) return this.addLocation()
-        const { lat, long } = JSON.parse(initData)
-        if (!lat || !long) return this.addLocation()
-        this.props.history.push('/dashboard')
-    }
-
-    render() {
+    renderAnimation() {
         const defaultOptions = {
             loop: true,
             autoplay: true,
@@ -37,6 +44,16 @@ class App extends React.Component {
             width={400}
             isStopped={false}
         />
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Velkommen til tavla.en-tur.no</h1>
+                <p>For at vi skal lage en tavle til deg trenger vi at du godtar at vi kan bruke din posisjon</p>
+                <button onClick={this.addLocation}>Godta bruk av posisjon</button>
+            </div>
+        )
     }
 }
 
