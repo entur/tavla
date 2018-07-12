b3099d725adab29ca350c9ba2a2b41efaef6f134
Guro Golberg Brøto <guro.broto@bekk.no>
2018-07-12 10:08:42 +0200

    Remove bicycle animation

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Lottie from 'react-lottie'
-import * as animationData from '../components/animations/cycle_animation.json'
 
 const myStorage = window.localStorage
 
@@ -39,23 +37,6 @@ class App extends React.Component {
         this.props.history.push('/dashboard')
     }
 
-    renderAnimation() {
-        const defaultOptions = {
-            loop: true,
-            autoplay: true,
-            animationData,
-            rendererSettings: {
-                preserveAspectRatio: 'xMidYMid slice',
-            },
-        }
-
-        return <Lottie
-            options={defaultOptions}
-            height={400}
-            width={400}
-            isStopped={false}
-        />
-    }
 
     render() {
         return (
