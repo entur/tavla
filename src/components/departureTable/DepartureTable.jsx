d692151f288cf5de59db208039ef05e0b83e8f9a
Guro Golberg Brøto <guro.broto@bekk.no>
2018-07-18 10:55:17 +0200

    Fix automergemistake

--- src/components/departureTable/DepartureTable.jsx
+++ src/components/departureTable/DepartureTable.jsx
@@ -2,45 +2,48 @@ import React from 'react'
 import { getIcon, groupBy } from '../../utils'
 import './departureTable.css'
 
-const DepartureTable = ({ lineData }) => {
+const DepartureTable = ({ lineData, visible }) => {
     return (
-        lineData.filter(({ departures }) => departures.length > 0).map(({
-            departures, name, id,
-        }) => {
-            const groupedDepartures = groupBy(departures, 'route')
-            const routes = Object.keys(groupedDepartures)
-            return (
-                <div className="table-container" key={id}>
-                    <div className="table-header">
-                        <h2>{name}</h2>
-                    </div>
-                    <div>
-                        {
-                            routes.map(route => {
-                                const routeData = groupedDepartures[route]
-                                const routeType = routeData[0].type
-                                return (
-                                    <div key={route}>
-                                        <div className="table-route-name">
-                                            <div className="route-icon">{ getIcon(routeType, { height: '11' })}</div>
-                                            <p className="route-name-text">{route}</p>
+        lineData
+            .filter(({ departures }) => departures.length > 0)
+            .filter(({ id }) => !visible.includes(id))
+            .map(({
+                departures, name, id,
+            }) => {
+                const groupedDepartures = groupBy(departures, 'route')
+                const routes = Object.keys(groupedDepartures)
+                return (
+                    <div className="table-container" key={id}>
+                        <div className="table-header">
+                            <h2>{name}</h2>
+                        </div>
+                        <div>
+                            {
+                                routes.map(route => {
+                                    const routeData = groupedDepartures[route]
+                                    const routeType = routeData[0].type
+                                    return (
+                                        <div key={route}>
+                                            <div className="table-route-name">
+                                                <div className="route-icon">{ getIcon(routeType, { height: '11' })}</div>
+                                                <p className="route-name-text">{route}</p>
+                                            </div>
+                                            <div className="table-route-departures">
+                                                { routeData.map((data, index) => {
+                                                    return (
+                                                        <div className="table-route-departure-time"key={index}>
+                                                            {data.time}
+                                                        </div>)
+                                                })}
+                                            </div>
                                         </div>
-                                        <div className="table-route-departures">
-                                            { routeData.map((data, index) => {
-                                                return (
-                                                    <div className="table-route-departure-time"key={index}>
-                                                        {data.time}
-                                                    </div>)
-                                            })}
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
-                </div>
-            )
-        })
+                )
+            })
     )
 }
 
