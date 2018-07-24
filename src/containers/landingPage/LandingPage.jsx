7df98d56da7af8f5147e52b1e45fb6e16e1fd48f
Guro Golberg Brøto <guro.broto@bekk.no>
2018-07-24 12:28:09 +0200

    Add geocoder to LandingPage

--- src/containers/landingPage/LandingPage.jsx
+++ src/containers/landingPage/LandingPage.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import SearchPanel from '../../components/searchPanel/SearchPanel'
 import { TavlaLogo } from '../../assets/icons'
 import coverPhoto from '../../assets/bilde.png'
 import './styles.scss'
 
-const LandingPage = ({ addLocation }) => (
-    <div className="landing-page-wrapper">
-        <div className="title-container">
-            <img src={coverPhoto} />
-            <TavlaLogo />
-            <p>Velkommen til Entur Tavla, sanntidstavla du kan tilpasse etter dine behov.</p>
+const LandingPage = ({ addLocation }) => {
+    return (
+        <div className="landing-page-wrapper">
+            <div className="title-container">
+                <img src={coverPhoto} />
+                <TavlaLogo />
+                <p>Velkommen til Entur Tavla, sanntidstavla du kan tilpasse etter dine behov.</p>
+            </div>
+            <div className="round-shape"/>
+            <div className="content-container">
+                <SearchPanel handleCoordinatesSelected={addLocation}/>
+                <p>For å kunne opprette en tavle,  trenger vi å vite hvor du er.</p>
+            </div>
         </div>
-        <div className="round-shape"/>
-        <div className="content-container">
-            <button className="landing-button" onClick={addLocation}>Finn min posisjon</button>
-            <p>For å kunne opprette en tavle der du er, trenger vi din lokasjon</p>
-        </div>
-    </div>
-)
+    )
+}
+
 
 export default LandingPage
