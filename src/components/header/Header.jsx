5eac6168d62e4d70942d46ba2fadfaf3c7ed70e3
Guro Golberg Brøto <guro.broto@bekk.no>
2018-07-31 12:43:28 +0200

    Move settingsBtn to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -23,6 +23,7 @@ class Header extends React.Component {
     render() {
         return (
             <div className="header">
+                {this.props.settingsBtn}
                 {this.state.time}
             </div>
         )
