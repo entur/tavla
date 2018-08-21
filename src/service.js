94e510fdf23513f82d6b42e4785947c03460ac4e
Mats Byrkeland <matsbyr@gmail.com>
2018-08-21 11:10:30 +0200

    Add staging deployment script

--- src/service.js
+++ src/service.js
@@ -3,7 +3,7 @@ import EnturService from '@entur/sdk'
 export default new EnturService({
     clientName: 'entur-tavla',
     hosts: {
-        journeyplanner: process.env.JOURNEYPLANNER_HOST || 'https://api.entur.org/journeyplanner/2.0/index',
-        geocoder: process.env.GEOCODER_HOST || 'https://api.entur.org/api/geocoder/1.1',
+        journeyplanner: process.env.JOURNEYPLANNER_HOST,
+        geocoder: process.env.GEOCODER_HOST,
     },
 })
