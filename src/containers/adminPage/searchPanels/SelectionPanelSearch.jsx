a0050647ddce644d0eaaceb89433db366aa5eec7
Didrik Kvanvik <didrik.kvanvik@entur.org>
2018-09-20 15:50:28 +0200

    Add check if there exists no stops from the selected stop

--- src/containers/adminPage/searchPanels/SelectionPanelSearch.jsx
+++ src/containers/adminPage/searchPanels/SelectionPanelSearch.jsx
@@ -51,7 +51,8 @@ class SelectionPanelSearch extends React.Component {
             longitude: suggestion.coordinates.lon,
         }
 
-        service.getStopPlacesByPosition(coordinates, 10).then(stop => {
+        service.getStopPlacesByPosition(coordinates, 100).then(stop => {
+            if (stop.length === 0) return
             service.getStopPlaceDepartures(stop[0].id, { onForBoarding: true, departures: 50 })
                 .then(departures => {
                     const updatedStop = {
