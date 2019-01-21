b7f97d28657779e79fb5714da6abf36948bd53c6
Kim Hafr <kim.hafr@gmail.com>
2019-01-21 14:11:22 +0100

    Added Google Analytics for tracking page views

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import {
     Route, Switch, Redirect,
 } from 'react-router-dom'
+import analytics from 'universal-ga'
 import './main.scss'
 import DepartureBoard from './containers/departureBoard/DepartureBoard'
 import AdminPage from './containers/adminPage/AdminPage'
 import App from './containers/App'
+
+analytics.initialize('UA-108877193-6')
+analytics.set('anonymizeIp', true)
+
+analytics.set('page', window.location.pathname)
+analytics.pageview(window.location.pathname)
 
 const Layout = ({ children }) => (
     <div className="main-container">
