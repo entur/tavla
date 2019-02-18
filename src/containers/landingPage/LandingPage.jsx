246dba5160d1ac1475810d6e0f725b004e9764f2
Caroline Odden <caroline.odden@entur.org>
2019-02-18 14:56:02 +0100

    GoToPrivacy button on landingpage

--- src/containers/landingPage/LandingPage.jsx
+++ src/containers/landingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ import { TavlaLogo } from '../../assets/icons'
 import coverPhoto from '../../assets/images/cover-photo.jpg'
 import './styles.scss'
 
-const LandingPage = ({ addLocation }) => {
+const LandingPage = ({ addLocation, goToPrivacy }) => {
     return (
         <div className="landing-page-wrapper">
             <div className="title-container">
@@ -16,6 +16,8 @@ const LandingPage = ({ addLocation }) => {
                 <p className="searchPanel-subtext">For å opprette en tavle trenger vi å vite hvilket område du er interessert i.</p>
                 <img src={coverPhoto} className="cover-photo"/>
             </div>
+
+            <button className="privacy-button" onClick={ goToPrivacy }>Personvern</button>
         </div>
     )
 }
