e6955401a6a854ee72c2d3d7ce020d637378ffd5
Mats Byrkjeland <matsbyr@gmail.com>
2019-06-18 10:11:05 +0200

    Convert some class components to function comps with hooks

--- src/containers/adminPage/BikePanel.jsx
+++ src/containers/adminPage/BikePanel.jsx
@@ -1,85 +1,78 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Checkbox } from '@entur/component-library'
-import './styles.scss'
 import BikePanelSearch from './searchPanels/BikePanelSearch.jsx'
+import './styles.scss'
 
-class BikePanel extends Component {
-    state = {
-        checked: false,
+function BikePanel(props) {
+    const [checked, setChecked] = useState(false)
+
+    const {
+        onCheck, updateHiddenList, stations,
+        position, handleAddNewStation,
+    } = props
+
+    const onChange = () => {
+        props.updateHiddenListForAll(!checked, 'stations')
+        setChecked(!checked)
     }
 
-    onChange = () => {
-        this.props.updateHiddenListForAll(!this.state.checked, 'stations')
-        this.setState({
-            checked: !this.state.checked,
-        })
+    if (!stations.length) {
+        return <div className="selection-panel"/>
     }
 
-    render() {
-        const {
-            onCheck, updateHiddenList, stations,
-            position, handleAddNewStation,
-        } = this.props
-
-        if (!stations.length) {
-            return <div className="selection-panel"/>
-        }
-
-        return (
-            <div className="selection-panel">
-                <div>
-                    <div className="search-stop-places">
-                        <BikePanelSearch position={position} handleAddNewStation={handleAddNewStation}/>
-
-                    </div>
-                    <div className="city-bike-panel">
-                        <div className="stop-place-header">
-                            <div className="selection-panel-title">Bysykler</div>
-                            <div className="checkbox-container-check-all">
-                                <Checkbox
-                                    id="check-all-stop-places-bike"
-                                    name="check-all-stop-places-bike"
-                                    label="Velg alle"
-                                    className="entur-radio-checkbox--squared"
-                                    onChange={this.onChange}
-                                    checked={this.state.checked}
-                                />
-                            </div>
+    return (
+        <div className="selection-panel">
+            <div>
+                <div className="search-stop-places">
+                    <BikePanelSearch position={position} handleAddNewStation={handleAddNewStation}/>
+                </div>
+                <div className="city-bike-panel">
+                    <div className="stop-place-header">
+                        <div className="selection-panel-title">Bysykler</div>
+                        <div className="checkbox-container-check-all">
+                            <Checkbox
+                                id="check-all-stop-places-bike"
+                                name="check-all-stop-places-bike"
+                                label="Velg alle"
+                                className="entur-radio-checkbox--squared"
+                                onChange={onChange}
+                                checked={checked}
+                            />
                         </div>
-                        {
-                            stations.map(({
-                                name, id,
-                            }, index) => {
-                                const isChecked = !onCheck(id, 'stations')
-                                return (
-                                    <div key={index}>
-                                        <div className="selection-row" key={index}>
-                                            <div className="checkbox-container">
-                                                <Checkbox
-                                                    key={id}
-                                                    id={id}
-                                                    checked={isChecked}
-                                                    onChange={() => updateHiddenList(id, 'stations')}
-                                                    className="entur-radio-checkbox--round entur-radio-checkbox--margin"
-                                                    variant="midnight"
-                                                />
-                                            </div>
-                                            <div className="selection-data-wrapper">
-                                                <div className="selection-data-container">
-                                                    <div className="city-bike-name">{name}</div>
-                                                </div>
+                    </div>
+                    {
+                        stations.map(({
+                            name, id,
+                        }, index) => {
+                            const isChecked = !onCheck(id, 'stations')
+                            return (
+                                <div key={index}>
+                                    <div className="selection-row" key={index}>
+                                        <div className="checkbox-container">
+                                            <Checkbox
+                                                key={id}
+                                                id={id}
+                                                checked={isChecked}
+                                                onChange={() => updateHiddenList(id, 'stations')}
+                                                className="entur-radio-checkbox--round entur-radio-checkbox--margin"
+                                                variant="midnight"
+                                            />
+                                        </div>
+                                        <div className="selection-data-wrapper">
+                                            <div className="selection-data-container">
+                                                <div className="city-bike-name">{name}</div>
                                             </div>
                                         </div>
-                                        <div className="city-bike-row-bottom-border" />
                                     </div>
-                                )
-                            })
-                        }
-                    </div>
+                                    <div className="city-bike-row-bottom-border" />
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BikePanel
--- src/containers/adminPage/SelectionPanel.jsx
+++ src/containers/adminPage/SelectionPanel.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import {
     Accordion,
     AccordionItem,
@@ -11,117 +11,112 @@ import SelectionPanelSearch from './searchPanels/SelectionPanelSearch.jsx'
 import { getIcon, getIconColor } from '../../utils'
 import './styles.scss'
 
-class SelectionPanel extends Component {
-    state = {
-        checked: false,
+function SelectionPanel(props) {
+    const [checked, setChecked] = useState(false)
+
+    const {
+        onCheck, updateHiddenList, stops, position, handleAddNewStop,
+    } = props
+
+    const onChange = () => {
+        props.updateHiddenListForAll(!checked, 'stops')
+        setChecked(!checked)
     }
 
-    onChange = () => {
-        this.props.updateHiddenListForAll(!this.state.checked, 'stops')
-        this.setState({
-            checked: !this.state.checked,
-        })
-    }
-
-    render() {
-        const {
-            onCheck, updateHiddenList, stops, position, handleAddNewStop,
-        } = this.props
-        return (
-            <div className="selection-panel">
-                { (stops.length > 0)
-                    ? <div>
-                        <div className="search-stop-places">
-                            <SelectionPanelSearch position={position} handleAddNewStop={handleAddNewStop}/>
-                        </div>
-                        <div className="stop-place-panel">
-                            {<div className="stop-place-header">
-                                <div className="selection-panel-title">Stoppesteder</div>
-                                <div className="checkbox-container-check-all">
-                                    <Checkbox
-                                        id="check-all-stop-places"
-                                        name="check-all-stop-places"
-                                        label="Velg alle"
-                                        onChange={this.onChange}
-                                        checked={this.state.checked}
-                                        className="entur-radio-checkbox--squared"
-                                    />
-                                </div>
-                            </div>}
-                            {
-                                stops.map(({
-                                    name, id, departures,
-                                }, index) => {
-                                    const isChecked = !onCheck(id, 'stops')
-                                    return (
-                                        <Accordion className="selection-row" key={index} allowZeroExpanded>
-                                            <div className="checkbox-container">
-                                                <Checkbox
-                                                    id={id}
-                                                    className="entur-radio-checkbox--round"
-                                                    checked={isChecked}
-                                                    onChange={() => updateHiddenList(id, 'stops')}
-                                                    variant="midnight"
-                                                />
-                                            </div>
-                                            <AccordionItem
-                                                className="selection-data-wrapper"
-                                                key={id}
-                                            >
-                                                <AccordionItemHeading>
-                                                    <AccordionItemButton className="selection-data-container stop-place-container">
-                                                        <div className="stop-place-title">
-                                                            {name}
-                                                        </div>
-                                                        <div className="show-button">
-                                                            <p className="show-button--text">Endre</p>
-                                                            <div className="accordion__arrow" role="presentation" />
-                                                        </div>
-                                                    </AccordionItemButton>
-                                                </AccordionItemHeading>
-                                                <div className="selection-data-wrapper--border" />
-                                                <AccordionItemPanel>
-                                                    <table className="admin-route-table">
-                                                        <tbody>
-                                                            { departures.map(({ route, type }, i) => {
-                                                                const isVisible = !onCheck(route, 'routes')
-                                                                const Icon = getIcon(type)
-                                                                const iconColor = getIconColor(type)
-                                                                return (
-                                                                    <tr
-                                                                        className="admin-route-row"
-                                                                        key={i}
-                                                                    >
-                                                                        <td className="admin-route-icon">
-                                                                            <Icon height={ 28 } width={ 28 } color={ iconColor } />
-                                                                        </td>
-                                                                        <td className="admin-route-title">{route}</td>
-                                                                        <td>
-                                                                            <SlideSwitch
-                                                                                key={i}
-                                                                                id="SlideSwitch"
-                                                                                className="mode-sort-slide-switch-stops"
-                                                                                onChange={() => { updateHiddenList(route, 'routes') }}
-                                                                                checked={isVisible}
-                                                                            />
-                                                                        </td>
-                                                                    </tr>
-                                                                )
-                                                            }) }
-                                                        </tbody>
-                                                    </table>
-                                                </AccordionItemPanel>
-                                            </AccordionItem>
-                                        </Accordion>
-                                    )
-                                })
-                            }
-                        </div>
+    return (
+        <div className="selection-panel">
+            { (stops.length > 0)
+                ? <div>
+                    <div className="search-stop-places">
+                        <SelectionPanelSearch position={position} handleAddNewStop={handleAddNewStop}/>
                     </div>
-                    : null }
-            </div>
-        )
-    }
+                    <div className="stop-place-panel">
+                        <div className="stop-place-header">
+                            <div className="selection-panel-title">Stoppesteder</div>
+                            <div className="checkbox-container-check-all">
+                                <Checkbox
+                                    id="check-all-stop-places"
+                                    name="check-all-stop-places"
+                                    label="Velg alle"
+                                    onChange={onChange}
+                                    checked={checked}
+                                    className="entur-radio-checkbox--squared"
+                                />
+                            </div>
+                        </div>
+                        {
+                            stops.map(({
+                                name, id, departures,
+                            }, index) => {
+                                const isChecked = !onCheck(id, 'stops')
+                                return (
+                                    <Accordion className="selection-row" key={index} allowZeroExpanded>
+                                        <div className="checkbox-container">
+                                            <Checkbox
+                                                id={id}
+                                                className="entur-radio-checkbox--round"
+                                                checked={isChecked}
+                                                onChange={() => updateHiddenList(id, 'stops')}
+                                                variant="midnight"
+                                            />
+                                        </div>
+                                        <AccordionItem
+                                            className="selection-data-wrapper"
+                                            key={id}
+                                        >
+                                            <AccordionItemHeading>
+                                                <AccordionItemButton className="selection-data-container stop-place-container">
+                                                    <div className="stop-place-title">
+                                                        {name}
+                                                    </div>
+                                                    <div className="show-button">
+                                                        <p className="show-button--text">Endre</p>
+                                                        <div className="accordion__arrow" role="presentation" />
+                                                    </div>
+                                                </AccordionItemButton>
+                                            </AccordionItemHeading>
+                                            <div className="selection-data-wrapper--border" />
+                                            <AccordionItemPanel>
+                                                <table className="admin-route-table">
+                                                    <tbody>
+                                                        { departures.map(({ route, type }, i) => {
+                                                            const isVisible = !onCheck(route, 'routes')
+                                                            const Icon = getIcon(type)
+                                                            const iconColor = getIconColor(type)
+                                                            return (
+                                                                <tr
+                                                                    className="admin-route-row"
+                                                                    key={i}
+                                                                >
+                                                                    <td className="admin-route-icon">
+                                                                        <Icon height={ 28 } width={ 28 } color={ iconColor } />
+                                                                    </td>
+                                                                    <td className="admin-route-title">{route}</td>
+                                                                    <td>
+                                                                        <SlideSwitch
+                                                                            key={i}
+                                                                            id="SlideSwitch"
+                                                                            className="mode-sort-slide-switch-stops"
+                                                                            onChange={() => { updateHiddenList(route, 'routes') }}
+                                                                            checked={isVisible}
+                                                                        />
+                                                                    </td>
+                                                                </tr>
+                                                            )
+                                                        }) }
+                                                    </tbody>
+                                                </table>
+                                            </AccordionItemPanel>
+                                        </AccordionItem>
+                                    </Accordion>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                : null }
+        </div>
+    )
 }
 
 export default SelectionPanel
