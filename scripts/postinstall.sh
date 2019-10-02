d978088ef4b1c0bbd57528fdf1628d709059abc9
511V41 <yukkuri.sinai@gmail.com>
2019-10-02 20:33:36 +0900

    rewrite postinstall script

--- scripts/postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FONT_FOLDER="node_modules/@entur/fonts"
-FONT_INDEX_FILE="$FONT_FOLDER/index.css"
-
-if [ ! -f $FONT_INDEX_FILE ]; then
-    mkdir -p $FONT_FOLDER
-    touch $FONT_INDEX_FILE
-fi
