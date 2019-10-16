7683ed43ae8aa3ba9d01e9636641674f59af75de
Mats Byrkjeland <matsbyr@gmail.com>
2019-10-16 14:39:19 +0200

    Add versioning and migration scheme for UrlStorage

--- src/settings/UrlStorage.ts
+++ src/settings/UrlStorage.ts
@@ -13,23 +13,69 @@ const DEFAULT_SETTINGS: Settings = {
     newStops: [],
 }
 
+const VERSION_PREFIX_REGEX = /^v(\d)+::/
+const CURRENT_VERSION = 1
+
+function migrateFromV0(settings: string): Settings {
+    if (!settings) {
+        return DEFAULT_SETTINGS
+    }
+    const parsed = JSON.parse(atob(settings))
+    const migratedHiddenRoutes = parsed.hiddenRoutes
+        .map(idAndRouteString => idAndRouteString.split('$'))
+        .reduce((routeMap, [stopPlaceId, routeName]) => ({
+            ...routeMap,
+            [stopPlaceId]: [...routeMap[stopPlaceId] || [], routeName],
+        }), {})
+
+    return {
+        ...parsed,
+        hiddenRoutes: migratedHiddenRoutes,
+    }
+}
+
+function migrate(fromVersion: number, settingsString: string): Settings {
+    switch (fromVersion) {
+        case 0:
+            return migrateFromV0(settingsString)
+        default:
+            return DEFAULT_SETTINGS
+    }
+}
+
 export function persist(settings: Settings): void {
     const hash = lz.compressToEncodedURIComponent(JSON.stringify(settings))
     const currentPathname = window.location.pathname
 
     const urlParts = currentPathname.split('/')
-    urlParts[urlParts.length - 1] = hash
+    urlParts[urlParts.length - 1] = `v${CURRENT_VERSION}::${hash}`
     const newPathname = urlParts.join('/')
 
     window.history.pushState(window.history.state, document.title, newPathname)
 }
 
 export function restore(): Settings {
-    const settings = window.location.pathname.split('/')[3]
-    if (!settings) {
+    const settingsString = window.location.pathname.split('/')[3]
+    if (!settingsString) {
         return DEFAULT_SETTINGS
     }
 
-    const decompressed = lz.decompressFromEncodedURIComponent(settings)
-    return JSON.parse(decompressed)
+    const versionMatch = settingsString.match(VERSION_PREFIX_REGEX)
+    const version = versionMatch ? Number(versionMatch[1]) : 0
+
+    if (version !== CURRENT_VERSION) {
+        const migratedSettings = migrate(version, settingsString)
+        persist(migratedSettings)
+        return migratedSettings
+    }
+
+    const settingsWithoutVersionPrefix = settingsString.replace(VERSION_PREFIX_REGEX, '')
+
+    try {
+        const decompressed = lz.decompressFromEncodedURIComponent(settingsWithoutVersionPrefix)
+        const settings = JSON.parse(decompressed)
+        return settings
+    } catch (error) {
+        return DEFAULT_SETTINGS
+    }
 }
