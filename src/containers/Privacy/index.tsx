c9c917a772856c8d1e0ebcd1e1928accaf2ab686
Mats Byrkjeland <matsbyr@gmail.com>
2019-10-29 13:42:39 +0100

    Fix layout on privacy page

--- src/containers/Privacy/index.tsx
+++ src/containers/Privacy/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {
+    Heading1, Heading2, Heading3, Paragraph,
+} from '@entur/typography'
 
 import BackButton from '../../components/backButton/BackButton'
 
@@ -10,39 +13,40 @@ const Privacy = ({ history }: Props): JSX.Element => {
     }
 
     return (
-        <article>
-            <div className="main-container privacy-header">
+        <article className="privacy">
+            <div className="privacy__header">
                 <BackButton
-                    className="privacy-header--back-button"
+                    size="medium"
+                    className="privacy__header__back-button"
                     action={goBackToDashboard}
                 />
 
-                <h1>Personvern</h1>
+                <Heading1>Personvern</Heading1>
             </div>
 
-            <div className="privacy">
-                <h2>Analyseverktøy</h2>
-                <p>
+            <div className="privacy__body">
+                <Heading2>Analyseverktøy</Heading2>
+                <Paragraph>
                     Entur bruker analyseverktøyene Google Analytics og Firebase
                     for å samle info om hvordan du bruker tjenesten, ved hjelp
                     av informasjonskapsler. Som behandlingsansvarlig for
                     Entur-appen er det Entur AS som bestemmer hvilke
                     opplysninger Google Analytics kan innhente om bruken av våre
                     tjenester.
-                </p>
-                <p>
+                </Paragraph>
+                <Paragraph>
                     Analyseverktøyet kan anslå din omtrentlige geografiske
                     plassering, men adressen kan ikke brukes til å identifisere
                     deg.
-                </p>
-                <p>
+                </Paragraph>
+                <Paragraph>
                     Google Analytics mottar generell web- og appstatistikk, som
                     for eksempel tidspunkt, språk og generell informasjon om din
                     mobilenhet. Hendelser rundt reisesøk, kjøp og billettering
                     logges anonymt til bruk for å forbedre tjenesten. Ingenting
                     av dette kan spores tilbake til enkeltbrukere.
-                </p>
-                <p>
+                </Paragraph>
+                <Paragraph>
                     Vi benytter to informasjonskapsler av typen _ga, én av typen
                     _gat samt én _gid. Varighet og nærmere beskrivelse av disse
                     finnes{' '}
@@ -50,34 +54,34 @@ const Privacy = ({ history }: Props): JSX.Element => {
                         her
                     </a>
                     .
-                </p>
+                </Paragraph>
 
-                <h2>Tilganger</h2>
-                <p>
+                <Heading2>Tilganger</Heading2>
+                <Paragraph>
                     For at reiseplanleggeren skal fungere optimalt trenger den
                     forskjellige tilganger til telefonen din. Entur-appen kan be
                     om følgende tilganger:
-                </p>
-                <h3>Din posisjon</h3>
-                <p>
+                </Paragraph>
+                <Heading3>Din posisjon</Heading3>
+                <Paragraph>
                     For at søket med «Fra din posisjon» skal fungere, må Entur
                     vite hvor du befinner deg. Denne tilgangen brukes også til å
                     vise de nærmeste stoppestedene. Posisjonen finner vi ved
                     hjelp av telefonens GPS-funksjon, WiFi-tilkoblinger,
                     IP-adresser, RFID, Bluetooth, MAC-adresser og
                     GSM/CDMA-celle-ID.
-                </p>
-                <p>
+                </Paragraph>
+                <Paragraph>
                     Om vi vil vite hvor du er, spør vi om lov først. Du kan
                     alltid regulere denne tilgangen i telefonens eller
                     nettleserens innstillinger.
-                </p>
-                <h3>Nettverkskommunikasjon</h3>
-                <p>
+                </Paragraph>
+                <Heading3>Nettverkskommunikasjon</Heading3>
+                <Paragraph>
                     For at vi skal kunne søke etter de beste reisene for deg,
                     trenger du tilgang til internett. Dette får du enten via
                     WiFi eller mobilt nettverk.
-                </p>
+                </Paragraph>
             </div>
         </article>
     )
