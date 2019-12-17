d6596b21235248b401da7c21db00a04fd0fe538a
Mats Byrkjeland <matsbyr@gmail.com>
2019-12-17 13:48:15 +0100

    Implement new Race design

--- src/dashboards/Race/index.tsx
+++ src/dashboards/Race/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Heading2 } from '@entur/typography'
 import { LegBone } from '@entur/component-library'
+import { LegMode } from '@entur/sdk'
 import { colors } from '@entur/tokens'
 
 import '@entur/component-library/lib/index.css'
@@ -8,13 +9,14 @@ import '@entur/component-library/lib/index.css'
 import {
     getIcon, getIconColor, timeUntil, useCounter,
 } from '../../utils'
+import { LineData } from '../../types'
 
 import { useStopPlacesWithDepartures } from '../../logic'
 import DashboardWrapper from '../../containers/DashboardWrapper'
 
 import './styles.scss'
 
-const TICKS = [-2, 0, 1, 2, 3, 5, 10, 15, 20, 30]
+const TICKS = [-1, 0, 1, 2, 3, 4, 5, 10, 15, 20, 30]
 
 // Use this to scale the race track.
 const ZOOM = 1
@@ -28,12 +30,44 @@ function diffSincePreviousTick(minute: number): number {
 }
 
 function competitorPosition(waitTime: number): number {
-    const baseOffset = 25
     const negativeTickOffset = Math.abs(TICKS.filter(tick => tick < 0).reduce((a, b) => a + b, 0))
-    return (waitTime * ZOOM) + (negativeTickOffset * 60 * ZOOM) + baseOffset
+    return ZOOM * (waitTime + negativeTickOffset * 60)
 }
 
-function Tick({ minutes, index }): JSX.Element {
+function groupDeparturesByMode(departures: Array<LineData>): { [mode in LegMode]?: Array<LineData> } {
+    return departures.reduce((map, departure) => ({
+        ...map,
+        [departure.type]: [
+            ...map[departure.type] || [],
+            departure,
+        ],
+    }), {})
+}
+
+const MODE_ORDER = ['rail', 'metro', 'tram', 'bus', 'water', 'air']
+
+function orderModes(modeA: string, modeB: string): number {
+    return MODE_ORDER.indexOf(modeA) - MODE_ORDER.indexOf(modeB)
+}
+
+function getLegBonePattern(mode: LegMode): 'line' | 'dashed' | 'dotted' | 'wave' {
+    switch (mode) {
+        case 'bus':
+            return 'dashed'
+        case 'bicycle':
+            return 'dotted'
+        case 'water':
+            return 'wave'
+        case 'metro':
+        case 'rail':
+        case 'tram':
+        case 'air':
+        default:
+            return 'line'
+    }
+}
+
+function Tick({ minutes, mode, index }): JSX.Element {
     let label = `${minutes} min`
     let marginLeft = -30
 
@@ -47,13 +81,13 @@ function Tick({ minutes, index }): JSX.Element {
     }
 
     const width = diffSincePreviousTick(minutes) * (60 * ZOOM)
-    const color = minutes < 0 ? 'grey' : colors.brand.coral
+    const color = minutes < 0 ? colors.blues.blue30 : getIconColor(mode)
 
     return (
         <div style={{ minWidth: width }}>
             <LegBone
                 className="race__leg-bone"
-                pattern="line"
+                pattern={getLegBonePattern(mode)}
                 color={color}
                 showStop={index <= TICKS.length}
                 showStart={index === 0}
@@ -61,14 +95,6 @@ function Tick({ minutes, index }): JSX.Element {
             <div className="race__tick" style={{ marginLeft }}>
                 { label }
             </div>
-        </div>
-    )
-}
-
-function Label({ children }): JSX.Element {
-    return (
-        <div className="race__label">
-            {children}
         </div>
     )
 }
@@ -90,33 +116,49 @@ const RaceDashboard = ({ history }: Props): JSX.Element => {
                         .map((stop) => (
                             <div key={stop.id} className="race__stop">
                                 <Heading2 margin="none" style={{ margin: 0 }}>{stop.name}</Heading2>
-                                <div className="race__track">
-                                    { stop.departures.map(({
-                                        type, serviceJourneyId, expectedDepartureTime, route,
-                                    }) => {
-                                        const waitTime = timeUntil(expectedDepartureTime)
-                                        const Icon = getIcon(type)
-                                        const color = getIconColor(type)
-                                        return (
-                                            <div
-                                                key={serviceJourneyId}
-                                                className="race__competitor"
-                                                style={{ right: competitorPosition(waitTime) }}
-                                            >
-                                                <Label>{ route }</Label>
-                                                <Icon
-                                                    color={color}
-                                                    size="large"
-                                                />
-                                            </div>
-                                        )
-                                    })}
-                                </div>
-                                <div className="race__line">
-                                    { [...TICKS].reverse().map((minutes, index) => {
-                                        return <Tick key={minutes} minutes={minutes} index={index} />
-                                    }) }
-                                </div>
+                                {
+                                    Object.entries(groupDeparturesByMode(stop.departures))
+                                        .sort(([ modeA ], [ modeB ]) => orderModes(modeA, modeB))
+                                        .map(([mode, departures]) => (
+                                            <>
+                                                <div className="race__track">
+                                                    { departures.map(({
+                                                        type, serviceJourneyId, expectedDepartureTime, route,
+                                                    }) => {
+                                                        const waitTime = timeUntil(expectedDepartureTime)
+                                                        const Icon = getIcon(type)
+                                                        const color = getIconColor(type)
+                                                        return (
+                                                            <div
+                                                                key={serviceJourneyId}
+                                                                className="race__competitor"
+                                                                style={{ right: competitorPosition(waitTime) }}
+                                                            >
+                                                                <div className="race__label">
+                                                                    {route}
+                                                                </div>
+                                                                <Icon
+                                                                    color={color}
+                                                                    size="large"
+                                                                />
+                                                            </div>
+                                                        )
+                                                    })}
+                                                </div>
+                                                <div className="race__line">
+                                                    { [...TICKS].reverse().map((minutes, index) => {
+                                                        return (
+                                                            <Tick
+                                                                key={minutes}
+                                                                mode={mode}
+                                                                minutes={minutes}
+                                                                index={index}
+                                                            />
+                                                        ) }) }
+                                                </div>
+                                            </>
+                                        ))
+                                }
                             </div>
                         ))
                 }
