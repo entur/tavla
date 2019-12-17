66a2d66866cb08fb4e2d30d0be449c384a92d991
Mats Byrkjeland <matsbyr@gmail.com>
2019-12-17 10:07:44 +0100

    Add hovering route labels above competitors

--- src/dashboards/Race/index.tsx
+++ src/dashboards/Race/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Heading2 } from '@entur/typography'
 
 import {
     getIcon, getIconColor, timeUntil, useCounter,
@@ -9,6 +10,8 @@ import DashboardWrapper from '../../containers/DashboardWrapper'
 
 import './styles.scss'
 
+const TICKS = [1, 2, 3, 5, 10, 15]
+
 function Tick({ minutes }): JSX.Element {
     return (
         <div
@@ -17,6 +20,14 @@ function Tick({ minutes }): JSX.Element {
         >
             <div style={{ width: 2, height: 16, backgroundColor: 'white' }} />
             <div>{`${minutes} min`}</div>
+        </div>
+    )
+}
+
+function Label({ children }): JSX.Element {
+    return (
+        <div className="race__label">
+            {children}
         </div>
     )
 }
@@ -37,26 +48,28 @@ const RaceDashboard = ({ history }: Props): JSX.Element => {
                         .filter(({ departures }) => departures.length > 0)
                         .map((stop) => (
                             <div key={stop.id} className="race__stop">
-                                <h2>{stop.name}</h2>
+                                <Heading2 margin="none" style={{ margin: 0 }}>{stop.name}</Heading2>
                                 <div className="race__track">
                                     { stop.departures.map(({
-                                        type, serviceJourneyId, expectedDepartureTime,
+                                        type, serviceJourneyId, expectedDepartureTime, route,
                                     }) => {
                                         const waitTime = timeUntil(expectedDepartureTime)
                                         const Icon = getIcon(type)
                                         const color = getIconColor(type)
                                         return (
-                                            <Icon
-                                                key={serviceJourneyId}
-                                                className="race__competitor"
-                                                style={{ width: 100, right: waitTime + 5 * 16, marginBottom: 6 }}
-                                                color={color}
-                                                size="large"
-                                            />
+                                            <div className="race__competitor" style={{ right: waitTime + 5 * 16 }}>
+                                                <Label>{ route }</Label>
+                                                <Icon
+                                                    key={serviceJourneyId}
+                                                    style={{ right: waitTime + 5 * 16 }}
+                                                    color={color}
+                                                    size="large"
+                                                />
+                                            </div>
                                         )
                                     })}
                                 </div>
-                                { [1, 2, 3, 5, 10, 15].map(minutes => <Tick key={minutes} minutes={minutes} />) }
+                                { TICKS.map(minutes => <Tick key={minutes} minutes={minutes} />) }
                             </div>
                         ))
                 }
