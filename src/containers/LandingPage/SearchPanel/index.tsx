d5ea33bdee3c33c29ae9ebd7d505ec2526dfef21
Mats Byrkjeland <matsbyr@gmail.com>
2020-03-24 09:40:45 +0100

    Replace Autosuggest on LandingPage

--- src/containers/LandingPage/SearchPanel/index.tsx
+++ src/containers/LandingPage/SearchPanel/index.tsx
@@ -1,37 +1,34 @@
-import React, { memo, useState, useMemo, useEffect } from 'react'
-import debounce from 'lodash.debounce'
-import ReactAutosuggest from 'react-autosuggest'
+import React, { memo, useState, useEffect } from 'react'
 import { Button } from '@entur/button'
-import { Coordinates } from '@entur/sdk'
+import { Coordinates, Feature } from '@entur/sdk'
+import { Dropdown } from '@entur/dropdown'
 
-import { Spinner, GeoLocation } from '../../../assets/icons'
 import service from '../../../service'
 import { useLocationPermission } from '../../../hooks'
-import { mapFeaturesToSuggestions, Suggestion } from '../../../utils'
 import './styles.scss'
 
 const YOUR_POSITION = 'Posisjonen din'
 
-function getSuggestionValue(suggestion: Suggestion): string {
-    return suggestion.name
-}
-
-function shouldRenderSuggestions(): boolean {
-    return true
-}
-
-function renderSuggestion(suggestion: Suggestion): JSX.Element {
-    if (suggestion.name !== YOUR_POSITION) {
-        return <span>{suggestion.name}</span>
+interface Item {
+    value: string
+    label: string
+    coordinates?: {
+        longitude: number
+        latitude: number
     }
-    return (
-        <span>
-            {suggestion.name}
-            <span className="location-icon">
-                <GeoLocation size={15} />
-            </span>
-        </span>
-    )
+}
+
+function mapFeaturesToItems(features: Feature[]): Item[] {
+    return features.map(({ geometry, properties: { id, name, locality } }) => {
+        return {
+            value: id,
+            label: locality ? `${name}, ${locality}` : name,
+            coordinates: {
+                longitude: geometry.coordinates[0],
+                latitude: geometry.coordinates[1],
+            },
+        }
+    })
 }
 
 function getErrorMessage(error): string {
@@ -43,33 +40,9 @@ function getErrorMessage(error): string {
     }
 }
 
-const renderSpinner = (): JSX.Element => {
-    return (
-        <div className="spinner-container">
-            <Spinner className="spinner" />
-        </div>
-    )
-}
-
-const getFeaturesDebounced = debounce(
-    async (value, showMyPosition, callback) => {
-        const inputLength = value.trim().length
-
-        const defaultSuggestions = showMyPosition
-            ? [{ name: YOUR_POSITION }]
-            : []
-
-        if (!inputLength) return callback(defaultSuggestions)
-
-        const featuresData = await service.getFeatures(value)
-        const suggestedFeatures = mapFeaturesToSuggestions(featuresData)
-        const features = [...defaultSuggestions, ...suggestedFeatures]
-        return callback(features)
-    },
-    500,
-)
-
 const SearchPanel = ({ handleCoordinatesSelected }: Props): JSX.Element => {
+    console.count('render searchpanel')
+
     const [{ denied }, refreshLocationPermission] = useLocationPermission()
 
     const [showPositionInList, setShowPositionInList] = useState(true)
@@ -80,7 +53,6 @@ const SearchPanel = ({ handleCoordinatesSelected }: Props): JSX.Element => {
         }
     }, [denied])
 
-    const [formValue, setFormValue] = useState('')
     const [errorMessage, setErrorMessage] = useState(null)
 
     const [location, setLocation] = useState({
@@ -88,40 +60,14 @@ const SearchPanel = ({ handleCoordinatesSelected }: Props): JSX.Element => {
         selectedLocationName: null,
     })
 
-    const [suggestions, setSuggestions] = useState([{ name: YOUR_POSITION }])
-    const [waiting, setWaiting] = useState<boolean>(false)
     const [chosenCoord, setChosenCoord] = useState<Coordinates | null>(null)
 
-    const onChange = (_, { newValue }): void => {
-        setFormValue(newValue)
-    }
-
-    const onSuggestionsFetchRequested = ({ value }): void => {
-        if (value !== location.selectedLocationName) {
-            setLocation({
-                hasLocation: false,
-                selectedLocationName: null,
-            })
-
-            setChosenCoord(null)
-        }
-        getFeaturesDebounced(value, showPositionInList, setSuggestions)
-    }
-
     const getAddressFromPosition = (position: Coordinates): void => {
-        setFormValue(YOUR_POSITION)
         setChosenCoord(position)
         setLocation({
             hasLocation: true,
             selectedLocationName: YOUR_POSITION,
         })
-    }
-
-    const onSuggestionsClearRequested = (): void => {
-        const newSuggestions = showPositionInList
-            ? [{ name: YOUR_POSITION }]
-            : []
-        setSuggestions(newSuggestions)
     }
 
     const handleSuccessLocation = (data: Position): void => {
@@ -131,26 +77,21 @@ const SearchPanel = ({ handleCoordinatesSelected }: Props): JSX.Element => {
             longitude: data.coords.longitude,
         }
         getAddressFromPosition(position)
-        setWaiting(false)
     }
 
     const handleDeniedLocation = (error): void => {
         refreshLocationPermission()
-        setFormValue('')
         setErrorMessage(getErrorMessage(error))
-        setSuggestions([])
         setLocation({
             hasLocation: false,
             selectedLocationName: null,
         })
-        setWaiting(false)
     }
 
-    const onSuggestionSelected = (_, { suggestion }): void => {
-        if (suggestion.name === YOUR_POSITION) {
-            setWaiting(true)
-            setLocation(v => ({
-                ...v,
+    const onItemSelected = (item: Item): void => {
+        if (item.value === YOUR_POSITION) {
+            setLocation(previousLocation => ({
+                ...previousLocation,
                 selectedLocationName: YOUR_POSITION,
             }))
 
@@ -159,10 +100,10 @@ const SearchPanel = ({ handleCoordinatesSelected }: Props): JSX.Element => {
                 handleDeniedLocation,
             )
         } else {
-            setChosenCoord(suggestion.coordinates)
+            setChosenCoord(item.coordinates)
             setLocation({
                 hasLocation: true,
-                selectedLocationName: suggestion.name,
+                selectedLocationName: item.label,
             })
         }
     }
@@ -174,17 +115,19 @@ const SearchPanel = ({ handleCoordinatesSelected }: Props): JSX.Element => {
         }
     }
 
-    const inputProps = useMemo(
-        () => ({
-            placeholder: 'Adresse eller sted',
-            value: formValue,
-            onChange,
-            onFocus: (): void => {
-                setErrorMessage(null)
-            },
-        }),
-        [formValue],
-    )
+    const getItems = async (query: string): Promise<Item[]> => {
+        const defaultSuggestions = showPositionInList
+            ? [{ value: YOUR_POSITION, label: YOUR_POSITION }]
+            : []
+
+        if (!query) {
+            return defaultSuggestions
+        }
+
+        const featuresData = await service.getFeatures(query)
+        const suggestedFeatures = mapFeaturesToItems(featuresData)
+        return [...defaultSuggestions, ...suggestedFeatures]
+    }
 
     return (
         <form className="search-panel" onSubmit={handleGoToBoard}>
@@ -192,21 +135,14 @@ const SearchPanel = ({ handleCoordinatesSelected }: Props): JSX.Element => {
                 <div className="input-container">
                     <span>Område</span>
                     <div className="input-spinner-container">
-                        <ReactAutosuggest
-                            suggestions={suggestions}
-                            shouldRenderSuggestions={shouldRenderSuggestions}
-                            onSuggestionsFetchRequested={
-                                onSuggestionsFetchRequested
-                            }
-                            onSuggestionsClearRequested={
-                                onSuggestionsClearRequested
-                            }
-                            onSuggestionSelected={onSuggestionSelected}
-                            getSuggestionValue={getSuggestionValue}
-                            renderSuggestion={renderSuggestion}
-                            inputProps={inputProps}
+                        <Dropdown
+                            searchable
+                            openOnFocus
+                            debounceTimeout={500}
+                            placeholder="Adresse eller sted"
+                            items={getItems}
+                            onChange={onItemSelected}
                         />
-                        {waiting && renderSpinner()}
                     </div>
                 </div>
                 <Button
