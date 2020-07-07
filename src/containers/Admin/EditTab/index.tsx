a14e1067ca008f49601cad1c5ecfbd65c0184096
pernilak <pernille.andresen.klevstuen@entur.org>
2020-07-07 15:49:24 +0200

    :construction: Tabs now displayed in Admin panel, functionality for editiing still missinng

---
+++ src/containers/Admin/EditTab/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Heading2 } from '@entur/typography'
+
+const EditTab = (): JSX.Element => {
+    return (
+        <div>
+            <Heading2>Rediger Innhold</Heading2>
+        </div>
+    )
+}
+
+export default EditTab
---
+++ src/containers/Admin/LogoTab/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Heading2, Paragraph } from '@entur/typography'
+
+const LogoTab = (): JSX.Element => {
+    return (
+        <div>
+            <Heading2>Last opp logo</Heading2>
+            <Paragraph>
+                Snart kommer det mulighet for å laste opp egen logo på tavla.
+            </Paragraph>
+        </div>
+    )
+}
+
+export default LogoTab
