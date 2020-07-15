c2fc4599ea40dadf3a0861ca6fa4b0c682430842
Peter Rydberg <hans.martin.peter.rydberg@entur.org>
2020-07-15 09:34:45 +0200

    Add login and logout to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { LeadParagraph } from '@entur/typography'
-import { ClosedLockIcon, UserIcon } from '@entur/icons'
+import { ClosedLockIcon, UserIcon, LogOutIcon } from '@entur/icons'
+import { useToast } from '@entur/alert'
 
 import { TavlaLogo, Github } from '../../assets/icons'
 import Clock from '../Clock'
+import LoginModal from '../LoginModal'
+
+import { useSettingsContext } from '../../settings'
+import { useUser } from '../../auth'
+import firebase from 'firebase'
 
 import './styles.scss'
-import { useSettingsContext } from '../../settings'
 
 function Header({ dashboard, theme }: Props): JSX.Element {
     const [{ logo, logoSize, description }] = useSettingsContext()
+    const [displayLogin, setDisplayLogin] = useState<boolean>(false)
+
+    const userLoggedin = !useUser().isAnonymous
+    const { addToast } = useToast()
 
     const login = (): void => {
-        console.log('YEET')
+        event.preventDefault()
+        setDisplayLogin(true)
+    }
+
+    const logout = (): void => {
+        event.preventDefault()
+        addToast({
+            title: 'Logget ut',
+            content: 'Du er nå logget ut av din konto',
+            variant: 'success',
+        })
+        firebase.auth().signOut()
     }
 
     if (dashboard) {
@@ -38,21 +58,38 @@ function Header({ dashboard, theme }: Props): JSX.Element {
         )
     }
 
+    const loginModal = !userLoggedin ? (
+        <LoginModal
+            open={displayLogin}
+            onDismiss={(): void => setDisplayLogin(false)}
+        />
+    ) : null
+
+    const userItem = !userLoggedin ? (
+        <div className="header__resources__item" onClick={login}>
+            <p className="header__resources__item__text">Logg inn</p>
+            <UserIcon className="header__resources__item__icon" size="1.5rem" />
+        </div>
+    ) : (
+        <div className="header__resources__item" onClick={logout}>
+            <p className="header__resources__item__text">Logg ut</p>
+            <LogOutIcon
+                className="header__resources__item__icon"
+                size="1.5rem"
+            />
+        </div>
+    )
+
     return (
         <div className="header">
+            {loginModal}
             <div className={`header__logo-wrapper`}>
                 <a href="/">
                     <TavlaLogo className={`header__logo`} theme={theme} />
                 </a>
             </div>
             <div className="header__resources">
-                <div className="header__resources__item" onClick={login}>
-                    <p className="header__resources__item__text">Logg inn</p>
-                    <UserIcon
-                        className="header__resources__item__icon"
-                        size="1.5rem"
-                    />
-                </div>
+                {userItem}
                 <div className="header__resources__item">
                     <a href="https://tavla.entur.no/privacy">
                         <p className="header__resources__item__text">
