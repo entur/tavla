23218f0a7d4835ef9d207f30d4f6d380c82d8366
Peter Rydberg <hans.martin.peter.rydberg@entur.org>
2020-07-16 09:21:06 +0200

    Added different check icon

--- src/components/RadioCard/RadioBox/index.tsx
+++ src/components/RadioCard/RadioBox/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
-import { CheckIcon } from '@entur/icons'
-import { colors } from '@entur/tokens'
+import ValidationCheck from '../../../assets/icons/ValidationCheck.svg'
 
 import './styles.scss'
 
@@ -13,12 +12,10 @@ interface Props {
 export function RadioBox({ selected, className }: Props): JSX.Element {
     return (
         <div
-            className={`radio checkmark ${
-                selected ? 'radio__checked' : ''
-            } ${className}`}
+            className={`radio ${selected ? 'radio__checked' : ''} ${className}`}
         >
             {selected ? (
-                <CheckIcon className="radio__icon" color={colors.brand.blue} />
+                <img src={ValidationCheck} className="radio__icon" />
             ) : null}
         </div>
     )
