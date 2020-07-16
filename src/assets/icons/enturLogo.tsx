bb473f9c2574107f935651257775b9b0fcecb8ce
Peter Rydberg <hans.martin.peter.rydberg@entur.org>
2020-07-16 11:14:02 +0200

    Simplify EnturLogo.tsc

--- src/assets/icons/enturLogo.tsx
+++ src/assets/icons/enturLogo.tsx
@@ -4,18 +4,7 @@ import EnturWhite from './../logos/Enturlogo_white.svg'
 import EnturBlack from './../logos/Enturlogo_black.svg'
 
 function EnturLogo({ className, style, height }: Props): JSX.Element {
-    let enturLogo = ''
-
-    switch (style) {
-        case 'white':
-            enturLogo = EnturWhite
-            break
-        case 'black':
-            enturLogo = EnturBlack
-            break
-        default:
-            enturLogo = EnturWhite
-    }
+    const enturLogo = style === 'black' ? EnturBlack : EnturWhite
 
     return <img src={enturLogo} height={height} className={className} />
 }
