ef441b59c00104c1d10be050c7a9f1c84245fc72
Peter Rydberg <hans.martin.peter.rydberg@entur.org>
2020-07-20 08:24:49 +0200

    Fix styling and classnames

--- src/containers/MyBoards/BoardCard/index.tsx
+++ src/containers/MyBoards/BoardCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Heading3, Paragraph } from '@entur/typography'
+import { Heading3 } from '@entur/typography'
 
 import { Settings } from '../../../settings'
 import { ThemeDashbboardPreview } from '../../../assets/icons/ThemeDashboardPreview'
@@ -17,18 +17,16 @@ function BoardCard({ settings, id, callback, className }: Props): JSX.Element {
                 className="board-card__preview"
                 src={preview[`${dashboardType}`]}
             />
-            <div className="board-card__radio-container">
-                <div className="board-card__radio-container__header-wrapper">
-                    <Heading3
-                        className="board-card__radio-container__header-wrapper__title"
-                        margin="none"
-                    >
-                        {'title'}
-                    </Heading3>
-                </div>
-                <Paragraph className="board-card__radio-container__description">
+            <div className="board-card__text-container">
+                <Heading3
+                    className="board-card__text-container__title"
+                    margin="none"
+                >
+                    {'title'}
+                </Heading3>
+                <span className="board-card__text-container__description">
                     {id}
-                </Paragraph>
+                </span>
             </div>
         </div>
     )
