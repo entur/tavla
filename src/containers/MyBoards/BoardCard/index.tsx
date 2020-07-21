28bbbfecef23805c5dcaad4d901c37a9c5a9297a
Peter Rydberg <hans.martin.peter.rydberg@entur.org>
2020-07-21 09:14:50 +0200

    Update board names

--- src/containers/MyBoards/BoardCard/index.tsx
+++ src/containers/MyBoards/BoardCard/index.tsx
@@ -5,6 +5,7 @@ import { LinkIcon, ClockIcon } from '@entur/icons'
 
 import { Settings } from '../../../settings'
 import { ThemeDashbboardPreview } from '../../../assets/icons/ThemeDashboardPreview'
+import { persist } from '../../../settings/FirestoreStorage'
 
 import './styles.scss'
 
@@ -38,6 +39,20 @@ function createTimeString(date: Date): string {
 function BoardCard({ settings, id, timestamp, className }: Props): JSX.Element {
     const [titleEditMode, setTitleEditMode] = useState<boolean>(false)
 
+    const onClickTitle = useCallback(() => {
+        event.preventDefault()
+        setTitleEditMode(true)
+    }, [setTitleEditMode])
+
+    const onBlurTitle = useCallback(
+        (e) => {
+            event.preventDefault()
+            setTitleEditMode(false)
+            persist(id, 'boardName', e.target.value)
+        },
+        [id, setTitleEditMode],
+    )
+
     const preview = ThemeDashbboardPreview(settings.theme)
     const dashboardType = settings.dashboard ? settings.dashboard : 'Chrono'
     const timeString = timestamp
@@ -51,23 +66,13 @@ function BoardCard({ settings, id, timestamp, className }: Props): JSX.Element {
             defaultValue={boardTitle}
             ref={boardTitleEditorRef}
             autoFocus={true}
+            onBlur={onBlurTitle}
         />
     ) : (
         <Heading3 className="board-card__text-container__title" margin="none">
             {boardTitle}
         </Heading3>
     )
-
-    const onClickTitle = useCallback(() => {
-        event.preventDefault()
-        setTitleEditMode(true)
-    }, [setTitleEditMode])
-
-    const onBlurTitle = useCallback(() => {
-        event.preventDefault()
-        setTitleEditMode(false)
-        //save new title
-    }, [setTitleEditMode])
 
     return (
         <div className={`board-card ${className ? className : ''}`}>
@@ -79,9 +84,7 @@ function BoardCard({ settings, id, timestamp, className }: Props): JSX.Element {
             </a>
 
             <div className="board-card__text-container">
-                <span onClick={onClickTitle} onBlur={onBlurTitle}>
-                    {boardTitleElement}
-                </span>
+                <span onClick={onClickTitle}>{boardTitleElement}</span>
 
                 <div className="board-card__text-container__text">
                     <ClockIcon className="board-card__text-container__text__icon" />
