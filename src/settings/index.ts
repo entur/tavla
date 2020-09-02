4ca32c8104e2da372276bf91b92f64b761b02e24
Mats Byrkjeland <mats.byrkjeland@entur.org>
2020-09-02 17:32:50 +0200

    Reduce a lot of unnecessary calls to JourneyPlanner when on admin
    When editing some setting on the admin page, the new settings is persisted to Firestore.
    When something is edited on Firestore, this triggers a new onSnapshot call.
    The new snapshot call sets a new Settings object, although nothing is really changed.
    This triggers a bunch of hooks that fetches new data.
    
    This PR fixes this by ignoring the onSnapshot setter when on admin

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -150,7 +150,12 @@ export function useSettings(): [Settings | null, SettingsSetters] {
                     })
                 }
 
-                setSettings(settingsWithDefaults)
+                setSettings((prevSettings) => {
+                    const onAdmin = location.pathname.split('/')[1] === 'admin'
+                    return prevSettings && onAdmin
+                        ? prevSettings
+                        : settingsWithDefaults
+                })
             })
         }
 
