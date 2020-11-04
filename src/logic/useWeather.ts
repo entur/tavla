21f6ae191f190934751dc0f42245a6f09fd0b79e
Kenan <kenan.mahic@bekk.no>
2020-11-04 09:42:10 +0100

    Added a custom hook for fetching locationforecast data from Yr
    Removed console.logs

---
+++ src/logic/useWeather.ts
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+
+import { REFRESH_INTERVAL } from '../constants'
+
+import { useSettingsContext } from '../settings'
+
+async function getWeather(
+    latitude: number | null,
+    longitude: number | null,
+): Promise<TimeseriesPoint[]> {
+    const url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${latitude}&lon=${longitude}`
+    const weather = await fetch(url).then((response) => {
+        if (!response.ok) {
+            throw new Error(response.statusText)
+        }
+        return response.json()
+    })
+    return weather.properties.timeseries.map((point: TimeseriesPoint) => point)
+}
+
+export default function useWeather(): TimeseriesPoint[] | null {
+    const [settings] = useSettingsContext()
+    const [weather, setWeather] = useState<TimeseriesPoint[] | null>(null)
+
+    const coordinates = settings?.coordinates
+    useEffect(() => {
+        getWeather(
+            coordinates?.longitude ?? 0,
+            coordinates?.latitude ?? 0,
+        ).then(setWeather)
+
+        const intervalId = setInterval(() => {
+            getWeather(coordinates?.longitude ?? 0, coordinates?.latitude ?? 0)
+                .then((res) => res)
+                .then(setWeather)
+        }, REFRESH_INTERVAL)
+        return (): void => clearInterval(intervalId)
+    }, [coordinates])
+    return weather
+}
+
+interface TimeseriesPoint {
+    data: {
+        instant: { details: WeatherDetails }
+        next_1_hours: {
+            details: { precipitation_amount: number }
+            summary: { symbol_code: string }
+        }
+        next_6_hours: {
+            details: { precipitation_amount: number }
+            summary: { symbol_code: string }
+        }
+        next_12_hours: {
+            summary: { symbol_code: string }
+        }
+    }
+    time: Date
+}
+
+interface WeatherDetails {
+    air_pressure_at_sea_level: number
+    air_temperature: number
+    cloud_area_fraction: number
+    relative_humidity: number
+    wind_from_direction: number
+    wind_speed: number
+}
