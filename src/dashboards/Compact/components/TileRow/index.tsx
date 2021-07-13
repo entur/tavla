3d9f0af99888af62b6335bfb14b99944379350e9
Frida Solheim <fridasoso@gmail.com>
2021-07-13 12:56:54 +0200

    applied changes to compact

--- src/dashboards/Compact/components/TileRow/index.tsx
+++ src/dashboards/Compact/components/TileRow/index.tsx
@@ -48,28 +48,24 @@ function SubLabelIcon({
     subLabel: TileSubLabel
     hideSituations?: boolean
 }): JSX.Element | null {
+    if (!hideSituations && subLabel?.situation)
+        if (isMobileWeb())
+            return (
+                <div className="tilerow__sublabel__situation">
+                    <SituationModal situationMessage={subLabel.situation} />
+                </div>
+            )
+        else
+            return (
+                <div className="tilerow__sublabel__situation">
+                    <ValidationExclamation />
+                </div>
+            )
+
     if (subLabel.hasCancellation)
         return (
             <div className="tilerow__sublabel__cancellation">
                 <ValidationError />
-            </div>
-        )
-
-    if (
-        subLabel.hasSituation &&
-        !hideSituations &&
-        isMobileWeb() &&
-        subLabel?.situation
-    )
-        return (
-            <div className="tilerow__sublabel__situation">
-                <SituationModal situationMessage={subLabel.situation} />
-            </div>
-        )
-    else if (subLabel.hasSituation && !hideSituations)
-        return (
-            <div className="tilerow__sublabel__situation">
-                <ValidationExclamation />
             </div>
         )
     return null
