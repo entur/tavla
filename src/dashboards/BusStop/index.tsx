a7ee6f3d5bfda51ca24c06ca91c9e82197b7e7a1
Frida Solheim <fridasoso@gmail.com>
2021-07-19 11:16:09 +0200

    removed unused classname

--- src/dashboards/BusStop/index.tsx
+++ src/dashboards/BusStop/index.tsx
@@ -48,7 +48,7 @@ function BusStop({ history }: Props): JSX.Element | null {
             history={history}
             stopPlacesWithDepartures={stopPlacesWithDepartures}
         >
-            <GridContainer className="busStop__container">
+            <GridContainer>
                 <GridItem large={mediumWidth} medium={12} small={12}>
                     <div className={!settings?.showMap ? 'busStop__tiles' : ''}>
                         {(stopPlacesWithDepartures || []).map((stop, index) => (
