0f8c8c8ae4c99e58e0761226e384f5e99b214523
Annelise Østby <annelise.ostby@bekk.no>
2021-07-23 09:09:22 +0200

    simplify code

--- src/dashboards/Compact/MapTile/index.tsx
+++ src/dashboards/Compact/MapTile/index.tsx
@@ -12,7 +12,7 @@ import { StopPlaceWithDepartures } from '../../../types'
 function MapTile(data: Props): JSX.Element {
     return (
         <div className="maptile">
-            <MapView {...data} interactive={true}></MapView>
+            <MapView {...data} interactive></MapView>
         </div>
     )
 }
