40ad6463c877ba10684611ae159aad160cfb43d0
= <hakoncb@gmail.com>
2021-08-19 16:38:17 +0200

    Extract helper function

---
+++ functions/src/utils.ts
@@ -0,0 +1,10 @@
+export const extractPathFromUrl = (url: string): string => {
+    const PATH_REGEX = /(images.*\??)(?=\?)/gi //yolo
+    const matches = url.match(PATH_REGEX)
+    if (!matches) {
+        throw new Error('Image path could not be found')
+    }
+    const rawPath = matches[0]
+    const path = rawPath.replace('%', '/')
+    return path
+}
