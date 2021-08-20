7047f1f23f2e779790facbdb925d09add63a31de
= <hakoncb@gmail.com>
2021-08-20 14:55:22 +0200

    Remove comment

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,5 +1,5 @@
 export const extractPathFromUrl = (url: string): string => {
-    const PATH_REGEX = /(images.*\??)(?=\?)/gi //yolo
+    const PATH_REGEX = /(images.*\??)(?=\?)/gi
     const matches = url.match(PATH_REGEX)
     if (!matches) {
         throw new Error('Image path could not be found')
