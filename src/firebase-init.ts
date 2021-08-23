2818ad67f79fcf80fe36e98b7128f142bdca65de
= <hakoncb@gmail.com>
2021-08-23 14:16:09 +0200

    Redirect firebase services to emulators

--- src/firebase-init.ts
+++ src/firebase-init.ts
@@ -4,5 +4,11 @@ if (!process.env.FIREBASE_CONFIG) {
     // eslint-disable-next-line no-console
     console.error('The environment variable FIREBASE_CONFIG is missing!')
 } else {
-    firebase.initializeApp(JSON.parse(process.env.FIREBASE_CONFIG))
+    const app = firebase.initializeApp(JSON.parse(process.env.FIREBASE_CONFIG))
+    if (window.location.hostname === 'localhost') {
+        app.functions().useEmulator('localhost', 5001)
+        app.auth().useEmulator('http://localhost:9099')
+        app.firestore().useEmulator('localhost', 8080)
+        app.storage().useEmulator('localhost', 9199)
+    }
 }
