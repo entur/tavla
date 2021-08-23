39612f44334b3402d235618471e2df6151ebf8d0
= <hakoncb@gmail.com>
2021-08-23 16:10:56 +0200

    Connect to emulators

--- src/firebase-init.ts
+++ src/firebase-init.ts
@@ -5,7 +5,7 @@ if (!process.env.FIREBASE_CONFIG) {
     console.error('The environment variable FIREBASE_CONFIG is missing!')
 } else {
     const app = firebase.initializeApp(JSON.parse(process.env.FIREBASE_CONFIG))
-    if (window.location.hostname === 'localhost') {
+    if (process.env.FIREBASE_ENV === 'local') {
         app.functions().useEmulator('localhost', 5001)
         app.auth().useEmulator('http://localhost:9099')
         app.firestore().useEmulator('localhost', 8080)
