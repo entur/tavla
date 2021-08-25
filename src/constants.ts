06ed69cddd7c67256b9070886471318fa09a4286
Bendik Kvernevik <bendiksem93@gmail.com>
2021-08-25 13:46:50 +0200

    Removed Bolt from active operators
    Will be implemented when logo is ready

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,6 @@ export const MAX_DISTANCE = 1000
 export const DEFAULT_DISTANCE = 500
 export const REFRESH_INTERVAL = 30000
 export const ALL_ACTIVE_OPERATOR_IDS = {
-    BOLT: 'YBO:Operator:bolt',
     LIME: 'YLI:Operator:lime',
     VOI: 'YVO:Operator:voi',
     TIER: 'YTI:Operator:Tier',
