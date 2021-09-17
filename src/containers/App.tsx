3ac57514bef9b6a29b59c3d642081364890f2cbc
Magnus Rand <magnus.rand@gmail.com>
2021-09-17 15:51:23 +0200

    FIx path error in updateManifest function and update manifest.json

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -75,13 +75,49 @@ function updateManifest(pathName: string): void {
             lang: 'no',
             icons: [
                 {
-                    src: '/images/logo/logo-192x192.png',
+                    src: `${pathName}/images/logo/logo-72x72.png`,
+                    sizes: '72x72',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+                {
+                    src: `${pathName}images/logo/logo-96x96.png`,
+                    sizes: '96x96',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+                {
+                    src: `${pathName}images/logo/logo-128x128.png`,
+                    sizes: '128x128',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+                {
+                    src: `${pathName}images/logo/logo-144x144.png`,
+                    sizes: '144x144',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+                {
+                    src: `${pathName}images/logo/logo-152x152.png`,
+                    sizes: '152x152',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+                {
+                    src: `${pathName}images/logo/logo-192x192.png`,
                     sizes: '192x192',
                     type: 'image/png',
                     purpose: 'any maskable',
                 },
                 {
-                    src: '/images/logo/logo-512x512.png',
+                    src: `${pathName}images/logo/logo-384x384.png`,
+                    sizes: '384x384',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+                {
+                    src: `${pathName}images/logo/logo-512x512.png`,
                     sizes: '512x512',
                     type: 'image/png',
                     purpose: 'any maskable',
@@ -89,7 +125,7 @@ function updateManifest(pathName: string): void {
             ],
             splash_screen: [
                 {
-                    src: '/images/splash/startup-image-1284x2778.png',
+                    src: `${pathName}/images/splash/startup-image-1284x2778.png`,
                     sizes: '1284x2778',
                     type: 'image/png',
                 },
