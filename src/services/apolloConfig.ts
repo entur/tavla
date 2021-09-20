e6a3b8aec8acf13c30678e722efc61c8abb82080
= <hakoncb@gmail.com>
2021-09-20 14:50:47 +0200

    Only use prod api

--- src/services/apolloConfig.ts
+++ src/services/apolloConfig.ts
@@ -7,14 +7,14 @@ export const getGraphqlEndpoint = (): string => {
     if (REALTIME_GRAPHQL_ENDPOINT) {
         return REALTIME_GRAPHQL_ENDPOINT
     }
-
+    //Satt mot prod alt sammen da det i skrivende stund er ganske stor forskjell på dataen man får fra de forskjellige miljøene
     switch (window.location.host) {
         case 'tavla.staging.entur.no':
-            return 'https://api.staging.entur.io/realtime/v1/vehicles/graphql'
+            return 'https://api.entur.io/realtime/v1/vehicles/graphql'
         case 'tavla.entur.no':
             return 'https://api.entur.io/realtime/v1/vehicles/graphql'
         default:
-            return 'https://api.dev.entur.io/realtime/v1/vehicles/graphql'
+            return 'https://api.entur.io/realtime/v1/vehicles/graphql'
     }
 }
 
@@ -22,13 +22,13 @@ export const getSubscriptionsEndpoint = (): string => {
     if (REALTIME_SUBSCRIPTIONS_ENDPOINT) {
         return REALTIME_SUBSCRIPTIONS_ENDPOINT
     }
-
+    //Satt mot prod alt sammen da det i skrivende stund er ganske stor forskjell på dataen man får fra de forskjellige miljøene
     switch (window.location.host) {
         case 'tavla.staging.entur.no':
-            return 'wss://api.staging.entur.io/realtime/v1/vehicles/subscriptions'
+            return 'wss://api.entur.io/realtime/v1/vehicles/subscriptions'
         case 'tavla.entur.no':
             return 'wss://api.entur.io/realtime/v1/vehicles/subscriptions'
         default:
-            return 'wss://api.dev.entur.io/realtime/v1/vehicles/subscriptions'
+            return 'wss://api.entur.io/realtime/v1/vehicles/subscriptions'
     }
 }
