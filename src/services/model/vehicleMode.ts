5d68f538302aa3eaf32270dad3950e871f5dca65
= <hakoncb@gmail.com>
2021-09-20 13:06:24 +0200

    Make uppercase

--- src/services/model/vehicleMode.ts
+++ src/services/model/vehicleMode.ts
@@ -1,9 +1,9 @@
 export enum VEHICLE_MODE {
-  AIR = "air",
-  BUS = "bus",
-  COACH = "coach",
-  FERRY = "ferry",
-  METRO = "metro",
-  RAIL = "rail",
-  TRAM = "tram",
+    AIR = 'AIR',
+    BUS = 'BUS',
+    COACH = 'COACH',
+    FERRY = 'FERRY',
+    METRO = 'METRO',
+    RAIL = 'RAIL',
+    TRAM = 'TRAM',
 }
