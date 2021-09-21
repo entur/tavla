b1ad6f552561072b2d2eee7ce39dc569038d9b28
= <hakoncb@gmail.com>
2021-09-21 12:35:33 +0200

    add correct font

--- src/dashboards/Map/DepartureTag/index.tsx
+++ src/dashboards/Map/DepartureTag/index.tsx
@@ -3,11 +3,7 @@ import React from 'react'
 import { colors } from '@entur/tokens'
 import { Heading4 } from '@entur/typography'
 
-import {
-    StopPlaceWithDepartures,
-    IconColorType,
-    LineData,
-} from '../../../types'
+import { StopPlaceWithDepartures, IconColorType } from '../../../types'
 
 import {
     getIcon,
