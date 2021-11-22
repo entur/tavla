cb9a7b057e927e00bfaf19acda61375c230c7b68
= <hakoncb@gmail.com>
2021-11-22 14:59:17 +0100

    Set color of qr code based on theme

--- src/components/QRTile/index.tsx
+++ src/components/QRTile/index.tsx
@@ -4,28 +4,34 @@ import QRCode from 'react-qr-code'
 import { colors } from '@entur/tokens'
 import { Link, Paragraph } from '@entur/typography'
 
-import { CustomQRTile } from '../../types'
+import { CustomQRTile, Theme } from '../../types'
+import { useSettingsContext } from '../../settings'
 
 import './styles.scss'
 
-const QRTile = ({ linkAddress, description }: CustomQRTile): JSX.Element => (
-    <div className="qr-tile__wrapper">
-        <div className="qr-tile__code">
-            <QRCode
-                className="qr-code"
-                value={linkAddress}
-                size={256}
-                fgColor={colors.brand.blue}
-                level="Q"
-            ></QRCode>
+const QRTile = ({ linkAddress, description }: CustomQRTile): JSX.Element => {
+    const [settings] = useSettingsContext()
+    const { theme = Theme.DEFAULT } = settings || {}
+
+    return (
+        <div className="qr-tile__wrapper">
+            <div className="qr-tile__code">
+                <QRCode
+                    className="qr-code"
+                    value={linkAddress}
+                    size={256}
+                    fgColor={theme !== Theme.DARK ? colors.brand.blue : 'black'}
+                    level="Q"
+                ></QRCode>
+            </div>
+            <Link className="qr-tile__link">{linkAddress}</Link>
+            {description && (
+                <Paragraph className="qr-tile__description">
+                    {description}
+                </Paragraph>
+            )}
         </div>
-        <Link className="qr-tile__link">{linkAddress}</Link>
-        {description && (
-            <Paragraph className="qr-tile__description">
-                {description}
-            </Paragraph>
-        )}
-    </div>
-)
+    )
+}
 
 export default QRTile
