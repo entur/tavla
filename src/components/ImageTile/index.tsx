036d41ec6ca1f0dd6c65df57d7a80790363ce952
= <hakoncb@gmail.com>
2021-11-22 13:38:35 +0100

    Make image background

--- src/components/ImageTile/index.tsx
+++ src/components/ImageTile/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+
+import { Heading3, Paragraph } from '@entur/typography'
+
 import { CustomImageTile } from '../../types'
 
-import './style.scss'
+import './styles.scss'
 
 const ImageTile = ({
     linkAddress,
     description,
     displayHeader,
 }: CustomImageTile): JSX.Element => (
-    <img
-        className="image-tile__image"
-        src={linkAddress}
-        alt="Random image"
-        draggable={false}
-    />
+    <div
+        className="image-tile"
+        style={{ backgroundImage: `url("${linkAddress}")` }}
+    >
+        <div className="image-tile__info-box">
+            <Heading3>{displayHeader.toUpperCase()}</Heading3>
+            {description && <Paragraph>{description}</Paragraph>}
+        </div>
+    </div>
 )
 
 export default ImageTile
