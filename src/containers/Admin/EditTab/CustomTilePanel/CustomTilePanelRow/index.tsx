804833de7e1ba594b2bd123bff9706e625076ede
= <hakoncb@gmail.com>
2021-11-25 11:34:09 +0100

    Factor out panelRow to own component

---
+++ src/containers/Admin/EditTab/CustomTilePanel/CustomTilePanelRow/index.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+import { Checkbox } from '@entur/form'
+import { DeleteIcon, EditIcon } from '@entur/icons'
+
+import { useSettingsContext } from '../../../../../settings'
+import { CustomTile } from '../../../../../types'
+
+import '../styles.scss'
+
+interface Props extends CustomTile {
+    setSelectedTileId: (id: string) => void
+    setIsOpenModal: (isOpen: boolean) => void
+}
+
+const CustomTilePanelRow = ({
+    id,
+    displayName,
+    setSelectedTileId,
+    setIsOpenModal,
+}: Props): JSX.Element => {
+    const [settings, setSettings] = useSettingsContext()
+    const {
+        hiddenCustomTileIds = [],
+        customImageTiles = [],
+        customQrTiles = [],
+    } = settings || {}
+
+    return (
+        <div className="custom-tile-panel__row">
+            <Checkbox
+                onChange={() => {
+                    hiddenCustomTileIds.find((hiddenId) => hiddenId === id)
+                        ? setSettings({
+                              hiddenCustomTileIds: hiddenCustomTileIds.filter(
+                                  (hiddenId) => hiddenId !== id,
+                              ),
+                          })
+                        : setSettings({
+                              hiddenCustomTileIds: [...hiddenCustomTileIds, id],
+                          })
+                }}
+                checked={!hiddenCustomTileIds.includes(id)}
+            ></Checkbox>
+            <span>{displayName}</span>
+            <div className="custom-tile-panel__icons">
+                <EditIcon
+                    onClick={() => {
+                        setSelectedTileId(id)
+                        setIsOpenModal(true)
+                    }}
+                />
+                <DeleteIcon
+                    onClick={() =>
+                        setSettings({
+                            customImageTiles: customImageTiles.filter(
+                                (el) => el.id !== id,
+                            ),
+                            customQrTiles: customQrTiles.filter(
+                                (el) => el.id !== id,
+                            ),
+                            hiddenCustomTileIds: hiddenCustomTileIds.filter(
+                                (el) => el !== id,
+                            ),
+                        })
+                    }
+                />
+            </div>
+        </div>
+    )
+}
+
+export default CustomTilePanelRow
--- src/containers/Admin/EditTab/CustomTilePanel/index.tsx
+++ src/containers/Admin/EditTab/CustomTilePanel/index.tsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react'
 
 import { PrimaryButton } from '@entur/button'
-import { Checkbox } from '@entur/form'
-import { DeleteIcon, EditIcon } from '@entur/icons'
 
 import CustomTileModal from '../../../../components/CustomTileModal'
 import { useSettingsContext } from '../../../../settings'
 
+import CustomTilePanelRow from './CustomTilePanelRow'
+
 import './styles.scss'
 
 const CustomTilePanel = (): JSX.Element => {
-    const [settings, setSettings] = useSettingsContext()
-    const { hiddenCustomTileIds = [] } = settings || {}
+    const [settings] = useSettingsContext()
     const { customQrTiles = [], customImageTiles = [] } = settings || {}
     const [isOpenModal, setIsOpenModal] = useState(false)
     const [selectedTileId, setSelectedTileId] = useState<string | undefined>(
@@ -35,58 +34,14 @@ const CustomTilePanel = (): JSX.Element => {
                     selectedTileId={selectedTileId}
                 ></CustomTileModal>
             )}
-            {[...customQrTiles, ...customImageTiles].map(
-                ({ id, displayName }) => (
-                    <div key={id} className="custom-tile-panel__row">
-                        <Checkbox
-                            onChange={() => {
-                                hiddenCustomTileIds.find(
-                                    (hiddenId) => hiddenId === id,
-                                )
-                                    ? setSettings({
-                                          hiddenCustomTileIds:
-                                              hiddenCustomTileIds.filter(
-                                                  (hiddenId) => hiddenId !== id,
-                                              ),
-                                      })
-                                    : setSettings({
-                                          hiddenCustomTileIds: [
-                                              ...hiddenCustomTileIds,
-                                              id,
-                                          ],
-                                      })
-                            }}
-                            checked={!hiddenCustomTileIds.includes(id)}
-                        ></Checkbox>
-                        <span>{displayName}</span>
-                        <div className="custom-tile-panel__icons">
-                            <EditIcon
-                                onClick={() => {
-                                    setSelectedTileId(id)
-                                    setIsOpenModal(true)
-                                }}
-                            />
-                            <DeleteIcon
-                                onClick={() =>
-                                    setSettings({
-                                        customImageTiles:
-                                            customImageTiles.filter(
-                                                (el) => el.id !== id,
-                                            ),
-                                        customQrTiles: customQrTiles.filter(
-                                            (el) => el.id !== id,
-                                        ),
-                                        hiddenCustomTileIds:
-                                            hiddenCustomTileIds.filter(
-                                                (el) => el !== id,
-                                            ),
-                                    })
-                                }
-                            />
-                        </div>
-                    </div>
-                ),
-            )}
+            {[...customQrTiles, ...customImageTiles].map((customTile) => (
+                <CustomTilePanelRow
+                    key={customTile.id}
+                    {...customTile}
+                    setIsOpenModal={setIsOpenModal}
+                    setSelectedTileId={setSelectedTileId}
+                />
+            ))}
         </div>
     )
 }
