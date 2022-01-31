946e8860726de091e9697cca0a4a7d10e98d70f7
Mats Byrkjeland <mats.byrkjeland@entur.org>
2022-01-31 17:26:37 +0100

    Move functions to Europe, same region as Firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import { https, firestore as firestoreDB, region } from 'firebase-functions'
+import { https, region } from 'firebase-functions'
 import { initializeApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 import { getFirestore } from 'firebase-admin/firestore'
@@ -6,60 +6,62 @@ import { getStorage } from 'firebase-admin/storage'
 
 initializeApp()
 
-export const getImageUploadToken = https.onCall(async (data, context) => {
-    if (!context.auth) {
-        throw new https.HttpsError(
-            'failed-precondition',
-            'The function must be called while authenticated.',
+export const getImageUploadToken = region('europe-west3').https.onCall(
+    async (data, context) => {
+        if (!context.auth) {
+            throw new https.HttpsError(
+                'failed-precondition',
+                'The function must be called while authenticated.',
+            )
+        }
+
+        if (typeof data.imageUid !== 'string') {
+            throw new https.HttpsError(
+                'invalid-argument',
+                'Image UID must be a string.',
+            )
+        }
+
+        const doc = await getFirestore()
+            .collection('settings')
+            .doc(data.imageUid)
+            .get()
+
+        if (!doc.exists) {
+            throw new https.HttpsError(
+                'invalid-argument',
+                'Image UID must refer to an existing settings document.',
+            )
+        }
+
+        const documentData = doc.data()
+
+        if (
+            !documentData ||
+            (documentData.owners.length > 0 &&
+                !documentData.owners.includes(context.auth.uid))
+        ) {
+            throw new https.HttpsError(
+                'permission-denied',
+                'Account running function must have access to settings document.',
+            )
+        }
+
+        const metadata = {
+            uploadUid: doc.exists && data.imageUid,
+        }
+
+        const uploadToken = await getAuth().createCustomToken(
+            context.auth.uid,
+            metadata,
         )
-    }
 
-    if (typeof data.imageUid !== 'string') {
-        throw new https.HttpsError(
-            'invalid-argument',
-            'Image UID must be a string.',
-        )
-    }
+        return { uploadToken }
+    },
+)
 
-    const doc = await getFirestore()
-        .collection('settings')
-        .doc(data.imageUid)
-        .get()
-
-    if (!doc.exists) {
-        throw new https.HttpsError(
-            'invalid-argument',
-            'Image UID must refer to an existing settings document.',
-        )
-    }
-
-    const documentData = doc.data()
-
-    if (
-        !documentData ||
-        (documentData.owners.length > 0 &&
-            !documentData.owners.includes(context.auth.uid))
-    ) {
-        throw new https.HttpsError(
-            'permission-denied',
-            'Account running function must have access to settings document.',
-        )
-    }
-
-    const metadata = {
-        uploadUid: doc.exists && data.imageUid,
-    }
-
-    const uploadToken = await getAuth().createCustomToken(
-        context.auth.uid,
-        metadata,
-    )
-
-    return { uploadToken }
-})
-
-export const deleteImagefromStorage = firestoreDB
-    .document('settings/{settingsID}')
+export const deleteImagefromStorage = region('europe-west3')
+    .firestore.document('settings/{settingsID}')
     .onUpdate(async (change, context) => {
         if (change.before.data().logo && !change.after.data().logo) {
             try {
@@ -76,7 +78,7 @@ export const deleteImagefromStorage = firestoreDB
         }
     })
 
-export const scheduledDeleteOfDocumentsSetToBeDeleted = region('us-central1')
+export const scheduledDeleteOfDocumentsSetToBeDeleted = region('europe-west3')
     .pubsub.schedule('every day 04:00')
     .timeZone('Europe/Oslo')
     .onRun(() => {
@@ -103,50 +105,50 @@ export const scheduledDeleteOfDocumentsSetToBeDeleted = region('us-central1')
         return null
     })
 
-export const getOwnersDataByBoardIdAsOwner = https.onCall(
-    async (data, context) => {
-        if (!context.auth) {
-            throw new https.HttpsError(
-                'failed-precondition',
-                'The function must be called while authenticated.',
-            )
-        }
+export const getOwnersDataByBoardIdAsOwner = region(
+    'europe-west3',
+).https.onCall(async (data, context) => {
+    if (!context.auth) {
+        throw new https.HttpsError(
+            'failed-precondition',
+            'The function must be called while authenticated.',
+        )
+    }
 
-        const boardRef = getFirestore().collection('settings').doc(data.boardId)
-        const boardSnapshot = await boardRef.get()
-        if (!boardSnapshot.exists) {
-            throw new https.HttpsError(
-                'invalid-argument',
-                'Requested board was not found.',
-            )
-        }
-        const boardOwners = boardSnapshot.data()?.owners
-        if (!boardOwners.includes(context.auth?.uid)) {
-            throw new https.HttpsError(
-                'permission-denied',
-                'You need to be an owner of this board to get data about other owners.',
-            )
-        }
+    const boardRef = getFirestore().collection('settings').doc(data.boardId)
+    const boardSnapshot = await boardRef.get()
+    if (!boardSnapshot.exists) {
+        throw new https.HttpsError(
+            'invalid-argument',
+            'Requested board was not found.',
+        )
+    }
+    const boardOwners = boardSnapshot.data()?.owners
+    if (!boardOwners.includes(context.auth?.uid)) {
+        throw new https.HttpsError(
+            'permission-denied',
+            'You need to be an owner of this board to get data about other owners.',
+        )
+    }
 
-        const boardOwnersMapped = boardOwners.map((id: string) => ({ uid: id }))
+    const boardOwnersMapped = boardOwners.map((id: string) => ({ uid: id }))
 
-        try {
-            const usersResult = await getAuth().getUsers(boardOwnersMapped)
-            const ownersData = usersResult.users.map((user) => ({
-                uid: user.uid,
-                email: user.email,
-            }))
-            return ownersData
-        } catch {
-            throw new https.HttpsError(
-                'invalid-argument',
-                'Could not get requested data about owners.',
-            )
-        }
-    },
-)
+    try {
+        const usersResult = await getAuth().getUsers(boardOwnersMapped)
+        const ownersData = usersResult.users.map((user) => ({
+            uid: user.uid,
+            email: user.email,
+        }))
+        return ownersData
+    } catch {
+        throw new https.HttpsError(
+            'invalid-argument',
+            'Could not get requested data about owners.',
+        )
+    }
+})
 
-export const addInvitedUserToBoardOwners = https.onCall(
+export const addInvitedUserToBoardOwners = region('europe-west3').https.onCall(
     async (data, context) => {
         if (!context.auth) {
             throw new https.HttpsError(
