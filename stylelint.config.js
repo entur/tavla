699d1508e51a572b562b48ad33f01b14b9d08e23
Ørjan Helstrøm <orjan.helstrom@gmail.com>
2022-10-05 14:07:10 +0200

    update-dependencies * Update dependencies * Fix issues after upgrading stylelint packages * Add stylelint-scss to support linting scss better * Rename .stylelintrc -> stylelint.config.js to be able to add comments in the configuration file
    Signed-off-by: Ørjan Helstrøm <orjan.helstrom@gmail.com>

---
+++ stylelint.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    plugins: ['stylelint-scss'],
+    extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+    rules: {
+        'at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: [
+                    'extend',
+                    'at-root',
+                    'debug',
+                    'warn',
+                    'error',
+                    'if',
+                    'else',
+                    'for',
+                    'each',
+                    'while',
+                    'mixin',
+                    'include',
+                    'content',
+                    'return',
+                    'function',
+                ],
+            },
+        ],
+        'no-descending-specificity': [true, { severity: 'warning' }],
+        'selector-class-pattern': null,
+        'shorthand-property-no-redundant-values': null,
+        'color-function-notation': 'legacy',
+        // This should be turned on if we remove all vendor-prefixes
+        'property-no-vendor-prefix': null,
+        'alpha-value-notation': 'number',
+        'declaration-block-no-redundant-longhand-properties': [
+            true,
+            {
+                ignoreShorthands: ['flex-flow'],
+            },
+        ],
+        // Needs to be disabled when using stylelint-scss
+        'function-no-unknown': null,
+        'scss/function-no-unknown': [
+            true,
+            {
+                // Check if we can delete these rules when Slider/styles.scss is deleted
+                ignoreFunctions: ['var', '-webkit-gradient', 'color-stop'],
+            },
+        ],
+    },
+}
