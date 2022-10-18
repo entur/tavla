8a98ac4d44a01e44a0ea97c531e9d9401cc3b7bb
birgitte swensson <birgitte.swensson@bekk.no>
2022-10-18 13:27:52 +0200

    moved fetching of scooters from ScooterTile to poster

--- src/dashboards/Poster/components/ScooterTile/ScooterTile.tsx
+++ src/dashboards/Poster/components/ScooterTile/ScooterTile.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import { ScooterIcon } from '@entur/icons'
-import { FormFactor } from '@entur/sdk/lib/mobility/types'
 import { NumberDisplay } from '../NumberDisplay/NumberDisplay'
-import { useMobility } from '../../../../logic/useMobility'
 
-const ScooterTile = ({ numberOfScooters }: ScooterTileProps): JSX.Element => {
-    const scooters = useMobility(FormFactor.SCOOTER)
-    return (
-        <>
-            <ScooterIcon />
-            <NumberDisplay numberOfVehicles={scooters?.length || 0} />
-        </>
-    )
-}
+const ScooterTile = ({ numberOfScooters }: ScooterTileProps): JSX.Element => (
+    <>
+        <ScooterIcon />
+        <NumberDisplay numberOfVehicles={numberOfScooters} />
+    </>
+)
 
 interface ScooterTileProps {
     numberOfScooters: number
 }
-
 export { ScooterTile }
+
