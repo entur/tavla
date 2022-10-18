7a0e04170d0abb984650db250ed951ac0a451766
Ørjan Helstrøm <orjan.helstrom@gmail.com>
2022-10-18 13:20:41 +0200

    graphql-multi-endpoint * Add dependency on @habx/apollo-multi-endpoint-link to support multiple endpoints with the same ApolloClient * Add @api directive in VEHICLES_QUERY and VEHICLE_UPDATES_SUBSCRIPTION. This is needed with the new ApolloClient to direct the query/subscription to the right endpoint. * Small refactor to remove a unnecessary useState in useRealtimeVehicleData * Add .npmrc file with save-exact=true to avoid '^' when installing dependencies

--- src/services/realtimeVehicles/realtimeVehiclesService.ts
+++ src/services/realtimeVehicles/realtimeVehiclesService.ts
@@ -1,37 +1,35 @@
-import { split, HttpLink, ApolloClient, InMemoryCache } from '@apollo/client'
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloLink,
+    createHttpLink,
+} from '@apollo/client'
 import { WebSocketLink } from '@apollo/client/link/ws'
-import { getMainDefinition } from '@apollo/client/utilities'
+import { MultiAPILink } from '@habx/apollo-multi-endpoint-link'
 
-const httpLink = new HttpLink({
-    uri:
-        process.env.VEHICLES_REALTIME_HOST ??
-        'https://api.entur.io/realtime/v1/vehicles/graphql',
-})
-
-const wsLink = new WebSocketLink({
-    uri:
-        process.env.VEHICLES_REALTIME_SUBSCRIPTIONS_HOST ??
-        'wss://api.entur.io/realtime/v1/vehicles/subscriptions',
-    options: {
-        reconnect: true,
-    },
-})
-
-const splitLink = split(
-    ({ query }) => {
-        const definition = getMainDefinition(query)
-        return (
-            definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
-        )
-    },
-    wsLink,
-    httpLink,
-)
-
-export const realtimeVehiclesClient = new ApolloClient({
-    link: splitLink,
+const realtimeVehiclesClient = new ApolloClient({
+    link: ApolloLink.from([
+        new MultiAPILink({
+            endpoints: {
+                vehicles:
+                    process.env.VEHICLES_REALTIME_HOST ??
+                    'https://api.entur.io/realtime/v1/vehicles',
+            },
+            httpSuffix: '/graphql',
+            wsSuffix: '/subscriptions',
+            createHttpLink: () => createHttpLink(),
+            createWsLink: (endpoint) =>
+                new WebSocketLink({
+                    uri: endpoint,
+                    options: {
+                        reconnect: true,
+                    },
+                }),
+        }),
+    ]),
     cache: new InMemoryCache({
         addTypename: false,
     }),
 })
+
+export { realtimeVehiclesClient }
