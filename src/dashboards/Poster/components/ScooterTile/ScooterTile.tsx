dcf8128ee223cab0bc0c1b2586b256488a8e8d17
birgitte swensson <birgitte.swensson@bekk.no>
2022-10-18 09:05:04 +0200

    start

--- src/dashboards/Poster/components/ScooterTile/ScooterTile.tsx
+++ src/dashboards/Poster/components/ScooterTile/ScooterTile.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { ScooterIcon } from '@entur/icons'
+import { FormFactor } from '@entur/sdk/lib/mobility/types'
 import { NumberDisplay } from '../NumberDisplay/NumberDisplay'
+import { useMobility } from '../../../../logic/useMobility'
 
-const ScooterTile = ({ numberOfScooters }: ScooterTileProps): JSX.Element => (
-    <>
-        <ScooterIcon />
-        <NumberDisplay numberOfVehicles={numberOfScooters} />
-    </>
-)
+const ScooterTile = ({ numberOfScooters }: ScooterTileProps): JSX.Element => {
+    const scooters = useMobility(FormFactor.SCOOTER)
+    return (
+        <>
+            <ScooterIcon />
+            <NumberDisplay numberOfVehicles={scooters?.length || 0} />
+        </>
+    )
+}
 
 interface ScooterTileProps {
     numberOfScooters: number
