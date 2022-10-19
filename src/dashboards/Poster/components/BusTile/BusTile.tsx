72ae4228442d2bb15c066da55c97b953aa9be566
BirgitteBS <67414026+BirgitteBS@users.noreply.github.com>
2022-10-19 12:37:20 +0200

    Styling - font size in bus line box in Poster view (#752)
    * added check on the size of a bus route number making it possible to adjust font size in order to make the route number fit in the red box

--- src/dashboards/Poster/components/BusTile/BusTile.tsx
+++ src/dashboards/Poster/components/BusTile/BusTile.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react'
+import classNames from 'classnames'
 import { compareAsc } from 'date-fns'
 import { BusIcon } from '@entur/icons'
 import { TransportMode } from '@entur/sdk'
@@ -31,7 +32,13 @@ function BusTile(): JSX.Element {
                     <div key={departure.id} className="poster-bus-tile-row">
                         <div className="poster-bus-tile-route">
                             <BusIcon className="poster-bus-tile-icon" />
-                            <div className="poster-bus-tile-route-number">
+                            <div
+                                className={classNames({
+                                    'poster-bus-tile-route-number': true,
+                                    'poster-bus-tile-route-number--text-small':
+                                        (routeNumber?.length ?? 0) > 3,
+                                })}
+                            >
                                 {routeNumber}
                             </div>
                         </div>
