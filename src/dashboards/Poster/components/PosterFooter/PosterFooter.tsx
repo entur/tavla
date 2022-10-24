2ee532f20457a58e87678ae201c78ee121f0fd22
Katarina Van de Pontseele <54016606+KatarinaVdP@users.noreply.github.com>
2022-10-24 12:58:29 +0200

    Har croppet bildene i footeren, slik at de 'titter opp' fra nederst på… (#756)
    * Har croppet bildene i footeren, slik at de 'titter opp' fra nederst på skjermen
    
    * Made footer header bolder to match design sketches
    
    Co-authored-by: Ferdinand Løberg <ferdloeb@gmail.com>

--- src/dashboards/Poster/components/PosterFooter/PosterFooter.tsx
+++ src/dashboards/Poster/components/PosterFooter/PosterFooter.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Images from '../../../../assets/images/app_images.webp'
+import Images from '../../../../assets/images/app_images_cropped.webp'
 import './PosterFooter.scss'
 
 const PosterFooter = (): JSX.Element => (
     <div className="poster-footer-wrapper">
-        <div className="poster-footer-text">
-            <h2 className="poster-footer-heading">Last ned Entur-appen</h2>
+        <div>
+            <h2 className="poster-footer-heading">Last ned Entur-appen!</h2>
             <h3 className="poster-footer-description">
                 Her finner du kollektiv- og mobilitetstilbud i hele Norge.
             </h3>
