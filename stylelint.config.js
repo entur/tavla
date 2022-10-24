b363c5e8c835d5d4c6df054febe3d60b6adbe0eb
ydalir <ferdloeb@gmail.com>
2022-10-24 09:48:02 +0200

    Sass import change to reduce css duplication (#753)
    * Convert to @use over @import and remove unnecessary imports
    
    * Removed tilde on sass imports from node_modules, no longer necessary

--- stylelint.config.js
+++ stylelint.config.js
@@ -21,6 +21,7 @@ module.exports = {
                     'content',
                     'return',
                     'function',
+                    'use',
                 ],
             },
         ],
