a3a15a844565a7ff859e7a1d02c55b2293cff834
Ferdinand Løberg <ferdloeb@gmail.com>
2022-11-04 13:10:31 +0100

    Changed to use css for responsive tile height

--- src/dashboards/BusStop/BusStopDashboard.tsx
+++ src/dashboards/BusStop/BusStopDashboard.tsx
@@ -1,39 +1,17 @@
 import React, { useState, useEffect, useMemo } from 'react'
-import { Layouts, Layout, WidthProvider, Responsive } from 'react-grid-layout'
-import { useLocation, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { Loader } from '@entur/loader'
 import { DashboardWrapper } from '../../containers/DashboardWrapper/DashboardWrapper'
 import { BREAKPOINTS } from '../../constants'
 import { useStopPlacesWithDepartures, useWalkInfo } from '../../logic'
 import { WalkInfo } from '../../logic/use-walk-info/useWalkInfo'
-import {
-    getFromLocalStorage,
-    saveToLocalStorage,
-} from '../../settings/LocalStorage'
+import { getFromLocalStorage } from '../../settings/LocalStorage'
 import { useSettings } from '../../settings/SettingsProvider'
 import { Item } from '../../components/RearrangeModal/RearrangeModal'
 import { isEqualUnsorted } from '../../utils/array'
 import { DepartureTile } from './DepartureTile/DepartureTile'
 import './BusStopDashboard.scss'
 
-const ResponsiveReactGridLayout = WidthProvider(Responsive)
-
-function getDefaultBreakpoint() {
-    if (window.innerWidth > BREAKPOINTS.lg) {
-        return 'lg'
-    } else if (window.innerWidth > BREAKPOINTS.md) {
-        return 'md'
-    }
-    return 'sm'
-}
-
-const COLS: { [key: string]: number } = {
-    lg: 1,
-    md: 1,
-    sm: 1,
-    xs: 1,
-    xxs: 1,
-}
 function getWalkInfoForStopPlace(
     walkInfos: WalkInfo[],
     id: string,
@@ -41,34 +19,12 @@ function getWalkInfoForStopPlace(
     return walkInfos?.find((walkInfo) => walkInfo.stopId === id)
 }
 
-function getDataGrid(
-    index: number,
-    maxWidth: number,
-): { [key: string]: number } {
-    return {
-        w: 1,
-        maxW: maxWidth,
-        minH: 1,
-        h: 6,
-        x: index % maxWidth,
-        y: 0,
-    }
-}
-
 const BusStopDashboard = (): JSX.Element | null => {
     const [settings] = useSettings()
-    const location = useLocation()
-    const [breakpoint, setBreakpoint] = useState<string>(getDefaultBreakpoint())
-    const [numberOfStopPlaces, setNumberOfStopPlaces] = useState<number>(0)
     const stopPlacesWithDepartures = useStopPlacesWithDepartures()
-
-    const dashboardKey = location.key
 
     const { documentId: boardId } = useParams<{ documentId: string }>()
 
-    const [gridLayouts, setGridLayouts] = useState<Layouts | undefined>(
-        getFromLocalStorage(dashboardKey as string),
-    )
     const [tileOrder, setTileOrder] = useState<Item[] | undefined>(
         boardId ? getFromLocalStorage(boardId + '-tile-order') : undefined,
     )
@@ -82,7 +38,6 @@ const BusStopDashboard = (): JSX.Element | null => {
     }, [stopPlacesWithDepartures])
     const walkInfo = useWalkInfo(walkInfoDestinations)
 
-    const maxWidthCols = COLS[breakpoint] || 1
     const stopPlacesHasLoaded = Boolean(
         stopPlacesWithDepartures ||
             settings?.hiddenModes?.includes('kollektiv'),
@@ -95,7 +50,6 @@ const BusStopDashboard = (): JSX.Element | null => {
                 id: item.id,
                 name: item.name,
             }))
-            setNumberOfStopPlaces(filtered.length)
             defaultTileOrder = filtered
         }
         const storedTileOrder: Item[] | undefined = getFromLocalStorage(
@@ -167,45 +121,18 @@ const BusStopDashboard = (): JSX.Element | null => {
                 </div>
             ) : (
                 <div className="busStop__tiles">
-                    <ResponsiveReactGridLayout
-                        key={breakpoint}
-                        breakpoints={BREAKPOINTS}
-                        cols={COLS}
-                        layouts={gridLayouts}
-                        margin={[32, 32]}
-                        isDraggable={false}
-                        onBreakpointChange={(newBreakpoint: string) => {
-                            setBreakpoint(newBreakpoint)
-                        }}
-                        onLayoutChange={(
-                            layout: Layout[],
-                            layouts: Layouts,
-                        ): void => {
-                            if (numberOfStopPlaces > 0) {
-                                setGridLayouts(layouts)
-                                saveToLocalStorage(
-                                    dashboardKey as string,
-                                    layouts,
-                                )
-                            }
-                        }}
-                    >
-                        {(stopPlacesWithDepartures || []).map((stop, index) => (
-                            <div
-                                key={stop.id}
-                                data-grid={getDataGrid(index, maxWidthCols)}
-                            >
-                                <DepartureTile
-                                    key={index.toString()}
-                                    stopPlaceWithDepartures={stop}
-                                    walkInfo={getWalkInfoForStopPlace(
-                                        walkInfo || [],
-                                        stop.id,
-                                    )}
-                                />
-                            </div>
-                        ))}
-                    </ResponsiveReactGridLayout>
+                    {(stopPlacesWithDepartures || []).map((stop, index) => (
+                        <div key={stop.id}>
+                            <DepartureTile
+                                key={index.toString()}
+                                stopPlaceWithDepartures={stop}
+                                walkInfo={getWalkInfoForStopPlace(
+                                    walkInfo || [],
+                                    stop.id,
+                                )}
+                            />
+                        </div>
+                    ))}
                 </div>
             )}
         </DashboardWrapper>
