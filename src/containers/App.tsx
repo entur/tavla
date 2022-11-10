5e16adadff0e244baddd2471dc313ab4df57d01d
Ferdinand Løberg <ferdloeb@gmail.com>
2022-11-10 12:37:38 +0100

    Removed PwaPrompt

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,18 +1,12 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import classNames from 'classnames'
 import { ApolloProvider } from '@apollo/client'
 import { UserProvider } from '../UserProvider'
 import '../firebase-init'
 import { SettingsProvider } from '../settings/SettingsProvider'
-import PWAPrompt from '../../vendor/react-ios-pwa-prompt'
 import { apolloClient } from '../apollo-client'
 import { DashboardResolver } from '../dashboards/DashboardResolver'
-import {
-    getFromLocalStorage,
-    saveToLocalStorage,
-} from '../settings/LocalStorage'
-import { isMobileWeb } from '../utils/utils'
 import { ToastProvider } from '../types'
 import { useUpdateManifest } from '../hooks/useUpdateManifest'
 import { AdminPage } from './Admin/AdminPage'
@@ -22,58 +16,11 @@ import { Privacy } from './Privacy/Privacy'
 import { MyBoards } from './MyBoards/MyBoards'
 import './styles.scss'
 
-const numberOfVisits = getFromLocalStorage<number>('numberOfVisits') || 1
-
-const hidePWA = (pathName: string) =>
-    pathName === '/' ||
-    pathName.includes('admin') ||
-    window.matchMedia('(display-mode: standalone)').matches ||
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    window.navigator.standalone ||
-    document.referrer.includes('android-app://') ||
-    getFromLocalStorage('pwaPromptShown') ||
-    numberOfVisits < 3 ||
-    ![
-        'iPad Simulator',
-        'iPhone Simulator',
-        'iPod Simulator',
-        'iPad',
-        'iPhone',
-        'iPod',
-    ].includes(navigator.platform)
-
-const ProgressiveWebAppPrompt: React.FC = () => {
-    const location = useLocation()
-
-    useEffect(() => {
-        saveToLocalStorage('numberOfVisits', numberOfVisits + 1)
-    }, [])
-
-    if (hidePWA(location.pathname) || isMobileWeb()) {
-        return null
-    }
-
-    return (
-        <div className="pwa-prompt">
-            <PWAPrompt
-                debug // forcing prompt to show
-                copyTitle="Legg til Tavla på hjemskjermen"
-                copyShareButtonLabel="1) I Safari, trykk på 'Del'-knappen på menyen under."
-                copyAddHomeButtonLabel="2) Trykk 'Legg til på hjemskjerm'."
-                copyClosePrompt="Lukk"
-                onClose={() => saveToLocalStorage('pwaPromptShown', true)}
-            />
-        </div>
-    )
-}
-
 const Content = (): JSX.Element => {
     useUpdateManifest()
 
     return (
         <ApolloProvider client={apolloClient}>
-            <ProgressiveWebAppPrompt />
             <div className={classNames('themeBackground')}>
                 <ToastProvider>
                     <Routes>
