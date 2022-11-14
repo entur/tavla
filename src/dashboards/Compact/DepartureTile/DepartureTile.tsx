520fbe5692e6ac1ae7fa543fd3ab28dc20962b04
Ørjan Helstrøm <orjan.helstrom@gmail.com>
2022-11-14 09:36:29 +0100

    refactor-compact-tiles * Use same Tile and TileHeader in DepartureTile

--- src/dashboards/Compact/DepartureTile/DepartureTile.tsx
+++ src/dashboards/Compact/DepartureTile/DepartureTile.tsx
@@ -6,7 +6,6 @@ import {
     LineData,
     IconColorType,
 } from '../../../types'
-import { Tile } from '../components/Tile/Tile'
 import { TileRow } from '../components/TileRow/TileRow'
 import { useSettings } from '../../../settings/SettingsProvider'
 import { WalkInfo } from '../../../logic/use-walk-info/useWalkInfo'
@@ -15,6 +14,8 @@ import {
     getIconColorType,
     getTransportHeaderIcons,
 } from '../../../utils/icon'
+import { Tile } from '../../../components/Tile/Tile'
+import { TileHeader } from '../../../components/TileHeader/TileHeader'
 
 const DepartureTile = ({
     stopPlaceWithDepartures,
@@ -36,11 +37,12 @@ const DepartureTile = ({
     }, [settings])
 
     return (
-        <Tile
-            title={name}
-            icons={getTransportHeaderIcons(departures, iconColorType)}
-            walkInfo={!settings.hideWalkInfo ? walkInfo : undefined}
-        >
+        <Tile>
+            <TileHeader
+                title={name}
+                icons={getTransportHeaderIcons(departures, iconColorType)}
+                walkInfo={!settings.hideWalkInfo ? walkInfo : undefined}
+            />
             {routes.map((route) => {
                 const routeData = groupedDepartures[route]
                 const firstRouteData = routeData && routeData[0]
