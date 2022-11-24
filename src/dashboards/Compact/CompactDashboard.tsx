7540fc05f9bd0e0b77effb712d9358ed27016b72
ydalir <ferdloeb@gmail.com>
2022-11-24 12:35:48 +0100

    Minor bug fixes (#826)
    * Minor bug fixes
    Removed <text> field
    Changed from 1.8 to 2 height for app qr tile used in ResponsiveReactGridLayout for even spacing

--- src/dashboards/Compact/CompactDashboard.tsx
+++ src/dashboards/Compact/CompactDashboard.tsx
@@ -147,8 +147,8 @@ const CompactDashboard = (): JSX.Element | null => {
                             data-grid={getDataGrid(
                                 maxWidthCols - 1,
                                 maxWidthCols,
-                                1.8,
-                                1.8,
+                                2,
+                                2,
                                 Infinity,
                             )}
                         >
