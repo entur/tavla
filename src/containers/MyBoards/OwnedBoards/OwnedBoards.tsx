66c068aae976c03a31de72fad4501694b2c11349
Ferdinand Løberg <ferdloeb@gmail.com>
2022-12-06 14:57:15 +0100

    Styled chosen board picker, and added switch functionality

--- src/containers/MyBoards/OwnedBoards/OwnedBoards.tsx
+++ src/containers/MyBoards/OwnedBoards/OwnedBoards.tsx
@@ -2,25 +2,41 @@ import React, { useState } from 'react'
 import type { User } from 'firebase/auth'
 import type { DocumentData } from 'firebase/firestore'
 import { SegmentedChoice, SegmentedControl } from '@entur/form'
+import { Contrast } from '@entur/layout'
 import { GridView } from './GridView/GridView'
 import { ListView } from './ListView/ListView'
+import classes from './OwnedBoards.module.scss'
 
 const OwnedBoards = ({ boards, user, preview }: Props): JSX.Element => {
     const [chosenBoardView, setChosenBoardView] = useState('grid')
+
+    const ChosenBoardView = (boardView: string) => {
+        switch (boardView) {
+            case 'grid':
+                return (
+                    <GridView boards={boards} user={user} preview={preview} />
+                )
+            case 'list':
+                return <ListView boards={boards} />
+        }
+    }
+
     return (
         <div>
-            <SegmentedControl
-                label="halla"
-                selectedValue={chosenBoardView}
-                onChange={(value) => {
-                    setChosenBoardView(value || 'grid')
-                }}
-            >
-                <SegmentedChoice value="grid">Rutenett</SegmentedChoice>
-                <SegmentedChoice value="list">Liste</SegmentedChoice>
-            </SegmentedControl>
-            <GridView boards={boards} user={user} preview={preview} />
-            <ListView boards={boards} />
+            <Contrast className={classes.SwitchWrapper}>
+                Velg visningstype:
+                <SegmentedControl
+                    className={classes.Switch}
+                    selectedValue={chosenBoardView}
+                    onChange={(value) => {
+                        setChosenBoardView(value || 'grid')
+                    }}
+                >
+                    <SegmentedChoice value="grid">Rutenett</SegmentedChoice>
+                    <SegmentedChoice value="list">Liste</SegmentedChoice>
+                </SegmentedControl>
+            </Contrast>
+            {ChosenBoardView(chosenBoardView)}
         </div>
     )
 }
