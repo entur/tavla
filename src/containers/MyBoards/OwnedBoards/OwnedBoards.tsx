59a4449e402ec914dcc389ef7dad438272fc102f
birgitte swensson <birgitte.swensson@bekk.no>
2022-12-07 10:38:40 +0100

    styled create tavle button on list view

--- src/containers/MyBoards/OwnedBoards/OwnedBoards.tsx
+++ src/containers/MyBoards/OwnedBoards/OwnedBoards.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import type { User } from 'firebase/auth'
 import type { DocumentData } from 'firebase/firestore'
 import { SegmentedChoice, SegmentedControl } from '@entur/form'
 import { Contrast } from '@entur/layout'
+import { AddIcon } from '@entur/icons'
 import { GridView } from './GridView/GridView'
 import { ListView } from './ListView/ListView'
 import classes from './OwnedBoards.module.scss'
@@ -23,18 +25,31 @@ const OwnedBoards = ({ boards, user, preview }: Props): JSX.Element => {
 
     return (
         <div>
-            <Contrast className={classes.SwitchWrapper}>
-                Velg visningstype:
-                <SegmentedControl
-                    className={classes.Switch}
-                    selectedValue={chosenBoardView}
-                    onChange={(value) => {
-                        setChosenBoardView(value || 'grid')
-                    }}
-                >
-                    <SegmentedChoice value="grid">Rutenett</SegmentedChoice>
-                    <SegmentedChoice value="list">Liste</SegmentedChoice>
-                </SegmentedControl>
+            <Contrast className={classes.Header}>
+                {chosenBoardView == 'list' ? (
+                    <Link to="/">
+                        <div className={classes.NewBoardWrapper}>
+                            <AddIcon />
+                            Lag ny tavle
+                        </div>
+                    </Link>
+                ) : (
+                    <div></div>
+                )}
+
+                <div className={classes.Wrapper}>
+                    Velg visningstype:
+                    <SegmentedControl
+                        className={classes.Switch}
+                        selectedValue={chosenBoardView}
+                        onChange={(value) => {
+                            setChosenBoardView(value || 'grid')
+                        }}
+                    >
+                        <SegmentedChoice value="grid">Rutenett</SegmentedChoice>
+                        <SegmentedChoice value="list">Liste</SegmentedChoice>
+                    </SegmentedControl>
+                </div>
             </Contrast>
             {ChosenBoardView(chosenBoardView)}
         </div>
