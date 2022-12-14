ba20b218ad5048dd77eaaaf5e2c3884d2c5596f5
Ørjan Helstrøm <orjan.helstrom@gmail.com>
2022-12-14 13:48:03 +0100

    edit-tab-again * Use css-modules in CustomTilePanel

--- src/containers/Admin/EditTab/CustomTile/CustomTilePanel/CustomTilePanel.tsx
+++ src/containers/Admin/EditTab/CustomTile/CustomTilePanel/CustomTilePanel.tsx
@@ -3,7 +3,7 @@ import { PrimaryButton } from '@entur/button'
 import { CustomTileModal } from '../../../../../components/CustomTileModal/CustomTileModal'
 import { useSettings } from '../../../../../settings/SettingsProvider'
 import { CustomTilePanelRow } from './CustomTilePanelRow/CustomTilePanelRow'
-import './CustomTilePanel.scss'
+import classes from './CustomTilePanel.module.scss'
 
 const CustomTilePanel = (): JSX.Element => {
     const [settings] = useSettings()
@@ -13,13 +13,13 @@ const CustomTilePanel = (): JSX.Element => {
     )
 
     return (
-        <div className="custom-tile-panel">
+        <div>
             <PrimaryButton
+                className={classes.AddButton}
                 onClick={() => {
                     setSelectedTileId(undefined)
                     setIsOpenModal(true)
                 }}
-                className="custom-tile-panel__add-button"
             >
                 Legg til nytt bilde eller QR-boks
             </PrimaryButton>
