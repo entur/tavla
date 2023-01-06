2a0e857d921d8a6f16f558c20238cbaac7b925cf
birgitte.swensson@bekk.no <birgitte.swensson@bekk.no>
2023-01-06 14:08:46 +0100

    footer styling + icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { ExternalIcon } from '@entur/icons'
 import classes from './Footer.module.scss'
 
 const Footer: React.FC = () => (
     <div className={classes.Footer}>
-        Tilgjengelighetserklæring <a>hei</a>
+        <a
+            href="https://uustatus.no/nb/erklaringer/publisert/9fd7ca5e-7d93-4dac-8c4b-9b0b75502627"
+            target="_blank"
+            rel="noreferrer"
+            className={classes.Link}
+            aria-label="Tilgjengelighetserklæring (bokmål)"
+        >
+            Tilgjengelighetserklæring (bokmål)
+            <ExternalIcon />
+        </a>
+        <a
+            href="https://uustatus.no/nn/erklaringer/publisert/9fd7ca5e-7d93-4dac-8c4b-9b0b75502627"
+            target="_blank"
+            rel="noreferrer"
+            className={classes.Link}
+            aria-label="Tilgjengelighetserklæring (nynorsk)"
+        >
+            <>
+                Tilgjengelighetserklæring (nynorsk)
+                <ExternalIcon />
+            </>
+        </a>
     </div>
 )
 
