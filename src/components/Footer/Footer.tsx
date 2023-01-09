c0dd0cc7142b9a340ca55812c0246d9892ea88d1
BirgitteBS <67414026+BirgitteBS@users.noreply.github.com>
2023-01-09 12:18:35 +0100

    Link til tilgjengelighetserklæring i footer (#889)
    * created footer for links
    
    * footer styling + icon
    
    * space between icon and text in link
    
    * admin page footer + margin
    
    * linting
    
    * .
    
    * correct scss syntax
    
    * Moved Footer component to app
    
    * linting
    
    * removed div

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { ExternalIcon } from '@entur/icons'
+import classes from './Footer.module.scss'
+
+const Footer: React.FC = () => (
+    <div className={classes.Footer}>
+        <a
+            href="https://uustatus.no/nb/erklaringer/publisert/9fd7ca5e-7d93-4dac-8c4b-9b0b75502627"
+            target="_blank"
+            rel="noreferrer"
+            className={classes.Link}
+            aria-label="Tilgjengelighetserklæring (bokmål)"
+        >
+            Tilgjengelighetserklæring (bokmål)
+            <ExternalIcon className={classes.Icon} />
+        </a>
+        <a
+            href="https://uustatus.no/nn/erklaringer/publisert/9fd7ca5e-7d93-4dac-8c4b-9b0b75502627"
+            target="_blank"
+            rel="noreferrer"
+            className={classes.Link}
+            aria-label="Tilgjengelighetserklæring (nynorsk)"
+        >
+            Tilgjengelighetserklæring (nynorsk)
+            <ExternalIcon className={classes.Icon} />
+        </a>
+    </div>
+)
+
+export { Footer }
