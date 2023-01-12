8e95b1c7110f5643d52d7e7c248b7c0374eb29ea
birgitte.swensson@bekk.no <birgitte.swensson@bekk.no>
2023-01-12 12:32:38 +0100

    linting

--- src/containers/Sitemap/Sitemap.tsx
+++ src/containers/Sitemap/Sitemap.tsx
@@ -4,53 +4,47 @@ import { Contrast } from '@entur/layout'
 import { Navbar } from '../Navbar/Navbar'
 import classes from './Sitemap.module.scss'
 
-const Sitemap: React.FC = () => {
-    return (
-        <Contrast>
-            <Helmet>
-                <title>Mine tavler - Tavla - Entur</title>
-            </Helmet>
-            <Navbar />
-            <div className={classes.Wrapper}>
-                <div>
-                    <h1 className={classes.Title}>Nettstedkart</h1>
-                    <p className={classes.LeadParagraph}>
-                        I listen under kan du se hvordan nettsiden er bygd opp
-                        og hvor sidene ligger i forhold til hverandre.
-                    </p>
-                    <p className={classes.LittleText}>
-                        Scroll nedover siden eller bruk den innbygde
-                        søkefunksjonen i nettleseren (⌘+F) for å finne det du
-                        leter etter.
-                    </p>
-                    <div className={classes.MapWrapper}>
-                        <h2 className={classes.SitemapHeader}>Innhold</h2>
-                        <ul className={classes.SiteMap}>
-                            <li className={classes.SitemapItem}>
-                                <a href="/privacy" className={classes.MapLink}>
-                                    Mine tavler
-                                </a>
-                            </li>
+const Sitemap: React.FC = () => (
+    <Contrast>
+        <Helmet>
+            <title>Mine tavler - Tavla - Entur</title>
+        </Helmet>
+        <Navbar />
+        <div className={classes.Wrapper}>
+            <div>
+                <h1 className={classes.Title}>Nettstedkart</h1>
+                <p className={classes.LeadParagraph}>
+                    I listen under kan du se hvordan nettsiden er bygd opp og
+                    hvor sidene ligger i forhold til hverandre.
+                </p>
+                <p className={classes.LittleText}>
+                    Scroll nedover siden eller bruk den innbygde søkefunksjonen
+                    i nettleseren (⌘+F) for å finne det du leter etter.
+                </p>
+                <div className={classes.MapWrapper}>
+                    <h2 className={classes.SitemapHeader}>Innhold</h2>
+                    <ul className={classes.SiteMap}>
+                        <li className={classes.SitemapItem}>
+                            <a href="/privacy" className={classes.MapLink}>
+                                Mine tavler
+                            </a>
+                        </li>
 
-                            <li className={classes.SitemapItem}>
-                                <a href="/privacy" className={classes.MapLink}>
-                                    Personvern
-                                </a>
-                            </li>
-                            <li className={classes.SitemapItem}>
-                                <a
-                                    href="/nettstedkart"
-                                    className={classes.MapLink}
-                                >
-                                    Nettstedkart
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                        <li className={classes.SitemapItem}>
+                            <a href="/privacy" className={classes.MapLink}>
+                                Personvern
+                            </a>
+                        </li>
+                        <li className={classes.SitemapItem}>
+                            <a href="/nettstedkart" className={classes.MapLink}>
+                                Nettstedkart
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </Contrast>
-    )
-}
+        </div>
+    </Contrast>
+)
 
 export { Sitemap }
