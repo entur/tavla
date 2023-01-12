44104b5bc4421acca081879951e08be77ba1b446
birgitte.swensson@bekk.no <birgitte.swensson@bekk.no>
2023-01-12 14:42:53 +0100

    comments from pr

--- src/containers/Sitemap/Sitemap.tsx
+++ src/containers/Sitemap/Sitemap.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { Contrast } from '@entur/layout'
+import { Heading1, Heading2, Heading3, Paragraph } from '@entur/typography'
 import { Navbar } from '../Navbar/Navbar'
 import classes from './Sitemap.module.scss'
 
@@ -11,37 +12,35 @@ const Sitemap: React.FC = () => (
         </Helmet>
         <Navbar />
         <div className={classes.Wrapper}>
-            <div>
-                <h1 className={classes.Title}>Nettstedkart</h1>
-                <p className={classes.LeadParagraph}>
-                    I listen under kan du se hvordan nettsiden er bygd opp og
-                    hvor sidene ligger i forhold til hverandre.
-                </p>
-                <p className={classes.LittleText}>
-                    Scroll nedover siden eller bruk den innbygde søkefunksjonen
-                    i nettleseren (⌘+F) for å finne det du leter etter.
-                </p>
-                <div className={classes.MapWrapper}>
-                    <h2 className={classes.SitemapHeader}>Innhold</h2>
-                    <ul className={classes.SiteMap}>
-                        <li className={classes.SitemapItem}>
-                            <a href="/privacy" className={classes.MapLink}>
-                                Mine tavler
-                            </a>
-                        </li>
+            <Heading1>Nettstedkart</Heading1>
+            <Heading3>
+                I listen under kan du se hvordan nettsiden er bygd opp og hvor
+                sidene ligger i forhold til hverandre.
+            </Heading3>
+            <Paragraph>
+                Scroll nedover siden eller bruk den innbygde søkefunksjonen i
+                nettleseren (⌘+F) for å finne det du leter etter.
+            </Paragraph>
+            <div className={classes.MapWrapper}>
+                <Heading2 className={classes.SitemapHeader}>Innhold</Heading2>
+                <ul className={classes.SiteMap}>
+                    <li className={classes.SitemapItem}>
+                        <a href="/privacy" className={classes.MapLink}>
+                            Mine tavler
+                        </a>
+                    </li>
 
-                        <li className={classes.SitemapItem}>
-                            <a href="/privacy" className={classes.MapLink}>
-                                Personvern
-                            </a>
-                        </li>
-                        <li className={classes.SitemapItem}>
-                            <a href="/nettstedkart" className={classes.MapLink}>
-                                Nettstedkart
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                    <li className={classes.SitemapItem}>
+                        <a href="/privacy" className={classes.MapLink}>
+                            Personvern
+                        </a>
+                    </li>
+                    <li className={classes.SitemapItem}>
+                        <a href="/nettstedkart" className={classes.MapLink}>
+                            Nettstedkart
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </Contrast>
