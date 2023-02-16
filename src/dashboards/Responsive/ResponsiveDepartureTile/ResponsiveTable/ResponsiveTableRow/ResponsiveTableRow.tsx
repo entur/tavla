b69d3a4ff88223ef3b4b5d1e13a2c83e68ecd3f1
birgitte.swensson@bekk.no <birgitte.swensson@bekk.no>
2023-02-16 14:33:55 +0100

    style(responsive-dashboard): feedback from pr

--- src/dashboards/Responsive/ResponsiveDepartureTile/ResponsiveTable/ResponsiveTableRow/ResponsiveTableRow.tsx
+++ src/dashboards/Responsive/ResponsiveDepartureTile/ResponsiveTable/ResponsiveTableRow/ResponsiveTableRow.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { SituationInfo } from 'components/SituationInfo/SituationInfo'
 import { TransportModeIcon } from 'components/TransportModeIcon/TransportModeIcon'
 import { Departure } from 'logic/use-stop-place-with-estimated-calls/departure'
-import { IconColorType, Theme } from 'src/types'
+import { IconColorType } from 'src/types'
 import { getIconColor } from 'utils/icon'
+import { isDarkOrDefaultTheme } from 'utils/utils'
 import { useSettings } from 'settings/SettingsProvider'
 import classes from './ResponsiveTableRow.module.scss'
 
@@ -13,7 +14,7 @@ const ResponsiveTableRow: React.FC<{ departure: Departure }> = ({
     const [settings] = useSettings()
     const iconColor = getIconColor(
         departure.transportMode,
-        settings.theme == Theme.DEFAULT || settings.theme == Theme.DARK
+        isDarkOrDefaultTheme(settings.theme)
             ? IconColorType.CONTRAST
             : IconColorType.DEFAULT,
     )
@@ -30,7 +31,6 @@ const ResponsiveTableRow: React.FC<{ departure: Departure }> = ({
                     <TransportModeIcon
                         transportMode={departure.transportMode}
                         color="inherit"
-                        className={classes.TransportModeIcon}
                     />
                     <span className={classes.PublicCode}>
                         {departure.publicCode}
