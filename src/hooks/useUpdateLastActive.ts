1fe1c87f29ecd220f2bff05cc2aa844962faabe2
Ferdinand Løberg <ferdloeb@gmail.com>
2023-02-17 11:20:18 +0100

    fix(lastActive): lastactive is set when opening tavle

--- src/hooks/useUpdateLastActive.ts
+++ src/hooks/useUpdateLastActive.ts
@@ -1,22 +1,25 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useSettings } from 'settings/SettingsProvider'
 
 const useUpdateLastActive = () => {
     const [settings, setSettings] = useSettings()
 
+    const updateLastActive = useCallback(() => {
+        // Updates lastActive if previous value is more than 1 day ago
+        if (Date.now() - settings.lastActive > 86400000) {
+            setSettings({
+                lastActive: Date.now(),
+            })
+        }
+    }, [settings.lastActive, setSettings])
+
     useEffect(() => {
-        const intervalId = setInterval(() => {
-            // Updates lastActive if previous value is more than 1 day ago
-            if (Date.now() - settings.lastActive > 86400000) {
-                setSettings({
-                    lastActive: Date.now(),
-                })
-            }
-        }, 3600000) // Runs every hour
+        updateLastActive()
+        const intervalId = setInterval(updateLastActive, 3600000) // Runs every hour
         return () => {
             clearInterval(intervalId)
         }
-    }, [settings.lastActive, setSettings])
+    }, [updateLastActive])
 }
 
 export { useUpdateLastActive }
