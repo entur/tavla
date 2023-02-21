f0e927a4b10b1078a13e41aa7c65a86ce9c3ee4b
Carl Joachim Otvik <carl.joachim.otvik@entur.org>
2023-02-21 09:49:11 +0100

    Initialize Posthog analysis tool on Tavla

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import classNames from 'classnames'
 import { ApolloProvider } from '@apollo/client'
 import { UserProvider } from 'src/UserProvider'
@@ -8,6 +8,7 @@ import { DashboardResolver } from 'dashboards/DashboardResolver'
 import { ToastProvider } from 'src/types'
 import '../firebase-init'
 import { apolloClient } from '../apollo-client'
+import { logPageViews } from '../posthog'
 import { AdminPage } from './Admin/AdminPage'
 import { PageDoesNotExist } from './Error/ErrorPages'
 import { LandingPage } from './LandingPage/LandingPage'
@@ -16,31 +17,39 @@ import { MyBoards } from './MyBoards/MyBoards'
 import { Sitemap } from './Sitemap/Sitemap'
 import classes from './App.module.scss'
 
-const Content = (): JSX.Element => (
-    <ApolloProvider client={apolloClient}>
-        <div className={classNames(classes.ThemeBackground)}>
-            <ToastProvider>
-                <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                    <Route element={<SettingsProvider />}>
-                        <Route
-                            path="/t/:documentId"
-                            element={<DashboardResolver />}
-                        />
-                        <Route
-                            path="/admin/:documentId"
-                            element={<AdminPage />}
-                        />
-                    </Route>
-                    <Route path="/tavler" element={<MyBoards />} />
-                    <Route path="/privacy" element={<Privacy />} />
-                    <Route path="/nettstedkart" element={<Sitemap />} />
-                    <Route path="*" element={<PageDoesNotExist />} />
-                </Routes>
-            </ToastProvider>
-        </div>
-    </ApolloProvider>
-)
+function Content(): JSX.Element {
+    const location = useLocation()
+
+    useEffect(() => {
+        logPageViews()
+    }, [location.pathname])
+
+    return (
+        <ApolloProvider client={apolloClient}>
+            <div className={classNames(classes.ThemeBackground)}>
+                <ToastProvider>
+                    <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                        <Route element={<SettingsProvider />}>
+                            <Route
+                                path="/t/:documentId"
+                                element={<DashboardResolver />}
+                            />
+                            <Route
+                                path="/admin/:documentId"
+                                element={<AdminPage />}
+                            />
+                        </Route>
+                        <Route path="/tavler" element={<MyBoards />} />
+                        <Route path="/privacy" element={<Privacy />} />
+                        <Route path="/nettstedkart" element={<Sitemap />} />
+                        <Route path="*" element={<PageDoesNotExist />} />
+                    </Routes>
+                </ToastProvider>
+            </div>
+        </ApolloProvider>
+    )
+}
 
 const App = (): JSX.Element => (
     <BrowserRouter>
