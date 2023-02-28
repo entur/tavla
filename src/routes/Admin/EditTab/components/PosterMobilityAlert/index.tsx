6598c6921e4f8385a134b1bf738c0a9302d37b2e
lindtvedtsebastian <46536611+lindtvedtsebastian@users.noreply.github.com>
2023-02-28 10:16:46 +0100

    chore(tiles): refactoring of tiles into /dashboard and /admin folders (#961)
    * chore(tiles): moved EditTab tiles to /tiles folder
    
    * chore(tiles): /dashboard and /admin folders in tiles
    
    * feat(admin tile): Renamed EditTile -> AdminTile

---
+++ src/routes/Admin/EditTab/components/PosterMobilityAlert/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSettings } from 'settings/SettingsProvider'
+import { ToastAlertBox } from '@entur/alert'
+
+function PosterMobilityAlert() {
+    const [settings] = useSettings()
+
+    if (settings.hiddenModes.length === 0 && settings.dashboard === 'Poster') {
+        return (
+            <ToastAlertBox variant="info" title="Nå har du valgt for mange!">
+                Du kan kun velge tre mobilitetstyper for denne visningstypen.
+            </ToastAlertBox>
+        )
+    }
+    return <></>
+}
+
+export { PosterMobilityAlert }
---
+++ src/tiles/admin/CustomTile/components/CustomTilePanel/index.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useState } from 'react'
+import { useSettings } from 'settings/SettingsProvider'
+import { CustomTileModal } from 'tiles/dashboard/CustomTileModal'
+import { PrimaryButton } from '@entur/button'
+import { CustomTilePanelRow } from './CustomTilePanelRow/CustomTilePanelRow'
+import classes from './CustomTilePanel.module.scss'
+
+function CustomTilePanel() {
+    const [settings] = useSettings()
+    const [isOpenModal, setIsOpenModal] = useState(false)
+    const [selectedTileId, setSelectedTileId] = useState<string | undefined>(
+        undefined,
+    )
+
+    const handleAdd = useCallback(() => {
+        setSelectedTileId(undefined)
+        setIsOpenModal(true)
+    }, [])
+
+    return (
+        <div>
+            <PrimaryButton className={classes.AddButton} onClick={handleAdd}>
+                Legg til nytt bilde eller QR-boks
+            </PrimaryButton>
+            {isOpenModal && (
+                <CustomTileModal
+                    setIsOpen={setIsOpenModal}
+                    selectedTileId={selectedTileId}
+                />
+            )}
+            {[...settings.customQrTiles, ...settings.customImageTiles].map(
+                (customTile) => (
+                    <CustomTilePanelRow
+                        key={customTile.id}
+                        {...customTile}
+                        setIsOpenModal={setIsOpenModal}
+                        setSelectedTileId={setSelectedTileId}
+                    />
+                ),
+            )}
+        </div>
+    )
+}
+
+export { CustomTilePanel }
