fdfdbd4b42439947a6306482e960e9dd07bf47b8
Ferdinand Løberg <ferdloeb@gmail.com>
2023-03-20 11:43:59 +0100

    feat(platform): add platform to responsive dashboard

--- src/routes/Dashboard/Responsive/ResponsiveDepartureTile/ResponsiveTable/ResponsiveTableHeader/ResponsiveTableHeader.tsx
+++ src/routes/Dashboard/Responsive/ResponsiveDepartureTile/ResponsiveTable/ResponsiveTableHeader/ResponsiveTableHeader.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useSettings } from 'settings/SettingsProvider'
 import classes from './ResponsiveTableHeader.module.scss'
 
 function ResponsiveTableHeader() {
+    const [settings] = useSettings()
+
     return (
         <thead>
             <tr className={classes.TableHeader}>
                 <th>Linje</th>
+                {!settings.hideTracks && <th>Plattform</th>}
                 <th className={classes.HeaderDestination}>Destinasjon</th>
                 <th className={classes.HeaderDepartureTime}>Avgang</th>
             </tr>
