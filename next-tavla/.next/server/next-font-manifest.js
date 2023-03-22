40f63cb6819faf1af33a0a497bff2c0a8002bb7e
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-03-22 13:05:17 +0100

    feat(setup): initial commit

---
+++ next-tavla/.next/server/next-font-manifest.js
@@ -0,0 +1,13 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/sebastian/git/entur/next-tavla/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/2aaf0723e720e8b9-s.p.woff2"
+    ],
+    "/Users/sebastian/git/entur/next-tavla/node_modules/next/font/google/target.css.webpack[javascript/auto]!=!/Users/sebastian/git/entur/next-tavla/node_modules/next/dist/build/webpack/loaders/css-loader/src/index.js??ruleSet[1].rules[2].oneOf[3].use[1]!/Users/sebastian/git/entur/next-tavla/node_modules/next/dist/build/webpack/loaders/next-font-loader/index.js??ruleSet[1].rules[2].oneOf[3].use[2]!/Users/sebastian/git/entur/next-tavla/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/2aaf0723e720e8b9-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
