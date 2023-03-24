e9b48587edc94e9a00c7b22e9e1c6e99bfd2ca09
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-03-24 10:40:14 +0100

    feat(tile): dynamic flex allocation based on column

--- next-tavla/src/components/Board/index.tsx
+++ next-tavla/src/components/Board/index.tsx
@@ -4,8 +4,11 @@ import classes from "./styles.module.css";
 function Board({ id }: { id: string }) {
   return (
     <div className={classes.board}>
-      <Tile id="NSR:StopPlace:337" />
-      <Tile id="NSR:StopPlace:58366" />
+      <Tile stopPlaceID="NSR:StopPlace:337" />
+      <Tile
+        stopPlaceID="NSR:StopPlace:58366"
+        columns={["time", "line", "destination"]}
+      />
     </div>
   );
 }
--- next-tavla/src/types/stopPlace.ts
+++ next-tavla/src/types/stopPlace.ts
@@ -1,23 +1,25 @@
 export type StopPlaceData = {
   name: string;
-  estimatedCalls: Array<{
-    destinationDisplay: {
-      frontText: string;
-    };
-    expectedDepartureTime: string;
-    serviceJourney: {
-      transportMode: string;
-      transportSubmode: string;
-      line: {
-        publicCode: string;
-        authority: {
-          name: string;
-        };
-        presentation: {
-          textColour?: string;
-          colour?: string;
-        };
+  estimatedCalls: Departure[];
+};
+
+export type Departure = {
+  destinationDisplay: {
+    frontText: string;
+  };
+  expectedDepartureTime: string;
+  serviceJourney: {
+    transportMode: string;
+    transportSubmode: string;
+    line: {
+      publicCode: string;
+      authority: {
+        name: string;
+      };
+      presentation: {
+        textColour?: string;
+        colour?: string;
       };
     };
-  }>;
+  };
 };
