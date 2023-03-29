0b90ecfd80a74b640832e3e0cae6fde7567a0d1e
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-03-29 10:20:06 +0200

    feat(props): typing of params

--- next-tavla/src/components/Board/index.tsx
+++ next-tavla/src/components/Board/index.tsx
@@ -1,5 +1,5 @@
 import { Tile } from "@/components/Tile";
-import { firebase, TSettings } from "@/types/settings";
+import { TSettings } from "@/types/settings";
 import classes from "./styles.module.css";
 
 function Board({ settings }: { settings: TSettings }) {
