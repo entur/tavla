e625d6f7507cdfb3184c3ce00772a0d31ef07fb5
Ferdinand Løberg <ferdloeb@gmail.com>
2023-03-29 18:33:23 +0200

    feat(table): add rows to fit screen height

--- next-tavla/src/components/DepartureTile/index.tsx
+++ next-tavla/src/components/DepartureTile/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import stopPlaceQuery from "@/graphql/stopPlaceQuery.graphql";
 import { StopPlaceData } from "@/types/stopPlace";
 import { TDepartureTile } from "@/types/tile";
@@ -25,15 +25,38 @@ export function DepartureTile({
     };
   }, [placeId]);
 
+  const tableRef = useRef<HTMLDivElement | null>(null);
+  const [updateNumberOfRows, setUpdateNumberOfRows] = useState(true);
+  const [numberOfRows, setNumberOfRows] = useState(0);
+
+  useEffect(() => {
+    if (!updateNumberOfRows) return;
+    if (!data) return;
+    if (!tableRef.current) return;
+    if (numberOfRows >= data.estimatedCalls.length) return;
+
+    const element = tableRef.current;
+
+    if (element.scrollHeight === element.clientHeight) {
+      setNumberOfRows(numberOfRows + 1);
+    } else {
+      setNumberOfRows(numberOfRows - 1);
+      setUpdateNumberOfRows(false);
+    }
+  }, [updateNumberOfRows, data, numberOfRows]);
+
   if (!data) {
     return <div className="tile">Data not found</div>;
   }
 
   return (
-    <div className="tile">
+    <div className="tile" style={{ display: "flex", flexDirection: "column" }}>
       <h3>{data.name}</h3>
-      <div style={{ overflow: "hidden", height: "100%" }}>
-        <Table columns={uniqueColumns} departures={data.estimatedCalls} />
+      <div style={{ overflow: "hidden", height: "100%" }} ref={tableRef}>
+        <Table
+          columns={uniqueColumns}
+          departures={data.estimatedCalls.slice(0, numberOfRows)}
+        />
       </div>
     </div>
   );
