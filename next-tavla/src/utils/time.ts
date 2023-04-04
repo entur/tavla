63bffb24235e1989d4cbb2c94a50f73d9f5755e0
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-04-04 13:55:28 +0200

    chore(time): move format time to separate function

--- next-tavla/src/utils/time.ts
+++ next-tavla/src/utils/time.ts
@@ -2,10 +2,13 @@ export function getRelativeTimeString(timestamp: string) {
   const timeDiff = Date.parse(timestamp) - Date.now();
   if (timeDiff < 60_000) return "Nå";
   else if (timeDiff < 900_000) return Math.floor(timeDiff / 60_000) + " min";
-  else
-    return Intl.DateTimeFormat("no-NB", {
-      hour12: false,
-      hour: "2-digit",
-      minute: "2-digit",
-    }).format(Date.parse(timestamp));
+  else return formatTime(timestamp);
+}
+
+export function formatTime(timestamp: string) {
+  return Intl.DateTimeFormat("no-NB", {
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(Date.parse(timestamp));
 }
