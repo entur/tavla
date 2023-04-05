82a2037eea4feecbe2e79237f5a6045928125005
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-04-05 12:08:52 +0200

    feat(transport/types): use transport types from schema

--- next-tavla/src/types/settings.ts
+++ next-tavla/src/types/settings.ts
@@ -1,8 +1,8 @@
 import { TTile } from "./tile";
 
-type TTheme = "default" | "dark"
+export type TTheme = "default" | "dark";
 
 export type TSettings = {
   tiles: TTile[];
-  theme?: TTheme
+  theme?: TTheme;
 };
