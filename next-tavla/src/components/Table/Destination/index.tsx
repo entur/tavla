9fceb65ab97c2e87e9e52edec3b46cc2d4d367f0
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-04-11 09:10:12 +0200

    chore(cleanup): remove mock situations

--- next-tavla/src/components/Table/Destination/index.tsx
+++ next-tavla/src/components/Table/Destination/index.tsx
@@ -1,34 +1,16 @@
 import { Situation } from "@/components/Situation";
 import { useNonNullContext } from "@/hooks/useNonNullContext";
-import { TSituation } from "@/types/graphql";
 import { DepartureContext } from "../contexts";
 import classes from "./styles.module.css";
 
 function Destination() {
   const departure = useNonNullContext(DepartureContext);
 
-  const situations: TSituation[] = departure.situations.length
-    ? departure.situations
-    : Math.random() * 2 > 1
-    ? [
-        {
-          id: "situation:1231231",
-          description: [
-            {
-              value:
-                "veldig forsinket, personalet har forsovet seg veldig forsinket, personalet er dessverre veldig forsinket forsovet seg veldig forsinket,personalet har forsovet seg veldig forsinket, personalet har forsovet seg personalet har forsovet seg veldig forsinket, personalet har forsovet seg",
-              language: "no",
-            },
-          ],
-          summary: [{ value: "forsinket", language: "no" }],
-        },
-      ]
-    : [];
   return (
     <td>
       <div className={classes.destination}>
         <div>{departure.destinationDisplay?.frontText}</div>
-        {situations.map((situation) => (
+        {departure.situations.map((situation) => (
           <Situation key={situation.id} situation={situation} />
         ))}
       </div>
