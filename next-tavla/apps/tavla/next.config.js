6cb6b1b6e2799400a729b31de48d850d3b577d7a
Ferdinand Løberg <ferdloeb@gmail.com>
2023-04-17 11:51:35 +0200

    feat(map): add maptile
    Use exact version with yarn add
    Use env variables for tokens

--- next-tavla/apps/tavla/next.config.js
+++ next-tavla/apps/tavla/next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.extensions.push(".graphql");
-    config.module.rules.push({
-      test: /\.graphql$/,
-      type: "asset/source",
-    });
-    config.module.rules.push({
-      test: /\.(svg|png|jpe?g|gif|eot|webp|webm|woff2?)$/,
-      type: "asset",
-      generator: {
-        filename: "assets/[hash][ext][query]",
-      },
-    });
-    return config;
-  },
   output: "standalone",
-  transpilePackages: ["shared"],
+  transpilePackages: ["shared", "react-map-gl"],
 };
 
 module.exports = nextConfig;
--- next-tavla/apps/tavla/src/components/MapTile/index.tsx
+++ next-tavla/apps/tavla/src/components/MapTile/index.tsx
@@ -1,7 +1,20 @@
 import { TMapTile } from "@/types/tile";
+import Mapbox from "react-map-gl";
 
-function MapTile({ placeId }: TMapTile) {
-  return <div className="tile">{placeId}</div>;
+function MapTile({ longitude, latitude, zoom }: TMapTile) {
+  return (
+    <div className="tile">
+      <Mapbox
+        zoom={zoom}
+        longitude={longitude}
+        latitude={latitude}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapStyle={process.env.NEXT_PUBLIC_MAPBOX_STYLE}
+        interactive={false}
+        reuseMaps
+      />
+    </div>
+  );
 }
 
 export { MapTile };
