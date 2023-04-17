fc44e9d608769319c4a5059664eb05e2def54d3e
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-04-17 09:45:03 +0200

    feat(tsconfig): setup import aliases

--- next-tavla/shared/dist/test.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-export declare function Test({ name }: {
-    name: string;
-}): JSX.Element;
--- next-tavla/shared/dist/test.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export function Test(_a) {
-    var name = _a.name;
-    return <div>{name}</div>;
-}
