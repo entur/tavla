ce3c1ffe56979e35d2ff410c398913ff5620a4e2
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-04-18 11:51:34 +0200

    feat(styles): moved styles to shared/ and import in admin and tavla

--- next-tavla/apps/admin/pages/_app.tsx
+++ next-tavla/apps/admin/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@shared/styles/global.css";
+import "@shared/styles/reset.css";
+import "@shared/styles/fonts.css";
+import "@shared/styles/spacing.css";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
