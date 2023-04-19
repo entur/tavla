6c7762386a5e6a12ce904b85c1d2779d4cb00470
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-04-19 13:18:00 +0200

    feat(docker): basic docker setup

--- next-tavla/tavla-lite/tavla/next.config.js
+++ next-tavla/tavla-lite/tavla/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: { outputFileTracingIncludes: { "/": ["../shared"] } },
   transpilePackages: ["shared", "react-map-gl"],
 };
 
