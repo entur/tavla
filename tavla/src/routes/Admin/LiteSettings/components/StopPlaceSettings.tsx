d70d7c7b0e5d13f236580939f418f7ce1a9c2814
ydalir <ferdloeb@gmail.com>
2023-04-27 10:08:38 +0200

    feat(lite): add 'add column' column (#1030)

--- tavla/src/routes/Admin/LiteSettings/components/StopPlaceSettings.tsx
+++ tavla/src/routes/Admin/LiteSettings/components/StopPlaceSettings.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable func-style */
-import React from 'react'
+import React, { useState } from 'react'
 import { DndContext, type DragEndEvent } from '@dnd-kit/core'
 import {
     arrayMove,
@@ -8,7 +8,10 @@ import {
     useSortable,
 } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-import { TStopPlaceTile, TColumn } from '../types/tile'
+import { xor } from 'lodash'
+import { Radio, RadioGroup } from '@entur/form'
+import { Button } from '@entur/button'
+import { TStopPlaceTile, TColumn, Columns } from '../types/tile'
 
 function LiteTileColumn({ column }: { column: string }) {
     const { attributes, listeners, setNodeRef, transform, transition } =
@@ -38,6 +41,52 @@ function LiteTileColumn({ column }: { column: string }) {
     )
 }
 
+function AddColumnSettings({
+    selectedColumns,
+    addColumn,
+}: {
+    selectedColumns: TColumn[]
+    addColumn: (column: TColumn) => void
+}) {
+    const [column, setColumn] = useState<TColumn | null>(null)
+
+    return (
+        <div
+            style={{
+                backgroundColor: 'var(--tavla-box-background-color)',
+                padding: '1rem',
+            }}
+        >
+            <RadioGroup
+                name="new-column-settings"
+                label="Velg kolonne"
+                onChange={(e) => {
+                    setColumn(e.target.value as TColumn)
+                }}
+                value={column}
+            >
+                {xor(selectedColumns, Object.keys(Columns) as TColumn[]).map(
+                    (key) => (
+                        <Radio key={key} value={key}>
+                            {Columns[key]}
+                        </Radio>
+                    ),
+                )}
+            </RadioGroup>
+            <Button
+                variant="primary"
+                disabled={!column}
+                onClick={() => {
+                    if (column) addColumn(column)
+                    setColumn(null)
+                }}
+            >
+                Legg til kolonne
+            </Button>
+        </div>
+    )
+}
+
 function StopPlaceSettings({
     tile,
     setTile,
@@ -61,6 +110,13 @@ function StopPlaceSettings({
         }
     }
 
+    const addColumn = (newColumn: TColumn) => {
+        setTile({
+            ...tile,
+            columns: [...(tile.columns || []), newColumn],
+        })
+    }
+
     return (
         <DndContext onDragEnd={handleColumnSwap}>
             <div
@@ -74,7 +130,14 @@ function StopPlaceSettings({
                 }}
             >
                 {tile.placeId}
-                <div style={{ display: 'flex', height: '500px', gap: '10px' }}>
+                <div
+                    style={{
+                        display: 'flex',
+                        height: '500px',
+                        gap: '10px',
+                        color: 'white',
+                    }}
+                >
                     <SortableContext
                         items={columns}
                         strategy={horizontalListSortingStrategy}
@@ -83,6 +146,10 @@ function StopPlaceSettings({
                             <LiteTileColumn key={column} column={column} />
                         ))}
                     </SortableContext>
+                    <AddColumnSettings
+                        addColumn={addColumn}
+                        selectedColumns={columns}
+                    />
                 </div>
             </div>
         </DndContext>
