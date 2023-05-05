40bf0bd64c63fb7e6ee836a08186ed4c48934b61
ydalir <ferdloeb@gmail.com>
2023-05-05 10:44:52 +0200

    fix(firebase): fix engines field and remove unneccessary files from git (#1048)

--- next-tavla/.firebase/lite-tavla/functions/server.js
+++
@@ -1,4 +0,0 @@
-const { onRequest } = require('firebase-functions/v2/https');
-  const server = import('firebase-frameworks');
-  exports.ssrlitetavla = onRequest({"region":"europe-west1"}, (req, res) => server.then(it => it.handle(req, res)));
-  
