f42e0e3c9de7ab72613a8c801aa973ee84646ab8
Ferdinand Løberg <ferdloeb@gmail.com>
2023-05-22 09:51:31 +0200

    fix(styling): correct styling for error page

--- next-tavla/pages/404.tsx
+++ next-tavla/pages/404.tsx
@@ -1,10 +1,13 @@
 import { Header } from 'components/Header'
+import classes from 'styles/board.module.css'
 
 function NotFoundPage() {
     return (
-        <div className="root">
-            <Header />
-            <p className="pl-2">Tavlen kunne ikke hentes!</p>
+        <div className={classes.root}>
+            <div className={classes.rootContainer}>
+                <Header />
+                <p className="pl-2">Tavlen kunne ikke hentes!</p>
+            </div>
         </div>
     )
 }
