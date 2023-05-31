e5b5c31e082dc83f28b8985e286131f38875f029
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-05-31 14:53:45 +0200

    feat(cypress): basic cypress component test for AddTile

---
+++ next-tavla/src/scenarios/Admin/components/AddTile/indexAddTile.cy.tsx
@@ -0,0 +1,35 @@
+import React, { useReducer } from 'react'
+import {
+    SettingsDispatchContext,
+    settingsReducer,
+} from 'scenarios/Admin/reducer'
+import { TilesSettings } from '../TilesSettings'
+import { AddTile } from './index'
+
+describe('<AddTile />', () => {
+    const TestComponent = () => {
+        const [settings, dispatch] = useReducer(settingsReducer, {
+            tiles: [],
+        })
+
+        return (
+            <SettingsDispatchContext.Provider value={dispatch}>
+                <TilesSettings tiles={settings.tiles} />
+                <AddTile />
+            </SettingsDispatchContext.Provider>
+        )
+    }
+    it('renders', () => {
+        // see: https://on.cypress.io/mounting-react
+        cy.mount(<TestComponent />)
+    })
+
+    it('can add a stop place tile', () => {
+        cy.mount(<TestComponent />)
+        cy.get('input').type('Jernbanetorget')
+        cy.get('ul > li').click()
+        cy.contains('Legg til').click()
+
+        cy.get('[data-cy="tiles"]').children().should('have.length', 1)
+    })
+})
