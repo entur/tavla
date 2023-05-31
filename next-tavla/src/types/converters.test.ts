9c2efc79ff1d15a2198faa3a879da0710b501548
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-05-31 12:42:29 +0200

    chore(jest): removed jest for dom testing

--- next-tavla/src/types/converters.test.ts
+++ next-tavla/src/types/converters.test.ts
@@ -1,4 +1,5 @@
 import { V1, V2, convertSettingsVersion, currentVersion } from './converters'
+import { jest, test, expect } from '@jest/globals'
 
 jest.mock('nanoid', () => {
     return { nanoid: () => '1234' }
