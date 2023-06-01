ae71d64c4499cb10530b9004157ce1ab6bb0e98f
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-06-01 12:42:08 +0200

    chore(jest): remove jsdom from jest config

--- next-tavla/jest.config.mjs
+++ next-tavla/jest.config.mjs
@@ -8,7 +8,6 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-  testEnvironment: 'jest-environment-jsdom',
 };
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
