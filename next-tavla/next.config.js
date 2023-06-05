41252436e7dc080be107f86fbc7d3cd6e1ae8596
Ferdinand Løberg <ferdloeb@gmail.com>
2023-06-05 13:26:31 +0200

    fix(swr): transpile for older browser support

--- next-tavla/next.config.js
+++ next-tavla/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-};
+    output: 'standalone',
+    transpilePackages: ['swr'],
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
