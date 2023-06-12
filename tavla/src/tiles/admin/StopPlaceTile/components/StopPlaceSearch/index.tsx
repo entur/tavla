d7bd15ab285836644a133e600ec6f0011acdd5dd
Ferdinand Løberg <ferdloeb@gmail.com>
2023-06-12 10:20:39 +0200

    fix(admin): better handling of duplicate stopplaces

--- tavla/src/tiles/admin/StopPlaceTile/components/StopPlaceSearch/index.tsx
+++ tavla/src/tiles/admin/StopPlaceTile/components/StopPlaceSearch/index.tsx
@@ -4,40 +4,23 @@ import {
     fetchAutocomplete,
 } from 'utils/geocoder/fetchAutocomplete'
 import { useSettings } from 'settings/SettingsProvider'
-import { useStopPlaceIds } from 'hooks/useStopPlaceIds'
 import { Dropdown } from '@entur/dropdown'
 import classes from './StopPlaceSearch.module.scss'
 
 function StopPlaceSearch() {
     const [settings, setSettings] = useSettings()
 
-    const { stopPlaceIds } = useStopPlaceIds({
-        distance: settings.distance,
-        filterHidden: false,
-    })
-
     const handleChange = useCallback(
         (item: AutocompleteItem | null) => {
-            if (item) {
+            if (item && !settings.newStops.includes(item.value)) {
                 const stopId = item.value
 
-                const numberOfDuplicates = [
-                    ...stopPlaceIds,
-                    ...settings.newStops,
-                ]
-                    .map((id) => id.replace(/-\d+$/, ''))
-                    .filter((id) => id === stopId).length
-
-                const id = !numberOfDuplicates
-                    ? stopId
-                    : `${stopId}-${numberOfDuplicates}`
-
                 setSettings({
-                    newStops: [...settings.newStops, id],
+                    newStops: [...settings.newStops, stopId],
                 })
             }
         },
-        [stopPlaceIds, settings.newStops, setSettings],
+        [settings.newStops, setSettings],
     )
 
     return (
