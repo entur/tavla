e154fd66b5cb4a479dad3903ca2b2f80dfacd025
SharmaTarun1111111 <tarun.sharma@bekk.no>
2023-06-20 12:09:53 +0200

    feat(typography): adds entur typography

--- next-tavla/src/Landing/index.tsx
+++ next-tavla/src/Landing/index.tsx
@@ -1,12 +1,14 @@
 import { Button } from '@entur/button'
 import { Contrast } from '@entur/layout'
+import {Heading1, Heading2} from "@entur/typography"
 import classes from './styles.module.css'
 
 function Landing() {
     return (
         <Contrast>
             <div className={classes.container}>
-                <h1>Fin overskrift</h1>
+                <Heading1 className={classes.header}>EnTur Tavla 2.0</Heading1>
+                <Heading2 className={classes.header}>Vil du lage din egen tavle????</Heading2>
                 <Button variant="primary">Opprett ny tavle</Button>
             </div>
         </Contrast>
