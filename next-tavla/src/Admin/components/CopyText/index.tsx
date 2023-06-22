93236a1d01cdc799a45dddbda87bfed0dce277c0
asemwilhelmsen <ase.wilhelmsen@bekk.no>
2023-06-22 12:40:38 +0200

    feat(edit): add toast to copy text functionality

--- next-tavla/src/Admin/components/CopyText/index.tsx
+++ next-tavla/src/Admin/components/CopyText/index.tsx
@@ -2,15 +2,19 @@ import { Tooltip } from '@entur/tooltip'
 import { CopyIcon } from '@entur/icons'
 import { IconButton } from '@entur/button'
 import classes from './styles.module.css'
+import { useToast } from '@entur/alert'
 
 function CopyText({ text }: { text: string }) {
+    const { addToast } = useToast()
+
     return (
         <div className={classes.copyText}>
             <p>{text}</p>
-            <Tooltip content="kopier" placement="top">
+            <Tooltip content="Kopier lenke" placement="top">
                 <IconButton
                     onClick={() => {
                         navigator.clipboard.writeText(text)
+                        addToast('Kopiert')
                     }}
                 >
                     <CopyIcon />
