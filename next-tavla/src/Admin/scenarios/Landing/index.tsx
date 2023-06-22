7bf3cf99abe78dab451430c1e9a22f6db3281551
SharmaTarun1111111 <tarshar98@gmail.com>
2023-06-22 10:47:42 +0200

    fix(landing): css cleanup and fix redirect url

--- next-tavla/src/Admin/scenarios/Landing/index.tsx
+++ next-tavla/src/Admin/scenarios/Landing/index.tsx
@@ -12,18 +12,15 @@ function Landing() {
     async function handleCreateNewBoard() {
         setLoading(true)
         const createdBoard = await addBoardSettings({ tiles: [] })
-        await router.push('/admin/' + createdBoard.id)
+        await router.push('/edit/' + createdBoard.id)
     }
 
     return (
         <div className={classes.container}>
-            <Heading1 className={classes.header}>Entur Tavla</Heading1>
-            <Heading2 className={classes.header}>
-                Her kan du lage din egen tavle
-            </Heading2>
+            <Heading1>Entur Tavla</Heading1>
+            <Heading2>Her kan du lage din egen tavle</Heading2>
 
             <Button
-                className={classes.createButton}
                 onClick={handleCreateNewBoard}
                 variant="primary"
                 disabled={loading}
