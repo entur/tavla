e7a24ae49c1ca1e857637cd42edb11421cf3193f
stianjsu <stianjsu@gmail.com>
2023-06-26 09:58:06 +0200

    refactor(via): simplifies stringify via destinations

--- next-tavla/src/Board/scenarios/Table/components/Via/index.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Via/index.tsx
@@ -1,27 +1,16 @@
 import { useNonNullContext } from 'hooks/useNonNullContext'
-import { DepartureContext } from '../../contexts'
+import { DepartureContext } from 'Board/scenarios/Table/contexts'
 import classes from './styles.module.css'
+import { isNotNullOrUndefined } from 'utils/typeguards'
 
 function Via() {
     const departure = useNonNullContext(DepartureContext)
 
     const viaDestinations = departure.destinationDisplay?.via
+        ?.filter(isNotNullOrUndefined)
+        .join(', ')
 
-    function stringifyViaDestinations(viaDest: typeof viaDestinations) {
-        if (!viaDest || !viaDest.length) return null
-
-        return viaDest.reduce((prev, next) => {
-            if (!prev) return next
-            if (!next) return prev
-            return prev + ', ' + next
-        })
-    }
-
-    return (
-        <td className={classes.via}>
-            {stringifyViaDestinations(viaDestinations)}
-        </td>
-    )
+    return <td className={classes.via}>{viaDestinations}</td>
 }
 
 export { Via }
