85ad551d09ea080a2f84823ca26a7eeb6dac4331
Vilde Oppegård <vilde.dahl.opp@gmail.com>
2023-06-26 10:26:16 +0200

    fix(Board): relocated logic to show footer to the footertext component

--- next-tavla/src/Board/scenarios/FooterText/index.tsx
+++ next-tavla/src/Board/scenarios/FooterText/index.tsx
@@ -1,6 +1,7 @@
 import classes from './styles.module.css'
 
-function FooterText({ text }: { text: string }) {
+function FooterText({ text }: { text?: string }) {
+    if (!text) return null
     return <div className={classes.footerText}>{text}</div>
 }
 
