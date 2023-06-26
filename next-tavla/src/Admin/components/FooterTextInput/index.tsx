d38fb363056de6c9370b26c4cc89ac589112ba0a
Vilde Oppegård <vilde.dahl.opp@gmail.com>
2023-06-26 10:42:01 +0200

    fix(edit): removed add footer button

--- next-tavla/src/Admin/components/FooterTextInput/index.tsx
+++ next-tavla/src/Admin/components/FooterTextInput/index.tsx
@@ -1,35 +1,30 @@
-import { PrimaryButton } from '@entur/button'
 import { TextField } from '@entur/form'
 import { useSettingsDispatch } from 'Admin/utils/contexts'
-import { useState } from 'react'
 import classes from './styles.module.css'
 
 function FooterTextInput({ footer = '' }: { footer?: string }) {
     const dispatch = useSettingsDispatch()
-    const [text, setText] = useState(footer)
 
     return (
         <div className={classes.footerInput}>
             <TextField
-                onChange={(e) => setText(e.target.value)}
-                size="medium"
-                label="Footer"
-                clearable={true}
-                value={text}
-                onClear={() => {
-                    setText('')
-                }}
-            ></TextField>
-            <PrimaryButton
-                onClick={() =>
+                onChange={(e) =>
                     dispatch({
                         type: 'addFooter',
-                        footer: text,
+                        footer: e.target.value,
                     })
                 }
-            >
-                Legg til footer
-            </PrimaryButton>
+                size="medium"
+                label="Footer"
+                value={footer}
+                clearable={true}
+                onClear={() =>
+                    dispatch({
+                        type: 'addFooter',
+                        footer: '',
+                    })
+                }
+            ></TextField>
         </div>
     )
 }
