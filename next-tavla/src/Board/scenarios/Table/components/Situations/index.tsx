2316d656e970ca3d2b0257df1b55892bb27cdf6e
stianjsu <stianjsu@gmail.com>
2023-06-29 14:37:56 +0200

    feat(board): dynamic animation time to fix scrollspeed

--- next-tavla/src/Board/scenarios/Table/components/Situations/index.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Situations/index.tsx
@@ -2,14 +2,38 @@ import { Situation } from '../Situation'
 import { useNonNullContext } from 'hooks/useNonNullContext'
 import { DepartureContext } from '../../contexts'
 import classes from './styles.module.css'
+import { useEffect, useRef, useState } from 'react'
+
+function widthToAnimationSeconds(width: number) {
+    console.log(width / 15)
+    return width / 16 + 's'
+}
 
 function Situations() {
     const departure = useNonNullContext(DepartureContext)
 
+    const animationWrapperRef = useRef<HTMLDivElement>(null)
+    const [animationSeconds, setAnimationSeconds] = useState<
+        string | undefined
+    >()
+
+    useEffect(() => {
+        if (!animationWrapperRef.current) return
+        setAnimationSeconds(
+            widthToAnimationSeconds(animationWrapperRef.current.clientWidth),
+        )
+    }, [animationWrapperRef.current])
+
     return (
         <td>
             <div>
-                <div className={classes.situationsWrapper}>
+                <div
+                    className={classes.situationsWrapper}
+                    ref={animationWrapperRef}
+                    style={{
+                        animationDuration: animationSeconds,
+                    }}
+                >
                     {departure.situations.map((situation) => (
                         <Situation key={situation.id} situation={situation} />
                     ))}
