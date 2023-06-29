3a7fdacc01338254b41ec953dfb273e45ce6140c
asemwilhelmsen <ase.wilhelmsen@bekk.no>
2023-06-29 12:45:53 +0200

    fix(test): change from entur to mørk

--- next-tavla/src/Admin/scenarios/ThemeSettings/ThemeSettings.cy.tsx
+++ next-tavla/src/Admin/scenarios/ThemeSettings/ThemeSettings.cy.tsx
@@ -24,12 +24,12 @@ describe('<ThemeSettings />', () => {
 
     it('can change theme', () => {
         cy.mount(<TestComponent />)
-        cy.findByRole('radio', { name: 'Entur' }).should('be.checked')
+        cy.findByRole('radio', { name: 'Mørk' }).should('be.checked')
         cy.findByRole('radio', { name: 'Lyst' }).should('not.be.checked')
 
         cy.findByRole('radio', { name: 'Lyst' }).parent().click()
 
-        cy.findByRole('radio', { name: 'Entur' }).should('not.be.checked')
+        cy.findByRole('radio', { name: 'Mørk' }).should('not.be.checked')
         cy.findByRole('radio', { name: 'Lyst' }).should('be.checked')
     })
 })
