7162787f9390271261ee1b9617b99da3b366f6bd
stianjsu <stianjsu@gmail.com>
2023-07-04 10:57:28 +0200

    test(converters): creates test for converting to v4

--- next-tavla/src/Shared/utils/converters.test.ts
+++ next-tavla/src/Shared/utils/converters.test.ts
@@ -2,6 +2,7 @@ import {
     V1,
     V2,
     V3,
+    V4,
     convertSettingsVersion,
     currentVersion,
 } from './converters'
@@ -16,9 +17,9 @@ test('upgrade from base to v1', () => {
                 placeId: 'NSR:StopPlace:60066',
             },
             {
-                placeId: 'NSR:StopPlace:20123',
+                placeId: 'NSR:Quay:404',
                 columns: ['time'],
-                type: 'stop_place',
+                type: 'quay',
             },
         ],
     })
@@ -49,8 +50,8 @@ test('upgrade from base to v1', () => {
                         type: 'time',
                     },
                 ],
-                placeId: 'NSR:StopPlace:20123',
-                type: 'stop_place',
+                placeId: 'NSR:Quay:404',
+                type: 'quay',
             },
         ],
     })
@@ -83,8 +84,8 @@ test('upgrade from v1 to v2', () => {
                         type: 'time',
                     },
                 ],
-                placeId: 'NSR:StopPlace:20123',
-                type: 'stop_place',
+                placeId: 'NSR:Quay:404',
+                type: 'quay',
             },
         ],
     })
@@ -117,8 +118,8 @@ test('upgrade from v1 to v2', () => {
                         type: 'time',
                     },
                 ],
-                placeId: 'NSR:StopPlace:20123',
-                type: 'stop_place',
+                placeId: 'NSR:Quay:404',
+                type: 'quay',
             },
         ],
     })
@@ -154,8 +155,8 @@ test('upgrade from v2 to v3', () => {
                         type: 'time',
                     },
                 ],
-                placeId: 'NSR:StopPlace:20123',
-                type: 'stop_place',
+                placeId: 'NSR:Quay:404',
+                type: 'quay',
             },
         ],
     })
@@ -189,8 +190,80 @@ test('upgrade from v2 to v3', () => {
                         type: 'time',
                     },
                 ],
-                placeId: 'NSR:StopPlace:20123',
+                placeId: 'NSR:Quay:404',
+                type: 'quay',
+            },
+        ],
+    })
+})
+test('upgrade from v3 to v4', () => {
+    const v4 = V4({
+        theme: 'entur',
+        tiles: [
+            {
                 type: 'stop_place',
+                placeId: 'NSR:StopPlace:60066',
+                uuid: '1234',
+                columns: [
+                    {
+                        type: 'platform',
+                    },
+                    {
+                        type: 'line',
+                    },
+                    {
+                        type: 'destination',
+                    },
+                    {
+                        type: 'time',
+                    },
+                ],
+            },
+            {
+                uuid: '1234',
+                columns: [
+                    {
+                        type: 'time',
+                    },
+                ],
+                placeId: 'NSR:Quay:404',
+                type: 'quay',
+            },
+        ],
+    })
+
+    expect(v4).toStrictEqual({
+        theme: 'entur',
+        tiles: [
+            {
+                type: 'stop_place',
+                placeId: 'NSR:StopPlace:60066',
+                uuid: '1234',
+                columns: [
+                    {
+                        type: 'platform',
+                    },
+                    {
+                        type: 'line',
+                    },
+                    {
+                        type: 'destination',
+                    },
+                    {
+                        type: 'time',
+                    },
+                ],
+            },
+            {
+                uuid: '1234',
+                columns: [
+                    {
+                        type: 'time',
+                    },
+                ],
+                stopPlaceId: '',
+                placeId: 'NSR:Quay:404',
+                type: 'quay',
             },
         ],
     })
@@ -205,9 +278,9 @@ test('upgrade from base to latest version', () => {
                 placeId: 'NSR:StopPlace:60066',
             },
             {
-                placeId: 'NSR:StopPlace:20123',
+                placeId: 'NSR:Quay:404',
                 columns: ['time'],
-                type: 'stop_place',
+                type: 'quay',
             },
         ],
     }
@@ -241,8 +314,9 @@ test('upgrade from base to latest version', () => {
                         type: 'time',
                     },
                 ],
-                placeId: 'NSR:StopPlace:20123',
-                type: 'stop_place',
+                stopPlaceId: '',
+                placeId: 'NSR:Quay:404',
+                type: 'quay',
             },
         ],
     }
