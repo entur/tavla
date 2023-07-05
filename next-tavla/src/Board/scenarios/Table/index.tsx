9b2dd66b0065b04ea9b77b758187e28c1a1a22ce
asemwilhelmsen <ase.wilhelmsen@bekk.no>
2023-07-05 08:59:33 +0200

    fix(table): not showing columns where selected field is false

--- next-tavla/src/Board/scenarios/Table/index.tsx
+++ next-tavla/src/Board/scenarios/Table/index.tsx
@@ -13,7 +13,7 @@ import { Via } from './components/Via'
 const columnTest: Record<TColumn, TColumnSettingTest> = {
     line: { size: 10, selected: true },
     destination: { size: 20, selected: true },
-    platform: { size: 15, selected: true },
+    platform: { size: 15, selected: false },
     situations: { size: 40, selected: true },
     time: { size: 15, selected: false },
     via: { size: 15, selected: true },
@@ -42,16 +42,19 @@ function Table({ departures }: { departures: TDepartureFragment[] }) {
             <table className={classes.table}>
                 <thead>
                     <tr>
-                        {columnOrder.map((colName) => (
-                            <th
-                                style={{
-                                    width: columnTest[colName].size,
-                                }}
-                                key={colName}
-                            >
-                                {Columns[colName]}
-                            </th>
-                        ))}
+                        {columnOrder.map(
+                            (colName) =>
+                                columnTest[colName].selected && (
+                                    <th
+                                        style={{
+                                            width: columnTest[colName].size,
+                                        }}
+                                        key={colName}
+                                    >
+                                        {Columns[colName]}
+                                    </th>
+                                ),
+                        )}
                     </tr>
                 </thead>
                 <tbody>
@@ -62,7 +65,11 @@ function Table({ departures }: { departures: TDepartureFragment[] }) {
                             <DepartureContext.Provider value={departure}>
                                 {columnOrder.map((colName) => {
                                     const Component = columnComponents[colName]
-                                    return <Component key={colName} />
+                                    return (
+                                        columnTest[colName].selected && (
+                                            <Component key={colName} />
+                                        )
+                                    )
                                 })}
                             </DepartureContext.Provider>
                         </tr>
