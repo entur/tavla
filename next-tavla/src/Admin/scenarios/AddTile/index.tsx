fd09b19bb5887a8f4676fcdcd6e197f8485bd8ab
SharmaTarun1111111 <tarshar98@gmail.com>
2023-07-10 11:48:47 +0200

    feat(addtile): functionality for adding a tile

--- next-tavla/src/Admin/scenarios/AddTile/index.tsx
+++ next-tavla/src/Admin/scenarios/AddTile/index.tsx
@@ -1,13 +1,13 @@
-//import React, { useState } from 'react'
+import React, { useState } from 'react'
 import { Button } from '@entur/button'
-//import classes from './styles.module.css'
-//import { useSettingsDispatch } from 'Admin/utils/contexts'
+import { useSettingsDispatch } from 'Admin/utils/contexts'
 import { Dropdown } from '@entur/dropdown'
 import { fetchItems } from 'Admin/utils'
 import { SearchIcon } from '@entur/icons'
 
 function AddTile() {
-    //const dispatch = useSettingsDispatch()
+    const dispatch = useSettingsDispatch()
+    const [value, setTileValue] = useState('')
 
     return (
         <div>
@@ -18,15 +18,20 @@ function AddTile() {
                 debounceTimeout={1000}
                 clearable
                 prepend={<SearchIcon />}
+                onChange={(e) => {
+                    if (e?.value) {
+                        setTileValue(e.value)
+                    }
+                }}
             />
 
             <Button
                 variant="primary"
                 onClick={() => {
-                    // dispatch({
-                    //     type: 'addTile',
-                    //     tile,
-                    // })
+                    dispatch({
+                        type: 'addTile',
+                        tile: { type: 'stop_place', placeId: value },
+                    })
                 }}
             >
                 Legg til
