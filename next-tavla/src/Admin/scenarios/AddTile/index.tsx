3be4b9e7cb3f29fd71805d0d1e4c8218ea26cca4
SharmaTarun1111111 <tarshar98@gmail.com>
2023-07-10 15:44:05 +0200

    feat(addtile): added header and styling of dropdown and button

--- next-tavla/src/Admin/scenarios/AddTile/index.tsx
+++ next-tavla/src/Admin/scenarios/AddTile/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { Button } from '@entur/button'
+import classes from './styles.module.css'
 import { useSettingsDispatch } from 'Admin/utils/contexts'
 import { Dropdown } from '@entur/dropdown'
 import { fetchItems } from 'Admin/utils'
 import { SearchIcon } from '@entur/icons'
+import { Heading1 } from '@entur/typography'
 
 function AddTile() {
     const dispatch = useSettingsDispatch()
@@ -11,31 +13,36 @@ function AddTile() {
 
     return (
         <div>
-            <Dropdown
-                items={fetchItems}
-                label="Søk etter holdeplass..."
-                searchable
-                debounceTimeout={1000}
-                clearable
-                prepend={<SearchIcon />}
-                onChange={(e) => {
-                    if (e?.value) {
-                        setTileValue(e.value)
-                    }
-                }}
-            />
+            <Heading1 className={classes.Heading1}>Holdeplasser</Heading1>
 
-            <Button
-                variant="primary"
-                onClick={() => {
-                    dispatch({
-                        type: 'addTile',
-                        tile: { type: 'stop_place', placeId: value },
-                    })
-                }}
-            >
-                Legg til
-            </Button>
+            <div className={classes.SearchContainer}>
+                <Dropdown
+                    className={classes.DropDown}
+                    items={fetchItems}
+                    label="Søk etter holdeplass..."
+                    searchable
+                    debounceTimeout={1000}
+                    clearable
+                    prepend={<SearchIcon />}
+                    onChange={(e) => {
+                        if (e?.value) {
+                            setTileValue(e.value)
+                        }
+                    }}
+                />
+
+                <Button
+                    variant="primary"
+                    onClick={() => {
+                        dispatch({
+                            type: 'addTile',
+                            tile: { type: 'stop_place', placeId: value },
+                        })
+                    }}
+                >
+                    Legg til
+                </Button>
+            </div>
         </div>
     )
 }
