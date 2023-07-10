284ab755b1af031cdf1029ab1dbdc6d2d731a505
SharmaTarun1111111 <tarshar98@gmail.com>
2023-07-10 09:02:00 +0200

    feat(addtile): searchable dropdown menu applies only for stop places

--- next-tavla/src/Admin/scenarios/AddTile/index.tsx
+++ next-tavla/src/Admin/scenarios/AddTile/index.tsx
@@ -1,62 +1,36 @@
-import React, { useState } from 'react'
-import { TTileType } from 'types/tile'
+//import React, { useState } from 'react'
 import { Button } from '@entur/button'
-import { RadioGroup, RadioPanel } from '@entur/form'
-import classes from './styles.module.css'
-import { AddStopPlaceTile } from './components/AddStopPlaceTile'
-import { AddQuayTile } from './components/AddQuayTile'
-import { TAnonTiles } from 'Admin/types'
-import { useSettingsDispatch } from 'Admin/utils/contexts'
-
-const components: Record<
-    TTileType,
-    (props: { setTile: (tile: TAnonTiles) => void }) => JSX.Element
-> = {
-    stop_place: AddStopPlaceTile,
-    quay: AddQuayTile,
-}
+//import classes from './styles.module.css'
+//import { useSettingsDispatch } from 'Admin/utils/contexts'
+import { Dropdown } from '@entur/dropdown'
+import { fetchItems } from 'Admin/utils'
+import { SearchIcon } from '@entur/icons'
 
 function AddTile() {
-    const [tileType, setTileType] = useState<TTileType>('stop_place')
-    const [tile, setTile] = useState<TAnonTiles>()
-
-    const Component = components[tileType]
-
-    const dispatch = useSettingsDispatch()
+    //const dispatch = useSettingsDispatch()
 
     return (
-        <div className={classes.AddTile}>
-            <RadioGroup
-                name="tile-type"
-                label="Legg til ny rute"
-                onChange={(e) => {
-                    setTileType(e.target.value as TTileType)
+        <div>
+            <Dropdown
+                items={fetchItems}
+                label="Søk etter holdeplass..."
+                searchable
+                debounceTimeout={1000}
+                clearable
+                prepend={<SearchIcon />}
+            />
+
+            <Button
+                variant="primary"
+                onClick={() => {
+                    // dispatch({
+                    //     type: 'addTile',
+                    //     tile,
+                    // })
                 }}
-                value={tileType}
             >
-                <div className={classes.RadioCards}>
-                    <RadioPanel title="Stoppested" value="stop_place">
-                        Rute med alle avganger for et stoppested.
-                    </RadioPanel>
-                    <RadioPanel title="Plattform" value="quay">
-                        Rute med avganger for en valgt platform/retning.
-                    </RadioPanel>
-                </div>
-            </RadioGroup>
-            <Component setTile={setTile} />
-            {tile && (
-                <Button
-                    variant="primary"
-                    onClick={() => {
-                        dispatch({
-                            type: 'addTile',
-                            tile,
-                        })
-                    }}
-                >
-                    Legg til
-                </Button>
-            )}
+                Legg til
+            </Button>
         </div>
     )
 }
