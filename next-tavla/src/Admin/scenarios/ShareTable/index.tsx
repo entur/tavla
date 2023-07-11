4803dbe90c7a074238b307d258d13f4db1c7be9e
Vilde Oppegård <vilde.dahl.opp@gmail.com>
2023-07-11 12:48:13 +0200

    fix(sharetable): changed heading to be h1

--- next-tavla/src/Admin/scenarios/ShareTable/index.tsx
+++ next-tavla/src/Admin/scenarios/ShareTable/index.tsx
@@ -1,11 +1,11 @@
-import { Heading2, Paragraph } from '@entur/typography'
+import { Heading1, Paragraph } from '@entur/typography'
 import { CopyText } from 'Admin/components/CopyText'
 import classes from './styles.module.css'
 
 function ShareTable({ text }: { text: string }) {
     return (
         <div>
-            <Heading2>Del avganstavla</Heading2>
+            <Heading1 className={classes.heading}>Del avganstavla</Heading1>
             <Paragraph className={classes.paragraph}>
                 Trykk på knappen for å kopiere linken til avganstavla og dele
                 den med andre.
