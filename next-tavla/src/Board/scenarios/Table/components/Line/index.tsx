afc5a3f3759812acf384e0de986172390be8a1b2
vildeopp <64434819+vildeopp@users.noreply.github.com>
2023-07-17 15:47:51 +0200

    feat(board): removed use of thirdparty colors (#1130)
    * feat(board): removed use of thirdparty colors
    
    * chore(table): removed colors utils file
    
    * fix(line): changed variable name in line to transportmodecolor
    
    * fix(table): fixed fontsize in table

--- next-tavla/src/Board/scenarios/Table/components/Line/index.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Line/index.tsx
@@ -1,16 +1,12 @@
 import { useNonNullContext } from 'hooks/useNonNullContext'
-import { getPresentation } from 'Board/utils/colors'
 import { DepartureContext } from '../../contexts'
 import classes from './styles.module.css'
 
 function Line() {
     const departure = useNonNullContext(DepartureContext)
 
-    const presentation = getPresentation(
-        departure.serviceJourney.line.presentation,
-        departure.serviceJourney.line.id,
-        departure.serviceJourney.transportMode,
-    )
+    const mode = departure.serviceJourney.transportMode ?? 'unknown'
+    const transportModeColor = `var(--table-transport-${mode}-color)`
 
     const publicCode = departure.serviceJourney.line.publicCode
 
@@ -19,8 +15,7 @@ function Line() {
             <div
                 className={classes.lineWrapper}
                 style={{
-                    backgroundColor: presentation.backgroundColor,
-                    color: presentation.color,
+                    backgroundColor: transportModeColor,
                 }}
             >
                 {publicCode}
