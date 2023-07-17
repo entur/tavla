eb2fda3cc41bff26144bb1eea45eaeec16cfef2e
stianjsu <stianjsu@gmail.com>
2023-07-17 13:34:24 +0200

    fix(db): removes undefined props from settings obj before updating db

--- next-tavla/src/Shared/utils/firebase.ts
+++ next-tavla/src/Shared/utils/firebase.ts
@@ -21,7 +21,9 @@ export async function getBoardSettings(boardId: string) {
 
 export async function setBoardSettings(boardId: string, settings: TSettings) {
     const docRef = doc(firestore, 'settings-v2', boardId)
-    await setDoc(docRef, settings)
+    // Removes explicitly assigned undefined properties on settings
+    const sanatizedSettings = JSON.parse(JSON.stringify(settings))
+    await setDoc(docRef, sanatizedSettings)
 }
 
 export async function addBoardSettings(settings: TSettings) {
