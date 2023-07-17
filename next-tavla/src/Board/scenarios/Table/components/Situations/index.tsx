22229676ecda214400a708f04571007b38c2a322
Vilde Oppegård <vilde.dahl.opp@gmail.com>
2023-07-17 13:20:54 +0200

    feat(table): made deviations alternate if there are more than one deviation for a line

--- next-tavla/src/Board/scenarios/Table/components/Situations/index.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Situations/index.tsx
@@ -1,16 +1,33 @@
 import { Situation } from '../Situation'
 import { useNonNullContext } from 'hooks/useNonNullContext'
 import { DepartureContext } from '../../contexts'
+import { useEffect, useState } from 'react'
 
 function Situations() {
     const departure = useNonNullContext(DepartureContext)
 
+    const situations = departure.situations.map((situation) => (
+        <Situation key={situation.id} situation={situation} />
+    ))
+
+    const nSituations = situations.length
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        if (nSituations < 1) {
+            return
+        }
+        const interval = setInterval(
+            () => setIndex((i) => (i + 1) % nSituations),
+            5000,
+        )
+        return () => clearInterval(interval)
+    }, [nSituations])
+
     return (
         <td>
             <div>
-                {departure.situations.map((situation) => (
-                    <Situation key={situation.id} situation={situation} />
-                ))}
+                {situations.length > 1 ? situations[index] : situations[0]}
             </div>
         </td>
     )
