d0b1bf2a64d72e969ec78be8f61b0172194b3829
stianjsu <stianjsu@gmail.com>
2023-07-17 14:23:16 +0200

    feat(edit tile): adds title and paragraph to dropdown component

--- next-tavla/src/Admin/scenarios/TileSettings/components/PlatformDropdown/index.tsx
+++ next-tavla/src/Admin/scenarios/TileSettings/components/PlatformDropdown/index.tsx
@@ -1,10 +1,12 @@
 import { Dropdown } from '@entur/dropdown'
+import { Heading4, SubParagraph } from '@entur/typography'
 import { useSettingsDispatch } from 'Admin/utils/contexts'
 import { QuaysSearchQuery, TQuaysSearchQuery } from 'graphql/index'
 import { fetchQuery } from 'graphql/utils'
 import { useEffect, useState } from 'react'
 import { TTile } from 'types/tile'
 import { isNotNullOrUndefined } from 'utils/typeguards'
+import classes from './styles.module.css'
 
 const stopPlaceOption = { value: 'stopPlace', label: 'Vis Alle' } as const
 
@@ -45,31 +47,40 @@ function PlatformDropdown({
         : []
 
     return (
-        <Dropdown
-            items={() => dropDownOptions}
-            label="Velg plattform"
-            disabled={!stopPlaceId}
-            value={selectedValue}
-            onChange={(e) => {
-                if (e?.value) {
-                    if (e.value === 'stopPlace')
-                        setTile({
-                            type: 'stop_place',
-                            placeId: stopPlaceId,
-                            uuid: tile.uuid,
-                            columns: tile.columns,
-                        })
-                    else
-                        setTile({
-                            type: 'quay',
-                            stopPlaceId,
-                            placeId: e.value,
-                            uuid: tile.uuid,
-                            columns: tile.columns,
-                        })
-                }
-            }}
-        />
+        <div>
+            <Heading4>Velg plattform</Heading4>
+            <SubParagraph>
+                Du kan enten vise alle plattformer eller kun én. Valg av
+                plattform bestemmer også hvilke linjer i en bestemt retning du
+                kan velge å vise.
+            </SubParagraph>
+            <Dropdown
+                className={classes.dropdown}
+                items={() => dropDownOptions}
+                label="Velg plattform"
+                disabled={!stopPlaceId}
+                value={selectedValue}
+                onChange={(e) => {
+                    if (e?.value) {
+                        if (e.value === 'stopPlace')
+                            setTile({
+                                type: 'stop_place',
+                                placeId: stopPlaceId,
+                                uuid: tile.uuid,
+                                columns: tile.columns,
+                            })
+                        else
+                            setTile({
+                                type: 'quay',
+                                stopPlaceId,
+                                placeId: e.value,
+                                uuid: tile.uuid,
+                                columns: tile.columns,
+                            })
+                    }
+                }}
+            />
+        </div>
     )
 }
 
