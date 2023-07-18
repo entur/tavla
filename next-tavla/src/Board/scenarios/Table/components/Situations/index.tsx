9b0bceb6cdc35b355f001868296f9058bc6fd303
vildeopp <64434819+vildeopp@users.noreply.github.com>
2023-07-18 11:57:53 +0200

    feat(table): alternates between deviations in table (#1133)
    * feat(table): made deviations alternate if there are more than one deviation for a line
    
    * fix(table): made validationexclamation icon larger
    
    * fix(situastions): changed white-space to be normal
    
    * fix(situations): removed mapping of situations and uses index to return the current deviation
    
    * fix(situations): removed unnecessary div in situations
    
    * fix(situations): fixed undefined error in situations
    
    * fix(board): fixed potential out of bounds error

--- next-tavla/src/Board/scenarios/Table/components/Situations/index.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Situations/index.tsx
@@ -1,17 +1,29 @@
 import { Situation } from '../Situation'
 import { useNonNullContext } from 'hooks/useNonNullContext'
 import { DepartureContext } from '../../contexts'
+import { useEffect, useState } from 'react'
 
 function Situations() {
     const departure = useNonNullContext(DepartureContext)
 
+    const numberOfSituations = departure.situations.length
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        if (numberOfSituations <= 1) {
+            return
+        }
+        const interval = setInterval(() => setIndex((i) => i + 1), 5000)
+        return () => clearInterval(interval)
+    }, [numberOfSituations])
+
     return (
         <td>
-            <div>
-                {departure.situations.map((situation) => (
-                    <Situation key={situation.id} situation={situation} />
-                ))}
-            </div>
+            {numberOfSituations ? (
+                <Situation
+                    situation={departure.situations[index % numberOfSituations]}
+                />
+            ) : null}
         </td>
     )
 }
