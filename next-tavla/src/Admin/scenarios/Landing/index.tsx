cf5e686879e1b22a9991774ec11dde81b563ac6a
SharmaTarun1111111 <tarshar98@gmail.com>
2023-07-19 13:36:56 +0200

    chore(landingpage): styling of landingpae

--- next-tavla/src/Admin/scenarios/Landing/index.tsx
+++ next-tavla/src/Admin/scenarios/Landing/index.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@entur/button'
-import { Heading1, Heading2 } from '@entur/typography'
+import { Heading1, Paragraph } from '@entur/typography'
 import classes from './styles.module.css'
 import { addBoardSettings } from 'utils/firebase'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import Image from 'next/image'
+import landingImage from './Scenariolanding.png'
+import tavla from './tavla.png'
 
 function Landing() {
     const router = useRouter()
@@ -19,19 +22,75 @@ function Landing() {
 
     return (
         <div className={classes.container}>
-            <Heading1>Entur Tavla</Heading1>
-            <Heading2>Her kan du lage din egen tavle</Heading2>
+            <div className={classes.top}>
+                <div>
+                    <Heading1>Lag din egen avgangstavle</Heading1>
+                    <Heading1 className={classes.highlightText}>
+                        for reisende
+                    </Heading1>
 
-            <Button
-                onClick={handleCreateNewBoard}
-                variant="primary"
-                disabled={loading}
-                loading={loading}
-            >
-                Opprett ny tavle
-            </Button>
+                    <Button
+                        className={classes.button}
+                        onClick={handleCreateNewBoard}
+                        variant="primary"
+                        disabled={loading}
+                        loading={loading}
+                    >
+                        Opprett ny tavle
+                    </Button>
+                </div>
+                <div className={classes.image}>
+                    <Image
+                        src={landingImage}
+                        alt="illustration"
+                        width={402}
+                        height={377}
+                    />
+                </div>
+            </div>
+
+            <div className={classes.bottom}>
+                <div className={classes.img}>
+                    <Image
+                        src={tavla}
+                        alt="illustration"
+                        width={811}
+                        height={387}
+                    />
+                </div>
+
+                <div className={classes.bottomText}>
+                    <Heading1 className={classes.text}>Hva er Tavla?</Heading1>
+
+                    <Paragraph className={classes.text}>
+                        Tavla er en nettside som viser avgangene til all
+                        offentlig transport i Norge. Med Tavla kan du enkelt
+                        sette opp en avgangstavle fra de stoppestedene du ønsker
+                        å se, i sanntid.
+                    </Paragraph>
+                    <Paragraph className={classes.text}>
+                        Du kan f.eks. lage avgangstavler for:
+                        <Paragraph className={classes.list}>
+                            <ul>
+                                <li>holdeplasser</li>
+                                <li>knutepunkter</li>
+                            </ul>
+                        </Paragraph>
+                    </Paragraph>
+
+                    <Paragraph className={classes.text}>
+                        Ved å samle og tydeliggjøre viktig informasjon på ett
+                        sted kan Tavla gjøre det enklere for reisende å bruke
+                        det kollektivtrafikktilbudet som finnes.
+                    </Paragraph>
+                    <Paragraph className={classes.text}>
+                        Hver avgangstavle er knyttet til en lenke som kan brukes
+                        til å sende eller vise den fram der det passer deg.
+                        Trykk på “Opprett ny tavle” for å teste det ut!
+                    </Paragraph>
+                </div>
+            </div>
         </div>
     )
 }
-
 export { Landing }
