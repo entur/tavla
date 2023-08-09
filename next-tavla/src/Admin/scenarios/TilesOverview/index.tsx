2e8f164dac5f0a5cb36720e7a77d187bdbd0435a
lindtvedtsebastian <46536611+lindtvedtsebastian@users.noreply.github.com>
2023-08-09 14:29:53 +0200

    feat(info): improve info screen in admin when board is empty (#1163)

--- next-tavla/src/Admin/scenarios/TilesOverview/index.tsx
+++ next-tavla/src/Admin/scenarios/TilesOverview/index.tsx
@@ -2,14 +2,23 @@ import { TTile } from 'types/tile'
 import React from 'react'
 import { TileSettings } from 'Admin/scenarios/TileSettings'
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@entur/tab'
+import Image from 'next/image'
+import animals from 'assets/illustrations/Animals.png'
 import classes from './styles.module.css'
+import { Heading2, LeadParagraph } from '@entur/typography'
 
 function TilesOverview({ tiles }: { tiles: TTile[] }) {
     if (tiles.length === 0)
         return (
-            <div>
-                Legg til en holdeplass for å kunne bestemme plattformer og
-                linjer som skal vises på avgangstavla
+            <div className={classes.info}>
+                <Image src={animals} alt="illustration" />
+                <Heading2 className={classes.infoHeading}>
+                    Ingen holdeplasser i Tavla
+                </Heading2>
+                <LeadParagraph>
+                    Legg til en holdeplass for å kunne bestemme plattformer og
+                    linjer som skal vises på avgangstavla
+                </LeadParagraph>
             </div>
         )
 
