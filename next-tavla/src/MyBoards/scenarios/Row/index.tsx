a7c1fcbc1570380fec4fc9b9bce34d69b0283937
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-08-29 10:16:33 +0200

    feat(admin): row for a table with boards

---
+++ next-tavla/src/MyBoards/scenarios/Row/index.tsx
@@ -0,0 +1,52 @@
+import { TableRow, DataCell } from '@entur/table'
+import { IconButton } from '@entur/button'
+import { CopyIcon, EditIcon } from '@entur/icons'
+import '@entur/table/dist/styles.css'
+import '@entur/button/dist/styles.css'
+import { TSettings } from 'types/settings'
+import { useToast } from '@entur/alert'
+import { useRouter } from 'next/router'
+import classes from './styles.module.css'
+
+function Row({
+    board,
+}: {
+    board: { id: string; settings: TSettings | undefined }
+}) {
+    const link = `https://lite-tavla.web.app/${board.id}`
+    const { addToast } = useToast()
+    const router = useRouter()
+
+    async function editBoard() {
+        await router.push('/edit/' + board.id)
+    }
+
+    return (
+        <TableRow>
+            <DataCell>{board.settings?.title ?? 'Tavla'}</DataCell>
+            <DataCell>
+                <div className={classes.link}>
+                    {link}
+                    <IconButton
+                        aira-label="Kopier lenke"
+                        onClick={() => {
+                            navigator.clipboard.writeText(link)
+                            addToast('Lenke til Tavla kopiert')
+                        }}
+                    >
+                        <CopyIcon />
+                    </IconButton>
+                </div>
+            </DataCell>
+            <DataCell>
+                <div className={classes.iconButtons}>
+                    <IconButton aira-label="Rediger tavle" onClick={editBoard}>
+                        <EditIcon />
+                    </IconButton>
+                </div>
+            </DataCell>
+        </TableRow>
+    )
+}
+
+export { Row }
