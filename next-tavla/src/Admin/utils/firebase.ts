e6198291186cf9158b71131270061a094af8659b
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-08-29 14:27:59 +0200

    feat(auth): auth credential based on service account in env

--- next-tavla/src/Admin/utils/firebase.ts
+++ next-tavla/src/Admin/utils/firebase.ts
@@ -1,21 +1,18 @@
 import admin, { auth } from 'firebase-admin'
-import { applicationDefault, getApp } from 'firebase-admin/app'
+import { cert } from 'firebase-admin/app'
 
-const app = initializeOrGetAdminApp()
-
-export function initializeOrGetAdminApp() {
+export function initializeAdminApp() {
     if (admin.apps.length <= 0) {
-        return admin.initializeApp({
-            credential: applicationDefault(),
+        admin.initializeApp({
+            credential: cert(JSON.parse(process.env.SERVICE_ACCOUNT ?? '')),
         })
     }
-    return getApp()
 }
 
 export async function verifySession(session?: string) {
     if (!session) return null
     try {
-        return await auth(app).verifySessionCookie(session, true)
+        return await auth().verifySessionCookie(session, true)
     } catch {
         return null
     }
