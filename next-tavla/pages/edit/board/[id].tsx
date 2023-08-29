6489f0fec290d62596be25b2ba6e537c8d3bba01
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-08-29 10:19:31 +0200

    feat(admin): route for admin page

---
+++ next-tavla/pages/edit/board/[id].tsx
@@ -0,0 +1,62 @@
+import classes from 'styles/pages/admin.module.css'
+import { Contrast } from '@entur/layout'
+import { ToastProvider } from '@entur/alert'
+import { Header } from 'components/Header'
+import { Admin } from 'src/MyBoards/scenarios/Admin'
+import { getBoardSettings } from 'utils/firebase'
+import { TSettings } from 'types/settings'
+import { upgradeSettings } from 'utils/converters'
+
+export async function getServerSideProps() {
+    //when login is fixed:
+    // 1. take user id as params
+    // 2. create getBoards function in firebase.ts that gets all boards for a user based on user id
+    // 3. use getBoards function instead of getBoardSettings to get all boards for a user
+    // 4. remove ids const
+
+    const ids = [
+        'Malre1Dx5zLE086AzpFH',
+        'oMgfeCRUZ4sfD8xXXG8M',
+        '8EyKHHP5Ie2HisOO2ilt',
+    ]
+
+    const settingsPromises: Promise<{
+        id: string
+        settings: TSettings | undefined
+    }>[] = ids.map(async (id: string) => {
+        const settings = await getBoardSettings(id)
+        return { id, settings }
+    })
+
+    const allSettingsWithIds = await Promise.all(settingsPromises)
+
+    const convertedSettings = allSettingsWithIds.map(({ id, settings }) => {
+        return {
+            id,
+            settings: settings ? upgradeSettings(settings) : null,
+        }
+    })
+
+    return {
+        props: {
+            boards: convertedSettings,
+        },
+    }
+}
+
+function OverviewPage({
+    boards,
+}: {
+    boards: { id: string; settings: TSettings | undefined }[]
+}) {
+    return (
+        <Contrast className={classes.root}>
+            <ToastProvider>
+                <Header />
+                <Admin boards={boards} />
+            </ToastProvider>
+        </Contrast>
+    )
+}
+
+export default OverviewPage
