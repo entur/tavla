7f3a1a94cce2ff7dd446238369fdee0842a76988
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-08-29 10:17:11 +0200

    feat(admin): table that shows all boards

---
+++ next-tavla/src/MyBoards/scenarios/List/index.tsx
@@ -0,0 +1,27 @@
+import { Table, TableBody } from '@entur/table'
+import '@entur/table/dist/styles.css'
+import { TableHeader } from '../TableHeader'
+import { Row } from '../Row'
+import { TSettings } from 'types/settings'
+
+function List({
+    boards,
+}: {
+    boards: { id: string; settings: TSettings | undefined }[]
+}) {
+    return (
+        <div>
+            <Table>
+                <TableHeader />
+                <TableBody>
+                    {boards.map(
+                        (board) =>
+                            board.id && <Row key={board.id} board={board} />,
+                    )}
+                </TableBody>
+            </Table>
+        </div>
+    )
+}
+
+export { List }
