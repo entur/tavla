5ce020623a008794b55a1a9d995544e5d8007bb3
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-08-29 12:30:58 +0200

    feat(feature flags): implemented feature flag API endpoint

---
+++ next-tavla/pages/api/featureFlags.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { undefinedFeatureFlags } from 'types/featureFlag'
+import { stringToBoolean } from 'utils/converters'
+import { isFeature } from 'utils/typeguards'
+
+export default async function handler(
+    request: NextApiRequest,
+    response: NextApiResponse,
+) {
+    const featureFlags = undefinedFeatureFlags
+
+    for (const [key, value] of Object.entries(process.env)) {
+        if (key.startsWith('FEATURE_')) {
+            if (isFeature(key)) featureFlags[key] = stringToBoolean(value)
+        }
+    }
+
+    for (const [key, value] of Object.entries(featureFlags)) {
+        if (value === undefined) {
+            console.warn(
+                `Feature flag "${key}" is not defined, or set to a non-boolean value.`,
+            )
+        }
+    }
+
+    return response.status(200).json(featureFlags)
+}
