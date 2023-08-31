6e6cc3eef46f6bca6c4f9be020f4849010b01872
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-08-31 08:46:02 +0200

    feat(boards): seearchfield to search by boards titles

--- next-tavla/src/Admin/scenarios/List/index.tsx
+++ next-tavla/src/Admin/scenarios/List/index.tsx
@@ -2,16 +2,40 @@ import { TableHeader } from '../TableHeader'
 import { Row } from '../Row'
 import { TSettings } from 'types/settings'
 import classes from './styles.module.css'
+import { TextField } from '@entur/form'
+import { SearchIcon } from '@entur/icons'
+import { useState } from 'react'
 
 function List({ boards }: { boards: { id: string; settings?: TSettings }[] }) {
+    const [filterSearch, setFilterSearch] = useState('')
+    const textSearchRegex = new RegExp(filterSearch, 'i')
+    function searchBoards(
+        board: {
+            id: string
+            settings?: TSettings | undefined
+        },
+        search: RegExp,
+    ) {
+        const title = board.settings?.title || ''
+        return search.test(title)
+    }
     return (
-        <div>
+        <div className={classes.tableWrapper}>
+            <TextField
+                label="Søk på navn på tavle"
+                style={{ width: '15rem' }}
+                prepend={<SearchIcon inline />}
+                value={filterSearch}
+                onChange={(e) => setFilterSearch(e.target.value)}
+            />
             <div className={classes.table}>
                 <TableHeader />
                 <div className={classes.tableBody}>
-                    {boards.map((board) => (
-                        <Row key={board.id} board={board} />
-                    ))}
+                    {boards
+                        .filter((board) => searchBoards(board, textSearchRegex))
+                        .map((board) => (
+                            <Row key={board.id} board={board} />
+                        ))}
                 </div>
             </div>
         </div>
