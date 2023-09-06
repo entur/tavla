f712a154fa47dc352443b32586ad8f51f19ed5ab
Fredrik Busklein <61384979+fredrbus@users.noreply.github.com>
2023-09-06 14:10:10 +0200

    feat(aria): aria labelling (#1202)
    * feat(aria-labels): added aria-label to 'Delete stop'-button
    
    * feat(aria-labels): added descriptive label with transport mode and line number for rows in preview table
    
    * feat(aria): making svg-icons aria-hidden
    
    * feat(feature flags): feature flags now checked locally in the browser (#1200)
    
    * feat(boards): show additional board information (#1201)
    
    * feat(boards): show more info about the chosen board
    
    * feat(boards): styling popover
    
    * feat(boards): show info component in the row
    
    * feat(boards): updated packages
    
    * fix(boards): rename last column
    
    * fix(boards):  styling popover
    
    * fix(boards): control state of popover
    
    * chore(boards): update import
    
    * chore(boards): update styling
    
    * chore(boards): back to PopoverCloseButton from controlled state
    
    * chore(boards): contrast popover styling
    
    * chore(boards): implement correct headings
    
    * chore(boards): styling
    
    * feat(tooltips): added tooltips where necessary (#1203)
    
    * chore(buttons): add tooltip to packages/global imports
    
    * chore(cleanup): delete empty file
    
    * feat(title): add tooltip to edit board title
    
    * feat(delete tile): add tooltip to delete tile
    
    * feat(row): add tooltips to icon buttons in row
    
    * fix(boards): move info component
    
    ---------
    
    Co-authored-by: Natasha Tikhonova <64562118+natashatikhonova@users.noreply.github.com>

--- next-tavla/src/Board/scenarios/Table/components/Situation/index.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Situation/index.tsx
@@ -30,6 +30,7 @@ function Situation({
 function ValidationExclamation(props: SVGProps<SVGSVGElement>) {
     return (
         <svg
+            aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             width="0.5em"
             xmlSpace="preserve"
