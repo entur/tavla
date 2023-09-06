8c53de7b7d2e9d32114e7428667c6f6cd3967a20
Fredrik Busklein <61384979+fredrbus@users.noreply.github.com>
2023-09-06 09:13:23 +0200

    feat(feature flags): feature flags now checked locally in the browser (#1200)

--- next-tavla/src/Shared/utils/featureFlags.ts
+++ next-tavla/src/Shared/utils/featureFlags.ts
@@ -1,8 +1,8 @@
 import { Feature } from 'types/featureFlag'
 
 export function checkFeatureFlags(feature: Feature) {
-    if (!process.env.ENABLED_FEATURES) return false
-    const enabledFeatures = JSON.parse(process.env.ENABLED_FEATURES)
+    if (!process.env.NEXT_PUBLIC_ENABLED_FEATURES) return false
+    const enabledFeatures = JSON.parse(process.env.NEXT_PUBLIC_ENABLED_FEATURES)
 
     return enabledFeatures.includes(feature)
 }
