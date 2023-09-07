4baaf87f2c358d0c919929c0fbffe4a4dd084926
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-09-07 13:29:19 +0200

    feat(auth): rewrote getBearerToken

--- next-tavla/src/Admin/utils/index.ts
+++ next-tavla/src/Admin/utils/index.ts
@@ -14,24 +14,27 @@ type TPartialGeoResponse = {
 
 export async function getBearerTokenFromRequest(request: NextApiRequest) {
     const authorization = request?.headers?.authorization
+
     if (!authorization)
         throw new TavlaError({
             code: 'AUTHORIZATION',
             message: 'Authorization header missing.',
         })
-    if (authorization?.startsWith('Bearer ')) {
-        const idToken = authorization.split('Bearer ')[1]
-        if (!idToken)
-            throw new TavlaError({
-                code: 'AUTHORIZATION',
-                message: 'Bearer token missing.',
-            })
-        return idToken
-    } else
+
+    if (!authorization?.startsWith('Bearer '))
         throw new TavlaError({
             code: 'AUTHORIZATION',
             message: 'Bearer token was ill formatted.',
         })
+
+    const idToken = authorization.split('Bearer ')[1]
+    if (!idToken)
+        throw new TavlaError({
+            code: 'AUTHORIZATION',
+            message: 'Bearer token missing.',
+        })
+
+    return idToken
 }
 
 export async function fetchItems(
