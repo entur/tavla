849f63449523c726a8accc107da39ed6fc758b73
Natasha Tikhonova <64562118+natashatikhonova@users.noreply.github.com>
2023-09-08 08:01:34 +0200

    feat(board): show transport icon for each row (#1205)
    * chore(board): remove transport icons from header
    
    * chore(board): remove unused prop
    
    * chore(board): update styling
    
    * chore(board): take in optional color for filling icon
    
    * feat(board): add transport icon to row
    
    * fix(board):necessary import
    
    * chore(table): move icon to the line column
    
    * chore(style): update styling for one column
    
    * chore(transporticon): remove changes

--- next-tavla/src/Board/scenarios/Table/components/Line/index.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Line/index.tsx
@@ -3,12 +3,8 @@ import { DeparturesContext } from '../../contexts'
 import { TableColumn } from '../TableColumn'
 import { TableRow } from '../TableRow'
 import classes from './styles.module.css'
-<<<<<<< HEAD
 import { transportModeNames } from 'Admin/utils/transport'
 import { TransportIcon } from '../TransportIcon'
-=======
-import { transportModeNames } from 'Admin/utils'
->>>>>>> c570908a (feat(aria): aria labelling (#1202))
 
 function Line() {
     const departures = useNonNullContext(DeparturesContext)
