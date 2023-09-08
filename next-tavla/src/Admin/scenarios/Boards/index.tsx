156a9033d66a0042fdc592ad1b961170da42e5f7
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-09-08 08:16:00 +0200

    chore(boards): moved boards header from its own component to boards

--- next-tavla/src/Admin/scenarios/Boards/index.tsx
+++ next-tavla/src/Admin/scenarios/Boards/index.tsx
@@ -2,7 +2,8 @@ import classes from './styles.module.css'
 import { BoardList } from '../BoardList'
 import { TSettings } from 'types/settings'
 import dynamic from 'next/dynamic'
-import { BoardsHeader } from '../BoardsHeader'
+import { Heading1 } from '@entur/typography'
+import { CreateBoard } from '../CreateBoard'
 
 function Boards({
     boards,
@@ -11,7 +12,12 @@ function Boards({
 }) {
     return (
         <div className={classes.adminWrapper}>
-            <BoardsHeader />
+            <div className={classes.header}>
+                <Heading1>Mine Tavler</Heading1>
+                <div>
+                    <CreateBoard />
+                </div>
+            </div>
             <BoardList boards={boards} />
         </div>
     )
