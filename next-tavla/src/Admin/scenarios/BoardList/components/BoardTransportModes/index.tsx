39f598e1880f5446c2ec6889737fa54261ee6d4f
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-09-19 11:38:00 +0200

    chore(code quality): removed useStopPlacesTransportModes hook

--- next-tavla/src/Admin/scenarios/BoardList/components/BoardTransportModes/index.tsx
+++ next-tavla/src/Admin/scenarios/BoardList/components/BoardTransportModes/index.tsx
@@ -1,11 +1,21 @@
-import { useStopPlacesTransportModes } from 'Admin/hooks/useStopPlacesTransportModes'
 import { TransportIcon } from 'Board/scenarios/Table/components/TransportIcon'
+import { StopPlacesTransportModesQuery } from 'graphql/index'
+import { useQuery } from 'graphql/utils'
 import { TBoard } from 'types/settings'
+import { isNotNullOrUndefined } from 'utils/typeguards'
 
 function BoardTransportModes({ board }: { board: TBoard }) {
     const stopPlaceIds = board.tiles?.map((tile) => tile.placeId) ?? []
 
-    const transportModes = useStopPlacesTransportModes(stopPlaceIds)
+    const transportModes =
+        useQuery(StopPlacesTransportModesQuery, {
+            stopPlaceIds,
+        })
+            .data?.stopPlaces.flat()
+            .map((stopPlace) => stopPlace?.transportMode)
+            .flat()
+            .filter(isNotNullOrUndefined)
+            .filter((item, index, array) => array.indexOf(item) === index) ?? []
 
     return (
         <div>
