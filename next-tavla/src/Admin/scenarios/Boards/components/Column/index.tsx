3a9195d3c0bc677d317484e9da3f30027d025bab
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-09-26 13:54:48 +0200

    chore(columns): changed from switch to object when conditionally rendering columns

--- next-tavla/src/Admin/scenarios/Boards/components/Column/index.tsx
+++ next-tavla/src/Admin/scenarios/Boards/components/Column/index.tsx
@@ -4,20 +4,16 @@ import { LastModified } from './LastModified'
 import { Link } from './Link'
 import { Name } from './Name'
 import { TBoardsColumn } from 'Admin/types/boards'
+import { ReactNode } from 'react'
 
 function Column({ board, column }: { board: TBoard; column: TBoardsColumn }) {
-    switch (column) {
-        case 'name':
-            return <Name name={board.meta?.title} />
-        case 'url':
-            return <Link bid={board.id} />
-        case 'actions':
-            return <Actions board={board} />
-        case 'lastModified':
-            return <LastModified timestamp={board.meta?.dateModified} />
-        default:
-            return <div>Ukjent kolonne</div>
+    const columnComponents: Record<TBoardsColumn, ReactNode> = {
+        name: <Name name={board.meta?.title} />,
+        url: <Link bid={board.id} />,
+        actions: <Actions board={board} />,
+        lastModified: <LastModified timestamp={board.meta?.dateModified} />,
     }
+    return <>{columnComponents[column]}</>
 }
 
 export { Column }
