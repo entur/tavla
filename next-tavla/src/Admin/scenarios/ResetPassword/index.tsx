ba22688e1d859a88ea8d0c79ff6007ea13d5a434
Øyvind Grutle <oyvind.grutle@bekk.no>
2023-09-27 15:33:23 +0200

    feat(reset-password) reset password

---
+++ next-tavla/src/Admin/scenarios/ResetPassword/index.tsx
@@ -0,0 +1,92 @@
+import { Heading3, Paragraph } from '@entur/typography'
+import { TextField } from '@entur/form'
+import { PrimaryButton } from '@entur/button'
+import { SyntheticEvent, useState } from 'react'
+import { UserError } from '../Login/components/UserError'
+import { useFirebaseAuthError } from '../Login/hooks/useFirebaseAuthError'
+import { confirmPasswordReset } from 'firebase/auth'
+import { auth } from 'utils/firebase'
+import { FirebaseError } from 'firebase/app'
+import { useToast } from '@entur/alert'
+import Link from 'next/link'
+import dynamic from 'next/dynamic'
+
+function ResetPassword({ oob }: { oob: string }) {
+    const { error, setError, getTextFieldPropsForType } = useFirebaseAuthError()
+    const { addToast } = useToast()
+    const [showForm, setShowForm] = useState(true)
+
+    const submitResetPassword = async (event: SyntheticEvent) => {
+        event.preventDefault()
+        const data = event.currentTarget as unknown as {
+            email: HTMLInputElement
+            password: HTMLInputElement
+            repeat_password: HTMLInputElement
+        }
+
+        const password = data.password.value
+        const repeatPassword = data.repeat_password.value
+
+        try {
+            if (password !== repeatPassword)
+                throw new FirebaseError(
+                    'auth/password-no-match',
+                    'passwords does not match',
+                )
+            await confirmPasswordReset(auth, oob, password)
+            data.password.value = ''
+            data.repeat_password.value = ''
+            setShowForm(false)
+        } catch (error) {
+            console.log(error)
+            if (error instanceof FirebaseError) {
+                setError(error)
+            } else {
+                addToast({
+                    title: 'Noe gikk galt',
+                    content: 'Prøv igjen senere',
+                    variant: 'info',
+                })
+            }
+        }
+    }
+
+    return (
+        <div>
+            <Heading3>Tilbakestill passord</Heading3>
+            {showForm ? (
+                <form className="flexColumn" onSubmit={submitResetPassword}>
+                    <TextField
+                        name="password"
+                        label="Nytt passord"
+                        type="password"
+                        {...getTextFieldPropsForType('password')}
+                    />
+                    <TextField
+                        name="repeat_password"
+                        label="Gjenta passord"
+                        type="password"
+                        {...getTextFieldPropsForType('repeat_password')}
+                    />
+                    <UserError error={error} />
+                    <PrimaryButton type="submit">
+                        Tilbakestill passord
+                    </PrimaryButton>
+                </form>
+            ) : (
+                <>
+                    <Paragraph>Passord ble tilbakestilt</Paragraph>
+                    <PrimaryButton as={Link} href="/#login">
+                        Logg inn
+                    </PrimaryButton>
+                </>
+            )}
+        </div>
+    )
+}
+
+const NonSSRAdmin = dynamic(() => Promise.resolve(ResetPassword), {
+    ssr: false,
+})
+
+export { NonSSRAdmin as ResetPassword }
