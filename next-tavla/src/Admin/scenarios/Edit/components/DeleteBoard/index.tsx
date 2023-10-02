3fd9479cc38327f39a441c35ede03346057ae889
Øyvind Grutle <oyvind.grutle@bekk.no>
2023-10-02 12:12:05 +0200

    feat(delete) useRoute to redirect

--- next-tavla/src/Admin/scenarios/Edit/components/DeleteBoard/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/DeleteBoard/index.tsx
@@ -5,11 +5,33 @@ import { Modal } from '@entur/modal'
 import { Paragraph } from '@entur/typography'
 import { PrimaryButton, SecondaryButton } from '@entur/button'
 import { DeleteIcon } from '@entur/icons'
-import Link from 'next/link'
+import { deleteBoard } from 'Admin/scenarios/Boards/utils/delete'
+import { TavlaError } from 'Admin/types/error'
+import { useToast } from '@entur/alert'
+import { useRouter } from 'next/router'
 
 function DeleteBoard({ bid }: { bid?: TBoardID }) {
     const [isOpen, openModal, closeModal] = useToggle()
+    const { addToast } = useToast()
+    const router = useRouter()
 
+    const removeBoard = async () => {
+        try {
+            if (!bid)
+                throw new TavlaError({
+                    code: 'NOT_FOUND',
+                    message: 'Board not found',
+                })
+            await deleteBoard(bid)
+            router.push('/edit/boards')
+        } catch (error) {
+            addToast({
+                title: 'Noe gikk galt',
+                content: 'Kunne ikke slette tavle',
+                variant: 'info',
+            })
+        }
+    }
     return (
         <>
             <PrimaryButton onClick={openModal}>
@@ -30,7 +52,7 @@ function DeleteBoard({ bid }: { bid?: TBoardID }) {
                     <SecondaryButton onClick={closeModal}>
                         Avbryt
                     </SecondaryButton>
-                    <PrimaryButton as={Link} href={`/api/delete/${bid}`}>
+                    <PrimaryButton onClick={removeBoard}>
                         Ja, slett
                     </PrimaryButton>
                 </div>
