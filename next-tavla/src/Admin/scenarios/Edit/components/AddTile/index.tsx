7bb2e46e6c314156222780167d44cc64e4aa44f6
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-10-06 11:12:12 +0200

    feat(edit): wip county select on stop place search

--- next-tavla/src/Admin/scenarios/Edit/components/AddTile/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/AddTile/index.tsx
@@ -1,60 +1,48 @@
-import { useState } from 'react'
 import { Button } from '@entur/button'
-import classes from './styles.module.css'
-import { NormalizedDropdownItemType, SearchableDropdown } from '@entur/dropdown'
-import { fetchItems } from 'Admin/utils/fetch'
+import { MultiSelect, SearchableDropdown } from '@entur/dropdown'
 import { SearchIcon } from '@entur/icons'
 import { Heading1 } from '@entur/typography'
-import { useToast } from '@entur/alert'
-import { useEditSettingsDispatch } from '../../utils/contexts'
-import { useDebouncedFetch } from 'hooks/useDebouncedFetch'
+import { useStopPlaceSearch } from './hooks/useStopPlaceSearch'
+import { useCountiesSearch } from './hooks/useCountiesSearch'
 
 function AddTile() {
-    const dispatch = useEditSettingsDispatch()
-    const { addToast } = useToast()
-    const [selectedDropdownItem, setSelectedDropdownItem] =
-        useState<NormalizedDropdownItemType | null>(null)
+    const { counties, selectedCounties, setSelectedCounties } =
+        useCountiesSearch()
 
-    const debouncedFetch = useDebouncedFetch(500, fetchItems)
-
-    function handleAddTile() {
-        if (!selectedDropdownItem?.value) {
-            addToast({
-                title: 'Ingen holdeplass er valgt',
-                content: 'Vennligst velg en holdeplass å legge til',
-                variant: 'info',
-            })
-            return
-        }
-        dispatch({
-            type: 'addTile',
-            tile: {
-                type: 'stop_place',
-                placeId: selectedDropdownItem.value,
-                name:
-                    selectedDropdownItem.label.split(',')[0] ?? 'Ikke navngitt',
-            },
-        })
-        setSelectedDropdownItem(null)
-    }
+    const {
+        stopPlaceItems,
+        selectedStopPlace,
+        setSelectedStopPlace,
+        handleAddTile,
+    } = useStopPlaceSearch()
 
     return (
         <div>
             <Heading1>Holdeplasser i tavla</Heading1>
 
-            <div className={classes.SearchContainer}>
+            <div className="flexRow alignCenter g-1 pb-2">
+                <MultiSelect
+                    label="Velg fylker"
+                    items={counties}
+                    selectedItems={selectedCounties}
+                    onChange={setSelectedCounties}
+                    className="w-30"
+                    maxChips={3}
+                    hideSelectAll
+                />
+
                 <SearchableDropdown
-                    className={classes.DropDown}
-                    items={debouncedFetch}
+                    items={stopPlaceItems}
                     label="Søk etter holdeplass..."
                     clearable
-                    prepend={<SearchIcon aria-hidden="true" />}
-                    selectedItem={selectedDropdownItem}
-                    onChange={setSelectedDropdownItem}
+                    prepend={<SearchIcon />}
+                    selectedItem={selectedStopPlace}
+                    onChange={setSelectedStopPlace}
+                    className="w-30"
                 />
 
                 <Button variant="primary" onClick={handleAddTile}>
-                    Legg til
+                    Legg til ny holdeplass
                 </Button>
             </div>
         </div>
