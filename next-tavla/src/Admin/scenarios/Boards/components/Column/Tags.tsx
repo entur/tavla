4cd031eb6c22326faf24408f4eb19201e99cbaa5
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-10-10 10:31:57 +0200

    feat(tags): only showing three tags per column

--- next-tavla/src/Admin/scenarios/Boards/components/Column/Tags.tsx
+++ next-tavla/src/Admin/scenarios/Boards/components/Column/Tags.tsx
@@ -4,15 +4,25 @@ import { SortableColumn } from './SortableColumn'
 import { Badge } from '@entur/layout'
 import { colorsFromHash } from '../../utils/colorsFromHash'
 import { TagModal } from '../TagModal'
+import { useBoardsSettings } from '../../utils/context'
+import { sortArrayByOverlap } from '../../utils/sortArrayByOverlap'
 
 function Tags({ board }: { board: TBoard }) {
-    const tags = board.meta?.tags ?? []
+    let tags = (board.meta?.tags ?? []).sort()
+    const { filterTags } = useBoardsSettings()
+
+    const displayNumber = 3
+    const hiddenNumber = tags.length - displayNumber
+
+    if (filterTags.length) {
+        tags = sortArrayByOverlap(tags, filterTags)
+    }
 
     return (
         <SortableColumn column="tags">
             <div className={classes.tags}>
                 <TagModal board={board} />
-                {tags.sort().map((tag) => (
+                {tags.slice(0, displayNumber).map((tag) => (
                     <Badge
                         key={tag}
                         aria-label={tag}
@@ -25,6 +35,9 @@ function Tags({ board }: { board: TBoard }) {
                         {tag}
                     </Badge>
                 ))}
+                {hiddenNumber && (
+                    <Badge variant="neutral">{`+ ${hiddenNumber} til`}</Badge>
+                )}
             </div>
         </SortableColumn>
     )
