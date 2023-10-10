160957c63aece0a733f9a39c3a9d5cbf9b8bf7b6
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-10-10 15:20:32 +0200

    feat(tags): added tooltip with hidden tags

--- next-tavla/src/Admin/scenarios/Boards/components/Column/Tags.tsx
+++ next-tavla/src/Admin/scenarios/Boards/components/Column/Tags.tsx
@@ -6,23 +6,21 @@ import { colorsFromHash } from '../../utils/colorsFromHash'
 import { TagModal } from '../TagModal'
 import { useBoardsSettings } from '../../utils/context'
 import { sortArrayByOverlap } from '../../utils/sortArrayByOverlap'
+import { Tooltip } from '@entur/tooltip'
+import { TTag } from 'types/meta'
+import { ReactElement } from 'react'
 
-function Tags({ board }: { board: TBoard }) {
-    let tags = (board.meta?.tags ?? []).sort()
-    const { filterTags } = useBoardsSettings()
-
-    const displayNumber = 3
-    const hiddenNumber = tags.length - displayNumber
-
-    if (filterTags.length) {
-        tags = sortArrayByOverlap(tags, filterTags)
-    }
-
+function TagList({
+    tags,
+    tagsTooltip,
+}: {
+    tags: TTag[]
+    tagsTooltip?: ReactElement
+}) {
     return (
-        <SortableColumn column="tags">
+        <div>
             <div className={classes.tags}>
-                <TagModal board={board} />
-                {tags.slice(0, displayNumber).map((tag) => (
+                {tags.map((tag) => (
                     <Badge
                         key={tag}
                         aria-label={tag}
@@ -35,9 +33,46 @@ function Tags({ board }: { board: TBoard }) {
                         {tag}
                     </Badge>
                 ))}
-                {hiddenNumber && (
-                    <Badge variant="neutral">{`+ ${hiddenNumber} til`}</Badge>
-                )}
+                {tagsTooltip}
+            </div>
+        </div>
+    )
+}
+
+function Tags({ board }: { board: TBoard }) {
+    let tags = (board.meta?.tags ?? []).sort()
+    const { filterTags } = useBoardsSettings()
+
+    const displayNumber = 3
+    const hiddenNumber = tags.length - displayNumber
+
+    if (filterTags.length) {
+        tags = sortArrayByOverlap(tags, filterTags)
+    }
+
+    const hiddenTags = hiddenNumber ? (
+        <Tooltip
+            placement={'bottom'}
+            content={<TagList tags={tags.slice(displayNumber)} />}
+        >
+            <Badge
+                style={{ cursor: 'help' }}
+                variant="neutral"
+            >{`+ ${hiddenNumber} til`}</Badge>
+        </Tooltip>
+    ) : (
+        <></>
+    )
+    return (
+        <SortableColumn column="tags">
+            <TagModal board={board} />
+            <div className={classes.tags}>
+                {
+                    <TagList
+                        tags={tags.slice(0, displayNumber)}
+                        tagsTooltip={hiddenTags}
+                    />
+                }
             </div>
         </SortableColumn>
     )
