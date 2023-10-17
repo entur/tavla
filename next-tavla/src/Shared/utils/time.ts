e8a838fc14713eb1d8bb4e9d05c01c36d4eab871
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-10-17 07:26:04 +0200

    feat(time): show date if departure is not today

--- next-tavla/src/Shared/utils/time.ts
+++ next-tavla/src/Shared/utils/time.ts
@@ -1,16 +1,23 @@
 export function getRelativeTimeString(dateString: string) {
     const timeDiff = Date.parse(dateString) - Date.now()
+
     if (timeDiff < 60_000) return 'Nå'
     else if (timeDiff < 600_000) return Math.floor(timeDiff / 60_000) + ' min'
-    else return formatDateString(dateString)
+    else if (!isDateStringToday(dateString))
+        return formatDateString(dateString, true)
+    return formatDateString(dateString)
 }
 
-export function formatDateString(dateString: string) {
+export function formatDateString(dateString: string, date = false) {
     return Intl.DateTimeFormat('no-NB', {
         hour12: false,
+        day: date ? '2-digit' : undefined,
+        month: date ? 'short' : undefined,
         hour: '2-digit',
         minute: '2-digit',
-    }).format(Date.parse(dateString))
+    })
+        .format(Date.parse(dateString))
+        .replace(',', '')
 }
 
 export function formatTimeStamp(timestamp: number) {
@@ -31,4 +38,16 @@ export function formatDate(date: Date) {
             minute: '2-digit',
         })
         .replace(',', '')
+}
+
+export function isTimestampToday(timestamp: number) {
+    const today = new Date().setHours(0, 0, 0, 0)
+    const timestampDay = new Date(timestamp).setHours(0, 0, 0, 0)
+    return today === timestampDay
+}
+
+export function isDateStringToday(dateString: string) {
+    const today = new Date().setHours(0, 0, 0, 0)
+    const timestampDay = new Date(Date.parse(dateString)).setHours(0, 0, 0, 0)
+    return today === timestampDay
 }
