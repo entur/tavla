923248a0c8bbb93a057aecb3d90c12c443082e82
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-10-17 11:49:36 +0200

    fix(edit): update autosave

--- next-tavla/src/Admin/scenarios/Edit/components/AutoSave/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/AutoSave/index.tsx
@@ -1,9 +1,12 @@
 import { TBoard } from 'types/settings'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { formatTimestamp } from 'Admin/utils/time'
 
 function AutoSave({ board }: { board: TBoard }) {
-    const [status, setStatus] = useState('')
+    const [status, setStatus] = useState(
+        `Sist lagret: ${formatTimestamp(board.meta?.dateModified, true)}`,
+    )
+    const prevBoardRef = useRef<TBoard | null>(null)
 
     const setBoard = (board: TBoard) =>
         fetch('/api/board', {
@@ -12,8 +15,14 @@ function AutoSave({ board }: { board: TBoard }) {
         })
 
     useEffect(() => {
-        setBoard(board)
-        setStatus(`Sist lagret: ${formatTimestamp(Date.now(), true)}`)
+        if (
+            prevBoardRef.current !== null &&
+            JSON.stringify(prevBoardRef.current) !== JSON.stringify(board)
+        ) {
+            setBoard(board)
+            setStatus(`Sist lagret: ${formatTimestamp(Date.now(), true)}`)
+        }
+        prevBoardRef.current = board
     }, [board])
 
     return <div>{status}</div>
