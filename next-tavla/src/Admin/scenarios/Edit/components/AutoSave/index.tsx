d486eb545bfd703eab7d4d583ddb7763659b16e1
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-10-17 12:51:34 +0200

    fix(edit): check initial load before autosaving

--- next-tavla/src/Admin/scenarios/Edit/components/AutoSave/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/AutoSave/index.tsx
@@ -1,12 +1,12 @@
 import { TBoard } from 'types/settings'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { formatTimestamp } from 'Admin/utils/time'
 
 function AutoSave({ board }: { board: TBoard }) {
     const [status, setStatus] = useState(
         `Sist lagret: ${formatTimestamp(board.meta?.dateModified, true)}`,
     )
-    const prevBoardRef = useRef<TBoard | null>(null)
+    const [initialLoad, setInitialLoad] = useState(true)
 
     const setBoard = (board: TBoard) =>
         fetch('/api/board', {
@@ -15,15 +15,10 @@ function AutoSave({ board }: { board: TBoard }) {
         })
 
     useEffect(() => {
-        if (
-            prevBoardRef.current !== null &&
-            JSON.stringify(prevBoardRef.current) !== JSON.stringify(board)
-        ) {
-            setBoard(board)
-            setStatus(`Sist lagret: ${formatTimestamp(Date.now(), true)}`)
-        }
-        prevBoardRef.current = board
-    }, [board])
+        if (initialLoad) return () => setInitialLoad(false)
+        setBoard(board)
+        setStatus(`Sist lagret: ${formatTimestamp(Date.now(), true)}`)
+    }, [initialLoad, board])
 
     return <div>{status}</div>
 }
