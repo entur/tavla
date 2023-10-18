3386416bdd8ba5c2c7d93b6e97e6fbdbde8eac28
lindtvedtsebastian <46536611+lindtvedtsebastian@users.noreply.github.com>
2023-10-18 15:36:11 +0200

    fix(edit): fix crash in edit board (#1282)

--- next-tavla/src/Admin/scenarios/Edit/components/DeleteBoard/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/DeleteBoard/index.tsx
@@ -1,22 +1,34 @@
 import { useToggle } from 'hooks/useToggle'
 import { PrimaryButton } from '@entur/button'
 import { DeleteIcon } from '@entur/icons'
+import { TavlaError } from 'Admin/types/error'
+import { useToast } from '@entur/alert'
 import { useRouter } from 'next/router'
 import { DeleteModal } from 'Admin/components/DeleteModal'
-import { TBoard } from 'types/settings'
-import { useOptimisticDeleteBoard } from 'Admin/scenarios/Boards/utils/context'
+import { TBoard, TBoardID } from 'types/settings'
+import { deleteBoardRequest } from '../../utils/delete'
 
 function DeleteBoard({ board }: { board: TBoard }) {
-    const deleteDispatch = useOptimisticDeleteBoard()
-
     const [showModal, openModal, closeModal] = useToggle()
+    const { addToast } = useToast()
     const router = useRouter()
-
     const removeBoard = async () => {
-        await deleteDispatch(board)
-        router.push('/edit/boards')
+        try {
+            if (!board.id)
+                throw new TavlaError({
+                    code: 'NOT_FOUND',
+                    message: 'Board not found',
+                })
+            await deleteBoardRequest(board.id as TBoardID)
+            router.push('/edit/boards')
+        } catch (error) {
+            addToast({
+                title: 'Noe gikk galt',
+                content: 'Kunne ikke slette tavle',
+                variant: 'info',
+            })
+        }
     }
-
     return (
         <>
             <PrimaryButton onClick={openModal}>
