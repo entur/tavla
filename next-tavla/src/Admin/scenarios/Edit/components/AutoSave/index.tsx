42f7a46922235d8876f1e32a5b4bed8601935004
Natasha Tikhonova <64562118+natashatikhonova@users.noreply.github.com>
2023-10-18 08:24:31 +0200

    feat(save): useRef to keep track of initial load(#1279)
    * fix(edit): update autosave
    
    * fix(edit): check initial load before autosaving
    
    * feat(save): useRef to keep track of initial load
    
    ---------
    
    Co-authored-by: lindtvedtsebastian <lindtvedt.sebastian@gmail.com>

--- next-tavla/src/Admin/scenarios/Edit/components/AutoSave/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/AutoSave/index.tsx
@@ -1,9 +1,12 @@
 import { TBoard } from 'types/settings'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { formatTimestamp } from 'Admin/utils/time'
 
 function AutoSave({ board }: { board: TBoard }) {
-    const [status, setStatus] = useState('')
+    const [status, setStatus] = useState(
+        `Sist lagret: ${formatTimestamp(board.meta?.dateModified, true)}`,
+    )
+    const initialLoad = useRef(true)
 
     const setBoard = (board: TBoard) =>
         fetch('/api/board', {
@@ -12,9 +15,13 @@ function AutoSave({ board }: { board: TBoard }) {
         })
 
     useEffect(() => {
+        if (initialLoad.current) {
+            initialLoad.current = false
+            return
+        }
         setBoard(board)
         setStatus(`Sist lagret: ${formatTimestamp(Date.now(), true)}`)
-    }, [board])
+    }, [initialLoad, board])
 
     return <div>{status}</div>
 }
