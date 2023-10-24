1fec65175718edd823ec78346e1639d386f6f3f2
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-10-24 10:41:09 +0200

    chore(styling): class renaming, removing quarter values

--- next-tavla/src/Shared/components/Footer/index.tsx
+++ next-tavla/src/Shared/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import EnturLogo from 'assets/logos/entur/Enturlogo_White.svg'
 
 function Footer() {
     return (
-        <footer className="flexRow justifyStart alignCenter text-xs">
+        <footer className="flexRow justifyStart alignCenter text-rem-3">
             <span>Tjenesten leveres av</span>
             <Image src={EnturLogo} alt="Entur logo" height={70} />
         </footer>
