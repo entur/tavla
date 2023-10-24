df18e6980dc5740ff0bc4964d3b6266f734094f4
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-10-24 09:55:40 +0200

    feat(styling): global font sizes in ems and rems

--- next-tavla/src/Shared/components/Footer/index.tsx
+++ next-tavla/src/Shared/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import EnturLogo from 'assets/logos/entur/Enturlogo_White.svg'
 
 function Footer() {
     return (
-        <footer className="flexRow justifyStart alignCenter text-xl">
+        <footer className="flexRow justifyStart alignCenter text-xs">
             <span>Tjenesten leveres av</span>
             <Image src={EnturLogo} alt="Entur logo" height={70} />
         </footer>
