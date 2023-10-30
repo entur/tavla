62b18f1a2f41438403e35263ad617c6be0436c2e
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-10-30 19:25:40 +0100

    feat(org. user invitation): addded form feedback hook

---
+++ next-tavla/src/Shared/hooks/useFormFeedback.ts
@@ -0,0 +1,47 @@
+import { VariantType } from '@entur/form'
+import { useState } from 'react'
+
+type FormValidationError = 'invite/user-not-found' | 'invite/already-invited'
+type FromValidationSuccess = 'invite/success'
+
+type TFormValidationCode = FormValidationError | FromValidationSuccess
+
+type FormValidationFeedback = {
+    type: VariantType
+    message: string
+} | null
+
+export function useFormFeedback() {
+    const [feedback, setFeedback] = useState<FormValidationFeedback>(null)
+
+    const setFeedbackMessage = (type: TFormValidationCode) => {
+        switch (type) {
+            case 'invite/user-not-found':
+                return setFeedback({
+                    type: 'error',
+                    message: 'Fant ingen bruker med denne e-postadressen.',
+                })
+            case 'invite/already-invited':
+                return setFeedback({
+                    type: 'error',
+                    message: 'Brukeren er allerede medlem av organisasjonen',
+                })
+            case 'invite/success':
+                return setFeedback({
+                    type: 'success',
+                    message: 'Bruker lagt til!',
+                })
+            default:
+                return setFeedback({
+                    type: 'error',
+                    message: 'Noe gikk galt.',
+                })
+        }
+    }
+
+    const clearFeedback = () => {
+        setFeedback(null)
+    }
+
+    return { feedback, setFeedback: setFeedbackMessage, clearFeedback }
+}
