cb23e6e66d09d8dfa470773daa19dd013edb56cb
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-10-30 10:24:45 +0100

    feat(utils): added regex email validator

---
+++ next-tavla/src/Shared/utils/regex.ts
@@ -0,0 +1,5 @@
+export function validEmail(email: string) {
+    const re =
+        /([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)+/
+    return re.test(email)
+}
