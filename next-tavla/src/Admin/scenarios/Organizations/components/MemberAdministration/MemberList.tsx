8968b379fda61558ab0c3e9c16bab1af3963973d
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-11-02 16:10:23 +0100

    feat(styling): added styling to member list rows

--- next-tavla/src/Admin/scenarios/Organizations/components/MemberAdministration/MemberList.tsx
+++ next-tavla/src/Admin/scenarios/Organizations/components/MemberAdministration/MemberList.tsx
@@ -3,6 +3,7 @@ import { ExpandablePanel } from '@entur/expand'
 import { DeleteIcon } from '@entur/icons'
 import { fetchRemoveUserFromOrganization } from 'Admin/utils/fetch'
 import { TOrganizationID, TUser } from 'types/settings'
+import classes from './styles.module.css'
 
 function MemberList({
     oid,
@@ -17,7 +18,7 @@ function MemberList({
         <ExpandablePanel title="Medlemmer" defaultOpen={members.length < 10}>
             <div className="flexColumn g-1">
                 {members.map((member) => (
-                    <div className="flexRow justifyBetween" key={member.uid}>
+                    <div className={classes.memberListRow} key={member.uid}>
                         <div>{member.email}</div>
                         <IconButton
                             aria-label="Fjern bruker"
