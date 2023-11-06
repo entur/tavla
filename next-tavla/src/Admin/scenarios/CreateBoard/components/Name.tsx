1bcd1d292cd75c04295b14252d5fcdbf6edbe04c
Øyvind Grutle <oyvind.grutle@bekk.no>
2023-11-06 13:11:43 +0100

    chore(create) simplify set name

--- next-tavla/src/Admin/scenarios/CreateBoard/components/Name.tsx
+++ next-tavla/src/Admin/scenarios/CreateBoard/components/Name.tsx
@@ -2,10 +2,8 @@ import { PrimaryButton } from '@entur/button'
 import { TextField } from '@entur/form'
 import { Heading3, Paragraph } from '@entur/typography'
 import { TCreatePage } from 'Admin/types/createBoard'
-import { SyntheticEvent } from 'react'
 import { TBoard } from 'types/settings'
 import { useCreateBoardDispatch } from '../utils/context'
-import { useToast } from '@entur/alert'
 import { selectInput } from 'Admin/utils/selectInput'
 function Name({
     board,
@@ -14,31 +12,7 @@ function Name({
     board: TBoard
     pushPage: (page: TCreatePage) => void
 }) {
-    const { addToast } = useToast()
     const dispatch = useCreateBoardDispatch()
-
-    const handleSetName = (event: SyntheticEvent) => {
-        event.preventDefault()
-
-        const data = event.currentTarget as unknown as {
-            name: HTMLInputElement
-        }
-        const name = data.name.value
-
-        if (!name) {
-            return addToast({
-                title: 'Ingen navn er satt',
-                content: 'Vennligst sett et navn på tavla',
-                variant: 'info',
-            })
-        }
-
-        dispatch({
-            type: 'setTitle',
-            title: name,
-        })
-        pushPage('addStops')
-    }
 
     return (
         <div className="flexColumn g-2 h-100">
@@ -47,17 +21,25 @@ function Name({
                 Gi tavla et navn slik at det blir enklere å finne den igjen
                 senere.
             </Paragraph>
-            <form onSubmit={handleSetName} className="flexColumn g-2">
-                <TextField
-                    name="name"
-                    label="Navn på tavla"
-                    placeholder="Navn på tavla"
-                    value={board?.meta?.title}
-                    aria-label="Sett navn på tavla"
-                    ref={selectInput}
-                />
-                <PrimaryButton type="submit">Neste</PrimaryButton>
-            </form>
+
+            <TextField
+                name="name"
+                label="Navn på tavla"
+                placeholder="Navn på tavla"
+                value={board?.meta?.title}
+                aria-label="Sett navn på tavla"
+                ref={selectInput}
+                onChange={(e) => {
+                    dispatch({ type: 'setTitle', title: e.target.value })
+                }}
+            />
+            <PrimaryButton
+                onClick={() => {
+                    pushPage('addStops')
+                }}
+            >
+                Neste
+            </PrimaryButton>
         </div>
     )
 }
