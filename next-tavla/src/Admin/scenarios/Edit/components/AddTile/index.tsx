71c372d347cadf6b7064847ea45427dc32449368
Øyvind Grutle <oyvindgru@gmail.com>
2023-11-07 08:23:25 +0100

    styling(create): flexColumn for add stops in modal (#1301)
    * styling(create) flexColumn for add stops in modal
    
    * chore(create) remove nested div

--- next-tavla/src/Admin/scenarios/Edit/components/AddTile/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/AddTile/index.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@entur/button'
 import { Dropdown, MultiSelect, SearchableDropdown } from '@entur/dropdown'
 import { SearchIcon } from '@entur/icons'
-import { Heading1 } from '@entur/typography'
 import { useStopPlaceSearch } from '../../hooks/useStopPlaceSearch'
 import { useCountiesSearch } from '../../hooks/useCountiesSearch'
 import { useQuaySearch } from '../../hooks/useQuaySearch'
@@ -9,12 +8,14 @@ import { useToast } from '@entur/alert'
 
 function AddTile({
     addTile,
+    flexDirection = 'flexRow',
 }: {
     addTile: (
         name: string,
         placeId: string,
         type: 'quay' | 'stop_place',
     ) => void
+    flexDirection?: 'flexRow' | 'flexColumn'
 }) {
     const { addToast } = useToast()
 
@@ -52,37 +53,34 @@ function AddTile({
     }
 
     return (
-        <div>
-            <Heading1>Holdeplasser i tavla</Heading1>
-            <div className="flexRow g-2 pt-2 pb-2">
-                <MultiSelect
-                    label="Velg fylker"
-                    items={counties}
-                    selectedItems={selectedCounties}
-                    onChange={setSelectedCounties}
-                    prepend={<SearchIcon />}
-                    maxChips={2}
-                    hideSelectAll
-                />
-                <SearchableDropdown
-                    items={stopPlaceItems}
-                    label="Søk etter holdeplass..."
-                    clearable
-                    prepend={<SearchIcon />}
-                    selectedItem={selectedStopPlace}
-                    onChange={setSelectedStopPlace}
-                />
-                <Dropdown
-                    items={quays}
-                    label="Velg plattform/retning"
-                    clearable
-                    selectedItem={selectedQuay}
-                    onChange={setSelectedQuay}
-                />
-                <Button variant="primary" onClick={handleAddTile}>
-                    Legg til
-                </Button>
-            </div>
+        <div className={`${flexDirection} g-2 pt-2 pb-2`}>
+            <MultiSelect
+                label="Velg fylker"
+                items={counties}
+                selectedItems={selectedCounties}
+                onChange={setSelectedCounties}
+                prepend={<SearchIcon />}
+                maxChips={2}
+                hideSelectAll
+            />
+            <SearchableDropdown
+                items={stopPlaceItems}
+                label="Søk etter holdeplass..."
+                clearable
+                prepend={<SearchIcon />}
+                selectedItem={selectedStopPlace}
+                onChange={setSelectedStopPlace}
+            />
+            <Dropdown
+                items={quays}
+                label="Velg plattform/retning"
+                clearable
+                selectedItem={selectedQuay}
+                onChange={setSelectedQuay}
+            />
+            <Button variant="primary" onClick={handleAddTile}>
+                Legg til
+            </Button>
         </div>
     )
 }
