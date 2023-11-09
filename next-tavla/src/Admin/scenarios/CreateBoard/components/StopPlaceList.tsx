0ac100e1340048df2d00d2f1d74107aa974315ac
Øyvind Grutle <oyvind.grutle@bekk.no>
2023-11-09 09:41:08 +0100

    chore(create): remove unnecessary divs

--- next-tavla/src/Admin/scenarios/CreateBoard/components/StopPlaceList.tsx
+++ next-tavla/src/Admin/scenarios/CreateBoard/components/StopPlaceList.tsx
@@ -5,21 +5,17 @@ import { StopPlaceChip } from './StopPlaceChip'
 function StopPlaceList({ board }: { board: TBoard }) {
     if (board.tiles.length === 0) {
         return (
-            <div className="flexColumn">
-                <Paragraph>
-                    Du har ikke lagt til noen stoppesteder i tavla enda.
-                </Paragraph>
-            </div>
+            <Paragraph>
+                Du har ikke lagt til noen stoppesteder i tavla enda.
+            </Paragraph>
         )
     }
 
     return (
-        <div className="flexColumn g-2 pt-2 pb-2">
-            <div className="flexRow g-2">
-                {board.tiles.map((tile) => (
-                    <StopPlaceChip tile={tile} key={tile.uuid} />
-                ))}
-            </div>
+        <div className="flexRow g-2">
+            {board.tiles.map((tile) => (
+                <StopPlaceChip tile={tile} key={tile.uuid} />
+            ))}
         </div>
     )
 }
