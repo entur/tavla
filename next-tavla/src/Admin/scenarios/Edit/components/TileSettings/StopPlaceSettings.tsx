8bb3cb842ea2ac21f8414b6581514869dab4caf3
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-11-10 11:54:21 +0100

    feat(feat): remove preview from tile settings

--- next-tavla/src/Admin/scenarios/Edit/components/TileSettings/StopPlaceSettings.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/TileSettings/StopPlaceSettings.tsx
@@ -1,4 +1,4 @@
-import { StopPlaceQuery, StopPlaceSettingsQuery } from 'graphql/index'
+import { StopPlaceSettingsQuery } from 'graphql/index'
 import { useQuery } from 'graphql/utils'
 import { fieldsNotNull } from 'utils/typeguards'
 import { TStopPlaceTile } from 'types/tile'
@@ -6,7 +6,6 @@ import React from 'react'
 import classes from './styles.module.css'
 import { Heading3 } from '@entur/typography'
 import { DeleteTile } from '../DeleteTile'
-import { Preview } from '../Preview'
 import { ToggleColumns } from '../ToggleColumns'
 import { SelectLines } from '../SelectLines'
 
@@ -18,20 +17,12 @@ function StopPlaceSettings({ tile }: { tile: TStopPlaceTile }) {
             .data?.stopPlace?.quays?.flatMap((q) => q?.lines)
             .filter(fieldsNotNull) || []
 
-    const departures = useQuery(StopPlaceQuery, {
-        stopPlaceId: tile.placeId,
-        whitelistedLines: tile.whitelistedLines,
-        whitelistedTransportModes: tile.whitelistedTransportModes,
-        numberOfDepartures: 5,
-    }).data?.stopPlace?.estimatedCalls
-
     return (
         <div className={classes.tileSettingsWrapper}>
             <div className="flexRow alignCenter g-3">
                 <DeleteTile uuid={tile.uuid} />
                 <Heading3 className="m-0">{tile.name}</Heading3>
             </div>
-            <Preview tile={tile} departures={departures} />
             <ToggleColumns tile={tile} />
             <SelectLines tile={tile} lines={lines} />
         </div>
