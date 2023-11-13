fec86fe7f2c9a48dfa1d8263a9ef75e4d09b8932
lindtvedtsebastian <46536611+lindtvedtsebastian@users.noreply.github.com>
2023-11-13 13:19:00 +0100

    chore(next): add extra remotePatterns (#1312)

--- next-tavla/next.config.js
+++ next-tavla/next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'storage.googleapis.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+            },
         ],
     },
 }
