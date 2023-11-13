3537beafed20cf3a33610669b3eeca9c05a02e19
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-11-13 09:31:31 +0100

    feat(edit): update styling for board settings

--- next-tavla/src/Admin/scenarios/Edit/components/BoardTitle/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/BoardTitle/index.tsx
@@ -3,6 +3,7 @@ import { EditIcon } from '@entur/icons'
 import { ChangeEventHandler, useCallback } from 'react'
 import { useEditSettingsDispatch } from '../../utils/contexts'
 import { selectInput } from 'Admin/utils/selectInput'
+import { Heading4 } from '@entur/typography'
 
 function BoardTitle({ title }: { title?: string }) {
     const dispatch = useEditSettingsDispatch()
@@ -18,16 +19,19 @@ function BoardTitle({ title }: { title?: string }) {
     )
 
     return (
-        <TextField
-            value={title ?? ''}
-            className="w-30"
-            label="Navn på tavla"
-            placeholder="Navn på tavla"
-            aria-label="Endre navn på tavla"
-            prepend={<EditIcon aria-hidden="true" />}
-            onChange={dispatchTitle}
-            ref={selectInput}
-        />
+        <div className="w-100">
+            <Heading4 className="m-0">Hva skal tavla hete?</Heading4>
+            <TextField
+                value={title ?? ''}
+                className="w-30"
+                label="Navn på tavla"
+                placeholder="Navn på tavla"
+                aria-label="Endre navn på tavla"
+                prepend={<EditIcon aria-hidden="true" />}
+                onChange={dispatchTitle}
+                ref={selectInput}
+            />
+        </div>
     )
 }
 
--- next-tavla/src/Admin/scenarios/Edit/components/FontsizeSelector/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/FontsizeSelector/index.tsx
@@ -1,29 +1,31 @@
 import { ChoiceChip, ChoiceChipGroup } from '@entur/chip'
 import { TBoard } from 'types/settings'
-import classes from './styles.module.css'
 import { useEditSettingsDispatch } from '../../utils/contexts'
 import { TFontSize } from 'types/meta'
 import { defaultFontSize } from 'Board/scenarios/Board/utils'
+import { Heading4 } from '@entur/typography'
 
 function FontsizeSelector({ board }: { board: TBoard }) {
     const dispatch = useEditSettingsDispatch()
     return (
-        <ChoiceChipGroup
-            className={classes.choiceChipGroup}
-            name="city"
-            label="Velg tekststørrelse:"
-            value={board.meta?.fontSize || defaultFontSize(board)}
-            onChange={(e) => {
-                dispatch({
-                    type: 'changeFontSize',
-                    fontSize: e.target.value as TFontSize,
-                })
-            }}
-        >
-            <ChoiceChip value="small">Liten</ChoiceChip>
-            <ChoiceChip value="medium">Medium</ChoiceChip>
-            <ChoiceChip value="large">Stor</ChoiceChip>
-        </ChoiceChipGroup>
+        <div className="flexColumn g-1">
+            <Heading4 className="m-0">Velg tekststørrelse: </Heading4>
+            <ChoiceChipGroup
+                className="flexRow"
+                name="city"
+                value={board.meta?.fontSize || defaultFontSize(board)}
+                onChange={(e) => {
+                    dispatch({
+                        type: 'changeFontSize',
+                        fontSize: e.target.value as TFontSize,
+                    })
+                }}
+            >
+                <ChoiceChip value="small">Liten</ChoiceChip>
+                <ChoiceChip value="medium">Medium</ChoiceChip>
+                <ChoiceChip value="large">Stor</ChoiceChip>
+            </ChoiceChipGroup>
+        </div>
     )
 }
 export { FontsizeSelector }
