6d510bdd9e930d89630607bc43a8cdc394d3ac01
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-11-16 15:20:28 +0100

    feat(time): more accurately display timestamps that occurred yesterday

--- next-tavla/src/Admin/utils/time.ts
+++ next-tavla/src/Admin/utils/time.ts
@@ -1,20 +1,28 @@
 export function formatTimestamp(timestamp?: number, seconds = false) {
     if (!timestamp) return '-'
 
-    const timeDiff = Date.now() - timestamp
+    const shortFormat = Intl.DateTimeFormat('no-NB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: seconds ? '2-digit' : undefined,
+    })
 
-    if (timeDiff < 86_400_000)
-        return Intl.DateTimeFormat('no-NB', {
-            hour: '2-digit',
-            minute: '2-digit',
-            second: seconds ? '2-digit' : undefined,
-        }).format(timestamp)
-
-    return Intl.DateTimeFormat('no-NB', {
+    const fullFormat = Intl.DateTimeFormat('no-NB', {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric',
         hour: '2-digit',
         minute: '2-digit',
-    }).format(timestamp)
+    })
+
+    const dayThen = new Date(timestamp).toDateString()
+    const dayNow = new Date().toDateString()
+    const dayYesterday = new Date(Date.now() - 86_400_000).toDateString()
+
+    if (dayThen === dayNow) return shortFormat.format(timestamp)
+
+    if (dayThen === dayYesterday)
+        return 'i går, ' + shortFormat.format(timestamp)
+
+    return fullFormat.format(timestamp)
 }
