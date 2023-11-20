a0b3c9ddf6a41853bec12a712eb4ee034003a7ad
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-11-20 13:33:33 +0100

    feat(time strings): added optional timestamp formatting

--- next-tavla/src/Admin/scenarios/Edit/components/SaveStatus/index.tsx
+++ next-tavla/src/Admin/scenarios/Edit/components/SaveStatus/index.tsx
@@ -2,7 +2,11 @@ import { TBoard } from 'types/settings'
 import { formatTimestamp } from 'Admin/utils/time'
 
 function SaveStatus({ board }: { board: TBoard }) {
-    return <div>Sist lagret: {formatTimestamp(board.meta?.dateModified)}</div>
+    return (
+        <div>
+            Sist lagret: {formatTimestamp(board.meta?.dateModified, true)}
+        </div>
+    )
 }
 
 export { SaveStatus }
--- next-tavla/src/Admin/utils/time.ts
+++ next-tavla/src/Admin/utils/time.ts
@@ -5,10 +5,27 @@ const DAY = 24 * HOUR
 const MONTH = 30 * DAY
 const YEAR = 365 * DAY
 
-export function formatTimestamp(timestamp?: number) {
-    if (!timestamp) return '-'
+const shortFormat = Intl.DateTimeFormat('no-NB', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+}).format
 
-    const timeAgo = Date.now() - timestamp
+const fullFormat = Intl.DateTimeFormat('no-NB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+}).format
+
+export function formatTimestamp(millis?: number, asTimestamp = false) {
+    if (!millis) return '-'
+
+    const timeAgo = Date.now() - millis
+
+    if (asTimestamp)
+        return timeAgo < DAY ? shortFormat(millis) : fullFormat(millis)
 
     if (timeAgo > YEAR) return getTimeSince(timeAgo, YEAR)
     if (timeAgo > MONTH) return getTimeSince(timeAgo, MONTH)
