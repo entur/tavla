6fc751fcc4a95642b60d926eae27078016297ec0
Fredrik Busklein <61384979+fredrbus@users.noreply.github.com>
2023-11-21 13:03:56 +0100

    feat(time): more accurately display timestamps that occurred yesterday (#1328)
    * feat(time): more accurately display timestamps that occurred yesterday
    
    * fix(code quality): referenced method for better code compactness and readability
    
    * feat(time strings): changed format of last_updated to 'x y ago'
    
    * feat(time strings): added optional timestamp formatting

--- next-tavla/src/Admin/utils/time.ts
+++ next-tavla/src/Admin/utils/time.ts
@@ -1,20 +1,55 @@
-export function formatTimestamp(timestamp?: number, seconds = false) {
-    if (!timestamp) return '-'
+const SECOND = 1_000
+const MINUTE = 60 * SECOND
+const HOUR = 60 * MINUTE
+const DAY = 24 * HOUR
+const MONTH = 30 * DAY
+const YEAR = 365 * DAY
 
-    const timeDiff = Date.now() - timestamp
+const shortFormat = Intl.DateTimeFormat('no-NB', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+}).format
 
-    if (timeDiff < 86_400_000)
-        return Intl.DateTimeFormat('no-NB', {
-            hour: '2-digit',
-            minute: '2-digit',
-            second: seconds ? '2-digit' : undefined,
-        }).format(timestamp)
+const fullFormat = Intl.DateTimeFormat('no-NB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+}).format
 
-    return Intl.DateTimeFormat('no-NB', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-    }).format(timestamp)
+export function formatTimestamp(millis?: number, asTimestamp = false) {
+    if (!millis) return '-'
+
+    const timeAgo = Date.now() - millis
+
+    if (asTimestamp)
+        return timeAgo < DAY ? shortFormat(millis) : fullFormat(millis)
+
+    if (timeAgo > YEAR) return getTimeSince(timeAgo, YEAR)
+    if (timeAgo > MONTH) return getTimeSince(timeAgo, MONTH)
+    if (timeAgo > DAY) return getTimeSince(timeAgo, DAY)
+    if (timeAgo > HOUR) return getTimeSince(timeAgo, HOUR)
+    if (timeAgo > MINUTE) return getTimeSince(timeAgo, MINUTE)
+    if (timeAgo > 10 * SECOND) return getTimeSince(timeAgo, SECOND)
+    return 'nettopp'
+}
+
+function getTimeSince(timeAgo: number, divisor: number) {
+    const count = Math.floor(timeAgo / divisor)
+    const plural = count !== 1
+
+    const timeText = {
+        [SECOND]: plural ? 'sekunder' : 'sekund',
+        [MINUTE]: plural ? 'minutter' : 'minutt',
+        [HOUR]: plural ? 'timer' : 'time',
+        [DAY]: plural ? 'dager' : 'dag',
+        [MONTH]: plural ? 'måneder' : 'måned',
+        [YEAR]: 'år',
+    }
+
+    return timeText[divisor]
+        ? `${count} ${timeText[divisor]} siden`
+        : 'en stund siden'
 }
