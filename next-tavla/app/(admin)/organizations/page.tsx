5d9e7e6e2288434d26f078ea03929f07a6059f38
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-11-21 11:58:12 +0100

    feat(app router): moved organizations to app router

---
+++ next-tavla/app/(admin)/organizations/page.tsx
@@ -0,0 +1,28 @@
+import classes from 'styles/pages/admin.module.css'
+import { Organizations } from 'Admin/scenarios/Organizations'
+import { getOrganizationsWithUser, verifySession } from 'Admin/utils/firebase'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { Contrast } from 'Admin/components/Contrast'
+
+async function OrganizationsPage() {
+    const session = cookies().get('session')
+    const user = await verifySession(session?.value)
+
+    if (!user) redirect('/#login')
+
+    const organizations = await getOrganizationsWithUser(user.uid)
+
+    return (
+        <div className={classes.root}>
+            <Contrast>
+                <Organizations
+                    organizations={organizations}
+                    userId={user.uid}
+                />
+            </Contrast>
+        </div>
+    )
+}
+
+export default OrganizationsPage
