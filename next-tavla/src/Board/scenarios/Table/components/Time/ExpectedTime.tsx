03bf0c28ef6c97f649eae20eebe99a91d8d9cc84
lindtvedtsebastian <46536611+lindtvedtsebastian@users.noreply.github.com>
2023-11-24 13:48:06 +0100

    feat(departure): show cancellations (#1346)

--- next-tavla/src/Board/scenarios/Table/components/Time/ExpectedTime.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Time/ExpectedTime.tsx
@@ -16,6 +16,7 @@ function ExpectedTime() {
     const time = departures.map((departure) => ({
         aimedDepartureTime: departure.aimedDepartureTime,
         expectedDepartureTime: departure.expectedDepartureTime,
+        cancelled: departure.cancellation,
         key: `${departure.serviceJourney.id}_${departure.aimedDepartureTime}`,
     }))
 
@@ -26,6 +27,7 @@ function ExpectedTime() {
                     <Time
                         expectedDepartureTime={t.expectedDepartureTime}
                         aimedDepartureTime={t.aimedDepartureTime}
+                        cancelled={t.cancelled}
                     />
                 </TableRow>
             ))}
@@ -36,14 +38,27 @@ function ExpectedTime() {
 function Time({
     expectedDepartureTime,
     aimedDepartureTime,
+    cancelled,
 }: {
     expectedDepartureTime: string
     aimedDepartureTime: string
+    cancelled: boolean
 }) {
+    if (cancelled)
+        return (
+            <>
+                <div className={classes.cancelled}>Innstilt</div>
+                <div className={classes.aimedDepartureTime}>
+                    {formatDateString(aimedDepartureTime)}
+                </div>
+            </>
+        )
+
     const timeDeviation = Math.abs(
         (Date.parse(aimedDepartureTime) - Date.parse(expectedDepartureTime)) /
             1000,
     )
+
     if (timeDeviation > 120) {
         return (
             <>
