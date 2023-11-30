2666c36648b00d48cef6922e1ab4bea19bcef061
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-11-30 09:42:22 +0100

    fix(code quality): disable button after form submission

--- next-tavla/src/Shared/components/Form/SubmitButton.tsx
+++ next-tavla/src/Shared/components/Form/SubmitButton.tsx
@@ -4,7 +4,9 @@ import { useFormStatus } from 'react-dom'
 
 function SubmitButton(props: ButtonProps<typeof Button>) {
     const { pending } = useFormStatus()
-    return <Button {...props} type="submit" loading={pending} />
+    return (
+        <Button {...props} type="submit" loading={pending} disabled={pending} />
+    )
 }
 
 export { SubmitButton }
