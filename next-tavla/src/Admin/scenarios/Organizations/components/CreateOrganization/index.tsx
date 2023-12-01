36165c47b4b31431af394115332a440e895065d7
Fredrik Busklein <fredrik.busklein@bekk.no>
2023-12-01 11:12:32 +0100

    feat(app router): migrated create organization modal

--- next-tavla/src/Admin/scenarios/Organizations/components/CreateOrganization/index.tsx
+++ next-tavla/src/Admin/scenarios/Organizations/components/CreateOrganization/index.tsx
@@ -1,44 +1,13 @@
 'use client'
-import { useToast } from '@entur/alert'
 import { PrimaryButton } from '@entur/button'
-import { TextField, VariantType } from '@entur/form'
 import { AddIcon } from '@entur/icons'
 import { Modal } from '@entur/modal'
 import { Paragraph } from '@entur/typography'
-import { createOrganizationRequest } from 'Admin/utils/fetch'
 import { useToggle } from 'hooks/useToggle'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { CreateOrganizationForm } from './CreateOrganizationForm'
 
 function CreateOrganization() {
     const [showModal, openModal, closeModal] = useToggle()
-    const [organizationName, setOrganizationName] = useState('')
-
-    const router = useRouter()
-    const { addToast } = useToast()
-
-    const [textfieldProps, setTextFieldProps] = useState<{
-        variant?: VariantType | undefined
-        feedback?: string
-    }>()
-
-    const saveOrganization = async () => {
-        if (!organizationName || organizationName.length < 1) {
-            return setTextFieldProps({
-                variant: 'error',
-                feedback: 'Du må sette et navn på organisasjonen',
-            })
-        }
-        const req = await createOrganizationRequest(organizationName)
-        if (req.status !== 200)
-            return addToast({
-                title: 'Noe gikk galt',
-                content: 'Kunne ikke opprette organisajon',
-                variant: 'info',
-            })
-        const organization = await req.json()
-        router.push(`/organizations/${organization.oid}`)
-    }
 
     return (
         <>
@@ -58,18 +27,7 @@ function CreateOrganization() {
                     Organisasjonen gir deg mulighet til å samarbeide om tavler
                     med andre brukere.
                 </Paragraph>
-                <TextField
-                    value={organizationName}
-                    onChange={(e) => setOrganizationName(e.target.value)}
-                    size="medium"
-                    label="Navn på din organisasjon"
-                    className="w-50"
-                    variant={textfieldProps?.variant}
-                    feedback={textfieldProps?.feedback}
-                />
-                <PrimaryButton className="mt-2" onClick={saveOrganization}>
-                    Opprett
-                </PrimaryButton>
+                <CreateOrganizationForm closeModal={closeModal} />
             </Modal>
         </>
     )
