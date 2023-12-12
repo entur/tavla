5f218333ea829af8e150bba22691069786ddb315
lindtvedtsebastian <46536611+lindtvedtsebastian@users.noreply.github.com>
2023-12-12 13:19:20 +0100

    feat(deviation): deviations for stop place (#1365)

--- next-tavla/src/Board/scenarios/Table/components/Situations.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Situations.tsx
@@ -1,19 +1,10 @@
 import { Situation } from './Situation'
-import { useEffect, useState } from 'react'
 import { TSituationFragment } from 'graphql/index'
+import { useCycler } from '../useCycler'
 
 function Situations({ situations }: { situations: TSituationFragment[] }) {
-    const [index, setIndex] = useState(0)
-
+    const index = useCycler(situations)
     const numberOfSituations = situations.length
-
-    useEffect(() => {
-        if (numberOfSituations <= 1) {
-            return
-        }
-        const interval = setInterval(() => setIndex((i) => i + 1), 5000)
-        return () => clearInterval(interval)
-    }, [numberOfSituations])
 
     if (!situations.length) return null
     return <Situation situation={situations[index % numberOfSituations]} />
