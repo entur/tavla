47e2a152c6f7006bd5f319eba377aef95fd8df5b
Øyvind Grutle <oyvind.grutle@bekk.no>
2023-12-13 08:31:20 +0100

    fix(org): revalidsate path

--- next-tavla/app/(admin)/components/Delete/actions.ts
+++ next-tavla/app/(admin)/components/Delete/actions.ts
@@ -3,8 +3,10 @@
 import { TOrganizationID, TUserID } from 'types/settings'
 import { deleteOrganization as deleteOrg } from 'Admin/utils/firebase'
 import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
 
 export async function deleteOrganization(oid: TOrganizationID, uid: TUserID) {
     await deleteOrg(oid, uid)
+    revalidatePath('/')
     redirect('/organizations')
 }
