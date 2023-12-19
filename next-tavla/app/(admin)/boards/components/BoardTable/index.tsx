ec6d03e1bc92cdc54bc4e299268de0924044d554
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-12-19 10:11:19 +0100

    chore(boards): global styling

--- next-tavla/app/(admin)/boards/components/BoardTable/index.tsx
+++ next-tavla/app/(admin)/boards/components/BoardTable/index.tsx
@@ -1,5 +1,4 @@
 'use client'
-import classes from './styles.module.css'
 import { TableHeader } from 'app/(admin)/boards/components/TableHeader'
 import { TableRows } from 'app/(admin)/boards/components/TableRows'
 import { TBoard } from 'types/settings'
@@ -11,6 +10,7 @@ import { useSearchParam } from '../../hooks/useSearchParam'
 function BoardTable({ boards }: { boards: TBoard[] }) {
     const value = useSearchParam('columns')
     const columns = value?.split(',') ?? DEFAULT_BOARD_COLUMNS
+
     if (isEmpty(boards))
         return (
             <IllustratedInfo
@@ -18,9 +18,10 @@ function BoardTable({ boards }: { boards: TBoard[] }) {
                 description="Du har ikke laget noen Tavler ennå"
             />
         )
+
     return (
         <div
-            className={classes.boardTable}
+            className="grid alignCenter"
             style={{
                 gridTemplateColumns: `repeat(${columns.length},auto)`,
             }}
