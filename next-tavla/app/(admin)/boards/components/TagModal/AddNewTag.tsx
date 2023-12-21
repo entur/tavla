308d641a021366e05e2c43a2de5fe51849317479
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2023-12-21 11:20:06 +0100

    fix(boards): revert tags logic

--- next-tavla/app/(admin)/boards/components/TagModal/AddNewTag.tsx
+++ next-tavla/app/(admin)/boards/components/TagModal/AddNewTag.tsx
@@ -7,20 +7,20 @@ import { TBoard } from 'types/settings'
 import { useFormState } from 'react-dom'
 import { FormError } from 'app/(admin)/components/FormError'
 import { getFormFeedbackForField } from 'app/(admin)/utils'
+import { useRef } from 'react'
 
 function AddNewTag({ board }: { board: TBoard }) {
     const [state, action] = useFormState(addTagAction, undefined)
-    const handleFormSubmit = async (
-        event: React.FormEvent<HTMLFormElement>,
-    ) => {
-        event.preventDefault()
-        action(new FormData(event.currentTarget))
-        event.currentTarget.reset()
+    const form = useRef<HTMLFormElement>(null)
+
+    const submit = async (data: FormData) => {
+        action(data)
+        form.current?.reset()
     }
     return (
         <div className="flexCol g-1">
             <Heading3>Legg til ny merkelapp</Heading3>
-            <form onSubmit={handleFormSubmit} className="flexColumn g-2">
+            <form ref={form} action={submit} className="flexColumn g-2">
                 <div className="flexRow g-1 w-100">
                     <TextField
                         aria-label="Navn på ny merkelapp"
