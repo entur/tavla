8f79ee0d77ab110d2933ef57d3362afbe659cae6
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-02-06 12:07:26 +0100

    feat(edit): remove unused/deprecated components

--- next-tavla/src/Admin/scenarios/Edit/components/SelectLines/types.ts
+++
@@ -1,9 +0,0 @@
-import { TTransportMode } from 'types/graphql-schema'
-
-export type TLineFragment = {
-    __typename?: 'Line'
-    id: string
-    publicCode: string | null
-    name: string | null
-    transportMode: TTransportMode | null
-}
