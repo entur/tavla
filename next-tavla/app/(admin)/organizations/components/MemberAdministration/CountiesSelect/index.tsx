24d64386f5c01621a1cd7f4c215b38bcf7dc2238
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2024-02-22 08:21:17 +0100

    styling(organization): layout of checkboxes, button and add box around content

--- next-tavla/app/(admin)/organizations/components/MemberAdministration/CountiesSelect/index.tsx
+++ next-tavla/app/(admin)/organizations/components/MemberAdministration/CountiesSelect/index.tsx
@@ -5,7 +5,8 @@ import { TCountyID, TOrganizationID } from 'types/settings'
 import { useCountiesSearch } from 'app/(admin)/hooks/useCountiesSearch'
 import { Checkbox } from '@entur/form'
 import { setCounties } from './actions'
-import styles from '../styles.module.css'
+import styles from './styles.module.css'
+import { Heading3, Paragraph } from '@entur/typography'
 function CountiesSelect({
     oid,
     countiesList,
@@ -17,48 +18,53 @@ function CountiesSelect({
 
     return (
         <>
-            <form
-                action={async (data: FormData) => {
-                    const counties = data.getAll('county') as string[]
+            <Heading3>Velg fylke/fylker du skal sette opp tavler for</Heading3>
+            <div className={styles.countiesBox}>
+                <Paragraph>
+                    Når du søker etter stoppesteder vil du søke i alle fylker.
+                    Her kan du velge hvilke fylker du ønsker å begrense søket
+                    til.
+                </Paragraph>
+                <form
+                    action={async (data: FormData) => {
+                        const counties = data.getAll('county') as string[]
 
-                    if (!oid) return null
-                    setCounties(oid, counties)
-                }}
-            >
-                <div
-                    className={`flexColumn flexWrap ${styles.countiesSelectContainer}`}
+                        if (!oid) return null
+                        setCounties(oid, counties)
+                    }}
                 >
-                    {counties()
-                        .sort(
-                            (
-                                countyA: NormalizedDropdownItemType,
-                                countyB: NormalizedDropdownItemType,
-                            ) => countyA.label.localeCompare(countyB.label),
-                        )
-                        .map((county: NormalizedDropdownItemType) => (
-                            <>
-                                <Checkbox
-                                    key={county.value}
-                                    defaultChecked={
-                                        countiesList?.includes(county.value) ??
-                                        false
-                                    }
-                                    name="county"
-                                    value={county.value}
-                                >
-                                    {county.label}
-                                </Checkbox>
-                            </>
-                        ))}
-                </div>
-                <PrimaryButton
-                    type="submit"
-                    className="mt-2"
-                    aria-label="Lagre fylker"
-                >
-                    Lagre fylker
-                </PrimaryButton>
-            </form>
+                    <div className={styles.countiesSelectContainer}>
+                        {counties()
+                            .sort(
+                                (
+                                    countyA: NormalizedDropdownItemType,
+                                    countyB: NormalizedDropdownItemType,
+                                ) => countyA.label.localeCompare(countyB.label),
+                            )
+                            .map((county: NormalizedDropdownItemType) => (
+                                <>
+                                    <Checkbox
+                                        key={county.value}
+                                        defaultChecked={
+                                            countiesList?.includes(
+                                                county.value,
+                                            ) ?? false
+                                        }
+                                        name="county"
+                                        value={county.value}
+                                    >
+                                        {county.label}
+                                    </Checkbox>
+                                </>
+                            ))}
+                    </div>
+                    <div className="flexRow w-80 mt-4 mr-4  justifyEnd">
+                        <PrimaryButton type="submit" aria-label="Lagre fylker">
+                            Lagre fylker
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
