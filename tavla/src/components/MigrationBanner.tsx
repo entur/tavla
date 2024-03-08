19cde0d8f5a8e50b60f8d9981938ce4480a9a72f
lindtvedtsebastian <46536611+lindtvedtsebastian@users.noreply.github.com>
2024-03-08 10:23:34 +0100

    chore(banner): update information (#1426)

--- tavla/src/components/MigrationBanner.tsx
+++ tavla/src/components/MigrationBanner.tsx
@@ -12,7 +12,9 @@ function MigrationBanner() {
             Dette betyr at dine eksisterende tavler snart forsvinner, og at du
             må opprette tavlene dine på nytt i den nye løsningen. Frem til 2.
             april finner du den nye tavla på{' '}
-            <Link href="https://tavla.beta.entur.no">tavla.beta.entur.no</Link>
+            <Link href="https://tavla.beta.entur.no">tavla.beta.entur.no</Link>.
+            Alle tavlene du oppretter i den nye løsning vil fungere etter
+            2.april uten at du trenger å foreta deg noe.
         </BannerAlertBox>
     )
 }
