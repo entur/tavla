2329a82dbc6022aeb7d6160ea326a37cb32c8200
lindtvedtsebastian <46536611+lindtvedtsebastian@users.noreply.github.com>
2024-03-14 08:33:22 +0100

    feat(posthog): capture stop place search attempts (#1432)

--- next-tavla/app/(admin)/hooks/useStopPlaceSearch.ts
+++ next-tavla/app/(admin)/hooks/useStopPlaceSearch.ts
@@ -1,20 +1,38 @@
 import { NormalizedDropdownItemType } from '@entur/dropdown'
 import { fetchStopPlaces } from 'Admin/utils/fetch'
-import { useCallback, useState } from 'react'
+import { usePostHog } from 'posthog-js/react'
+import { useCallback, useRef, useState } from 'react'
 
 function useStopPlaceSearch(countyIds?: string[]) {
     const [selectedStopPlace, setSelectedStopPlace] =
         useState<NormalizedDropdownItemType | null>(null)
 
+    const posthog = usePostHog()
+
+    const count = useRef(0)
+
     const items = useCallback(
-        async (search: string) => await fetchStopPlaces(search, countyIds),
-        [countyIds],
+        async (search: string) => {
+            if (search !== '') count.current += 1
+            return await fetchStopPlaces(search, countyIds)
+        },
+        [countyIds, count],
     )
+    const chooseStopPlace = (stopPlace: NormalizedDropdownItemType | null) => {
+        if (stopPlace) {
+            posthog.capture('EDIT_STOP_PLACE_SEARCH', {
+                counties: countyIds && countyIds.length > 0,
+                count: count.current,
+            })
+        }
+        setSelectedStopPlace(stopPlace)
+        count.current = 0
+    }
 
     return {
         stopPlaceItems: items,
         selectedStopPlace,
-        setSelectedStopPlace,
+        setSelectedStopPlace: chooseStopPlace,
     }
 }
 
