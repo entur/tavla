24195ee66da7b5f9f2c81ff46344e507fa427eb4
Øyvind Grutle <oyvindgru@gmail.com>
2024-03-15 16:10:11 +0100

    styling(meta): align buttons (#1437)
    * styling(meta): align buttons
    
    * chore(address): rename

--- next-tavla/app/(admin)/edit/[id]/components/MetaSettings/index.tsx
+++ next-tavla/app/(admin)/edit/[id]/components/MetaSettings/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 import classes from './styles.module.css'
-import { Button } from '@entur/button'
 import { TextField } from '@entur/form'
 import { Heading4 } from '@entur/typography'
 import { DEFAULT_BOARD_NAME } from 'Admin/utils/constants'
@@ -8,13 +7,14 @@ import { TMeta } from 'types/meta'
 import { saveFont, saveTitle } from './actions'
 import { TBoardID } from 'types/settings'
 import { FontChoiceChip } from './FontChoiceChip'
-import { Adress } from './Adress'
 import { HiddenInput } from 'components/Form/HiddenInput'
+import { SubmitButton } from 'components/Form/SubmitButton'
+import { Address } from './Adress'
 
 function MetaSettings({ bid, meta }: { bid: TBoardID; meta: TMeta }) {
     return (
         <div className={classes.meta}>
-            <form action={saveTitle} className="box">
+            <form action={saveTitle} className="box flexColumn justifyBetween">
                 <Heading4 className="m-0">Navn på tavlen</Heading4>
                 <TextField
                     name="name"
@@ -22,21 +22,25 @@ function MetaSettings({ bid, meta }: { bid: TBoardID; meta: TMeta }) {
                     defaultValue={meta.title ?? DEFAULT_BOARD_NAME}
                     label="Navn på tavlen"
                 />
-                <Button variant="secondary" type="submit" className="mt-2">
-                    Lagre tittel
-                </Button>
+                <div className="flexRow w-100 mt-4 mr-2 justifyEnd">
+                    <SubmitButton variant="secondary" className="mt-2">
+                        Lagre tittel
+                    </SubmitButton>
+                </div>
                 <HiddenInput id="bid" value={bid} />
             </form>
-            <div className="box">
+            <div className="box flexColumn justifyBetween">
                 <Heading4 className="m-0">Hvor skal tavla stå?</Heading4>
-                <Adress bid={bid} location={meta.location} />
+                <Address bid={bid} location={meta.location} />
             </div>
-            <form action={saveFont} className="box">
+            <form action={saveFont} className="box flexColumn justifyBetween">
                 <Heading4 className="m-0">Velg tekststørrelse: </Heading4>
                 <FontChoiceChip font={meta.fontSize ?? 'medium'} />
-                <Button variant="secondary" type="submit" className="mt-2">
-                    Lagre tekststørrelse
-                </Button>
+                <div className="flexRow w-100 mt-4 mr-2 justifyEnd">
+                    <SubmitButton variant="secondary">
+                        Lagre tekststørrelse
+                    </SubmitButton>
+                </div>
                 <HiddenInput id="bid" value={bid} />
             </form>
         </div>
