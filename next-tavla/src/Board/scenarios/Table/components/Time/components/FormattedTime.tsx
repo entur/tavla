508d6331df2fa4e6f828256f0c939a5d8a4dc7e7
lindtvedtsebastian <46536611+lindtvedtsebastian@users.noreply.github.com>
2024-03-22 09:54:48 +0100

    fix(departure): show relative time on departure (#1445)

--- next-tavla/src/Board/scenarios/Table/components/Time/components/FormattedTime.tsx
+++ next-tavla/src/Board/scenarios/Table/components/Time/components/FormattedTime.tsx
@@ -1,10 +1,12 @@
-import { formatDateString, getDate, isDateStringToday } from 'utils/time'
+import { getDate, getRelativeTimeString, isDateStringToday } from 'utils/time'
 import classes from '../styles.module.css'
 
 function FormattedTime({ time }: { time: string }) {
     return (
         <>
-            <div className="textRight weight600">{formatDateString(time)}</div>
+            <div className="textRight weight600">
+                {getRelativeTimeString(time)}
+            </div>
             {!isDateStringToday(time) && (
                 <div className={classes.departureDate}>{getDate(time)}</div>
             )}
