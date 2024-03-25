7886bc328188ae6aea7a589cfa78fa2ece5e1ffd
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2024-03-25 15:52:52 +0100

    chore(edit): remove redundant button component and ternary operator

--- next-tavla/app/(admin)/edit/[id]/components/Buttons/index.tsx
+++ next-tavla/app/(admin)/edit/[id]/components/Buttons/index.tsx
@@ -2,78 +2,69 @@
 import { useToast } from '@entur/alert'
 import { Button, IconButton } from '@entur/button'
 import { CopyIcon, ExternalIcon } from '@entur/icons'
-import { Delete } from 'app/(admin)/boards/components/Column/Delete'
+import { Tooltip } from '@entur/tooltip'
 import { useLink } from 'hooks/useLink'
 import Link from 'next/link'
 import React from 'react'
-import { TBoard } from 'types/settings'
 
-function Buttons({ board }: { board: TBoard }) {
-    const link = useLink(board.id)
-    return (
-        <div className="flexRow g-2 w-100 alignCenter">
-            <OpenButton link={link} type="button" />
-
-            <CopyButton link={link} type="button" />
-
-            <Delete board={board} type="button" />
-        </div>
-    )
-}
-
-function CopyButton({
-    type,
-    link,
-}: {
-    type?: 'button' | 'icon'
-    link?: string
-}) {
+function CopyButton({ type, bid }: { type?: 'button' | 'icon'; bid?: string }) {
     const { addToast } = useToast()
-
+    const link = useLink(bid)
     const copy = () => {
         navigator.clipboard.writeText(link ?? '')
         addToast('Lenke til Tavla kopiert')
     }
-    return type === 'button' ? (
-        <Button variant="secondary" aria-label="Kopier tavle" onClick={copy}>
-            Kopier Tavle
-            <CopyIcon />
-        </Button>
-    ) : (
-        <IconButton aria-label="Kopier tavle" onClick={copy}>
-            <CopyIcon />
-        </IconButton>
+
+    if (type === 'button') {
+        return (
+            <Button
+                variant="secondary"
+                aria-label="Kopier tavle"
+                onClick={copy}
+            >
+                Kopier Tavle
+                <CopyIcon />
+            </Button>
+        )
+    }
+    return (
+        <Tooltip content="Kopier lenke" placement="bottom">
+            <IconButton aria-label="Kopier tavle" onClick={copy}>
+                <CopyIcon />
+            </IconButton>
+        </Tooltip>
     )
 }
 
-function OpenButton({
-    type,
-    link,
-}: {
-    type?: 'button' | 'icon'
-    link?: string
-}) {
-    return type === 'button' ? (
-        <Button
-            variant="primary"
-            as={Link}
-            aria-label="Åpne tavle"
-            href={link ?? '/'}
-            target="_blank"
-        >
-            Åpne Tavle
-            <ExternalIcon />
-        </Button>
-    ) : (
-        <IconButton
-            as={Link}
-            aria-label="Åpne tavle"
-            href={link ?? '/'}
-            target="_blank"
-        >
-            <ExternalIcon />
-        </IconButton>
+function OpenButton({ type, bid }: { type?: 'button' | 'icon'; bid?: string }) {
+    const link = useLink(bid)
+    if (type === 'button') {
+        return (
+            <Button
+                variant="primary"
+                as={Link}
+                aria-label="Åpne tavle"
+                href={link ?? '/'}
+                target="_blank"
+            >
+                Åpne Tavle
+                <ExternalIcon />
+            </Button>
+        )
+    }
+
+    return (
+        <Tooltip content="Åpne tavle" placement="bottom">
+            <IconButton
+                as={Link}
+                aria-label="Åpne tavle"
+                href={link ?? '/'}
+                target="_blank"
+            >
+                <ExternalIcon />
+            </IconButton>
+        </Tooltip>
     )
 }
 
-export { Buttons, OpenButton, CopyButton }
+export { OpenButton, CopyButton }
