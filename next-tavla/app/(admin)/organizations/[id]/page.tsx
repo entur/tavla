0c60be57c061d0d3b6a6daeea93e7f781062a111
emilielr <emilie.rognli@gmail.com>
2024-04-05 09:45:48 +0200

    styling(edit-org): add spacing to header

--- next-tavla/app/(admin)/organizations/[id]/page.tsx
+++ next-tavla/app/(admin)/organizations/[id]/page.tsx
@@ -50,8 +50,8 @@ async function EditOrganizationPage({ params }: TProps) {
 
     return (
         <main className={classes.root}>
-            <Heading1>{organization.name}</Heading1>
-            <LeadParagraph>
+            <Heading1 margin="top">{organization.name}</Heading1>
+            <LeadParagraph margin="none" className="mb-4">
                 Valgene som tas blir satt som standard når det opprettes en
                 tavle i organisasjonen &quot;{organization.name}&quot;. Valgene
                 kan fortsatt justeres i hver enkelt tavle med unntak av logo.
