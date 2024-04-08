a7b963420a592828fc616936669405e3b30e8890
Øyvind Grutle <oyvindgru@gmail.com>
2024-04-08 13:02:01 +0200

    feat(edit): toast on save (#1478)

--- next-tavla/app/(admin)/edit/[id]/components/MetaSettings/index.tsx
+++ next-tavla/app/(admin)/edit/[id]/components/MetaSettings/index.tsx
@@ -2,7 +2,7 @@
 import classes from './styles.module.css'
 import { TextField } from '@entur/form'
 import { Heading4 } from '@entur/typography'
-import { TMeta } from 'types/meta'
+import { TFontSize, TMeta } from 'types/meta'
 import { saveFont, saveTitle } from './actions'
 import { TBoardID } from 'types/settings'
 import { FontChoiceChip } from './FontChoiceChip'
@@ -10,11 +10,20 @@ import { HiddenInput } from 'components/Form/HiddenInput'
 import { SubmitButton } from 'components/Form/SubmitButton'
 import { Address } from './Adress'
 import { DEFAULT_BOARD_NAME } from 'app/(admin)/utils/constants'
+import { useToast } from '@entur/alert'
 
 function MetaSettings({ bid, meta }: { bid: TBoardID; meta: TMeta }) {
+    const { addToast } = useToast()
     return (
         <div className={classes.meta}>
-            <form action={saveTitle} className="box flexColumn justifyBetween">
+            <form
+                action={async (data: FormData) => {
+                    const name = data.get('name') as string
+                    await saveTitle(bid, name)
+                    addToast('Tittel lagret!')
+                }}
+                className="box flexColumn justifyBetween"
+            >
                 <Heading4 className="m-0">Navn på tavlen</Heading4>
                 <TextField
                     name="name"
@@ -27,13 +36,19 @@ function MetaSettings({ bid, meta }: { bid: TBoardID; meta: TMeta }) {
                         Lagre tittel
                     </SubmitButton>
                 </div>
-                <HiddenInput id="bid" value={bid} />
             </form>
             <div className="box flexColumn justifyBetween">
                 <Heading4 className="m-0">Hvor skal tavla stå?</Heading4>
                 <Address bid={bid} location={meta.location} />
             </div>
-            <form action={saveFont} className="box flexColumn justifyBetween">
+            <form
+                action={async (data: FormData) => {
+                    const font = data.get('font') as TFontSize
+                    await saveFont(bid, font)
+                    addToast('Tekststørrelse lagret!')
+                }}
+                className="box flexColumn justifyBetween"
+            >
                 <Heading4 className="m-0">Velg tekststørrelse: </Heading4>
                 <FontChoiceChip font={meta.fontSize ?? 'medium'} />
                 <div className="flexRow w-100 mt-4 mr-2 justifyEnd">
