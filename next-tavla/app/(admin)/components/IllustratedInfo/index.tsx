77b5214636283ed3f9de8f0b6e5bd693a2da5c6c
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2024-04-09 10:08:11 +0200

    styling(boards): spacing on empty board text

--- next-tavla/app/(admin)/components/IllustratedInfo/index.tsx
+++ next-tavla/app/(admin)/components/IllustratedInfo/index.tsx
@@ -14,7 +14,7 @@ function IllustratedInfo({
         <div className={classes.info}>
             <Image src={animals} aria-hidden="true" alt="" />
             <Heading2 className={classes.infoHeading}>{title}</Heading2>
-            <LeadParagraph>{description} </LeadParagraph>
+            <LeadParagraph className="pb-4 mt-0">{description} </LeadParagraph>
         </div>
     )
 }
