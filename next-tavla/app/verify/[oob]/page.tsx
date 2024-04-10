3f4e8ae3db52bf0bd95f57e77aa03f4a4f0119c6
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-04-10 09:20:15 +0200

    feat(verify): handle possible verify user errors

--- next-tavla/app/verify/[oob]/page.tsx
+++ next-tavla/app/verify/[oob]/page.tsx
@@ -3,6 +3,7 @@ import { applyActionCode, getAuth } from 'firebase/auth'
 import { Paragraph } from '@entur/typography'
 import { Button } from '@entur/button'
 import Link from 'next/link'
+import { FirebaseError } from 'firebase/app'
 
 async function Verify({ params }: { params: { oob: string } }) {
     let message = 'E-postadressen din er nå verifisert!'
@@ -10,8 +11,22 @@ async function Verify({ params }: { params: { oob: string } }) {
         const app = await getClientApp()
         const auth = getAuth(app)
         await applyActionCode(auth, params.oob)
-    } catch {
-        message = 'Noe gikk galt, prøv igjen senere.'
+    } catch (e) {
+        if (e instanceof FirebaseError) {
+            switch (e.code) {
+                case 'auth/expired-action-code':
+                    message =
+                        'Lenken du brukte for å verifisere kontoen din er utgått.'
+                case 'auth/invalid-action-code':
+                    message =
+                        'Lenken du brukte for å verifisere kontoen din er ugyldig.'
+                case 'auth/user-disabled':
+                    message =
+                        'Kontoen din er deaktivert og kunne ikke verifiseres.'
+                case 'auth/user-not-found':
+                    message = 'Fant ingen bruker som samsvarte med lenken.'
+            }
+        } else message = 'Noe gikk galt, prøv igjen senere.'
     }
     return (
         <div className="flexColumn alignCenter eds-contrast pt-4">
