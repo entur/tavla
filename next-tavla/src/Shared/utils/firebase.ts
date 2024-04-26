2601f3c499fb634abefaef38fbb44b814cc9c6bf
emilielr <emilie.rognli@gmail.com>
2024-04-26 09:15:41 +0200

    chore(): remove force emulators in firebase config

--- next-tavla/src/Shared/utils/firebase.ts
+++ next-tavla/src/Shared/utils/firebase.ts
@@ -7,10 +7,7 @@ import {
 import { getFirebaseClientConfig } from 'app/(admin)/actions'
 import { FIREBASE_DEV_CONFIG } from 'app/(admin)/utils/constants'
 
-if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.NEXT_PUBLIC_FORCE_EMULATORS === 'true'
-) {
+if (process.env.NODE_ENV === 'development') {
     const app = initializeApp(FIREBASE_DEV_CONFIG)
     const auth = getAuth(app)
     auth.setPersistence(inMemoryPersistence)
