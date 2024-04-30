3a53e65a394d3c71320911a8821301433950de41
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2024-04-30 11:43:03 +0200

    chore(config): linting

---
+++ next-tavla/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        container: {
+            padding: '2rem',
+        },
+        extend: {
+            colors: {
+                primary: 'var(--main-background-color)',
+            },
+        },
+    },
+    plugins: [],
+}
