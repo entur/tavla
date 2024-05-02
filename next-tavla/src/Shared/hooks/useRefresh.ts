3fb41224aa2a449042c2785deabac3578c5a762f
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-05-02 11:11:11 +0200

    feat(refresh): auto reconnect on websocket error

--- next-tavla/src/Shared/hooks/useRefresh.ts
+++ next-tavla/src/Shared/hooks/useRefresh.ts
@@ -1,36 +1,42 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { TBoard } from 'types/settings'
 
 function useRefresh(initialBoard: TBoard) {
+    const timeout = useRef<NodeJS.Timeout>()
     const [board, setBoard] = useState<TBoard>(initialBoard)
     const connection = useRef<WebSocket | null>(null)
 
-    useEffect(() => {
-        const t = setTimeout(() => {
-            const socket = new WebSocket(
-                `ws://localhost:3001/subscribe/${initialBoard.id}`,
+    const subscribe = useCallback(() => {
+        const socket = new WebSocket(
+            `ws://localhost:3001/subscribe/${initialBoard.id}`,
+        )
+
+        socket.addEventListener('open', () => {
+            socket.send(
+                `Connection with client on board with id ${initialBoard.id} established`,
             )
+        })
 
-            socket.addEventListener('open', () => {
-                console.log('established connection to websocket server')
-                socket.send(
-                    `Connection with client on board with id ${initialBoard.id} established`,
-                )
-            })
+        socket.addEventListener('message', (event) => {
+            const newBoard = JSON.parse(event.data) as TBoard
+            setBoard(newBoard)
+        })
 
-            socket.addEventListener('message', (event) => {
-                console.log('Data from server', event)
-                const newBoard = JSON.parse(event.data) as TBoard
-                setBoard(newBoard)
-            })
+        socket.addEventListener(
+            'error',
+            () => (timeout.current = setTimeout(subscribe, 10000)),
+        )
 
-            connection.current = socket
-        }, 10000)
+        connection.current = socket
+    }, [initialBoard.id])
+
+    useEffect(() => {
+        timeout.current = setTimeout(subscribe, 10000)
         return () => {
             connection.current?.close()
-            clearTimeout(t)
+            clearTimeout(timeout.current)
         }
-    }, [initialBoard])
+    }, [subscribe])
 
     return board
 }
