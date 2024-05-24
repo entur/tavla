b4f41692bc3acd563d70c1d4022f51f3aa1064de
purusott <74315929+purusott@users.noreply.github.com>
2024-05-24 13:21:04 +0200

    style(organization): footer settings box (#1514)
    * style(organization): footer settings box
    
    * chore(organization): refactor styling

--- next-tavla/app/(admin)/organizations/components/Footer/index.tsx
+++ next-tavla/app/(admin)/organizations/components/Footer/index.tsx
@@ -1,18 +1,16 @@
 'use client'
+import { useToast } from '@entur/alert'
 import { TextField } from '@entur/form'
-import { ValidationInfoIcon } from '@entur/icons'
-import { Heading3 } from '@entur/typography'
-import { Tooltip } from 'app/(admin)/components/Tooltip'
+import { Heading2, Paragraph } from '@entur/typography'
 import { SubmitButton } from 'components/Form/SubmitButton'
 import { TOrganizationID } from 'types/settings'
 import { setFooter } from './actions'
-import { useToast } from '@entur/alert'
 
 function Footer({ oid, footer }: { oid?: TOrganizationID; footer?: string }) {
     const { addToast } = useToast()
     return (
         <form
-            className="box flex flex-col justify-between"
+            className="box flex flex-col gap-1 "
             action={async (data: FormData) => {
                 if (!oid) return
                 const info = data.get('footer') as string
@@ -21,28 +19,25 @@ function Footer({ oid, footer }: { oid?: TOrganizationID; footer?: string }) {
                 addToast('Infomelding lagret!')
             }}
         >
-            <div className="flex flex-row items-center gap-2">
-                <Heading3 margin="none">Infomelding</Heading3>
-                <Tooltip
-                    content="Skriv en kort tekst som skal vises nederst i tavlen."
-                    placement="top"
-                >
-                    <ValidationInfoIcon />
-                </Tooltip>
-            </div>
-            <TextField
-                label="Infomelding"
-                name="footer"
-                defaultValue={footer ?? ''}
-            />
-            <div className="flex flex-row w-full mt-8 justify-end">
-                <SubmitButton
-                    variant="secondary"
-                    aria-label="Lagre kolonner"
-                    className="max-sm:w-full"
-                >
-                    Lagre infomelding
-                </SubmitButton>
+            <Heading2>Infomelding</Heading2>
+            <Paragraph>
+                Skriv en kort tekst som skal vises nederst i tavlen.
+            </Paragraph>
+
+            <div className="flex flex-col justify-between h-full">
+                <TextField
+                    label="Infomelding"
+                    name="footer"
+                    defaultValue={footer ?? ''}
+                />
+                <div className="flex flex-row w-full mt-8 justify-end">
+                    <SubmitButton
+                        variant="secondary"
+                        aria-label="Lagre kolonner"
+                    >
+                        Lagre infomelding
+                    </SubmitButton>
+                </div>
             </div>
         </form>
     )
