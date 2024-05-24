0133be2a364835149f05f27f4087194a133208c1
Øyvind Grutle <oyvind.grutle@bekk.no>
2024-05-24 13:54:18 +0200

    chore(board): move const out of function

--- next-tavla/src/Shared/utils/publicCode.ts
+++ next-tavla/src/Shared/utils/publicCode.ts
@@ -1,5 +1,6 @@
+const regex = /AVI:ServiceJourney:([A-Z0-9]+)-\d{2}-\d+/
+
 export function getAirPublicCode(id: string) {
-    const regex = /AVI:ServiceJourney:([A-Z0-9]+)-\d{2}-\d+/
     const match = id.match(regex)
     return match ? match[1] : undefined
 }
