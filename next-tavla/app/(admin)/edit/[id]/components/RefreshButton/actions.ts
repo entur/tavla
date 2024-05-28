c22b7e442736eb4d3b3283aa5b9d708fba6cbfdb
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-05-28 12:41:08 +0200

    chore(refresh): common function for fetching backend url

--- next-tavla/app/(admin)/edit/[id]/components/RefreshButton/actions.ts
+++ next-tavla/app/(admin)/edit/[id]/components/RefreshButton/actions.ts
@@ -3,21 +3,19 @@
 import { hasBoardEditorAccess } from 'app/(admin)/utils/firebase'
 import { redirect } from 'next/navigation'
 import { TBoard } from 'types/settings'
+import { getBackendUrl } from 'utils/index'
 
 export async function refreshBoard(board: TBoard) {
     const access = await hasBoardEditorAccess(board.id)
     if (!access) return redirect('/')
 
-    const res = await fetch(
-        `${process.env.BACKEND_SERVICE_HOST}/refresh/${board.id}`,
-        {
-            method: 'POST',
-            body: JSON.stringify(board),
-            headers: {
-                Authorization: `Bearer ${process.env.BACKEND_API_KEY}`,
-                'Content-Type': 'application/json',
-            },
+    const res = await fetch(`${getBackendUrl()}/refresh/${board.id}`, {
+        method: 'POST',
+        body: JSON.stringify(board),
+        headers: {
+            Authorization: `Bearer ${process.env.BACKEND_API_KEY}`,
+            'Content-Type': 'application/json',
         },
-    )
+    })
     return res.ok
 }
