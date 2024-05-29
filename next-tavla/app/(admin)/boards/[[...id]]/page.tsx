8ff6061a7088cd688171a46e32fdb9aae280e523
emilielr <emilie.rognli@gmail.com>
2024-05-29 09:34:49 +0200

    fix(boards): add heading

--- next-tavla/app/(admin)/boards/[[...id]]/page.tsx
+++ next-tavla/app/(admin)/boards/[[...id]]/page.tsx
@@ -11,6 +11,7 @@ import {
 } from 'app/(admin)/actions'
 import { initializeAdminApp } from 'app/(admin)/utils/firebase'
 import { getUserFromSessionCookie } from 'app/(admin)/utils/server'
+import { Heading1 } from '@entur/typography'
 
 initializeAdminApp()
 
@@ -52,7 +53,8 @@ async function OrganizationsBoardsPage({ params }: TProps) {
         : await getBoardsForUser()
 
     return (
-        <div className="flex flex-col mt-4 md:mt-8 gap-8 grow">
+        <div className="flex flex-col gap-8">
+            <Heading1>Tavler</Heading1>
             <div className="flex flex-col sm:flex-row md:items-center gap-3">
                 <Search />
                 <FilterButton boards={boards} />
