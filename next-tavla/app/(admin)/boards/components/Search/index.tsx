73af47df0c8448ff078ad112b45e84d2c3bbe439
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2024-06-06 15:07:04 +0200

    fix(search): clearable

--- next-tavla/app/(admin)/boards/components/Search/index.tsx
+++ next-tavla/app/(admin)/boards/components/Search/index.tsx
@@ -2,17 +2,28 @@
 import { TextField } from '@entur/form'
 import { SearchIcon } from '@entur/icons'
 import { useSearchParamReplacer } from '../../hooks/useSearchParamReplacer'
+import { useEffect, useState } from 'react'
 
 function Search() {
     const [value, replace] = useSearchParamReplacer('search')
+    const [search, setSearch] = useState(value)
+
+    useEffect(() => {
+        replace(search)
+    }, [replace, search])
     return (
         <TextField
             className="w-full md:w-1/2"
             label="Søk på navn på tavle"
             prepend={<SearchIcon inline aria-hidden="true" />}
-            defaultValue={value}
+            value={search}
             onChange={(e) => {
-                replace(e.target.value)
+                setSearch(e.target.value)
+            }}
+            id="search"
+            clearable
+            onClear={() => {
+                setSearch('')
             }}
         />
     )
