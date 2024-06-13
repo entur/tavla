a78836f0bae1b15094055d6e4e85d2e3131782ce
Øyvind Grutle <oyvind.grutle@bekk.no>
2024-06-13 15:38:00 +0200

    fix(preview): remove unused

--- next-tavla/app/(admin)/edit/[id]/components/ClientBoard.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { Board } from 'Board/scenarios/Board'
-import { TBoard } from 'types/settings'
-
-function ClientBoard({ board }: { board: TBoard }) {
-    return <Board board={board} />
-}
-export { ClientBoard }
