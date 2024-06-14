095bc9889e48804d92b984ae1dc415ab968397a4
Øyvind Grutle <oyvind.grutle@bekk.no>
2024-06-14 10:32:12 +0200

    fix(edit): fix wrong id

--- next-tavla/app/(admin)/edit/[id]/components/DuplicateBoard/index.tsx
+++ next-tavla/app/(admin)/edit/[id]/components/DuplicateBoard/index.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useToast } from '@entur/alert'
 import { Button } from '@entur/button'
 import { AddIcon } from '@entur/icons'
 import { OverflowMenuItem } from '@entur/menu'
@@ -14,23 +15,27 @@ function DuplicateBoard({
     oid?: TOrganizationID
     type?: 'button' | 'action'
 }) {
+    const { addToast } = useToast()
     const handleSelect = async () => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { id, ...boardWithoutId } = board
         await create(
             {
-                ...board,
+                ...boardWithoutId,
                 meta: {
-                    ...board.meta,
-                    title: board.meta.title + ' - duplikat',
+                    ...boardWithoutId.meta,
+                    title: boardWithoutId.meta.title + ' - duplikat',
                 },
             },
             oid,
         )
+        addToast('Tavle duplisert!')
     }
     if (type === 'button')
         return (
             <Button
                 variant="secondary"
-                aria-label="Slett tavle"
+                aria-label="Dupliser tavle"
                 onClick={handleSelect}
             >
                 Dupliser Tavle
