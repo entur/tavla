f2ee8b03512996425aaf9631388bd35f5390c5a0
emilielr <emilie.rognli@gmail.com>
2024-06-14 15:36:03 +0200

    fix(boards): fallback to default org name

--- next-tavla/app/(admin)/boards/components/Column/Organization.tsx
+++ next-tavla/app/(admin)/boards/components/Column/Organization.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 import { Column } from './Column'
 import { TOrganization } from 'types/settings'
+import { DEFAULT_ORGANIZATION_NAME } from 'app/(admin)/utils/constants'
 
 function Organization({ organization }: { organization?: TOrganization }) {
-    if (!organization) return <Column column="organization">Privat</Column>
+    if (!organization)
+        return (
+            <Column column="organization">{DEFAULT_ORGANIZATION_NAME}</Column>
+        )
 
     return (
         <Column column="organization">
