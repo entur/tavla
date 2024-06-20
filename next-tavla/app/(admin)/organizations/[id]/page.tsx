ecba07ec09f3128ab45702fa02151de3393eb701
emilielr <43408175+emilielr@users.noreply.github.com>
2024-06-20 07:49:39 +0200

    fix(organization): update member text (#1555)
    * fix(organization): update text on member administration
    
    * styling(oranization): add padding

--- next-tavla/app/(admin)/organizations/[id]/page.tsx
+++ next-tavla/app/(admin)/organizations/[id]/page.tsx
@@ -50,7 +50,7 @@ async function EditOrganizationPage({ params }: TProps) {
     return (
         <>
             <Heading1 margin="top">{organization.name}</Heading1>
-            <LeadParagraph margin="none" className="mb-8">
+            <LeadParagraph margin="bottom">
                 Valgene som tas blir satt som standard når det opprettes en
                 tavle i organisasjonen &quot;{organization.name}&quot;. Valgene
                 kan fortsatt justeres i hver enkelt tavle (med unntak av logo).
