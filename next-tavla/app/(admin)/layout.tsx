1a533e2a84c01b60dc17a1bae81a741d0b41d380
emilielr <emilie.rognli@gmail.com>
2024-06-28 11:57:02 +0200

    refactor(nav): move nav to root level

--- next-tavla/app/(admin)/layout.tsx
+++ next-tavla/app/(admin)/layout.tsx
@@ -1,22 +1,12 @@
 import { Metadata } from 'next'
 import { ReactNode } from 'react'
-import { cookies } from 'next/headers'
-import { TopNavigation } from './components/TopNavigation'
-import { verifySession } from './utils/firebase'
 
 export const metadata: Metadata = {
     title: 'Mine organisasjoner | Entur Tavla',
 }
 
 async function AdminLayout({ children }: { children: ReactNode }) {
-    const session = cookies().get('session')?.value
-    const loggedIn = (await verifySession(session)) !== null
-    return (
-        <>
-            <TopNavigation loggedIn={loggedIn} />
-            <main className="container mx-auto pt-4 pb-20">{children}</main>
-        </>
-    )
+    return <main className="container mx-auto pt-4 pb-20">{children}</main>
 }
 
 export default AdminLayout
