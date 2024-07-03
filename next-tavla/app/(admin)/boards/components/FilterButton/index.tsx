462f510d1c744f67ffc08fe7a39de3d0b842153f
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2024-07-03 10:54:39 +0200

    chore(filter): refactor

--- next-tavla/app/(admin)/boards/components/FilterButton/index.tsx
+++ next-tavla/app/(admin)/boards/components/FilterButton/index.tsx
@@ -10,27 +10,26 @@ import { Heading4, Paragraph } from '@entur/typography'
 import { CloseIcon, FilterIcon } from '@entur/icons'
 import { TTag } from 'types/meta'
 import { FilterChip } from '@entur/chip'
-import { uniq, xor } from 'lodash'
 import { NotificationBadge } from '@entur/layout'
-import { TBoardWithOrganizaion } from 'types/settings'
 import { useSearchParamReplacer } from '../../hooks/useSearchParamReplacer'
+import { useEffect, useState } from 'react'
+import { xor } from 'lodash'
 
-function FilterButton({
-    boardsWithOrg,
-}: {
-    boardsWithOrg: TBoardWithOrganizaion[]
-}) {
+function FilterButton({ filterOptions }: { filterOptions?: TTag[] }) {
     const [value, replace] = useSearchParamReplacer('tags')
-    const activeTags = value?.split(',') ?? []
-    const allTags = uniq(
-        boardsWithOrg.flatMap((boardWithOrg) => {
-            return boardWithOrg?.board.meta?.tags ?? []
-        }),
+
+    const [activeTags, setActiveTags] = useState<TTag[]>(
+        value?.split(',') ?? [],
     )
 
-    const filterTags: TTag[] = activeTags.filter((tag: TTag) =>
-        allTags.includes(tag),
-    )
+    useEffect(() => {
+        replace(activeTags.join(','))
+    }, [activeTags, replace])
+
+    const handleFilterChipChange = (tag: TTag) => {
+        const newTags = xor(activeTags, [tag])
+        setActiveTags(newTags)
+    }
 
     return (
         <Popover>
@@ -44,7 +43,7 @@ function FilterButton({
                         <FilterIcon aria-hidden="true" />
                     </SecondaryButton>
                     <NotificationBadge variant="primary" max={10}>
-                        {filterTags.length}
+                        {activeTags.length}
                     </NotificationBadge>
                     <span className="visuallyHidden">merkelapper valgt</span>
                 </div>
@@ -60,23 +59,23 @@ function FilterButton({
                         </PopoverCloseButton>
                     </div>
                     <div className="max-w-96 flex flex-wrap gap-2 pt-2">
-                        {allTags.length === 0 && (
+                        {filterOptions?.length === 0 ? (
                             <Paragraph>
                                 Du har ikke lagt til noen merkelapper.
                             </Paragraph>
+                        ) : (
+                            filterOptions?.sort().map((tag: TTag) => (
+                                <FilterChip
+                                    type="checkbox"
+                                    key={tag}
+                                    checked={activeTags.includes(tag)}
+                                    onChange={() => handleFilterChipChange(tag)}
+                                    value={tag}
+                                >
+                                    {tag}
+                                </FilterChip>
+                            ))
                         )}
-                        {allTags.sort().map((tag: TTag) => (
-                            <FilterChip
-                                key={tag}
-                                checked={filterTags.includes(tag)}
-                                onChange={() =>
-                                    replace(xor(activeTags, [tag]).join(','))
-                                }
-                                value={tag}
-                            >
-                                {tag}
-                            </FilterChip>
-                        ))}
                     </div>
                 </div>
             </PopoverContent>
