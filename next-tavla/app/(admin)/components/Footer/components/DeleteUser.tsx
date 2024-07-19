c8cdb1fd0242e83c71450bdd0568afe337e56b06
Øyvind Grutle <oyvindgru@gmail.com>
2024-07-19 08:33:34 +0200

    feat(user): delete user (#1587)
    * feat(user): delete user
    
    * fix(user): aria label
    
    * chore(user): refactor
    
    * chore(user): specify consequences of deleting user

---
+++ next-tavla/app/(admin)/components/Footer/components/DeleteUser.tsx
@@ -0,0 +1,74 @@
+import { SecondarySquareButton } from '@entur/button'
+import { CloseIcon } from '@entur/icons'
+import { Modal } from '@entur/modal'
+import { Heading2, Label, Paragraph } from '@entur/typography'
+import { useSearchParamsModal } from 'app/(admin)/hooks/useSearchParamsModal'
+import Image from 'next/image'
+import ducks from 'assets/illustrations/Ducks.png'
+import { SubmitButton } from 'components/Form/SubmitButton'
+import { TextField } from '@entur/form'
+import { useFormState } from 'react-dom'
+
+import { getFormFeedbackForField } from 'app/(admin)/utils'
+import { deleteProfile } from './actions'
+
+function DeleteUser() {
+    const [open, close] = useSearchParamsModal('deleteProfile')
+
+    const [state, action] = useFormState(deleteProfile, undefined)
+    return (
+        <>
+            <Modal
+                open={open}
+                size="small"
+                onDismiss={close}
+                closeLabel="Avbryt sletting"
+                className="flex flex-col justify-start items-center text-center"
+            >
+                <SecondarySquareButton
+                    aria-label="Avbryt sletting"
+                    className="ml-auto"
+                    onClick={close}
+                >
+                    <CloseIcon />
+                </SecondarySquareButton>
+                <Image src={ducks} alt="" className="h-1/2 w-1/2" />
+                <Heading2>Slett bruker</Heading2>
+                <Paragraph>
+                    Alle dine private tavler, samt tavler tilknyttet
+                    organisasjoner hvor du er det eneste medlemmet, vil også bli
+                    slettet.
+                </Paragraph>
+                <form
+                    action={action}
+                    className="flex flex-col w-full gap-4"
+                    aria-live="polite"
+                    aria-relevant="all"
+                >
+                    <Label className="font-medium text-left">
+                        Bekreft ved å skrive inn din e-postadresse
+                    </Label>
+                    <TextField
+                        name="email"
+                        label="E-post"
+                        type="text"
+                        required
+                        aria-required
+                        className="w-full"
+                        {...getFormFeedbackForField('email', state)}
+                    />
+
+                    <SubmitButton
+                        variant="primary"
+                        width="fluid"
+                        aria-label="Slett bruker"
+                    >
+                        Ja, slett
+                    </SubmitButton>
+                </form>
+            </Modal>
+        </>
+    )
+}
+
+export { DeleteUser }
