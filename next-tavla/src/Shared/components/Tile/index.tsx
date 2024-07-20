dcef182e998be8ae708d2f46580bbb4afca8cfd4
Ramazan Gunindi <codmyhesap@gmail.com>
2024-07-20 06:19:58 +0200

    Fix overflow issue in Live Schedule Display (#1592)

--- next-tavla/src/Shared/components/Tile/index.tsx
+++ next-tavla/src/Shared/components/Tile/index.tsx
@@ -2,21 +2,25 @@ import classNames from 'classnames'
 import React from 'react'
 
 function Tile({
-    className,
-    children,
-    ...rest
+  className,
+  children,
+  ...rest
 }: React.HTMLAttributes<HTMLDivElement>) {
-    return (
-        <div
-            className={classNames(
-                'h-full w-full text-primary bg-secondary p-em-1 rounded overflow-hidden',
-                className,
-            )}
-            {...rest}
-        >
-            {children}
-        </div>
-    )
+  return (
+    <div
+      className={classNames(
+        'h-full w-full text-primary bg-secondary p-em-1 rounded overflow-hidden',
+        className,
+      )}
+      style={{
+        overflow: 'auto',
+        maxHeight: '100vh'
+      }}
+      {...rest}
+    >
+      {children}
+    </div>
+  )
 }
 
 export { Tile }
