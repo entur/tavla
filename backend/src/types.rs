e253d766ad542256ca8f338fddbf59c7e895ab2a
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-08-01 10:04:21 +0200

    feat(backend): remove unused app state fields

--- backend/src/types.rs
+++ backend/src/types.rs
@@ -6,14 +6,11 @@ use axum::{
 use redis::{aio::MultiplexedConnection, Client};
 use serde::Serialize;
 use serde_json::{to_string, Value};
-use tokio_util::{sync::CancellationToken, task::TaskTracker};
 
 #[derive(Clone)]
 pub struct AppState {
     pub master: MultiplexedConnection,
     pub replicas: Client,
-    pub runtime_status: CancellationToken,
-    pub task_tracker: TaskTracker,
     pub key: String,
 }
 
