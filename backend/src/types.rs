6e1ad038413bc5f9c3d77e18f8f70fa6697c278e
emilielr <emilie.rognli@gmail.com>
2024-08-28 14:52:01 +0200

    chore(): remove println

--- backend/src/types.rs
+++ backend/src/types.rs
@@ -66,7 +66,6 @@ pub struct AppError {
 
 impl IntoResponse for AppError {
     fn into_response(self) -> axum::response::Response {
-        println!("{:?}", self);
         (StatusCode::INTERNAL_SERVER_ERROR).into_response()
     }
 }
