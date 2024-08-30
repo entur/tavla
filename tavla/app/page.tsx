b4c611260be4e609a96c3612c83352a9068c3b35
SelmaBergstrand <78426277+SelmaBergstrand@users.noreply.github.com>
2024-08-30 13:31:46 +0200

    style(landing): update design of landing page (#1626)
    * style(landing): change illustration and add buttons
    
    * feat(landing): buttons lead to corresponding pages
    
    * style(landing): change text and exampleboard size to match figma
    
    * fix(landing): no overflow of previewBoards in phone view
    
    * fix(landing): update illustration to better quality image
    
    * chore(landing): fix unused space in header
    
    * fix(landing): width of previewBoard is same as text on medium size screens
    
    * fix(landing): create user button only visible when user is not already logged in
    
    ---------
    
    Co-authored-by: asewilhelmsen <ase.wilhelmsen@bekk.no>

--- tavla/app/page.tsx
+++ tavla/app/page.tsx
@@ -1,4 +1,4 @@
-import landingImage from 'assets/illustrations/Main_city_2.svg'
+import landingImage from 'assets/illustrations/Landing_illustration.svg'
 import { Metadata } from 'next'
 import Image from 'next/image'
 import {
@@ -6,80 +6,159 @@ import {
     Heading2,
     Heading3,
     LeadParagraph,
+    ListItem,
     Paragraph,
+    UnorderedList,
 } from '@entur/typography'
 import { Preview } from './(admin)/components/Preview'
 import { previewBoards } from '../src/Shared/utils/previewBoards'
 import { Welcome } from './components/Welcome'
+import { Button } from '@entur/button'
+import Link from 'next/link'
+import { verifySession } from './(admin)/utils/firebase'
+import { cookies } from 'next/headers'
 
 export const metadata: Metadata = {
     title: 'Forside | Entur Tavla',
 }
 
-function Landing() {
+async function Landing() {
+    const session = cookies().get('session')?.value
+    const loggedIn = (await verifySession(session)) !== null
     return (
         <>
             <main>
                 <Welcome />
-                <div className="flex flex-col justify-center pb-10 pt-6">
-                    <div className="flex flex-col py-4 container">
-                        <Heading1 margin="none">
-                            Lag din egen avgangstavle
-                        </Heading1>
-                        <Heading1
-                            className="italic !text-highlight !font-normal"
-                            margin="bottom"
-                        >
-                            for reisende
-                        </Heading1>
-                    </div>
-                    <div className="flex flex-row justify-center overflow-hidden pt-2 pb-4">
-                        <Image
-                            src={landingImage}
-                            alt=""
-                            className="scale-125"
-                        />
+                <div className="flex flex-col justify-center pb-10">
+                    <div className="bg-secondary">
+                        <div className="flex flex-col container py-12 gap-10 xl:flex-row">
+                            <div className="flex flex-col xl:w-1/2">
+                                <Heading1 margin="none">
+                                    Lag en avgangstavle for
+                                </Heading1>
+                                <Heading1
+                                    className="italic !text-highlight !font-normal"
+                                    margin="bottom"
+                                >
+                                    kontoret
+                                </Heading1>
+                                <LeadParagraph className="w-full xl:w-2/3">
+                                    Tavla er en gratis tjeneste som gjør det
+                                    enkelt å sette opp avgangstavler for
+                                    offentlig transport i hele Norge! Vis
+                                    kollektivtilbudet i nærheten og hjelp folk
+                                    til å planlegge sin neste kollektivreise.
+                                </LeadParagraph>
+                                <div className="flex md:flex-row flex-col md:items-end w-full gap-4 ">
+                                    {!loggedIn && (
+                                        <Button
+                                            variant="success"
+                                            size="medium"
+                                            as={Link}
+                                            href="?login=create"
+                                        >
+                                            Opprett bruker
+                                        </Button>
+                                    )}
+                                    <Button
+                                        variant="secondary"
+                                        size="medium"
+                                        as={Link}
+                                        href="demo"
+                                    >
+                                        Test ut Tavla
+                                    </Button>
+                                </div>
+                            </div>
+                            <div className="flex flex-row mx-auto items-end xl:w-2/5">
+                                <Image
+                                    src={landingImage}
+                                    alt="En avgangstavle"
+                                />
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="flex flex-col justify-center py-4 container">
-                        <Heading2>Hva er Tavla?</Heading2>
-                        <LeadParagraph>
-                            Tavla er et verktøy som hjelper deg å lage
-                            avgangstavler for offentlig transport. Du kan f.eks.
-                            lage avgangstavler for knutepunkter, holdeplasser
-                            eller skoler, arbeidsplasser og idrettshaller.
-                        </LeadParagraph>
-                        <div className="flex flex-col xl:flex-row gap-4">
+                    <div className="flex flex-col mx-auto items-center justify-start py-4 container overflow-hidden">
+                        <div className="flex flex-col items-center justify-start gap-4 py-4 w-full">
                             <div
-                                className="xl:w-1/2 h-[60vh] overflow-y-hidden rounded-2xl"
+                                className="xl:w-1/2 h-[40vh] overflow-hidden rounded-2xl py-10 w-full"
                                 data-theme="dark"
                             >
                                 <Preview boards={previewBoards} />
                             </div>
 
                             <div className="xl:w-1/2">
-                                <Heading3>
-                                    Tavla - laget for og med kollektivselskaper
-                                </Heading3>
+                                <Heading2>Kort om Tavla</Heading2>
+                                <UnorderedList className="space-y-3">
+                                    <ListItem>
+                                        Du kan lage avgangstavler fra alle
+                                        stoppesteder, holdeplasser,
+                                        knutepunkter, fergekaier mm. i hele
+                                        Norge. Dette gjelder for alle typer
+                                        offentlig transport, inkludert ferger,
+                                        hurtigbåter og fly.
+                                    </ListItem>
+                                    <ListItem>
+                                        Det er helt gratis å implementere og
+                                        bruke Tavla.
+                                    </ListItem>
+                                    <ListItem>
+                                        Det er helt gratis å implementere og
+                                        bruke Tavla.
+                                    </ListItem>
+                                </UnorderedList>
                                 <Paragraph>
-                                    Tavla er et digitalt produkt som er under
-                                    kontinuerlig utvikling i samarbeid med
-                                    kollektivselskapene. Med Tavla kan du enkelt
-                                    opprette, administrere og samarbeide om
-                                    avgangstavler.
-                                </Paragraph>
-                                <Heading3>Tavla - for knutepunkter</Heading3>
-                                <Paragraph>
-                                    Tavla har støtte for alle stoppesteder i
-                                    Norge. Du velger selv hva slags informasjon
-                                    som skal vises på dine tavler.
+                                    Tavla sin kildekode er åpent tilgjengelig på
+                                    GitHub. Dette gjør at du kan følge
+                                    utviklingen av produktet direkte og foreslå
+                                    forbedringer selv.
                                 </Paragraph>
 
-                                <Heading3>Tavla for alle</Heading3>
+                                <Heading3>
+                                    Enkelt å tilpasse og samarbeide
+                                </Heading3>
+
+                                <UnorderedList className="space-y-3">
+                                    <ListItem>
+                                        Tilpass tekststørrelse, fargetema, logo
+                                        og hvilken informasjon som skal vises,
+                                        slik at tavlen(e) passer til dine
+                                        omgivelser og dine besøkendes behov.
+                                    </ListItem>
+                                    <ListItem>
+                                        Velg om du vil vise hele
+                                        kollektivtilbudet fra et stoppested,
+                                        eller kun vise spesifikke linjer,
+                                        stoppesteder eller fremkomstmidler.
+                                    </ListItem>
+                                    <ListItem>
+                                        Opprett organisasjoner (mapper) for å
+                                        samle tavler og gi andre tilgang til å
+                                        administrere dem. Her kan du også velge
+                                        standardinnstillinger som vil gjelde
+                                        alle tavler i organisasjonen.
+                                    </ListItem>
+                                </UnorderedList>
+
+                                <Heading2>Eksempler på bruk</Heading2>
+                                <Heading3>Hoteller</Heading3>
                                 <Paragraph>
-                                    Tavla er til for de reisende. Tavla muligjør
-                                    raske og effektive beslutninger for de
-                                    reisende gjennom pålitelig informasjon.
+                                    Plasser Tavla i resepsjonsområdet slik at
+                                    gjester kan se sanntidsinformasjon om
+                                    avganger fra stoppesteder i nærheten.
+                                </Paragraph>
+                                <Heading3>Arbeidsplasser</Heading3>
+                                <Paragraph>
+                                    Vis Tavla på informasjonspunkter eller
+                                    skjermer ved inngangen slik at besøkende
+                                    enkelt kan planlegge hjemreisen.
+                                </Paragraph>
+                                <Heading3>Kjøpesentre</Heading3>
+                                <Paragraph>
+                                    Sett opp Tavla på sentrale steder slik at
+                                    kunder alltid har oppdatert informasjon om
+                                    offentlig transport.
                                 </Paragraph>
                             </div>
                         </div>
