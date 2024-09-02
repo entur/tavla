c44855583b3b27561d2cfc2ac601e49504ac2cb8
emilielr <emilie.rognli@gmail.com>
2024-09-02 13:24:36 +0200

    feat(create): add organization to create board modal

--- tavla/app/(admin)/components/CreateBoard/index.tsx
+++ tavla/app/(admin)/components/CreateBoard/index.tsx
@@ -1,10 +1,10 @@
 'use client'
 import { AddIcon } from '@entur/icons'
 import { Modal } from '@entur/modal'
-import { Heading3, Label } from '@entur/typography'
+import { Heading3, Label, Paragraph } from '@entur/typography'
 import { useState } from 'react'
-import { TBoard } from 'types/settings'
-import { TextField } from '@entur/form'
+import { TBoard, TOrganizationID } from 'types/settings'
+import { Checkbox, TextField } from '@entur/form'
 import {
     TFormFeedback,
     getFormFeedbackForError,
@@ -13,11 +13,25 @@ import {
 import { SubmitButton } from 'components/Form/SubmitButton'
 import { create } from './actions'
 import { useSearchParamsModal } from 'app/(admin)/hooks/useSearchParamsModal'
+import { Dropdown } from '@entur/dropdown'
+import { HiddenInput } from 'components/Form/HiddenInput'
+import { useOrganizations } from 'app/(admin)/hooks/useOrganizations'
 
 function CreateBoard() {
     const [open, close] = useSearchParamsModal('board')
 
     const [state, setFormError] = useState<TFormFeedback | undefined>()
+
+    const { organizations, selectedOrganization, setSelectedOrganization } =
+        useOrganizations()
+
+    const [personal, setPersonal] = useState<boolean>(false)
+
+    const reset = () => {
+        setPersonal(false)
+        setSelectedOrganization(null)
+        setFormError(undefined)
+    }
 
     return (
         <>
@@ -26,7 +40,7 @@ function CreateBoard() {
                 size="medium"
                 className="flex flex-col items-center"
                 onDismiss={() => {
-                    setFormError(undefined)
+                    reset()
                     close()
                 }}
                 closeLabel="Avbryt opprettelse av tavle"
@@ -39,26 +53,80 @@ function CreateBoard() {
                                 getFormFeedbackForError('board/name-missing'),
                             )
                         }
-                        await create({
+
+                        const organization = data.get(
+                            'organization',
+                        ) as TOrganizationID
+                        const personal = data.get('personal')
+                        if (!organization && !personal) {
+                            return setFormError(
+                                getFormFeedbackForError(
+                                    'create/organization-missing',
+                                ),
+                            )
+                        }
+
+                        const board = {
                             tiles: [],
                             meta: {
-                                title: name.substring(0, 30),
+                                title: name.substring(0, 50),
                             },
-                        } as TBoard)
+                        } as TBoard
+
+                        await create(board, organization)
+
+                        reset()
                     }}
                     className="w-full md:w-3/4"
                 >
-                    <Heading3>Hva skal tavlen hete?</Heading3>
+                    <Heading3>Velg navn og organisasjon for tavlen</Heading3>
+                    <Paragraph className="!mb-4">
+                        Gi tavlen et navn og legg den til i en organisasjon.
+                        Velger du en organisasjon vil alle i organisasjonen ha
+                        tilgang til tavlen.
+                    </Paragraph>
                     <Label>Gi tavlen et navn</Label>
                     <TextField
                         size="medium"
                         label="Navn"
                         id="name"
                         name="name"
-                        maxLength={30}
+                        maxLength={50}
                         required
                         {...getFormFeedbackForField('name', state)}
                     />
+
+                    <div className="mt-4">
+                        <Label>Legg til i en organisasjon</Label>
+                        <Dropdown
+                            items={organizations}
+                            label="Dine organisasjoner"
+                            selectedItem={
+                                personal ? null : selectedOrganization
+                            }
+                            onChange={setSelectedOrganization}
+                            clearable
+                            aria-required="true"
+                            className="mb-4"
+                            disabled={personal}
+                            {...getFormFeedbackForField('organization', state)}
+                        />
+                        <Checkbox
+                            checked={personal}
+                            onChange={() => {
+                                setPersonal(!personal)
+                                setFormError(undefined)
+                            }}
+                            name="personal"
+                        >
+                            Privat tavle
+                        </Checkbox>
+                        <HiddenInput
+                            id="organization"
+                            value={selectedOrganization?.value.id}
+                        />
+                    </div>
+
                     <div className="flex flex-row mt-8 justify-end">
                         <SubmitButton
                             variant="primary"
