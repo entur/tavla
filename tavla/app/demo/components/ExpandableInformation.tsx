4feaa13b68cdc6e267bedc772f2caa043b624a84
asewilhelmsen <ase.wilhelmsen@bekk.no>
2024-09-03 15:19:49 +0200

    feat(demo): remove static text info and add expandable info

---
+++ tavla/app/demo/components/ExpandableInformation.tsx
@@ -0,0 +1,54 @@
+'use client'
+import { BaseExpand } from '@entur/expand'
+import { DownArrowIcon, UpArrowIcon } from '@entur/icons'
+import { Heading5, ListItem, UnorderedList } from '@entur/typography'
+import { useState } from 'react'
+
+function ExpandableInformation() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <div>
+            <div className="flex flex-row">
+                <div
+                    className={`flex justify-between items-center px-6  py-4 bg-blue80 w-full ${
+                        isOpen ? 'rounded-t' : 'rounded'
+                    }`}
+                    onClick={() => setIsOpen(!isOpen)}
+                    style={{ cursor: 'pointer' }}
+                >
+                    <Heading5 margin="none">
+                        Hva du kan gjøre med Tavla om du logger inn
+                    </Heading5>
+                    {isOpen ? <UpArrowIcon /> : <DownArrowIcon />}
+                </div>
+            </div>
+            <BaseExpand open={isOpen}>
+                <div className="bg-blue90 px-6  py-4 rounded-b">
+                    <UnorderedList className="space-y-3 flex flex-col gap-1 pl-6">
+                        <ListItem>Endre tekststørrelse</ListItem>
+                        <ListItem>
+                            Legge til en info-melding nederst i tavlen
+                        </ListItem>
+                        <ListItem>
+                            Endre fargetema (lys eller mørk modus)
+                        </ListItem>
+                        <ListItem>
+                            Legge inn adressen som tavlen står på og vise
+                            gåavstand fra tavlen til stoppested(ene)
+                        </ListItem>
+                        <ListItem>
+                            Opprette så mange tavler du vil og samle disse i
+                            ulike organisasjoner (mapper)
+                        </ListItem>
+                        <ListItem>
+                            Gi andre tilgang til å administrere tavlen
+                        </ListItem>
+                    </UnorderedList>
+                </div>
+            </BaseExpand>
+        </div>
+    )
+}
+
+export { ExpandableInformation }
