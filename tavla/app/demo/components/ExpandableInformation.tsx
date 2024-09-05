9c8fe8ccb3e477e131412be32a747eff34b6b7c5
emilielr <emilie.rognli@gmail.com>
2024-09-05 12:18:17 +0200

    refactor(demo): remove unneccessary styling

--- tavla/app/demo/components/ExpandableInformation.tsx
+++ tavla/app/demo/components/ExpandableInformation.tsx
@@ -9,43 +9,39 @@ function ExpandableInformation() {
 
     return (
         <div>
-            <div className="flex flex-row">
-                <div
-                    className={`flex justify-between items-center px-6  py-4 bg-blue80 w-full ${
-                        isOpen ? 'rounded-t' : 'rounded'
-                    }`}
-                    onClick={() => setIsOpen(!isOpen)}
-                    style={{ cursor: 'pointer' }}
-                >
-                    <Paragraph className="font-bold" margin="none">
-                        Hva du kan gjøre med Tavla om du logger inn
-                    </Paragraph>
-                    {isOpen ? <UpArrowIcon /> : <DownArrowIcon />}
-                </div>
+            <div
+                className={`flex flex-row justify-between items-center px-6  py-4 bg-blue80 w-full cursor-pointer ${
+                    isOpen ? 'rounded-t' : 'rounded'
+                }`}
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <Paragraph className="font-bold" margin="none">
+                    Hva kan du gjøre med Tavla om du logger inn?
+                </Paragraph>
+                {isOpen ? <UpArrowIcon /> : <DownArrowIcon />}
             </div>
-            <BaseExpand open={isOpen}>
-                <div className="bg-blue90 px-6  py-4 rounded-b">
-                    <UnorderedList className="space-y-3 flex flex-col gap-1 pl-6">
-                        <ListItem>Endre tekststørrelse</ListItem>
-                        <ListItem>
-                            Legge til en info-melding nederst i tavlen
-                        </ListItem>
-                        <ListItem>
-                            Endre fargetema (lys eller mørk modus)
-                        </ListItem>
-                        <ListItem>
-                            Legge inn adressen som tavlen står på og vise
-                            gåavstand fra tavlen til stoppested(ene)
-                        </ListItem>
-                        <ListItem>
-                            Opprette så mange tavler du vil og samle disse i
-                            ulike organisasjoner (mapper)
-                        </ListItem>
-                        <ListItem>
-                            Gi andre tilgang til å administrere tavlen
-                        </ListItem>
-                    </UnorderedList>
-                </div>
+            <BaseExpand
+                open={isOpen}
+                className="bg-blue90 px-6  py-4 rounded-b"
+            >
+                <UnorderedList className="space-y-3 gap-1 pl-6">
+                    <ListItem>Endre tekststørrelse</ListItem>
+                    <ListItem>
+                        Legge til en infomelding nederst i tavlen
+                    </ListItem>
+                    <ListItem>Endre fargetema (lys eller mørk modus)</ListItem>
+                    <ListItem>
+                        Legge inn adressen som tavlen står på og vise gåavstand
+                        fra tavlen til stoppested(ene)
+                    </ListItem>
+                    <ListItem>
+                        Opprette så mange tavler du vil og samle disse i ulike
+                        organisasjoner (mapper)
+                    </ListItem>
+                    <ListItem>
+                        Gi andre tilgang til å administrere tavlen
+                    </ListItem>
+                </UnorderedList>
             </BaseExpand>
         </div>
     )
