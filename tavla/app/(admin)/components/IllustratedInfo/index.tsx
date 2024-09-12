cfedebad69ca5968f4269a43a0ea99ae52f4c05b
SelmaBergstrand <78426277+SelmaBergstrand@users.noreply.github.com>
2024-09-12 14:31:31 +0200

    feat(boards): add button to empty board page (#1643)
    * feat(BoardTable): add button to empty board page
    
    * fix(board): send button as function parameter to IllustrationBoard
    
    * chore(boards): add children as prop to IllustratedInfo
    
    * chore(boards): make children-parameter optional in IllustratedInfo

--- tavla/app/(admin)/components/IllustratedInfo/index.tsx
+++ tavla/app/(admin)/components/IllustratedInfo/index.tsx
@@ -5,15 +5,18 @@ import { Heading2, LeadParagraph } from '@entur/typography'
 function IllustratedInfo({
     title,
     description,
+    children,
 }: {
     title: string
     description: string
+    children?: React.ReactNode
 }) {
     return (
         <div className="flex flex-col items-center bg-secondary rounded pb-16">
             <Image src={animals} aria-hidden="true" alt="" />
             <Heading2 className="my-4">{title}</Heading2>
             <LeadParagraph margin="bottom">{description}</LeadParagraph>
+            {children}
         </div>
     )
 }
