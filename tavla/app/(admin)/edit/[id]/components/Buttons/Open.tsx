a0d1a9fea0eb454fad026b3c967e93ce971cda70
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2024-09-27 09:26:06 +0200

    feat(board): btn click tracking

--- tavla/app/(admin)/edit/[id]/components/Buttons/Open.tsx
+++ tavla/app/(admin)/edit/[id]/components/Buttons/Open.tsx
@@ -4,10 +4,12 @@ import { ExternalIcon } from '@entur/icons'
 import { Tooltip } from '@entur/tooltip'
 import { useLink } from 'hooks/useLink'
 import Link from 'next/link'
+import { usePostHog } from 'posthog-js/react'
 import React from 'react'
 
 function Open({ type, bid }: { type?: 'button' | 'icon'; bid?: string }) {
     const link = useLink(bid)
+    const posthog = usePostHog()
     if (type === 'button') {
         return (
             <Button
@@ -16,6 +18,7 @@ function Open({ type, bid }: { type?: 'button' | 'icon'; bid?: string }) {
                 aria-label="Åpne tavle"
                 href={link ?? '/'}
                 target="_blank"
+                onClick={() => posthog.capture('OPEN_BOARD_BTN')}
             >
                 Åpne tavle
                 <ExternalIcon className="!top-[-2px]" />
@@ -30,6 +33,7 @@ function Open({ type, bid }: { type?: 'button' | 'icon'; bid?: string }) {
                 aria-label="Åpne tavle"
                 href={link ?? '/'}
                 target="_blank"
+                onClick={() => posthog.capture('OPEN_BOARD_BTN')}
             >
                 <ExternalIcon />
             </IconButton>
