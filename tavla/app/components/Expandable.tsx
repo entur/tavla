52da4247216b2bb20cf868cf9cdba4986efb68b2
purusott <74315929+purusott@users.noreply.github.com>
2024-09-30 07:32:34 +0200

    chore(edit): rename btn (#1661)
    * style(edit): rename btn and add tracking
    
    * style(contact): nitpick contact form
    
    * feat(board): btn click tracking
    
    * style(contact): min-width contact form

--- tavla/app/components/Expandable.tsx
+++ tavla/app/components/Expandable.tsx
@@ -15,7 +15,7 @@ function Expandable({
     children: React.ReactNode
 }) {
     return (
-        <div className="fixed bottom-0 right-3 w-full max-w-screen-sm z-10 drop-shadow-lg ">
+        <div className="fixed bottom-0 right-3 w-full min-w-96 md:w-1/3 max-w-screen-sm z-10 drop-shadow-lg ">
             <div
                 onClick={() => setIsOpen(!isOpen)}
                 className="flex justify-between items-center px-6 py-4 bg-blue80 w-full rounded-t"
