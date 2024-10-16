2e2b55d555f2e077d965cba54dba75b2a32892e0
SelmaBergstrand <selma.kristine@gmail.com>
2024-10-16 10:42:46 +0200

    fix(error): update logging of pages-error

--- tavla/pages/_error.tsx
+++ tavla/pages/_error.tsx
@@ -4,25 +4,12 @@ import { Button } from '@entur/button'
 import { Heading3 } from '@entur/typography'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect } from 'react'
 import { logger } from 'utils/logger'
 import BeaverIllustration from 'assets/illustrations/Beaver.png'
+import { NextPageContext } from 'next'
 
 const log = logger.child({ module: 'pagesLevelErrorHandler' })
-export default function Error({
-    error,
-}: {
-    error: Error & { digest?: string }
-}) {
-    useEffect(() => {
-        log.error({
-            cause: error.cause,
-            stacktrace: error.stack,
-            message: error.message,
-            url: window.location.href,
-        })
-    }, [error])
-
+function Error() {
     return (
         <div className="mx-auto h-[70vh] lg:w-1/4 flex flex-col justify-center items-center gap-5">
             <Heading3>Au da! Noe gikk galt!</Heading3>
@@ -38,3 +25,15 @@ export default function Error({
         </div>
     )
 }
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+    log.error({
+        statusCode: statusCode,
+        error: err,
+        res: res,
+    })
+    return { statusCode }
+}
+
+export default Error
