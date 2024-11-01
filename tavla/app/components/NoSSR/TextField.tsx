7d30c708ac807a505d4d0e01639b06a7c3db099d
Adrian Flatner <adrianrflatner@gmail.com>
2024-11-01 09:58:35 +0100

    fix(designsystem): Adding NoSSR-workarounds for designsystem flaws

---
+++ tavla/app/components/NoSSR/TextField.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TextField, TextFieldProps } from '@entur/form'
+import ClientOnlyComponent from './ClientOnlyComponent'
+
+function ClientOnlyTextField(props: TextFieldProps) {
+    return (
+        <ClientOnlyComponent>
+            <TextField {...props} />
+        </ClientOnlyComponent>
+    )
+}
+
+export default ClientOnlyTextField
