52ead79864242f3719073461f8f4f33136b36150
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2024-11-06 10:42:56 +0100

    feat(prometheus): basic auth

--- tavla/app/actuator/prometheus/route.ts
+++ tavla/app/actuator/prometheus/route.ts
@@ -1,11 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server'
 import client from 'prom-client'
 
 const collectDefaultMetrics = client.collectDefaultMetrics
 const Registry = client.Registry
 const register = new Registry()
 collectDefaultMetrics({ register })
+const username = process.env.METRICS_USERNAME || 'username'
+const password = process.env.METRICS_PASSWORD || 'password'
+const authString =
+    'Basic ' + Buffer.from(`${username}:${password}`).toString('base64')
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+    if (req.headers.get('authorization') !== authString) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
     const metrics = await register.metrics()
     return new Response(metrics, {
         headers: {
