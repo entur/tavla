1334648ec50238ea08f36ba59b7c6ccd6890930a
Adrian Flatner <adrianrflatner@gmail.com>
2024-11-18 15:42:25 +0100

    adding _document.tsx to use meta-tag for refreshing old browsers

---
+++ tavla/pages/_document.tsx
@@ -0,0 +1,64 @@
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+    DocumentInitialProps,
+} from 'next/document'
+import { isUnsupportedBrowser } from 'utils/browserDetection'
+
+interface MyDocumentProps extends DocumentInitialProps {
+    needsRefresh: boolean
+}
+
+class MyDocument extends Document {
+    static async getInitialProps(
+        context: DocumentContext,
+    ): Promise<MyDocumentProps> {
+        const initialProps = await Document.getInitialProps(context)
+        const ua = context.req?.headers['user-agent'] || ''
+
+        const needsRefresh = isUnsupportedBrowser(ua)
+        return {
+            ...initialProps,
+            needsRefresh,
+        }
+    }
+
+    render() {
+        const { needsRefresh } = this.props as unknown as MyDocumentProps
+
+        return (
+            <Html>
+                <Head>
+                    {needsRefresh && <meta httpEquiv="refresh" content="60" />}
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/site.webmanifest" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
