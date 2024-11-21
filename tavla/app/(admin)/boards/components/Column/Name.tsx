2f640fad5c5bb1a157325bb2ee9d13d45684e41b
SelmaBergstrand <selma.kristine@gmail.com>
2024-11-21 08:52:07 +0100

    fix(boards): set board name to be link in table in mobile view

--- tavla/app/(admin)/boards/components/Column/Name.tsx
+++ tavla/app/(admin)/boards/components/Column/Name.tsx
@@ -6,15 +6,9 @@ import Link from 'next/link'
 function Name({ board }: { board: TBoard }) {
     return (
         <Column column="name">
-            <Link
-                href={`/edit/${board.id}`}
-                className="hidden sm:block hover:underline"
-            >
+            <Link href={`/edit/${board.id}`} className="hover:underline">
                 {board.meta.title ?? DEFAULT_BOARD_NAME}
             </Link>
-            <p className="block sm:hidden">
-                {board.meta.title ?? DEFAULT_BOARD_NAME}
-            </p>
         </Column>
     )
 }
