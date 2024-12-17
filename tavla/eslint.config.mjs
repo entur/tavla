4454656d3eed2153c631205c807c9747804ae5fb
purusott <74315929+purusott@users.noreply.github.com>
2024-12-17 11:25:37 +0100

    chore(ci): migrate eslint config (#1766)

---
+++ tavla/eslint.config.mjs
@@ -0,0 +1,64 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import prettier from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+// Ensure compatibility with older plugins/configs
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+})
+
+export default [
+    {
+        ignores: ['src/types/graphql/*'],
+    },
+    ...compat.extends(
+        'next/core-web-vitals',
+        'next/typescript',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ),
+    {
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+            prettier,
+            react,
+        },
+
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+
+        rules: {
+            'no-unused-vars': 'off',
+            'no-console': 'warn',
+
+            'react/jsx-curly-brace-presence': [
+                'warn',
+                {
+                    props: 'never',
+                    children: 'never',
+                },
+            ],
+
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'prettier/prettier': 'warn',
+        },
+    },
+]
