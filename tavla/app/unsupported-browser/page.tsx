abaf428ed8fec0b2fadda68556f35e31d4b0f2d1
SelmaBergstrand <selma.kristine@gmail.com>
2025-01-30 15:23:21 +0100

    style(board): add . to unsupported page

--- tavla/app/unsupported-browser/page.tsx
+++ tavla/app/unsupported-browser/page.tsx
@@ -5,7 +5,7 @@ function UnsupportedBrowser() {
         <IllustratedInfo
             title="Beklager, vi støtter ikke nettleseren din!"
             description="Dessverre krever Tavla en nyere nettleser for å fungere som den
-                skal. For hjelp, kontakt oss på tavla@entur.org"
+                skal. For hjelp, kontakt oss på tavla@entur.org."
         ></IllustratedInfo>
     )
 }
