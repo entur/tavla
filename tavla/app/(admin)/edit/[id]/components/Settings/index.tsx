71bddefe91c8f56ee0836b7ecce19d5c19d88d88
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2025-02-14 12:35:19 +0100

    feat(edit): save multiple settings to db

--- tavla/app/(admin)/edit/[id]/components/Settings/index.tsx
+++ tavla/app/(admin)/edit/[id]/components/Settings/index.tsx
@@ -18,6 +18,9 @@ import { DEFAULT_BOARD_NAME } from 'app/(admin)/utils/constants'
 import { useOrganizations } from 'app/(admin)/hooks/useOrganizations'
 import { ViewTypeSetting } from '../ViewType'
 import { usePostHog } from 'posthog-js/react'
+import { HiddenInput } from 'components/Form/HiddenInput'
+import { useToast } from '@entur/alert'
+
 function Settings({
     board,
     meta,
@@ -40,13 +43,29 @@ function Settings({
     )
     const { organizations, selectedOrganization, setSelectedOrganization } =
         useOrganizations(organization)
-    //const [state, action] = useActionState(saveForm, undefined)
+    const { addToast } = useToast()
     return (
         <div className="rounded-md md:py-8 py-2 md:px-6 px-2 flex flex-col gap-4 bg-background">
             <Heading2>Innstillinger</Heading2>
             <form
                 className="grid grid-cols md:grid-cols-[repeat(auto-fill,minmax(400px,1fr))] gap-8"
-                action={submit}
+                onSubmit={async (e) => {
+                    e.preventDefault()
+
+                    const data = new FormData(e.currentTarget)
+                    data.append(
+                        'organization',
+                        selectedOrganization?.value.id as string,
+                    )
+                    data.append(
+                        'location',
+                        JSON.stringify(selectedPoint?.value),
+                    )
+                    const res = await saveForm(undefined, data)
+                    if (!res) {
+                        addToast('Innstillinger lagret!')
+                    }
+                }}
             >
                 <MetaSettings>
                     <Title title={meta?.title ?? DEFAULT_BOARD_NAME} />
@@ -74,6 +93,8 @@ function Settings({
                         bid={board.id!}
                         font={meta?.fontSize ?? 'medium'}
                     />
+                    <HiddenInput id="bid" value={board.id} />
+                    <HiddenInput id="fromOrg" value={organization?.id ?? ''} />
                 </BoardSettings>
                 <div>
                     <ButtonGroup className="flex flex-row mt-8">
