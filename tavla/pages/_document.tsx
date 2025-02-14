9437a640dfaff7019fb79a5c48a87c388c2f671b
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2025-02-14 05:49:10 +0100

    chore(board): add dark colors webmanifest

--- tavla/pages/_document.tsx
+++ tavla/pages/_document.tsx
@@ -27,7 +27,7 @@ class TavlaDocument extends Document {
                         sizes="16x16"
                         href="/favicon-16x16.png"
                     />
-                    <link rel="manifest" href="/site.webmanifest" />
+                    <link rel="manifest" href="/tavlevisning.webmanifest" />
                 </Head>
                 <body>
                     <Main />
