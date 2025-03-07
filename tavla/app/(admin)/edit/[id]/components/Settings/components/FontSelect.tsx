6970d1fb28489652c9e7ba6f4f2757ceb3d148a4
SelmaBergstrand <selma.kristine@gmail.com>
2025-03-07 13:54:00 +0100

    refactor(edit): fontselect is one component

--- tavla/app/(admin)/edit/[id]/components/Settings/components/FontSelect.tsx
+++ tavla/app/(admin)/edit/[id]/components/Settings/components/FontSelect.tsx
@@ -1,13 +1,26 @@
 'use client'
 import { Heading4 } from '@entur/typography'
 import { TFontSize } from 'types/meta'
-import { FontChoiceChip } from './FontChoiceChip'
+import { useState } from 'react'
+import { ChoiceChipGroup, ChoiceChip } from '@entur/chip'
 
 function FontSelect({ font = 'medium' }: { font?: TFontSize }) {
+    const [fontSize, setFontSize] = useState<TFontSize>(font)
+
     return (
         <div>
             <Heading4 margin="bottom">Tekststørrelse </Heading4>
-            <FontChoiceChip font={font} />
+            <ChoiceChipGroup
+                className="h-full"
+                name="font"
+                value={fontSize}
+                onChange={(e) => setFontSize(e.target.value as TFontSize)}
+                aria-label="Tekststørrelse"
+            >
+                <ChoiceChip value="small">Liten</ChoiceChip>
+                <ChoiceChip value="medium">Medium</ChoiceChip>
+                <ChoiceChip value="large">Stor</ChoiceChip>
+            </ChoiceChipGroup>
         </div>
     )
 }
