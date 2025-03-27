e8af41545c18f950a352f6582425e44b40f925a5
emilielr <emilie.rognli@gmail.com>
2025-03-27 14:46:16 +0100

    fix(edit): delete board

--- tavla/app/(admin)/boards/utils/actions.ts
+++ tavla/app/(admin)/boards/utils/actions.ts
@@ -4,7 +4,8 @@ import { revalidatePath } from 'next/cache'
 import { deleteBoard, initializeAdminApp } from 'app/(admin)/utils/firebase'
 import { redirect } from 'next/navigation'
 import { handleError } from 'app/(admin)/utils/handleError'
-import { TBoardID, TOrganizationID } from 'types/settings'
+import { TBoardID } from 'types/settings'
+import { getOrganizationWithBoard } from 'Board/scenarios/Board/firebase'
 
 initializeAdminApp()
 
@@ -13,13 +14,14 @@ export async function deleteBoardAction(
     data: FormData,
 ) {
     const bid = data.get('bid') as TBoardID
-    const oid = data.get('oid') as TOrganizationID
+    const organization = await getOrganizationWithBoard(bid)
+
     try {
         await deleteBoard(bid)
         revalidatePath('/')
     } catch (e) {
         return handleError(e)
     }
-    if (oid) redirect(`/boards/${oid}`)
+    if (organization) redirect(`/boards/${organization?.id}`)
     redirect('/boards')
 }
