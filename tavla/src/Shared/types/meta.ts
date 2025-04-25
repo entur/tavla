d6dbc638bcbb0060a85b390a45764bcf0eafe441
Annika Størvold <71342087+astoervold@users.noreply.github.com>
2025-04-25 12:02:19 +0200

    chore(oversikt): remove tags from oversikt again (#1869)

--- tavla/src/Shared/types/meta.ts
+++ tavla/src/Shared/types/meta.ts
@@ -5,11 +5,9 @@ export type TMeta = {
     dateModified?: number
     version?: number
     fontSize?: TFontSize
-    tags?: TTag[]
     location?: TLocation
 }
 
-export type TTag = string
 export type TFontSize = 'small' | 'medium' | 'large'
 export type TCoordinate = { lat: number; lng: number }
 export type TLocation = {
