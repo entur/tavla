2b923de2c24d33cb305e3245b0998904bc6dc123
Annika Størvold <astoervold@gmail.com>
2025-05-09 14:47:20 +0200

    chore(): save a read tile column script

---
+++ tavla/migrations/scripts/read_tile_column_count.py
@@ -0,0 +1,40 @@
+import os
+from google.cloud import firestore
+
+from example import init_dev_app, init_local_app, init_prod_app
+
+boards = "boards"
+
+def read_columns(db: firestore.Client): 
+    board_collection = db.collection(boards).stream()
+    total_boards = 0
+    multi_tile_boards = 0
+    different_columns_board = 0
+
+    for board in board_collection:
+        total_boards += 1
+
+        board_dict = board.to_dict()
+        board_tiles = board_dict.get("tiles")
+
+        if (len(board_tiles) > 1):
+            multi_tile_boards += 1 
+            temp_column = []
+            for tile in board_tiles:
+                if (len(temp_column) == 0):
+                    temp_column = tile["columns"]
+                else:
+                    if (temp_column != tile["columns"]):
+                        different_columns_board += 1
+                        break
+                
+    print(f"total boards: {total_boards} - boards with several tiles: {multi_tile_boards} - boards with tiles with different columns: {different_columns_board}")
+    print(f"percentage of all boards that have different columns: {round(different_columns_board/total_boards*100,2)}%\npercentage of boards with more than one column: {round(different_columns_board/multi_tile_boards*100,2)}%")
+
+def migrate():
+    db = init_local_app("ent-tavla-local-countcolumn")
+
+    print(f"db: {db.project}, {db._emulator_host}")
+    read_columns(db)
+
+migrate()
