6556f5be7c000f81b7a0e6fc3f3d4f0fab58df97
Ingelin Sundnes <ingelin.sundnes@bekk.no>
2025-05-16 12:57:02 +0200

    chore(help): update from accordion to expandablePanel which is now used in the design system

--- tavla/app/help/components/Questions.tsx
+++ tavla/app/help/components/Questions.tsx
@@ -1,12 +1,12 @@
 'use client'
-import { Accordion, AccordionItem } from '@entur/expand'
+import { ExpandablePanel } from '@entur/expand'
 import { ListItem } from '@entur/typography'
 import Link from 'next/link'
 
 function Questions() {
     return (
-        <Accordion>
-            <AccordionItem title="Må jeg opprette en bruker?">
+        <div className="w-full">
+            <ExpandablePanel title="Må jeg opprette en bruker?">
                 For å kunne lage en tavle må du opprette en bruker. Om du vil se
                 hvordan tavlen din blir, kan du først teste den i demoløsningen
                 vår{' '}
@@ -14,12 +14,12 @@ function Questions() {
                     her
                 </Link>
                 .
-            </AccordionItem>
-            <AccordionItem title="Er Tavla gratis å bruke?">
+            </ExpandablePanel>
+            <ExpandablePanel title="Er Tavla gratis å bruke?">
                 Ja, Tavla er helt gratis å bruke uavhengig av hvor mange tavler
                 man har.
-            </AccordionItem>
-            <AccordionItem title="Tavlen min funker ikke på skjermen min eller er blank, hva gjør jeg?">
+            </ExpandablePanel>
+            <ExpandablePanel title="Tavlen min funker ikke på skjermen min eller er blank, hva gjør jeg?">
                 Det er ikke alle skjermer hvor Tavla er støttet. Er skjermen din
                 veldig gammel, så kan det hende at nettleseren ikke klarer å
                 kjøre opp Tavla i det hele tatt. Da vil enten tavlen ikke se ut
@@ -27,8 +27,8 @@ function Questions() {
                 kan sjekke om det er mulig å oppdatere nettleseren på skjermen
                 til en nyere versjon. Hvis dette ikke er mulig eller ikke
                 hjelper, må du dessverre anskaffe deg en nyere skjerm.
-            </AccordionItem>
-            <AccordionItem title="Hvilke eldre nettlesere støttes?">
+            </ExpandablePanel>
+            <ExpandablePanel title="Hvilke eldre nettlesere støttes?">
                 Tavla er utviklet med tanke på at vi skal støtte eldre
                 nettlesere siden mange skjermer har eldre, innebygde nettlesere.
                 Vi støtter helt ned til disse versjonene og oppover:
@@ -41,18 +41,18 @@ function Questions() {
                 hvertfall funke på overnevnte versjoner og høyere. Om Tavla
                 funker eller ikke, kommer også an på hvilket operativsystem
                 skjermen har i bunnen.
-            </AccordionItem>
-            <AccordionItem title="Kan jeg invitere andre til å administrere tavlen min?">
+            </ExpandablePanel>
+            <ExpandablePanel title="Kan jeg invitere andre til å administrere tavlen min?">
                 Om du ønsker at andre også skal kunne redigere tavlen din må du
                 opprette en mappe og plassere tavlen som du ønsker skal deles
                 der. Da kan du invitere andre til å administrere alle tavlene i
                 mappen.
-            </AccordionItem>
-            <AccordionItem title="Lagres tavlene mine?">
+            </ExpandablePanel>
+            <ExpandablePanel title="Lagres tavlene mine?">
                 Når du oppretter en tavle på brukere din, så vil de lagres slik
                 at du kan endre på de senere.
-            </AccordionItem>
-            <AccordionItem title="Jeg har ikke fått en e-post om verifisering, hva gjør jeg?">
+            </ExpandablePanel>
+            <ExpandablePanel title="Jeg har ikke fått en e-post om verifisering, hva gjør jeg?">
                 Det kan hende det tar litt tid å få en e-post fra oss om
                 verifisering. Dette skal normalt ikke ta mer enn 10 minutter.
                 Hvis du ikke får den innen da, så prøv gjerne igjen. Hvis
@@ -65,21 +65,21 @@ function Questions() {
                     tavla@entur.org
                 </a>
                 .
-            </AccordionItem>
-            <AccordionItem title="Jeg har opprettet konto med e-post og passord. Kan jeg likevel logge meg inn med Google?">
+            </ExpandablePanel>
+            <ExpandablePanel title="Jeg har opprettet konto med e-post og passord. Kan jeg likevel logge meg inn med Google?">
                 Ja! Om du bruker samme e-post-adresse, vil du kunne logge på med
                 din Google-konto og finne alle tavler og mapper du tidligere har
                 opprettet.
-            </AccordionItem>
-            <AccordionItem title="Jeg har logget på med Google tidligere. Kan jeg likevel logge meg på med e-post og passord?">
+            </ExpandablePanel>
+            <ExpandablePanel title="Jeg har logget på med Google tidligere. Kan jeg likevel logge meg på med e-post og passord?">
                 Ja! Hvis du tidligere kun har logget på med Google, så har du
                 ikke enda satt et passord på din Tavla-konto. Bruk derfor lenken
                 for glemt passord, og du vil motta en lenke på e-post for å
                 sette passordet. Deretter kan du velge hvilken påloggingsmetode
                 du ønsker å bruke - du vil fortsatt ha tilgang til alle dine
                 tavler og mapper med begge metoder.
-            </AccordionItem>
-        </Accordion>
+            </ExpandablePanel>
+        </div>
     )
 }
 
