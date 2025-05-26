c823d8858793b6369829c395d5c0f76d58785796
Ingelin Sundnes <ingelin.sundnes@bekk.no>
2025-05-26 15:37:51 +0200

    chore(cookies): add cookies to layout

--- tavla/app/layout.tsx
+++ tavla/app/layout.tsx
@@ -2,7 +2,7 @@ import 'styles/imports.css'
 import 'styles/fonts.css'
 import 'styles/reset.css'
 import './globals.css'
-import { ReactNode, Suspense } from 'react'
+import { ReactNode, Suspense } from 'react' //TODO: add "useEffect"
 import { Metadata } from 'next'
 import { EnturToastProvider, PHProvider } from './providers'
 import { Footer } from './(admin)/components/Footer'
@@ -11,6 +11,21 @@ import { ContactForm } from './components/ContactForm'
 import PostHogPageView from './components/PostHogPageView'
 import { getUserFromSessionCookie } from './(admin)/utils/server'
 import Script from 'next/script'
+// import {
+//     CONSENT_UPDATED_EVENT,
+//     ConsentDetails,
+//     Consents,
+//     formatConsentEvent,
+//     waitFor,
+// } from '../src/Shared/utils/cmpUtils'
+
+declare global {
+    interface Window {
+        posthog?: {
+            identify: (id: string) => void
+        }
+    }
+}
 
 export const metadata: Metadata = {
     title: 'Entur Tavla',
@@ -38,6 +53,51 @@ export const metadata: Metadata = {
 
 async function RootLayout({ children }: { children: ReactNode }) {
     const loggedIn = (await getUserFromSessionCookie()) !== null
+    // useEffect(() => {
+    //     let previousConsents: Consents | null = null
+    //     const POSTHOG_SERVICE_NAME = 'PostHog.com'
+
+    //     //TODO: Sentry (evt andre) også inn her?
+
+    //     async function handleConsentUpdate(
+    //         event: Event & { detail?: ConsentDetails },
+    //     ) {
+    //         if (typeof window === 'undefined') return
+
+    //         const consents = formatConsentEvent(event)
+    //         const posthogConsent = consents?.find(
+    //             (c) => c.name === POSTHOG_SERVICE_NAME,
+    //         )
+
+    //         if (posthogConsent?.consentGiven) {
+    //             await waitFor(() => window.posthog !== undefined)
+    //             // @ts-expect-error identify does exist on posthog
+    //             window.posthog?.identify(event.detail?.consent.controllerId)
+    //         }
+
+    //         if (previousConsents !== null) {
+    //             const previousPostHogConsent = previousConsents?.find(
+    //                 (c) => c.name === POSTHOG_SERVICE_NAME,
+    //             )
+
+    //             const posthogDeclined =
+    //                 previousPostHogConsent?.consentGiven === true &&
+    //                 posthogConsent?.consentGiven === false
+
+    //             if (posthogDeclined) location.reload()
+    //         }
+
+    //         previousConsents = consents
+    //     }
+
+    //     window.addEventListener(CONSENT_UPDATED_EVENT, handleConsentUpdate)
+
+    //     return () =>
+    //         window.removeEventListener(
+    //             CONSENT_UPDATED_EVENT,
+    //             handleConsentUpdate,
+    //         )
+    // }, [])
     return (
         <html lang="nb">
             <PHProvider>
