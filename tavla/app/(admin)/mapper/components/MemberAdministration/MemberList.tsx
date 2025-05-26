9b982dffaf84e1ccc3fdbad91e28196b5ed8f7c4
Ingelin Sundnes <ingelin.sundnes@bekk.no>
2025-05-26 15:48:44 +0200

    chore(modal): remove modal over modal

--- tavla/app/(admin)/mapper/components/MemberAdministration/MemberList.tsx
+++ tavla/app/(admin)/mapper/components/MemberAdministration/MemberList.tsx
@@ -11,27 +11,27 @@ function MemberList({
     oid?: TOrganizationID
 }) {
     return (
-        <div>
-            <div className="grid items-center overflow-x-auto grid-cols-3">
-                <p className="flex items-center gap-1 bg-grey70 pl-2 h-10 font-medium py-0 px-0.5 col-span-2">
+        <div className="w-full">
+            <div className="grid items-center grid-cols-3">
+                <p className="flex items-center gap-1 bg-grey70 pl-2 h-10 font-medium col-span-2">
                     Medlemmer
                 </p>
-                <p className="flex items-center gap-1 bg-grey70 pl-2 h-10 font-medium py-0 px-0.5">
+                <p className="flex items-center gap-1 bg-grey70 pl-2 h-10 font-medium">
                     Handlinger
                 </p>
             </div>
 
-            <div className="grid items-center overflow-x-auto grid-cols-3 [&>*:nth-child(4n+3)]:bg-grey80 [&>*:nth-child(4n+4)]:bg-grey80">
+            <div className="grid items-center grid-cols-3 [&>*:nth-child(4n+3)]:bg-grey80 [&>*:nth-child(4n+4)]:bg-grey80">
                 {members.map((member) => (
                     <>
                         <p
-                            className="content-center pl-2 h-16 py-2 col-span-2"
+                            className="flex items-center pl-2 h-16 col-span-2"
                             key={'email' + member.uid}
                         >
                             {member.email}
                         </p>
                         <div
-                            className="content-center pl-2 h-16"
+                            className="flex items-center justify-center h-16"
                             key={member.uid}
                         >
                             {member.uid !== currentUserId && (
