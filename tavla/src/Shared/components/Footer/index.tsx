214db6e03ca40531f5e334391a04fb4c8861f9f5
Ingelin Sundnes <ingelin.sundnes@bekk.no>
2025-05-26 09:57:44 +0200

    chore(): remove classname truncate

--- tavla/src/Shared/components/Footer/index.tsx
+++ tavla/src/Shared/components/Footer/index.tsx
@@ -11,7 +11,7 @@ function Footer({ board, logo }: { board: TBoard; logo?: boolean }) {
     return (
         <footer className="flex flex-row justify-between min-h-[4vh] items-center gap-em-2">
             <div
-                className={`truncate text-primary ${
+                className={`text-primary ${
                     getFontScale(board.meta?.fontSize) || defaultFontSize(board)
                 }`}
             >
