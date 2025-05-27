1bac68172db93f6efbf8edff02475f34d780e6b0
Ingelin Sundnes <ingelin.sundnes@bekk.no>
2025-05-27 11:07:29 +0200

    chore(cookies): create new cookie consent handler

--- tavla/app/layout.tsx
+++ tavla/app/layout.tsx
@@ -2,7 +2,7 @@ import 'styles/imports.css'
 import 'styles/fonts.css'
 import 'styles/reset.css'
 import './globals.css'
-import { ReactNode, Suspense } from 'react' //TODO: add "useEffect"
+import { ReactNode, Suspense } from 'react'
 import { Metadata } from 'next'
 import { EnturToastProvider, PHProvider } from './providers'
 import { Footer } from './(admin)/components/Footer'
@@ -11,13 +11,7 @@ import { ContactForm } from './components/ContactForm'
 import PostHogPageView from './components/PostHogPageView'
 import { getUserFromSessionCookie } from './(admin)/utils/server'
 import Script from 'next/script'
-// import {
-//     CONSENT_UPDATED_EVENT,
-//     ConsentDetails,
-//     Consents,
-//     formatConsentEvent,
-//     waitFor,
-// } from '../src/Shared/utils/cmpUtils'
+import ConsentHandler from './components/ConsentHandler'
 
 declare global {
     interface Window {
@@ -53,51 +47,7 @@ export const metadata: Metadata = {
 
 async function RootLayout({ children }: { children: ReactNode }) {
     const loggedIn = (await getUserFromSessionCookie()) !== null
-    // useEffect(() => {
-    //     let previousConsents: Consents | null = null
-    //     const POSTHOG_SERVICE_NAME = 'PostHog.com'
 
-    //     //TODO: Sentry (evt andre) også inn her?
-
-    //     async function handleConsentUpdate(
-    //         event: Event & { detail?: ConsentDetails },
-    //     ) {
-    //         if (typeof window === 'undefined') return
-
-    //         const consents = formatConsentEvent(event)
-    //         const posthogConsent = consents?.find(
-    //             (c) => c.name === POSTHOG_SERVICE_NAME,
-    //         )
-
-    //         if (posthogConsent?.consentGiven) {
-    //             await waitFor(() => window.posthog !== undefined)
-    //             // @ts-expect-error identify does exist on posthog
-    //             window.posthog?.identify(event.detail?.consent.controllerId)
-    //         }
-
-    //         if (previousConsents !== null) {
-    //             const previousPostHogConsent = previousConsents?.find(
-    //                 (c) => c.name === POSTHOG_SERVICE_NAME,
-    //             )
-
-    //             const posthogDeclined =
-    //                 previousPostHogConsent?.consentGiven === true &&
-    //                 posthogConsent?.consentGiven === false
-
-    //             if (posthogDeclined) location.reload()
-    //         }
-
-    //         previousConsents = consents
-    //     }
-
-    //     window.addEventListener(CONSENT_UPDATED_EVENT, handleConsentUpdate)
-
-    //     return () =>
-    //         window.removeEventListener(
-    //             CONSENT_UPDATED_EVENT,
-    //             handleConsentUpdate,
-    //         )
-    // }, [])
     return (
         <html lang="nb">
             <PHProvider>
@@ -110,6 +60,7 @@ async function RootLayout({ children }: { children: ReactNode }) {
                         data-settings-id="4OOPZiVslbVZnE"
                         async
                     />
+                    <ConsentHandler />
                     <EnturToastProvider>
                         <TopNavigation loggedIn={loggedIn} />
                         <Suspense>
