d2423a0fbb6f316f958f5a9987f52d4a96543ac1
Ingelin Sundnes <ingelin.sundnes@bekk.no>
2025-05-27 09:06:04 +0200

    chore(clock): remove seconds

--- tavla/src/Shared/components/Clock/index.tsx
+++ tavla/src/Shared/components/Clock/index.tsx
@@ -10,7 +10,6 @@ function Clock() {
             return new Intl.DateTimeFormat('en-GB', {
                 hour: '2-digit',
                 minute: '2-digit',
-                // second: '2-digit',
                 hour12: false,
                 timeZone: 'Europe/Oslo',
             }).format(now)
