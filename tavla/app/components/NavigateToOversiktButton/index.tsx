8aed04a47dcc0b00bc9507bf7128af4f97e2e7a7
Ingelin Sundnes <ingelin.sundnes@bekk.no>
2025-05-28 11:55:07 +0200

    fix(navbutton): remove empty onclick

--- tavla/app/components/NavigateToOversiktButton/index.tsx
+++ tavla/app/components/NavigateToOversiktButton/index.tsx
@@ -5,13 +5,7 @@ import { ForwardIcon } from '@entur/icons'
 
 export function NavigateToOversiktButton() {
     return (
-        <Button
-            variant="primary"
-            size="medium"
-            as={Link}
-            href="oversikt"
-            onClick={() => {}}
-        >
+        <Button variant="primary" size="medium" as={Link} href="/oversikt">
             Gå til mine tavler
             <ForwardIcon />
         </Button>
