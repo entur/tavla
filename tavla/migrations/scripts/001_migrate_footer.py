5ff97aa3ebfa9c35968a0ac71d5f1fe1986d74a0
Silje <31701059+siljemoksnes@users.noreply.github.com>
2025-06-02 16:59:02 +0200

    chore(migrations): document migration scripts

--- tavla/migrations/scripts/001_migrate_footer.py
+++ tavla/migrations/scripts/001_migrate_footer.py
@@ -1,3 +1,19 @@
+"""
+Purpose: Migrate footer from organizations to boards
+
+Description:
+    Migrates the footer-value from organization/folder level down to board level as we
+    want to remove the footer-functionality from the organization/folder settings. If
+    a board has setting "override = true" on their footer, then the organization footer
+    value should be migrated to the board footer (and have the existing value overwritten). 
+
+Usage:
+    ./migration run scripts/001_migrate_footer.py
+
+Date: 2025-05-08
+Author: Annika and Silje
+"""
+
 from google.cloud import firestore
 
 import init
@@ -5,7 +21,8 @@ import init
 organizations = "organizations"
 boards = "boards"
 
-# Each migration has to be run through a firestore transaction
+# Each migration should to be run through a firestore transaction to ensure that no
+# migrations are halfway done. Ensures either complete failure or complete success.
 @firestore.transactional
 def update_board_footer(transaction, board_ref, org_footer, log_file):
     snapshot = board_ref.get(transaction=transaction)
@@ -24,7 +41,8 @@ def update_board_footer(transaction, board_ref, org_footer, log_file):
         })
         log_file.write(f"✅ Updating board '{board_ref.id}', board footer: '{footer.get("footer")}' -> organization footer: '{org_footer}'\n")
 
-# Reads through the database 
+# Reads through the database and creates a log-file, runs a transaction if all
+# requirements are met
 def migrate_footer(db: firestore.Client):
     org_collection = db.collection(organizations).stream()
     board_collection = db.collection(boards)
@@ -50,7 +68,7 @@ def migrate_footer(db: firestore.Client):
                 except Exception as e: 
                     log_file.write(f"❌ failed to update board '{board_id}': {e}\n")
 
-# Init the database connection and run the specified migration definition
+# Inits the database connection and runs the migration script
 def migrate():
     db = init.local()
     print(f"db: {db.project}, {db._emulator_host}")
