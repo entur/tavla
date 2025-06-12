67ba1058374abede9cff88bd44f50e9d05d6226b
Silje <31701059+siljemoksnes@users.noreply.github.com>
2025-06-12 16:04:10 +0200

    chore(migrations): document migration scripts (#1935)

--- tavla/migrations/scripts/test.py
+++ tavla/migrations/scripts/test.py
@@ -1,3 +1,16 @@
+"""
+Purpose: Virtual environment running check
+
+Description:
+    Check if the virtual environment is up and able to run scripts. 
+
+Usage:
+    ./migration run scripts/rename_user_field.py
+
+Date: 2025-05-08
+Author: Annika and Silje
+"""
+
 def test():
     print(f"A very simple test is working as intended!! 🎉")
 
