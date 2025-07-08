1ee91252543c055dd3fd6787adcea8448a7d8e51
Annika Størvold <astoervold@gmail.com>
2025-07-08 13:44:45 +0200

    wip

--- tavla/src/Shared/components/TravelTag/index.tsx
+++ tavla/src/Shared/components/TravelTag/index.tsx
@@ -60,7 +60,7 @@ function SmallTravelTag({
     return (
         <div
             aria-label={`${transportModeNames[transportMode]} - linje ${publicCode}`}
-            className={`flex h-5 w-full items-center justify-between rounded-sm p-1 font-bold text-background bg-${
+            className={`!ml-[2px] flex h-5 max-w-min items-center justify-between rounded-sm !py-[14px] px-1 font-bold text-background bg-${
                 transportMode ?? 'unknown'
             }`}
             key={`${transportMode}${publicCode}`}
