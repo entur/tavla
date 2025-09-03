e43b981b2306079118d3509aa6ff2a0d171ea2b0
Guro Brøto <gurogb@users.noreply.github.com>
2025-09-03 14:27:19 +0200

    feat(rediger): make styling improvements to Instillinger on admin page (#2010)
    * feat(rediger): move settings for walking distance and info message text to from Tavlevisning to Generelt
    
    * feat(rediger): change transport modes colors radio button to be row instead of column. Some smaller styling adjustments
    
    * feat(rediger): Change textlabel for walking distance

--- tavla/app/(admin)/tavler/[id]/rediger/components/Settings/components/TransportPaletteSelect.tsx
+++ tavla/app/(admin)/tavler/[id]/rediger/components/Settings/components/TransportPaletteSelect.tsx
@@ -62,63 +62,63 @@ function TransportPaletteSelect({
     }
 
     return (
-        <>
+        <div>
             <Heading4 margin="bottom" id="transport-palette-heading">
                 Farger på transportmidler
             </Heading4>
-            <Paragraph className="!mb-0">
+            <Paragraph className="mb-3">
                 Velg hvilke farger transportmidlene i tavlevisningen skal ha.
             </Paragraph>
-            <RadioGroup
-                name="transportPalette"
-                value={selectedValue}
-                aria-labelledby="transport-palette-heading"
-                onChange={(e) => {
-                    handleChange(e.target.value as TTransportPalette)
-                }}
-            >
-                {transportPalettes.map((palette) => (
-                    <div key={palette.value}>
-                        <Radio value={palette.value}>{palette.label}</Radio>
-                        <div
-                            className="flex max-w-max flex-col rounded-sm bg-background px-2 py-3"
-                            data-theme={theme}
-                            data-transport-palette={palette.value}
-                        >
-                            <div className="mb-4 flex flex-row gap-2">
-                                {busAndTrainModes.map((mode) => (
-                                    <div
-                                        className="max-w-min"
-                                        key={theme + mode.mode}
-                                    >
-                                        {TravelTag({
-                                            transportMode: mode.mode,
-                                            publicCode: '00',
-                                        })}
-                                    </div>
-                                ))}
-                            </div>
-                            <div className="flex flex-row flex-wrap gap-2">
-                                {transportModes.map((mode) => (
-                                    <div
-                                        className="max-w-min"
-                                        key={
-                                            theme + (mode.submode ?? mode.mode)
-                                        }
-                                    >
-                                        {TravelTag({
-                                            transportMode: mode.mode,
-                                            publicCode: '00',
-                                            transportSubmode: mode.submode,
-                                        })}
-                                    </div>
-                                ))}
+            <div className="flex flex-col gap-4 md:flex-row">
+                <RadioGroup
+                    name="transportPalette"
+                    value={selectedValue}
+                    aria-labelledby="transport-palette-heading"
+                    onChange={(e) => {
+                        handleChange(e.target.value as TTransportPalette)
+                    }}
+                >
+                    {transportPalettes.map((palette) => (
+                        <div key={palette.value}>
+                            <Radio value={palette.value}>{palette.label}</Radio>
+                            <div
+                                className="flex max-w-max flex-col rounded-sm bg-background px-2 py-3"
+                                data-theme={theme}
+                                data-transport-palette={palette.value}
+                            >
+                                <div className="grid grid-cols-2 gap-2">
+                                    {busAndTrainModes.map((mode) => (
+                                        <div
+                                            className="max-w-min"
+                                            key={theme + mode.mode}
+                                        >
+                                            {TravelTag({
+                                                transportMode: mode.mode,
+                                                publicCode: '00',
+                                            })}
+                                        </div>
+                                    ))}
+                                    {transportModes.map((mode) => (
+                                        <div
+                                            key={
+                                                theme +
+                                                (mode.submode ?? mode.mode)
+                                            }
+                                        >
+                                            {TravelTag({
+                                                transportMode: mode.mode,
+                                                publicCode: '00',
+                                                transportSubmode: mode.submode,
+                                            })}
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </RadioGroup>
-        </>
+                    ))}
+                </RadioGroup>
+            </div>
+        </div>
     )
 }
 
