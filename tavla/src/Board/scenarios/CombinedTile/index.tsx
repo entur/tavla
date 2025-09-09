46b86614fafc3789a60824583853da4c676ca261
Guro Brøto <guro.broto@bekk.no>
2025-09-09 13:32:38 +0200

    fix(tavla): move tile situations to bottom if few departures

--- tavla/src/Board/scenarios/CombinedTile/index.tsx
+++ tavla/src/Board/scenarios/CombinedTile/index.tsx
@@ -114,7 +114,7 @@ export function CombinedTile({ combinedTile }: { combinedTile: TTile[] }) {
 
     return (
         <Tile className="flex flex-col max-sm:min-h-[30vh]">
-            <div className="overflow-hidden">
+            <div className="h-full overflow-hidden">
                 <CombinedTileDeviation situations={combinedSituations} />
                 <Table
                     departures={sortedEstimatedCalls}
