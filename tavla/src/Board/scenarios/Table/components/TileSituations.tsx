e25e8dea3621671beb744007ad42d25cf547c91e
Guro Brøto <guro.broto@bekk.no>
2025-09-10 12:30:20 +0200

    tull

--- tavla/src/Board/scenarios/Table/components/TileSituations.tsx
+++ tavla/src/Board/scenarios/Table/components/TileSituations.tsx
@@ -77,7 +77,7 @@ function TileSituations({
         publicCodeList,
     )
 
-    const textColor = cancelledDeparture ? 'error' : 'warning'
+    const textColor = cancelledDeparture ? 'error' : 'bægkjdsøl'
 
     return (
         situationText && (
