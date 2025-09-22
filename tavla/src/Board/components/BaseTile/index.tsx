85ad9e95b7775ba66df59880c087107e20fce0ae
Erlend Sorknes <sorknerl@gmail.com>
2025-09-22 12:49:37 +0200

    🧹 kombinere Tile-Komponenter
    - kombinere tiles-komponenter til å bruke en Base-Tile
    - lage en hook for useTileData for all data-henting
    - fjerne duplikasjon av kode

---
+++ tavla/src/Board/components/BaseTile/index.tsx
@@ -0,0 +1,147 @@
+import {
+    DataFetchingFailed,
+    FetchErrorTypes,
+} from 'Board/components/DataFetchingFailed'
+import { TileLoader } from 'Board/components/TileLoader'
+import { TileSituations } from 'Board/scenarios/Table/components/TileSituations'
+import { Tile } from 'components/Tile'
+import { TDepartureFragment, TSituationFragment } from 'graphql/index'
+import { ReactNode } from 'react'
+import { TColumn } from 'types/column'
+import { TWalkingDistance } from 'types/tile'
+import { TileSituation } from '../../scenarios/Board/utils'
+import { Table } from '../../scenarios/Table'
+import { StopPlaceQuayDeviation } from '../../scenarios/Table/components/StopPlaceDeviation'
+import { TableHeader } from '../../scenarios/Table/components/TableHeader'
+
+interface BaseTileProps {
+    // Data
+    displayName: string
+    estimatedCalls: TDepartureFragment[]
+    situations: TSituationFragment[]
+    uniqueSituations: TileSituation[]
+    currentSituationIndex: number
+
+    // State
+    isLoading: boolean
+    error?: Error
+    hasData: boolean
+
+    // Configuration
+    columns: TColumn[]
+    walkingDistance?: TWalkingDistance
+
+    // Optional customization
+    className?: string
+    renderCustomHeader?: () => ReactNode
+    renderCustomDeviation?: () => ReactNode
+}
+
+export function BaseTile({
+    displayName,
+    estimatedCalls,
+    situations,
+    uniqueSituations,
+    currentSituationIndex,
+    isLoading,
+    error,
+    hasData,
+    columns,
+    walkingDistance,
+    className = 'flex flex-col max-sm:min-h-[30vh]',
+    renderCustomHeader,
+    renderCustomDeviation,
+}: BaseTileProps) {
+    // Loading state
+    if (isLoading && !hasData) {
+        return (
+            <Tile>
+                <TileLoader />
+            </Tile>
+        )
+    }
+
+    // Error state
+    if (error || !hasData) {
+        return (
+            <Tile>
+                <DataFetchingFailed
+                    timeout={error?.message === FetchErrorTypes.TIMEOUT}
+                />
+            </Tile>
+        )
+    }
+
+    // Empty state
+    if (!estimatedCalls || estimatedCalls.length === 0) {
+        return (
+            <Tile className={className}>
+                <div className="flex-grow overflow-hidden">
+                    {renderCustomHeader ? (
+                        renderCustomHeader()
+                    ) : (
+                        <TableHeader
+                            heading={displayName}
+                            walkingDistance={walkingDistance}
+                        />
+                    )}
+                </div>
+                <div className="flex h-full w-full items-center justify-center text-center text-tertiary">
+                    Ingen avganger i nærmeste fremtid
+                </div>
+            </Tile>
+        )
+    }
+
+    return (
+        <Tile className={className}>
+            <div className="flex-grow overflow-hidden">
+                {renderCustomHeader ? (
+                    renderCustomHeader()
+                ) : (
+                    <TableHeader
+                        heading={displayName}
+                        walkingDistance={walkingDistance}
+                    />
+                )}
+
+                {renderCustomDeviation ? (
+                    renderCustomDeviation()
+                ) : (
+                    <StopPlaceQuayDeviation situations={situations} />
+                )}
+
+                <Table
+                    columns={columns}
+                    departures={estimatedCalls}
+                    stopPlaceSituations={situations}
+                    currentVisibleSituationId={
+                        uniqueSituations?.[currentSituationIndex]?.situation.id
+                    }
+                    numberOfVisibleSituations={uniqueSituations?.length}
+                />
+            </div>
+
+            {uniqueSituations && uniqueSituations.length > 0 && (
+                <TileSituations
+                    situation={
+                        uniqueSituations[currentSituationIndex]?.situation
+                    }
+                    currentSituationNumber={currentSituationIndex}
+                    numberOfSituations={uniqueSituations.length}
+                    cancelledDeparture={
+                        uniqueSituations[currentSituationIndex]?.cancellation ??
+                        false
+                    }
+                    transportModeList={
+                        uniqueSituations[currentSituationIndex]
+                            ?.transportModeList
+                    }
+                    publicCodeList={
+                        uniqueSituations[currentSituationIndex]?.publicCodeList
+                    }
+                />
+            )}
+        </Tile>
+    )
+}
--- tavla/src/Board/scenarios/StopPlaceTile/index.tsx
+++ tavla/src/Board/scenarios/StopPlaceTile/index.tsx
@@ -1,92 +1,16 @@
-import {
-    DataFetchingFailed,
-    FetchErrorTypes,
-} from 'Board/components/DataFetchingFailed'
-import { TileLoader } from 'Board/components/TileLoader'
-import { TileSituations } from 'Board/scenarios/Table/components/TileSituations'
-import { Tile } from 'components/Tile'
-import { StopPlaceQuery } from 'graphql/index'
-import { useQuery } from 'hooks/useQuery'
+import { BaseTile } from 'Board/components/BaseTile'
+import { useStopPlaceTileData } from 'Board/hooks/useTileData'
+import { DEFAULT_COLUMNS } from 'types/column'
 import { TStopPlaceTile } from 'types/tile'
-import { getAccumulatedTileSituations } from '../Board/utils'
-import { Table } from '../Table'
-import { StopPlaceQuayDeviation } from '../Table/components/StopPlaceDeviation'
-import { TableHeader } from '../Table/components/TableHeader'
-import { useCycler } from '../Table/useCycler'
 
-export function StopPlaceTile({
-    placeId,
-    whitelistedLines,
-    whitelistedTransportModes,
-    columns,
-    walkingDistance,
-    offset,
-    displayName,
-}: TStopPlaceTile) {
-    const { data, isLoading, error } = useQuery(
-        StopPlaceQuery,
-        {
-            stopPlaceId: placeId,
-            whitelistedTransportModes,
-            whitelistedLines,
-        },
-        { poll: true, offset: offset },
-    )
-
-    const uniqueSituations = getAccumulatedTileSituations(
-        data?.stopPlace?.estimatedCalls,
-        data?.stopPlace?.situations,
-    )
-    const index = useCycler(uniqueSituations ?? [], 10000)
-
-    if (isLoading && !data) {
-        return (
-            <Tile>
-                <TileLoader />
-            </Tile>
-        )
-    }
-
-    if (error || !data || !data.stopPlace) {
-        return (
-            <Tile>
-                <DataFetchingFailed
-                    timeout={error?.message === FetchErrorTypes.TIMEOUT}
-                />
-            </Tile>
-        )
-    }
+export function StopPlaceTile(props: TStopPlaceTile) {
+    const tileData = useStopPlaceTileData(props)
 
     return (
-        <Tile className="flex flex-col max-sm:min-h-[30vh]">
-            <div className="flex-grow overflow-hidden">
-                <TableHeader
-                    heading={displayName ?? data.stopPlace.name}
-                    walkingDistance={walkingDistance}
-                />
-                <StopPlaceQuayDeviation
-                    situations={data.stopPlace.situations}
-                />
-                <Table
-                    departures={data.stopPlace.estimatedCalls}
-                    stopPlaceSituations={data.stopPlace.situations}
-                    columns={columns}
-                    currentVisibleSituationId={
-                        uniqueSituations?.[index]?.situation.id
-                    }
-                    numberOfVisibleSituations={uniqueSituations?.length}
-                />
-            </div>
-            <TileSituations
-                situation={uniqueSituations?.[index]?.situation}
-                currentSituationNumber={index}
-                numberOfSituations={uniqueSituations?.length}
-                cancelledDeparture={
-                    uniqueSituations?.[index]?.cancellation ?? false
-                }
-                transportModeList={uniqueSituations?.[index]?.transportModeList}
-                publicCodeList={uniqueSituations?.[index]?.publicCodeList}
-            />
-        </Tile>
+        <BaseTile
+            {...tileData}
+            columns={props.columns ?? DEFAULT_COLUMNS}
+            walkingDistance={props.walkingDistance}
+        />
     )
 }
