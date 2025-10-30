dd2f8b62dd106bf76ea11adf61b88dfbbb00847f
Jakob Helgøy <jakobnhelgoy@gmail.com>
2025-10-30 10:43:49 +0100

    Mindre knappestørrelse + hovereffekt på expandable

--- tavla/app/components/Expandable.tsx
+++ tavla/app/components/Expandable.tsx
@@ -1,4 +1,3 @@
-import { IconButton } from '@entur/button'
 import { DownArrowIcon, UpArrowIcon } from '@entur/icons'
 import { Heading5 } from '@entur/typography'
 import { Dispatch, SetStateAction } from 'react'
@@ -15,10 +14,11 @@ function Expandable({
     children: React.ReactNode
 }) {
     return (
-        <div className="fixed bottom-0 right-3 z-10 w-full min-w-96 max-w-screen-sm drop-shadow-lg md:w-1/3">
+        <div className="fixed bottom-0 right-3 z-10 drop-shadow-lg">
             <div
                 onClick={() => setIsOpen(!isOpen)}
-                className="flex w-full items-center justify-between rounded-t bg-blue20 px-6 py-4"
+                className={`flex w-full cursor-pointer items-center justify-between gap-2 rounded-t bg-blue20 px-6 py-2 ${!isOpen ? 'transition-all duration-150 ease-in-out hover:py-3' : ''}`}
+                aria-label={isOpen ? 'Åpne skjema' : 'Lukk skjema'}
             >
                 <Heading5
                     margin="none"
@@ -27,15 +27,12 @@ function Expandable({
                 >
                     {title}
                 </Heading5>
-                <IconButton
-                    className="border-0!"
-                    aria-label={isOpen ? 'Åpne skjema' : 'Lukk skjema'}
-                >
-                    {isOpen ? <DownArrowIcon /> : <UpArrowIcon />}
-                </IconButton>
+                {isOpen ? <DownArrowIcon /> : <UpArrowIcon />}
             </div>
             {isOpen && (
-                <div className="rounded-b bg-blue10 p-4">{children}</div>
+                <div className="w-full rounded-b bg-blue10 p-4 md:w-96 lg:w-[500px]">
+                    {children}
+                </div>
             )}
         </div>
     )
