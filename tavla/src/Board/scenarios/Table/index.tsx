9f1e8b1a9bc97f348bc40090a6ec97ecbde1ff2f
Guro Brøto <guro.broto@bekk.no>
2025-11-14 16:05:39 +0100

    Put back on smallTravelTag

--- tavla/src/Board/scenarios/Table/index.tsx
+++ tavla/src/Board/scenarios/Table/index.tsx
@@ -65,7 +65,9 @@ function Table({
                     />
                     {columns.includes('aimedTime') && <AimedTime />}
                     {columns.includes('arrivalTime') && <ArrivalTime />}
-                    {columns.includes('line') && <Line />}
+                    {columns.includes('line') && (
+                        <Line data-theme={theme ?? 'dark'} />
+                    )}
                     {columns.includes('destination') && <Destination />}
                     {columns.includes('name') && (
                         <Name customNames={customNames} />
