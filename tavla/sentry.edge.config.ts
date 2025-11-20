0f7b544e646ceee83dbb3e1dd4b4e8fc7ea756f2
Erlend Sorknes <69513675+erlendsorknes@users.noreply.github.com>
2025-11-20 13:01:25 +0100

    🔭 sette ned Sentry traces til en promille (#2119)
    ### 🥅 Bakgrunn
    Tavla sender ut alle traces i Sentry, noe som gjør at det blir dyrt. 
    
    ### ✨ Løsning
    - Sette ned Traces til å bare sende ut en promille av hva den trenger.
    
    
    ### ✅ Sjekkliste
    
    - [x] Testet i Chrome, Firefox og Safari

--- tavla/sentry.edge.config.ts
+++ tavla/sentry.edge.config.ts
@@ -8,7 +8,7 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN_URL,
 
-    tracesSampleRate: 1,
+    tracesSampleRate: 0.001,
 
     enabled: process.env.NODE_ENV !== 'development',
 })
