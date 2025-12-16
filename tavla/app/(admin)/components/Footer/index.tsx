aa78676703da3a692f97cf897d41a7623b541d42
Erlend Sorknes <sorknerl@gmail.com>
2025-12-16 09:24:35 +0100

    ♿ fra div til liste i footer

--- tavla/app/(admin)/components/Footer/index.tsx
+++ tavla/app/(admin)/components/Footer/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { GithubIcon } from '@entur/icons'
+import { ExternalIcon, GithubIcon } from '@entur/icons'
 import { Link as EnturLink, Heading3, Paragraph } from '@entur/typography'
 import { showUC_UI as showUserCentricsUI } from 'app/components/ConsentHandler'
 import TavlaLogo from 'assets/logos/Tavla-white.svg'
@@ -15,88 +15,112 @@ function Footer({ loggedIn }: { loggedIn: boolean }) {
             <div className="container pb-20 pt-16">
                 <Image src={TavlaLogo} alt="Entur Tavla logo" />
                 <div className="flex flex-col justify-between sm:flex-row">
-                    <div>
+                    <address className="not-italic">
                         <Heading3 as="h2">Entur AS</Heading3>
-                        <Paragraph className="items-center">
-                            Rådhusgata 5, 0151 Oslo
-                            <br aria-hidden />
-                            Postboks 1554, 0117 Oslo
-                        </Paragraph>
-                        <Paragraph className="items-center">
-                            Organisasjonsnummer:
-                            <br aria-hidden />
-                            917 422 575
-                        </Paragraph>
-                        <div className="flex flex-col gap-4">
-                            <EnturLink
-                                href="https://www.entur.org/kontakt-oss/"
-                                target="_blank"
-                                external
-                            >
-                                Kontakt kundesenteret
-                            </EnturLink>
-
-                            <EnturLink
-                                href="mailto:tavla@entur.org"
-                                target="_blank"
-                            >
-                                Kontakt Tavla på mail
-                            </EnturLink>
-                        </div>
-                    </div>
+                        <ul className="flex flex-col gap-0">
+                            <li>
+                                <address className="items-center">
+                                    Rådhusgata 5, 0151 Oslo
+                                    <br aria-hidden />
+                                    Postboks 1554, 0117 Oslo
+                                </address>
+                            </li>
+                            <li>
+                                <Paragraph className="items-center">
+                                    Organisasjonsnummer:
+                                    <br aria-hidden />
+                                    917 422 575
+                                </Paragraph>
+                            </li>
+                            <li>
+                                <Paragraph
+                                    className="flex flex-row items-center gap-1"
+                                    margin="none"
+                                >
+                                    <EnturLink href="https://www.entur.org/kontakt-oss/">
+                                        Kontakt kundesenteret
+                                    </EnturLink>
+                                    <ExternalIcon
+                                        aria-hidden
+                                        className="!top-0"
+                                    />
+                                </Paragraph>
+                            </li>
+                            <li>
+                                <Paragraph className="flex flex-row items-center gap-1">
+                                    <EnturLink
+                                        href="mailto:tavla@entur.org"
+                                        target="_blank"
+                                    >
+                                        Kontakt Tavla
+                                    </EnturLink>
+                                    <ExternalIcon
+                                        aria-hidden
+                                        className="!top-0"
+                                    />
+                                </Paragraph>
+                            </li>
+                        </ul>
+                    </address>
                     <div className="flex flex-col gap-4">
                         <Heading3 as="h2">Informasjon</Heading3>
-                        <div>
-                            <EnturLink
-                                href="/demo"
-                                as={Link}
-                                onClick={() =>
-                                    posthog.capture('DEMO_FROM_FOOTER')
-                                }
-                            >
-                                Test ut Tavla
-                            </EnturLink>
-                        </div>
-                        <div>
-                            <EnturLink href="/hjelp" as={Link}>
-                                Ofte stilte spørsmål
-                            </EnturLink>
-                        </div>
-                        <div className="flex flex-row items-center gap-1">
-                            <EnturLink
-                                as={Link}
-                                href="https://uustatus.no/nb/erklaringer/publisert/ffb3d21b-fbb4-48ed-9043-bb2a904f3143"
-                                target="_blank"
-                                external
-                            >
-                                Tilgjengelighetserklæring
-                            </EnturLink>
-                        </div>
-                        {loggedIn && (
-                            <div className="flex flex-row items-center gap-1">
-                                <DeleteAccount />
-                            </div>
-                        )}
-                        <EnturLink as={Link} href="/personvern">
-                            Personvernerklæring
-                        </EnturLink>
-                        <EnturLink
-                            as="button"
-                            onClick={showUserCentricsUI}
-                            className="self-start"
-                        >
-                            Informasjonskapsler
-                        </EnturLink>
-                        <div className="flex flex-row items-center gap-1">
-                            <EnturLink
-                                href="https://github.com/entur/tavla"
-                                external
-                                target="_blank"
-                            >
-                                GitHub
-                            </EnturLink>
-                            <GithubIcon size={20} aria-hidden />
-                        </div>
+                        <ul className="flex flex-col gap-4">
+                            <li>
+                                <EnturLink
+                                    href="/demo"
+                                    as={Link}
+                                    onClick={() =>
+                                        posthog.capture('DEMO_FROM_FOOTER')
+                                    }
+                                >
+                                    Test ut Tavla
+                                </EnturLink>
+                            </li>
+                            <li>
+                                <EnturLink href="/hjelp" as={Link}>
+                                    Ofte stilte spørsmål
+                                </EnturLink>
+                            </li>
+                            <li className="flex flex-row items-center gap-1">
+                                <EnturLink
+                                    as={Link}
+                                    href="https://uustatus.no/nb/erklaringer/publisert/ffb3d21b-fbb4-48ed-9043-bb2a904f3143"
+                                    target="_blank"
+                                >
+                                    Tilgjengelighetserklæring
+                                </EnturLink>
+                                <ExternalIcon aria-hidden />
+                            </li>
+                            {loggedIn && (
+                                <li className="flex flex-row items-center gap-1">
+                                    <DeleteAccount />
+                                </li>
+                            )}
+                            <li>
+                                <EnturLink as={Link} href="/personvern">
+                                    Personvernerklæring
+                                </EnturLink>
+                            </li>
+                            <li>
+                                <EnturLink
+                                    as="button"
+                                    onClick={showUserCentricsUI}
+                                    className="self-start"
+                                >
+                                    Informasjonskapsler
+                                </EnturLink>
+                            </li>
+                            <li className="flex flex-row items-center gap-1">
+                                <EnturLink
+                                    href="https://github.com/entur/tavla"
+                                    target="_blank"
+                                >
+                                    GitHub
+                                </EnturLink>
+                                <ExternalIcon aria-hidden />
+                                <GithubIcon size={25} aria-hidden />
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
