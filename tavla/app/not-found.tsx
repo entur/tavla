e9ae4144132ee018d41c70d75060837bb2d4b84f
Silje <31701059+siljemoksnes@users.noreply.github.com>
2026-01-05 11:20:10 +0100

    wip

--- tavla/app/not-found.tsx
+++ tavla/app/not-found.tsx
@@ -7,7 +7,10 @@ import Link from 'next/link'
 function Custom404() {
     return (
         <main className="container flex flex-col items-center pb-10">
-            <Heading3>Denne siden finnes ikke!</Heading3>
+            <Heading3>
+                Ai ai ai, denne tavla finnes ikke! Prøv å laste inn siden på
+                nytt eller gå tilbake til forsiden.
+            </Heading3>
             <Image
                 src={BeaverIllustration}
                 className="w-1/2 lg:w-1/4"
