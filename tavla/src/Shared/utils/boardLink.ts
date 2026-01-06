1ee6cd9c9f9d9fd32f01c4cda96b4b62b3a6a1da
Guro Brøto <guro.broto@bekk.no>
2026-01-06 12:06:47 +0100

    Bytt til å sjekke nodeEnv lik production

--- tavla/src/Shared/utils/boardLink.ts
+++ tavla/src/Shared/utils/boardLink.ts
@@ -1,13 +1,12 @@
 import { BoardDB } from 'types/db-types/boards'
 
 export function getBoardLink(bid: BoardDB['id']) {
-    const env = process.env.NEXT_PUBLIC_ENV
     const nodeEnv = process.env.NODE_ENV
 
     const baseUrl =
         nodeEnv === 'development'
             ? `http://localhost:5173/${bid}`
-            : env === 'prod'
+            : nodeEnv === 'production'
               ? `https://vis-tavla.entur.no/${bid}`
               : `https://vis-tavla.dev.entur.no/${bid}`
 
