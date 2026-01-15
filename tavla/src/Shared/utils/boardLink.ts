f8f0544438ccd2693aafeec2d222e6551150dbec
Guro Brøto <gurogb@users.noreply.github.com>
2026-01-15 10:02:27 +0100

    ✨ Endre lenker for å åpne/kopiere tavlelenke til å gå til tavla-visning   (#2203)
    ### 🥅 Bakgrunn
    Fungerer dårlig å sjekke env-variabler i browser, så endrer til å bruke
    window.location.host
    
    ### ✨ Løsning
    - #2199 
    - Endrer funksjon for å finne riktig url til board i tavla-visning til å
    bruke window.location.host, bruker samme funksjon alle steder
    
    
    ### ✅ Sjekkliste
    
    - [ ] Testet i Chrome, Firefox og Safari

--- tavla/src/Shared/utils/boardLink.ts
+++ tavla/src/Shared/utils/boardLink.ts
@@ -1,19 +1,38 @@
 import { BoardDB } from 'types/db-types/boards'
 
-export function getBoardLinkForIframe(bid: BoardDB['id']) {
+export function getBoardLinkClient(bid: BoardDB['id']) {
+    const host = window?.location?.host
+    if (!host) {
+        return `https://vis-tavla.entur.no/${bid}`
+    }
+    switch (host) {
+        case 'localhost:3000':
+            return `http://localhost:5173/${bid}`
+        case 'tavla.dev.entur.no':
+            return `https://vis-tavla.dev.entur.no/${bid}`
+        case 'tavla.entur.no':
+        default:
+            return `https://vis-tavla.entur.no/${bid}`
+    }
+}
+
+export function getBoardLinkServer(bid: BoardDB['id'], isPreview = false) {
     const isLocalDevelopment = process.env.NODE_ENV === 'development'
-    const isProductionEnvironment = process.env.COMMON_ENV === 'prd'
+    const isDevEnvironment = process.env.COMMON_ENV === 'dev'
 
     const baseUrl = isLocalDevelopment
         ? `http://localhost:5173/${bid}`
-        : isProductionEnvironment
-          ? `https://vis-tavla.entur.no/${bid}`
-          : `https://vis-tavla.dev.entur.no/${bid}`
+        : isDevEnvironment
+          ? `https://vis-tavla.dev.entur.no/${bid}`
+          : `https://vis-tavla.entur.no/${bid}`
 
     const queryParams = new URLSearchParams({
         v: Date.now().toString(),
         isPreview: 'true',
     })
 
-    return `${baseUrl}?${queryParams.toString()}`
+    if (isPreview) {
+        return `${baseUrl}?${queryParams.toString()}`
+    }
+    return baseUrl
 }
