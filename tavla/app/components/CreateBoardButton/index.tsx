42028618e0e2a8c8dd5b26acfbd23dd091c3d341
Guro Brøto <guro.broto@bekk.no>
2026-03-06 09:18:20 +0100

    ✨Test feature flag for lage board uten bruker

--- tavla/app/components/CreateBoardButton/index.tsx
+++ tavla/app/components/CreateBoardButton/index.tsx
@@ -2,10 +2,32 @@
 import { Button } from '@entur/button'
 import { ForwardIcon } from '@entur/icons'
 import { usePosthogTracking } from 'app/posthog/usePosthogTracking'
+import { useFeatureFlagEnabled } from 'posthog-js/react'
+
 import Link from 'next/link'
 
 function CreateBoardButton() {
     const posthog = usePosthogTracking()
+
+    const isCreateBoardWithoutUserEnabled = useFeatureFlagEnabled(
+        'create_board_without_user',
+    )
+    if (isCreateBoardWithoutUserEnabled === undefined) {
+        return <p>Flag is undefined</p>
+    }
+
+    if (isCreateBoardWithoutUserEnabled) {
+        return (
+            <Button
+                variant="primary"
+                size="medium"
+                onClick={() => alert('Heihei')}
+            >
+                Lag en tavle <ForwardIcon aria-hidden />
+            </Button>
+        )
+    }
+
     return (
         <Button
             variant="primary"
