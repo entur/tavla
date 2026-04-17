e969c897b4ec1273dab91192cedb492261a0d643
Ola Horg Jacobsen <ola.h.jacobsen@bekk.no>
2026-04-17 08:20:50 +0200

    Hvor brukes Tavla?

--- tavla/app/page.tsx
+++ tavla/app/page.tsx
@@ -1,13 +1,11 @@
-import { MapPinIcon, NewIcon, ShareIcon } from '@entur/icons'
-import {
-    Heading1,
-    Heading2,
-    Heading3,
-    LeadParagraph,
-    Paragraph,
-} from '@entur/typography'
+import { Button } from '@entur/button'
+import { ForwardIcon, MapPinIcon, NewIcon, ShareIcon } from '@entur/icons'
+import { Heading1, Heading3, LeadParagraph, Paragraph } from '@entur/typography'
 import { CreateBoardButton } from 'app/components/CreateBoardButton'
+import TavlaNorwayMap from 'assets/illustrations/Tavla-Norway.svg'
 import type { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 import type { BoardDB } from 'src/types/db-types/boards'
 import { getBoardLinkServer } from 'src/utils/boardLink'
 import { getUserFromSessionCookie } from './(admin)/utils/server'
@@ -71,12 +69,12 @@ async function Landing() {
                             kollektivreise.
                         </LeadParagraph>
                         <div className="mt-5 flex w-full flex-col gap-4 md:flex-row">
-                            {!loggedIn ? (
+                            {loggedIn ? (
+                                <NavigateToOversiktButton />
+                            ) : (
                                 <div className="flex w-full flex-col gap-4 md:flex-row">
                                     <CreateBoardButton />
                                 </div>
-                            ) : (
-                                <NavigateToOversiktButton />
                             )}
                         </div>
                     </div>
@@ -87,7 +85,7 @@ async function Landing() {
             <div className="container mx-auto flex flex-col justify-start gap-4 overflow-hidden py-14">
                 <PreviewCarousel previewBoards={previewBoardsWithLinks} />
 
-                <div className="lg:px-12 py-16 px-6 text-left lg:text-center">
+                <div className="lg:px-12 py-16 mx-24 text-left lg:text-center">
                     <Heading1 as="h2" margin="none">
                         Tavle på 1, 2, 3
                     </Heading1>
@@ -134,25 +132,52 @@ async function Landing() {
                     </div>
                 </div>
 
-                <div className="px-6 lg:px-12">
-                    <Heading2>Eksempler på bruk</Heading2>
-                    <Heading3>Hoteller</Heading3>
-                    <Paragraph>
-                        Plasser Tavla i resepsjonsområdet slik at gjester kan se
-                        sanntidsinformasjon om avganger fra stoppesteder i
-                        nærheten.
-                    </Paragraph>
-                    <Heading3>Arbeidsplasser</Heading3>
-                    <Paragraph>
-                        Vis Tavla på informasjonspunkter eller skjermer ved
-                        inngangen slik at besøkende enkelt kan planlegge
-                        hjemreisen.
-                    </Paragraph>
-                    <Heading3>Kjøpesentre</Heading3>
-                    <Paragraph>
-                        Sett opp Tavla på sentrale steder slik at kunder alltid
-                        har oppdatert informasjon om offentlig transport.
-                    </Paragraph>
+                <div className="bg-[#F5F6FA] rounded-3xl flex flex-row gap-8 overflow-hidden justify-center lg:justify-normal lg:mx-24">
+                    <div className="w-1/2 flex flex-col my-24 lg:ml-24 text-center lg:text-left">
+                        <Heading1 as="h2" margin="none">
+                            Hvor brukes Tavla?
+                        </Heading1>
+                        <Paragraph className="my-2 mb-10 text-lg">
+                            Overalt hvor folk tar kollektivt.
+                        </Paragraph>
+
+                        <ul className="flex flex-col gap-4 my-8 text-primary mx-auto lg:ml-0">
+                            {[
+                                'Busstopp',
+                                'Kollektivknutepunkt',
+                                'Kontorer',
+                                'Borettslag',
+                                'Hjemme',
+                                'Flyplasser',
+                                'Skoler og universiteter',
+                            ].map((item) => (
+                                <li
+                                    key={item}
+                                    className="flex items-center gap-3"
+                                >
+                                    <div className="w-2.5 h-2.5 rounded-full bg-coral shrink-0" />
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>
+
+                        <div className="mt-20 mx-auto lg:mx-0">
+                            {loggedIn ? (
+                                <NavigateToOversiktButton />
+                            ) : (
+                                <CreateBoardButton />
+                            )}
+                        </div>
+                    </div>
+                    <div className="w-1/2 relative items-center justify-center my-24 mr-12 -ml-12 lg:-ml-48 lg:mr-24 hidden lg:flex">
+                        <Image
+                            className="w-full h-auto object-contain max-h-[700px]"
+                            src={TavlaNorwayMap}
+                            alt={
+                                'Kart over Norge med pins på flere forskjellige steder, for å vise at Tavla er i bruk over hele landet.'
+                            }
+                        />
+                    </div>
                 </div>
             </div>
         </main>
