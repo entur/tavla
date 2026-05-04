b962ea6e2f7a56f6ff5c6759fdbd8099bd09aca0
ingebjrgb <ingebjrgb@gmail.com>
2026-05-04 14:23:48 +0200

    undoe en trykkleif

--- tavla/app/lag-tavle/components/CreateBoardLocally.tsx
+++ tavla/app/lag-tavle/components/CreateBoardLocally.tsx
@@ -106,7 +106,6 @@ function CreateBoardLocally() {
                     <DemoPreview board={board} />
                 </section>
             </div>
-            ¨
             {loaded && (
                 <SettingsForm board={board} onSubmit={handleSettingsSubmit} />
             )}
