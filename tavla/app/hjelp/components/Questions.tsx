482ae3ef56d7ab955941bd664aa8185c65adc751
Maiken Lie <maiken.lie@bekk.no>
2026-05-19 09:57:43 +0200

    Add questions, improve answers and remove not relevant questions on faq

--- tavla/app/hjelp/components/Questions.tsx
+++ tavla/app/hjelp/components/Questions.tsx
@@ -1,31 +1,59 @@
 'use client'
 import { ExpandablePanel } from '@entur/expand'
 import { ListItem } from '@entur/typography'
-import Link from 'next/link'
 
 function Questions() {
     return (
         <div className="w-full">
-            <ExpandablePanel title="Må jeg opprette en bruker?">
-                For å kunne lage en tavle må du opprette en bruker. Du kan også{' '}
-                <Link href="/demo" className="underline">
-                    teste ut demoløsningen
-                </Link>{' '}
-                for å se hvordan tavlene ser ut og fungerer før du registrerer
-                deg.
+            <ExpandablePanel title="Hva er Entur Tavla?">
+                Entur Tavla er en gratis avgangstavle for offentlig transport i
+                hele Norge. Du kan sette opp en sanntidstavle som viser avganger
+                fra valgfrie stoppesteder, og vise den på en skjerm. Tavla
+                brukes av alt fra kollektivselskaper og flyplasser til kontorer,
+                borettslag, skoler, kjøpesentre og hoteller.
             </ExpandablePanel>
-            <ExpandablePanel title="Er Tavla gratis å bruke?">
-                Ja, Tavla er helt gratis å bruke uavhengig av hvor mange tavler
-                man har.
+            <ExpandablePanel title="Er Tavla gratis?">
+                Ja, Tavla er helt gratis å bruke!
             </ExpandablePanel>
-            <ExpandablePanel title="Tavlen min funker ikke på skjermen min eller er blank, hva gjør jeg?">
-                Det er ikke alle skjermer hvor Tavla er støttet. Er skjermen din
-                veldig gammel, så kan det hende at nettleseren ikke klarer å
-                kjøre opp Tavla i det hele tatt. Da vil enten tavlen ikke se ut
-                som den skal, eller så kan det hende at skjermen er blank. Du
-                kan sjekke om det er mulig å oppdatere nettleseren på skjermen
-                til en nyere versjon. Hvis dette ikke er mulig eller ikke
-                hjelper, må du dessverre anskaffe deg en nyere skjerm.
+            <ExpandablePanel title="Må jeg opprette bruker for å lage en avgangstavle?">
+                Nei, du kan lage en tavle uten å registrere deg. Tavlen får en
+                lenke du kan åpne på en hvilken som helst skjerm, men uten
+                bruker kan du ikke endre tavlen i ettertid. Vil du oppdatere
+                den, må du lage en ny tavle og ta i bruk den nye lenken. Med en
+                bruker kan du redigere tavlen når som helst, organisere tavlene
+                dine i mapper, legge til egen logo på tavla og invitere andre
+                til å administrere dem.
+            </ExpandablePanel>
+            <ExpandablePanel title="Viser Tavla sanntidsinformasjon?">
+                Ja, Tavla viser sanntidsavganger der dette er tilgjengelig.
+                Dataene hentes fra Enturs nasjonale sanntidssystem og dekker
+                kollektivtrafikk i hele Norge.
+            </ExpandablePanel>
+            <ExpandablePanel title="Hvilke transportmidler vises i Tavla?">
+                Tavla viser alle kollektivtransportmidler i Norge, inkludert
+                blant annet buss, trikk, T-bane, tog, ferge og fly. Du kan
+                filtrere på linjer og transporttype etter behov.
+            </ExpandablePanel>
+            <ExpandablePanel title="Kan jeg tilpasse utseendet på tavlen?">
+                Ja. Du kan blant annet velge mellom mørkt og lyst tema, endre
+                farger på transportmidler, legge til din egen logo og justere
+                tekststørrelse.
+            </ExpandablePanel>
+            <ExpandablePanel title="Hvordan tar jeg avgangstavlen i bruk på en skjerm?">
+                Når du har satt opp tavlen din, får du en lenke. Åpne lenken i
+                nettleseren på skjermen du vil bruke, ingen app eller
+                installasjon er nødvendig.
+            </ExpandablePanel>
+            <ExpandablePanel title="Kan jeg vise avgangstavlen på en TV-skjerm?">
+                Ja, Tavla fungerer på alle skjermer med en nettleser, inkludert
+                TV-er. Åpne lenken til tavlen din i nettleseren på TV-en, og
+                tavlen vises umiddelbart. Mange bruker også en mediaspiller
+                eller en liten PC koblet til skjermen for å styre hva som vises.
+            </ExpandablePanel>
+            <ExpandablePanel title="Fungerer Tavla uten internett?">
+                Nei, Tavla krever internettilkobling for å hente sanntidsdata og
+                rutetider. Uten nett vil tavlen ikke kunne vise oppdatert
+                avgangsinformasjon.
             </ExpandablePanel>
             <ExpandablePanel title="Hvilke eldre nettlesere støttes?">
                 Tavla er utviklet med tanke på at vi skal støtte eldre
@@ -42,14 +70,8 @@ function Questions() {
                 skjermen har i bunnen.
             </ExpandablePanel>
             <ExpandablePanel title="Kan jeg invitere andre til å administrere tavlen min?">
-                Om du ønsker at andre også skal kunne redigere tavlen din må du
-                opprette en mappe og plassere tavlen som du ønsker skal deles
-                der. Da kan du invitere andre til å administrere alle tavlene i
-                mappen.
-            </ExpandablePanel>
-            <ExpandablePanel title="Lagres tavlene mine?">
-                Når du oppretter en tavle på brukere din, så vil de lagres slik
-                at du kan endre på de senere.
+                Ja. Opprett en mappe og plasser tavla der. Deretter kan du
+                invitere andre brukere til å administrere alle tavlene i mappen.
             </ExpandablePanel>
             <ExpandablePanel title="Jeg har ikke fått en e-post om verifisering, hva gjør jeg?">
                 Det kan hende det tar litt tid å få en e-post fra oss om
@@ -72,22 +94,10 @@ function Questions() {
                 opprettet.
             </ExpandablePanel>
             <ExpandablePanel title="Jeg har logget på med Google tidligere. Kan jeg likevel logge meg på med e-post og passord?">
-                Ja! Hvis du tidligere kun har logget på med Google, så har du
-                ikke enda satt et passord på din Tavla-konto. Bruk derfor lenken
-                for glemt passord, og du vil motta en lenke på e-post for å
-                sette passordet. Deretter kan du velge hvilken påloggingsmetode
-                du ønsker å bruke - du vil fortsatt ha tilgang til alle dine
-                tavler og mapper med begge metoder.
-            </ExpandablePanel>
-            <ExpandablePanel title="Hvordan kan jeg kombinere flere stoppesteder eller plattformer i samme liste?">
-                Du kan enkelt kombinere flere stoppesteder eller plattformer i
-                samme tavle, i stedet for å vise flere lister. Slik gjør du det:
-                <br /> 1. Legg til ønskede stoppesteder eller plattformer.{' '}
-                <br /> 2. Gå ned til <b>Innstillinger</b> {'>'}{' '}
-                <b>Tavlevisning</b> {'>'} <b>Visningstype</b> og velg &quot;Alle
-                stoppesteder i én liste&quot;. Husk å lagre og trykk på
-                &quot;Publiser tavle&quot;, slik at endringene vises og
-                oppdateres i tavlen.
+                Ja. Bruk «Glemt passord» lenken for å sette et passord for
+                kontoen din. Etter det kan du velge påloggingsmetode fritt. Alle
+                tavlene og mappene dine er tilgjengelige uansett hvilken metode
+                du bruker.
             </ExpandablePanel>
         </div>
     )
