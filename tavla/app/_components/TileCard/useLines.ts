9a4ba24cc9fcb29eca5ca6b3fbe6b979e7967076
Magnus Ouren <91123056+magnusouren@users.noreply.github.com>
2026-08-28 13:08:05 +0200

    setter quays etter at frontTexts er hentet for å unngå null fronTexts

--- tavla/app/_components/TileCard/useLines.ts
+++ tavla/app/_components/TileCard/useLines.ts
@@ -85,35 +85,20 @@ function useLines(
                 const json = await res.json()
                 const quays: TQuay[] = json.data?.stopPlace?.quays ?? []
 
-                setQuays(
-                    quays.map((q) => ({
-                        ...q,
-                        lines: q.lines.map((l) => ({
-                            ...l,
-                            frontTexts: [],
-                        })),
-                    })),
-                )
-
-                await Promise.all(
+                const quaysWithFrontTexts = await Promise.all(
                     quays.map(async (quay) => {
                         const frontTexts = await getFrontTextsForQuay(
                             quay.id,
                             isArrival,
                         )
-                        const quayWithFrontTexts = addFrontTextToQuay(
-                            quay,
-                            frontTexts,
-                        )
-                        setQuays((prev) =>
-                            (prev ?? []).map((q) =>
-                                q.id === quay.id
-                                    ? { ...q, lines: quayWithFrontTexts }
-                                    : q,
-                            ),
-                        )
+                        return {
+                            ...quay,
+                            lines: addFrontTextToQuay(quay, frontTexts),
+                        }
                     }),
                 )
+
+                setQuays(quaysWithFrontTexts)
             })
         }
 
