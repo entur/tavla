bd33648b9724b856bab3481a568339b65cfc00ed
Magnus Ouren <91123056+magnusouren@users.noreply.github.com>
2026-09-03 13:20:25 +0200

    Oppdaterer EditBoardHeader-komponenten for å bruke CopyableText og forbedrer layout

--- tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardHeader.tsx
+++ tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardHeader.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { Heading1, Paragraph } from '@entur/typography'
+import { CopyableText } from 'node_modules/@entur/alert'
 import type { BoardDB } from 'src/types/db-types/boards'
 import { getBoardLinkClient } from 'src/utils/boardLink'
-import { CopyBoardLink } from './BoardLink/CopyBoardLink'
 import { CustomUrl } from './BoardLink/CustomUrl'
 import { OpenBoard } from './BoardLink/OpenBoard'
 import { EditBoardTitle } from './EditTitle/EditTitle'
@@ -13,34 +13,29 @@ export function BoardHeader({ board }: { board: BoardDB }) {
 
     return (
         <>
-            <div className="flex flex-wrap items-center gap-2. w-full justify-between">
+            <div className="flex flex-wrap items-center gap-4 w-full justify-start">
                 <Heading1 as="h1" margin="none">
-                    {/* {board.isArrivals ? 'Ankomsttavle: ' : 'Avgangstavle: '} */}
                     {board.meta.title}
                 </Heading1>
                 <EditBoardTitle board={board} />
             </div>
 
-            <div className="flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-between">
-                <div className="flex min-w-0 flex-1 items-center gap-2">
-                    <Paragraph margin="none" color="subdued">
-                        Lenke til tavla
-                    </Paragraph>
-                    <Paragraph
-                        margin="none"
-                        className="min-w-0 flex-1 truncate rounded border bg-tintLight px-2 py-1"
-                    >
-                        {boardLink}
-                    </Paragraph>
-                    <div className="flex shrink-0 items-center gap-1">
-                        <CustomUrl bid={board.id} customUrl={board.customUrl} />
-                        <CopyBoardLink board={board} />
+            <div className="flex flex-col gap-4 lg:flex-row lg:items-end lg:justify-between">
+                <div className="flex min-w-0 items-end gap-2">
+                    <div className="flex flex-col min-w-0 flex-1 justify-start">
+                        <Paragraph margin="none" color="subdued">
+                            Lenke til tavla:
+                        </Paragraph>
+                        <CopyableText className="p-0 m-0" size="small">
+                            {boardLink}
+                        </CopyableText>
                     </div>
+                    <CustomUrl bid={board.id} customUrl={board.customUrl} />
                 </div>
 
-                <div className="flex shrink-0 items-center gap-4">
-                    <OpenBoard board={board} />
+                <div className="flex items-center gap-4">
                     <RefreshBoard board={board} />
+                    <OpenBoard board={board} />
                 </div>
             </div>
         </>
