633fbc07aab784575c15e4b442ed64d3e5755844
Magnus Ouren <91123056+magnusouren@users.noreply.github.com>
2026-09-03 15:35:54 +0200

    Oppdaterer EditBoardBeta for å inkludere BoardTitle-komponenten og forbedrer layout i EditBoardHeader og EditBoardSidebar

--- tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardBeta.tsx
+++ tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardBeta.tsx
@@ -1,6 +1,7 @@
 import { Preview } from 'app/(innlogget)/tavler/[id]/rediger/components/Preview'
 import type { BoardDB } from 'src/types/db-types/boards'
-import { BoardHeader } from './EditBoardHeader'
+import { BoardTitle } from './BoardTitle'
+import { BoardLinkActions } from './EditBoardHeader'
 import { EditBoardSidebar } from './EditBoardSidebar'
 
 export function EditBoardBeta({
@@ -13,16 +14,17 @@ export function EditBoardBeta({
     return (
         <div
             data-transport-palette={board.transportPalette}
-            className="flex flex-col gap-6 lg:flex-row lg:items-start"
+            className="flex flex-col gap-8 lg:flex-row lg:items-start"
         >
-            <section className="flex min-w-0 flex-1 flex-col gap-4 lg:sticky lg:top-2 lg:self-start">
-                <BoardHeader board={board} />
+            <section className="flex min-w-0 flex-1 flex-col gap-8 lg:sticky lg:top-[5vh] lg:self-start">
                 <div data-theme={board.theme ?? 'dark'}>
                     <Preview boardLink={boardLink} />
                 </div>
+                <BoardLinkActions board={board} />
             </section>
 
             <aside className="w-full shrink-0 lg:w-[536px]">
+                <BoardTitle board={board} />
                 <EditBoardSidebar />
             </aside>
         </div>
--- tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardHeader.tsx
+++ tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardHeader.tsx
@@ -1,43 +1,30 @@
 'use client'
-import { Heading1, Paragraph } from '@entur/typography'
 import { CopyableText } from 'node_modules/@entur/alert'
 import type { BoardDB } from 'src/types/db-types/boards'
 import { getBoardLinkClient } from 'src/utils/boardLink'
 import { CustomUrl } from './BoardLink/CustomUrl'
 import { OpenBoard } from './BoardLink/OpenBoard'
-import { EditBoardTitle } from './EditTitle/EditTitle'
 import { RefreshBoard } from './RefreshBoard/RefreshBoard'
 
-export function BoardHeader({ board }: { board: BoardDB }) {
+export function BoardLinkActions({ board }: { board: BoardDB }) {
     const boardLink = getBoardLinkClient(board.customUrl ?? board.id)
 
     return (
-        <>
-            <div className="flex flex-wrap items-center gap-4 w-full justify-start">
-                <Heading1 as="h1" margin="none">
-                    {board.meta.title}
-                </Heading1>
-                <EditBoardTitle board={board} />
+        <div className="flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-between">
+            <div className="flex min-w-0 items-center gap-2">
+                <div className="flex min-w-0 flex-1 justify-start w-full gap-2 items-center">
+                    <div className="flex-shrink-0">Lenke til tavla:</div>
+                    <CopyableText className="p-0 m-0" size="small">
+                        {boardLink}
+                    </CopyableText>
+                </div>
+                <CustomUrl bid={board.id} customUrl={board.customUrl} />
             </div>
 
-            <div className="flex flex-col gap-4 lg:flex-row lg:items-end lg:justify-between">
-                <div className="flex min-w-0 items-end gap-2">
-                    <div className="flex flex-col min-w-0 flex-1 justify-start">
-                        <Paragraph margin="none" color="subdued">
-                            Lenke til tavla:
-                        </Paragraph>
-                        <CopyableText className="p-0 m-0" size="small">
-                            {boardLink}
-                        </CopyableText>
-                    </div>
-                    <CustomUrl bid={board.id} customUrl={board.customUrl} />
-                </div>
-
-                <div className="flex items-center gap-4">
-                    <RefreshBoard board={board} />
-                    <OpenBoard board={board} />
-                </div>
+            <div className="flex items-center gap-4">
+                <RefreshBoard board={board} />
+                <OpenBoard board={board} />
             </div>
-        </>
+        </div>
     )
 }
