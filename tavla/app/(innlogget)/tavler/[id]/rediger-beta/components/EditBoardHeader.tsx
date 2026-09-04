0d014b22c11d812f83656acc175c93ff56849713
Magnus Ouren <91123056+magnusouren@users.noreply.github.com>
2026-09-04 12:59:45 +0200

    Fjerner unødvendig mellomrom i EditBoardHeader-komponenten for bedre kodekonsistens

--- tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardHeader.tsx
+++ tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardHeader.tsx
@@ -11,7 +11,7 @@ export function BoardLinkActions({ board }: { board: BoardDB }) {
 
     return (
         <div className="flex flex-col gap-4 xl:flex-row xl:items-center xl:justify-between">
-            <div className="flex min-w-0 items-end sm:items-center gap-2 ">
+            <div className="flex min-w-0 items-end sm:items-center gap-2">
                 <div className="flex min-w-0 flex-1 justify-start w-full gap-2 sm:items-center flex-col sm:flex-row">
                     <div className="flex-shrink-0">Lenke til tavla:</div>
                     <CopyableText
