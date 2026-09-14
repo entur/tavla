1726f14241030dbfea2872474e64a338b5820c4e
Magnus Ouren <91123056+magnusouren@users.noreply.github.com>
2026-09-14 14:59:14 +0200

    logger åpning og lukking av modal til posthog

--- tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/TileList/StopPlaceTile.tsx
+++ tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/TileList/StopPlaceTile.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { SecondaryButton } from '@entur/button'
+import { FeedbackText } from '@entur/form'
 import { EditIcon } from '@entur/icons'
 import { LeadParagraph } from '@entur/typography'
 import { useLines } from 'app/_components/TileCard/useLines'
 import TransportIcon from 'app/_components/TransportIcon/TransportIcon'
 import { getTransportModesFromLines } from 'app/_components/TransportIcon/utils'
+import { usePosthogTracking } from 'app/posthog/usePosthogTracking'
 import { useState } from 'react'
 import type { BoardTileDB } from 'src/types/db-types/boards'
 import { DeleteTileButton } from './DeleteTileButton'
@@ -18,6 +20,8 @@ export function StopPlaceTile({
     tile: BoardTileDB
 }) {
     const [isEditOpen, setIsEditOpen] = useState(false)
+    const [deleteError, setDeleteError] = useState<string | undefined>()
+    const { capture } = usePosthogTracking()
 
     const quays = useLines(tile, true) ?? []
 
@@ -25,40 +29,64 @@ export function StopPlaceTile({
         quays.flatMap(({ lines }) => lines),
     )
 
+    function handleSetIsEditOpen(open: boolean) {
+        if (open) {
+            capture('stop_place_edit_started', { location: 'edit_board_page' })
+        } else {
+            capture('stop_place_edit_cancelled', {
+                location: 'edit_board_page',
+                unsavedChanges: false,
+            })
+        }
+        setIsEditOpen(open)
+    }
+
     return (
-        <div className="flex items-center justify-between rounded bg-tintLight p-6">
-            <div className="flex items-center gap-4">
-                <LeadParagraph margin="none" className=" text-base md:text-lg">
-                    {tile.displayName ?? tile.name}
-                </LeadParagraph>
-                <div className="hidden md:flex md:gap-1">
-                    {transportModes.map((mode) => (
-                        <TransportIcon
-                            key={`${mode.transportMode}|${mode.transportSubmode ?? ''}`}
-                            transportMode={mode.transportMode}
-                            transportSubmode={mode.transportSubmode}
-                            background
-                            whiteIcon
-                            includeTooltip
-                        />
-                    ))}
+        <div className="flex flex-col gap-2">
+            <div className="flex items-center justify-between rounded bg-tintLight p-6">
+                <div className="flex items-center gap-4">
+                    <LeadParagraph
+                        margin="none"
+                        className=" text-base md:text-lg"
+                    >
+                        {tile.displayName ?? tile.name}
+                    </LeadParagraph>
+                    <div className="hidden md:flex md:gap-1">
+                        {transportModes.map((mode) => (
+                            <TransportIcon
+                                key={`${mode.transportMode}|${mode.transportSubmode ?? ''}`}
+                                transportMode={mode.transportMode}
+                                transportSubmode={mode.transportSubmode}
+                                background
+                                whiteIcon
+                                includeTooltip
+                            />
+                        ))}
+                    </div>
                 </div>
+                <div className="flex gap-2">
+                    <SecondaryButton
+                        size="small"
+                        onClick={() => handleSetIsEditOpen(true)}
+                        className="flex items-center gap-2 px-2 py-1"
+                    >
+                        <EditIcon /> Rediger
+                    </SecondaryButton>
+                    <DeleteTileButton
+                        boardId={boardId}
+                        tile={tile}
+                        onError={setDeleteError}
+                    />
+                </div>
+                <EditStopPlaceModal
+                    isOpen={isEditOpen}
+                    setIsOpen={handleSetIsEditOpen}
+                    tile={tile}
+                />
             </div>
-            <div className="flex gap-2">
-                <SecondaryButton
-                    size="small"
-                    onClick={() => setIsEditOpen(true)}
-                    className="flex items-center gap-2 px-2 py-1"
-                >
-                    <EditIcon /> Rediger
-                </SecondaryButton>
-                <DeleteTileButton boardId={boardId} tile={tile} />
-            </div>
-            <EditStopPlaceModal
-                isOpen={isEditOpen}
-                setIsOpen={setIsEditOpen}
-                tile={tile}
-            />
+            {deleteError && (
+                <FeedbackText variant="negative">{deleteError}</FeedbackText>
+            )}
         </div>
     )
 }
