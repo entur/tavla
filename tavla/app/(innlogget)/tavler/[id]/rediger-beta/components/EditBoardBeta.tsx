5fec08f8ddd8dac2c1984a25da6ff92c5ef6e49d
Agnes Birkemo <agnes.birkemo@hotmail.com>
2026-09-14 09:43:09 +0200

    legge til defualt-farger og unknown farge i de lokale palettene

--- tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardBeta.tsx
+++ tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/EditBoardBeta.tsx
@@ -12,7 +12,10 @@ export function EditBoardBeta({
     boardLink: string
 }) {
     return (
-        <div className="flex flex-col gap-12 lg:flex-row lg:items-start">
+        <div
+            data-transport-palette={board.transportPalette}
+            className="flex flex-col gap-12 lg:flex-row lg:items-start"
+        >
             <section className="flex min-w-0 flex-1 flex-col gap-12 lg:sticky lg:top-[15vh] lg:self-start">
                 <div data-theme={board.theme ?? 'dark'}>
                     <Preview boardLink={boardLink} />
