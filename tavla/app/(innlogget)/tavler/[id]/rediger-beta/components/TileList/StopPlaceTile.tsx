8e0d8d2a01bed6984ef90e7030d97fafa09697a7
Magnus Ouren <91123056+magnusouren@users.noreply.github.com>
2026-09-14 13:19:20 +0200

    henter quays og tilhørende transport modes til et stopp for å vise ikoner

--- tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/TileList/StopPlaceTile.tsx
+++ tavla/app/(innlogget)/tavler/[id]/rediger-beta/components/TileList/StopPlaceTile.tsx
@@ -3,6 +3,9 @@ import { useToast } from '@entur/alert'
 import { SecondaryButton } from '@entur/button'
 import { EditIcon } from '@entur/icons'
 import { LeadParagraph } from '@entur/typography'
+import { useLines } from 'app/_components/TileCard/useLines'
+import TransportIcon from 'app/_components/TransportIcon/TransportIcon'
+import { getTransportModesFromLines } from 'app/_components/TransportIcon/utils'
 import { useState } from 'react'
 import type { BoardTileDB } from 'src/types/db-types/boards'
 import { deleteTile } from './actions'
@@ -19,15 +22,35 @@ export function StopPlaceTile({
     const [isEditOpen, setIsEditOpen] = useState(false)
     const { addToast } = useToast()
 
+    const quays = useLines(tile, true) ?? []
+
+    const transportModes = getTransportModesFromLines(
+        quays.flatMap(({ lines }) => lines),
+    )
+
     function handleDelete() {
         deleteTile(boardId, tile).then(() => addToast(`${tile.name} fjernet!`))
     }
 
     return (
         <div className="flex items-center justify-between rounded bg-tintLight p-6">
-            <LeadParagraph margin="none">
-                {tile.displayName ?? tile.name}
-            </LeadParagraph>
+            <div className="flex items-center gap-4">
+                <LeadParagraph margin="none" className=" text-base md:text-lg">
+                    {tile.displayName ?? tile.name}
+                </LeadParagraph>
+                <div className="hidden md:flex md:gap-1">
+                    {transportModes.map((mode) => (
+                        <TransportIcon
+                            key={`${mode.transportMode}|${mode.transportSubmode ?? ''}`}
+                            transportMode={mode.transportMode}
+                            transportSubmode={mode.transportSubmode}
+                            background
+                            whiteIcon
+                            includeTooltip
+                        />
+                    ))}
+                </div>
+            </div>
             <div className="flex gap-2">
                 <SecondaryButton
                     size="small"
